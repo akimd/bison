9a1af7e6b2ba4581b98f18a64db7431fcd8d5b52
David MacKenzie <djm@djmnet.org>
1993-04-20 06:00:53 +0000

    *** empty log message ***

--- src/reader.c
+++ src/reader.c
@@ -1548,13 +1548,8 @@ record_rule_line ()
   if (nrules >= rline_allocated)
     {
       rline_allocated = nrules * 2;
-      rline = (short *) realloc (rline,
-				 rline_allocated * sizeof (short));
-      if (rline == 0)
-	{
-	  fprintf (stderr, "%s: memory exhausted\n", program_name);
-	  done (1);
-	}
+      rline = (short *) xrealloc (rline,
+				  rline_allocated * sizeof (short));
     }
   rline[nrules] = lineno;
 }
