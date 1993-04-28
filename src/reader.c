8f66601fe2c490695346cd828e357156e30612df
Noah Friedman <friedman@splode.com>
1993-04-28 19:15:14 +0000

    *** empty log message ***

--- src/reader.c
+++ src/reader.c
@@ -52,8 +52,6 @@ extern int failure;
 extern int expected_conflicts;
 extern char *token_buffer;
 
-extern int atoi ();
-
 extern void init_lex();
 extern void tabinit();
 extern void output_headers();
