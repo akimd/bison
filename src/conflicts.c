854c029b1591ebd146da66d03d38cd0b41b94ddf
Richard M. Stallman <rms@gnu.org>
1993-09-10 13:41:02 +0000

    *** empty log message ***

--- src/conflicts.c
+++ src/conflicts.c
@@ -29,7 +29,8 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #include "state.h"
 
 
-#ifdef __GNUC__
+#ifndef __GNUC__
+#undef alloca
 #define alloca __builtin_alloca
 #else
 #ifdef HAVE_ALLOCA_H
