808e202125b009c0b61a2f02e3aeff9e1102e6a3
Richard M. Stallman <rms@gnu.org>
1994-03-24 22:54:32 +0000

    entered into RCS

--- src/conflicts.c
+++ src/conflicts.c
@@ -742,9 +742,9 @@ int state;
 	  if (mask == 0)
 	    {
 	      mask = 1;
-	      /* This used to be fp1, but I think fp2 is right
-		 because fp2 is where the words are fetched to test with mask
-		 in this loop.  */
+	      /* We tried incrementing just fp1, and just fp2; both seem wrong.
+		 It seems necessary to increment both in sync.  */
+	      fp1++;
 	      fp2++;
 	    }
 	}
