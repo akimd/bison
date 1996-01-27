ba2e357c0d9b3abe67868d3660f5f049716394ba
Richard M. Stallman <rms@gnu.org>
1996-01-27 08:26:26 +0000

    (output_rule_data): Test YYERROR_VERBOSE in the conditional around the definition of ttyname.

--- src/output.c
+++ src/output.c
@@ -462,7 +462,7 @@ output_rule_data()
     }
 
   if (! toknumflag  && ! noparserflag)
-    fprintf(ftable, "\n#if YYDEBUG != 0\n\n");
+    fprintf(ftable, "\n#if YYDEBUG != 0 || defined (YYERROR_VERBOSE)\n\n");
 
   /* Output the table of symbol names.  */
 
