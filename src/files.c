6a5705cfe86144c3c1e6b25b6ffcc38a4dd6ccb2
Richard M. Stallman <rms@gnu.org>
1996-12-19 08:24:56 +0000

    [_MSC_VER] (XPFILE, XPFILE1): Define, if not already defined.

--- src/files.c
+++ src/files.c
@@ -29,6 +29,15 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #endif
 #endif
 
+#if defined (_MSC_VER)
+#ifndef XPFILE
+#define XPFILE "c:/usr/local/lib/bison.simple"
+#endif
+#ifndef XPFILE1
+#define XPFILE1 "c:/usr/local/lib/bison.hairy"
+#endif
+#endif
+
 #include <stdio.h>
 #include "system.h"
 #include "files.h"
