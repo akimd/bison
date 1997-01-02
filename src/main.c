42f95d63c2cdefffd471f76e6c64af7e4872ba24
Richard M. Stallman <rms@gnu.org>
1997-01-02 06:59:56 +0000

    (main): Return FAILURE as a value. (printable_version): Declare arg as int, not char.

--- src/main.c
+++ src/main.c
@@ -90,6 +90,7 @@ main(argc, argv)
   /* output the tables and the parser to ftable.  In file output. */
   output();
   done(failure);
+  return failure;
 }
 
 /* functions to report errors which prevent a parser from being generated */
@@ -100,7 +101,7 @@ main(argc, argv)
 
 char *
 printable_version(c)
-     char c;
+     int c;
 {
   static char buf[10];
   if (c < ' ' || c >= '\177')
