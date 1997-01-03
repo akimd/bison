8f047e5ecf6e93da0425059dea3a94d89f1026d0
Richard M. Stallman <rms@gnu.org>
1997-01-03 01:24:27 +0000

    [__STDC__ or _MSC_VER]: Declare calloc and realloc to return void *.

--- src/allocate.c
+++ src/allocate.c
@@ -21,8 +21,14 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
 #include <stdio.h>
 #include "system.h"
 
+#if defined (__STDC__) || defined (_MSC_VER)
+extern void *calloc ();
+extern void *realloc ();
+#else
 extern char *calloc ();
 extern char *realloc ();
+#endif
+
 extern void done ();
 
 extern char *program_name;
