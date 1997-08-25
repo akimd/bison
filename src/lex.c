b0180c64a5889d44f4792ffee06ad5e63acb39b2
Richard M. Stallman <rms@gnu.org>
1997-08-25 19:38:57 +0000

    (literalchar): Use warns, not warni.

--- src/lex.c
+++ src/lex.c
@@ -257,7 +257,7 @@ literalchar(pp, pcode, term)
 	}
       else
 	{
-	  warni (_("unknown escape sequence: `\\' followed by `%s'"),
+	  warns (_("unknown escape sequence: `\\' followed by `%s'"),
 		 printable_version(c));
 	  code = '?';
 	}
