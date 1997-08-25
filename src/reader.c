aba5ca6dea13dc8b004b94c22c408e5baf12d6dd
Richard M. Stallman <rms@gnu.org>
1997-08-25 19:40:12 +0000

    (stack_offset;): Change some warni to warns.

--- src/reader.c
+++ src/reader.c
@@ -1142,7 +1142,7 @@ int stack_offset;
 	      continue;
 	    }
 	  else
-	    warni(_("$%s is invalid"), printable_version(c));
+	    warns(_("$%s is invalid"), printable_version(c));
 
 	  break;
 
@@ -1156,7 +1156,7 @@ int stack_offset;
 	    }
 	  else
 	    {
-	      warni(_("@%s is invalid"), printable_version(c));
+	      warns(_("@%s is invalid"), printable_version(c));
 	      n = 1;
 	    }
 
@@ -1364,7 +1364,7 @@ int stack_offset;
 		  continue;
 		}
 	      else
-		warni(_("$%s is invalid"), printable_version(c));
+		warns(_("$%s is invalid"), printable_version(c));
 
 	      break;
 
