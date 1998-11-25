5c9ba6311a79c43ee525ba83d05f9710f7f43502
Jesse Thilo <jthilo@gnu.org>
1998-11-25 22:09:19 +0000

    Update bug report address.

--- src/getargs.c
+++ src/getargs.c
@@ -69,7 +69,8 @@ Usage: %s [-dhklntvyV] [-b file-prefix] [-o outfile] [-p name-prefix]\n\
        [--verbose] [--version] [--help] [--yacc]\n\
        [--no-parser] [--token-table]\n\
        [--file-prefix=prefix] [--name-prefix=prefix]\n\
-       [--output=outfile] grammar-file\n"),
+       [--output=outfile] grammar-file\n\n\
+Report bugs to bug-bison@gnu.org\n"),
 	   program_name);
 }
 
