bd2a2393ed0b50c067a80b6d39d5d8e3378c177a
Jesse Thilo <jthilo@gnu.org>
1998-12-29 21:54:26 +0000

    Use VERSION symbol from automake for version number.

---
+++ acconfig.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+@TOP@
+
+/* Name of package.  */
+#undef PACKAGE
+
+/* Version of package.  */
+#undef VERSION
+
+@BOTTOM@
+
+#endif  /* CONFIG_H */
---
+++ src/version.c
@@ -0,0 +1,3 @@
+#include <config.h>
+
+char *version_string = "GNU Bison version " VERSION "\n";
