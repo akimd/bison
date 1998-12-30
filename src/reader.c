74a53b4b48462dabfbbeda1f033c48398a68c454
Jesse Thilo <jthilo@gnu.org>
1998-12-30 06:54:16 +0000

    Update copyright date.

--- src/reader.c
+++ src/reader.c
@@ -1,5 +1,5 @@
 /* Input parser for bison
-   Copyright (C) 1984, 1986, 1989, 1992 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1986, 1989, 1992, 1998 Free Software Foundation, Inc.
 
 This file is part of Bison, the GNU Compiler Compiler.
 
