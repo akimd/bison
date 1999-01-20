cbbe75051dc980b667faeb0cc3681c90bb62aa20
Jesse Thilo <jthilo@gnu.org>
1999-01-20 22:56:40 +0000

    #Remove static from dummy().

--- src/gram.c
+++ src/gram.c
@@ -53,7 +53,7 @@ int error_token_number;
 /* This is to avoid linker problems which occur on VMS when using GCC,
    when the file in question contains data definitions only.  */
 
-static void
+void
 dummy (void)
 {
 }
