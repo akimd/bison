bbcb769c0ed26bc4a232df6570c6258e08b263e7
Jesse Thilo <jthilo@gnu.org>
1999-03-23 00:48:58 +0000

    Added i18n support.

--- acconfig.h
+++ acconfig.h
@@ -11,6 +11,21 @@
 /* Define if the compiler understands prototypes.  */
 #undef PROTOTYPES
 
+/* Define to 1 if NLS is requested.  */
+#undef ENABLE_NLS
+
+/* Define as 1 if you have catgets and don't want to use GNU gettext.  */
+#undef HAVE_CATGETS
+
+/* Define as 1 if you have gettext and don't want to use GNU gettext.  */
+#undef HAVE_GETTEXT
+
+/* Define if your locale.h file contains LC_MESSAGES.  */
+#undef HAVE_LC_MESSAGES
+
+/* Define to 1 if you have the stpcpy function.  */
+#undef HAVE_STPCPY
+
 /* The location of the simple parser (bison.simple).  */
 #undef XPFILE
 
