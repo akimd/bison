e5335b7401bedc87ffb9d031c0fff6049fac2a85
Jesse Thilo <jthilo@gnu.org>
1999-04-18 15:17:30 +0000

    (literalchar): fixed the code for escaping double quotes (thanks Jonathan Czisny.)

--- src/lex.c
+++ src/lex.c
@@ -271,7 +271,9 @@ literalchar (char **pp, int *pcode, char term)
      so that `\012' and `\n' can be interchangeable.  */
 
   p = *pp;
-  if (code == '\\')  {*p++ = '\\'; *p++ = '\\';}
+  if (code == term && wasquote)
+    *p++ = code;
+  else if (code == '\\')  {*p++ = '\\'; *p++ = '\\';}
   else if (code == '\'')  {*p++ = '\\'; *p++ = '\'';}
   else if (code == '\"')  {*p++ = '\\'; *p++ = '\"';}
   else if (code >= 040 && code < 0177)
