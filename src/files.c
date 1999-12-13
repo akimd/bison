5191ef2421ba4ce9016781f4a9274fb74920fc87
Jesse Thilo <jthilo@gnu.org>
1999-12-13 07:36:01 +0000

    OS/2 honors TMPDIR environment variable.

--- src/files.c
+++ src/files.c
@@ -154,7 +154,7 @@ openfiles (void)
   strlwr (infile);
 #endif /* _WIN32 && !__CYGWIN32__ */
 
-#if (defined(unix) || defined(__unix) || defined(__unix__))
+#if (defined(unix) || defined(__unix) || defined(__unix__) || defined(__EMX__))
   {
     char *tmp_ptr = getenv("TMPDIR");
 
