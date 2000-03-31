cb7db13e45ec65cda920b9b5b2519bed663d5b38
Akim Demaille <akim@epita.fr>
2000-03-31 14:37:10 +0000

    * src/main.c (main): Don't call `done', exit does it.

--- src/main.c
+++ src/main.c
@@ -104,7 +104,6 @@ main (int argc, char *argv[])
 
   /* output the tables and the parser to ftable.  In file output. */
   output();
-  done ();
 
   exit (complain_message_count ? 1 : 0);
 }
--- src/reduce.c
+++ src/reduce.c
@@ -142,12 +142,13 @@ reduce_grammar (void)
 	   tags[start_symbol]);
 
   reduce_grammar_tables();
-  /* if (verboseflag) {
-     fprintf(foutput, "REDUCED GRAMMAR\n\n");
-     dump_grammar();
-     }
-     */
-
+  /*
+  if (verboseflag)
+    {
+      fprintf(foutput, "REDUCED GRAMMAR\n\n");
+      dump_grammar();
+    }
+  */
   /**/ statisticsflag = FALSE; /* someday getopts should handle this */
   if (statisticsflag == TRUE)
     fprintf(stderr,
