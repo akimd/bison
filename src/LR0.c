403b315bb1f3b9b61630cf123f348cb19070dd6c
Akim Demaille <akim@epita.fr>
2000-10-02 08:14:22 +0000

    * src/LR0.h: New file. Propagate its use.

--- src/LR0.c
+++ src/LR0.c
@@ -28,6 +28,7 @@
 #include "state.h"
 #include "complain.h"
 #include "closure.h"
+#include "LR0.h"
 
 extern short *itemset;
 extern short *itemsetend;
@@ -37,8 +38,6 @@ int final_state;
 core *first_state;
 shifts *first_shift;
 reductions *first_reduction;
-
-extern void generate_states PARAMS ((void));
 
 static core *this_state;
 static core *last_state;
--- src/main.c
+++ src/main.c
@@ -31,11 +31,7 @@
 #include "reduce.h"
 #include "nullable.h"
 #include "print.h"
-
-#if 0				/* XXX currently unused.  */
-/* Nonzero means failure has been detected; don't write a parser file.  */
-static int failure;
-#endif
+#include "LR0.h"
 
 /* The name this program was run with, for messages.  */
 char *program_name;
@@ -43,13 +39,6 @@ char *program_name;
 extern void berror PARAMS((const char *));
 
 extern char *printable_version PARAMS ((int));
-
-extern void generate_states PARAMS ((void));
-extern void initialize_conflicts PARAMS ((void));
-extern void finalize_conflicts PARAMS ((void));
-
-
-/* VMS complained about using `int'.  */
 
 int
 main (int argc, char *argv[])
