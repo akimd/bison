c33638bb3048e7fa4283b18b7f15c0b3cf44aee4
Akim Demaille <akim@epita.fr>
2000-10-02 10:22:19 +0000

    * src/getargs.c (getargs): Don't dump `--help' on unrecognized options.

--- src/getargs.c
+++ src/getargs.c
@@ -214,7 +214,8 @@ getargs (int argc, char *argv[])
 	  break;
 
 	default:
-	  usage (stderr);
+	  fprintf (stderr, _("Try `%s --help' for more information.\n"),
+		   program_name);
 	  exit (1);
 	}
     }
