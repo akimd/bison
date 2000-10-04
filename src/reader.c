a9e64249c8ffa4f5113d0b98038437bb36af3c6e
Akim Demaille <akim@epita.fr>
2000-10-04 11:31:14 +0000

    * src/reader.c (parse_expect_decl): Keep `count' within the size of `buffer'. From Neil Booth.

--- src/reader.c
+++ src/reader.c
@@ -704,7 +704,7 @@ parse_expect_decl (void)
   count = 0;
   while (c >= '0' && c <= '9')
     {
-      if (count < 20)
+      if (count < sizeof(buffer) - 1)
 	buffer[count++] = c;
       c = getc (finput);
     }
