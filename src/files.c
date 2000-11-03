d1cdce7c1cbe3a63e5307cd3db7649c249eb8d63
Akim Demaille <akim@epita.fr>
2000-11-03 17:27:19 +0000

    For some reasons, this has not been applied. * src/files.c [VMS]: No longer include `ssdef.h', no longer define `unlink': it's no longer used.

--- src/files.c
+++ src/files.c
@@ -22,8 +22,6 @@
 #include "system.h"
 
 #if defined (VMS) & !defined (__VMS_POSIX)
-# include <ssdef.h>
-# define unlink delete
 # ifndef XPFILE
 #  define XPFILE "GNU_BISON:[000000]BISON.SIMPLE"
 # endif
