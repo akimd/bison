5af1f54933220f178509b15ec6b7f65a780630f1
Akim Demaille <akim@epita.fr>
2000-11-07 15:56:20 +0000

    * src/reader.c (reader): #define YYLSP_NEEDED to 1 instead of just defining. * src/bison.s1: s/#ifdef YYLSP_NEEDED/#if YYLSP_NEEDED/.

--- src/reader.c
+++ src/reader.c
@@ -1975,7 +1975,7 @@ reader (void)
   /* Write closing delimiters for actions and guards.  */
   output_trailers ();
   if (locations_flag)
-    obstack_grow_literal_string (&table_obstack, "#define YYLSP_NEEDED\n\n");
+    obstack_grow_literal_string (&table_obstack, "#define YYLSP_NEEDED 1\n\n");
   /* Assign the symbols their symbol numbers.  Write #defines for the
      token symbols into FDEFINES if requested.  */
   packsymbols ();
