8a4f41d6ed0848c04db9095ad564a2d2b1831fca
Akim Demaille <akim@epita.fr>
2000-11-07 11:54:01 +0000

    * src/output.c (output): Remove useless variables. * lib/obstack.c (obstack_grow, obstack_grow0): Rename the second argument `data' for consistency with the prototypes. Qualify it `const'. (obstack_copy, obstack_copy0): Rename the second argument as `address' for consistency.  Qualify it `const'. * lib/obstack.h (obstack_copy, obstack_copy0, obstack_grow) (obstack_grow0, obstack_ptr_grow, obstack_ptr_grow_fast): Qualify `const' their input argument (`data' or `address'). Adjust the corresponding macros to include `const' in casts.

--- src/output.c
+++ src/output.c
@@ -1308,8 +1308,6 @@ free_itemsets (void)
 void
 output (void)
 {
-  int c;
-
   /* output_token_defines(ftable);      / * JF put out token defines FIRST */
 
   /* If using a simple parser the definition of YYSTYPE are put into
