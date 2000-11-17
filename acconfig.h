7de23534ac9fae2f862249e66cf46b6d03e35d14
Akim Demaille <akim@epita.fr>
2000-11-17 10:03:37 +0000

    * acconfig.h: Remove, no longer used.

--- acconfig.h
+++
@@ -1,7 +0,0 @@
-/* Define as 1 if realloc must be declared even if <stdlib.h> is
-   included.  */
-#undef NEED_DECLARATION_REALLOC
-
-/* Define as 1 if calloc must be declared even if <stdlib.h> is
-   included.  */
-#undef NEED_DECLARATION_CALLOC
