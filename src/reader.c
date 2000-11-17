d7045ec67202b33b02242ecee50e96e2ee456514
Akim Demaille <akim@epita.fr>
2000-11-17 14:14:36 +0000

    * lib/obstack.h: Formatting changes. (obstack_grow, obstack_grow0): Don't cast WHERE at all: it prevents type checking. (obstack_ptr_grow, obstack_ptr_grow_fast): When assigning, don't cast the value to (void *): assigning a `foo *' to a `void *' variable is valid. (obstack_int_grow, obstack_int_grow_fast): Don't cast AINT to int. * src/reader.c (parse_union_decl): Typo: use obstack_1grow to append characters.

--- src/reader.c
+++ src/reader.c
@@ -793,7 +793,7 @@ parse_union_decl (void)
     {
       obstack_1grow (&attrs_obstack, c);
       if (defines_flag)
-	obstack_grow_literal_string (&defines_obstack, c);
+	obstack_1grow (&defines_obstack, c);
 
       switch (c)
 	{
