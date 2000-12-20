5d3214b854d2b49607df9f8fce90e39abd056f1d
Akim Demaille <akim@epita.fr>
2000-12-20 12:00:13 +0000

    * src/files.c (open_files): Actually the `.output' file is based on the short_base_name, not base_name. * tests/suite.at (Checking output file names): Adjust.

--- src/files.c
+++ src/files.c
@@ -252,7 +252,7 @@ open_files (void)
   if (verbose_flag)
     /* We used to use just .out if spec_name_prefix (-p) was used, but
        that conflicts with Posix.  */
-    foutput = xfopen (stringappend (base_name, EXT_OUTPUT), "w");
+    foutput = xfopen (stringappend (short_base_name, EXT_OUTPUT), "w");
 
   attrsfile = stringappend (short_base_name, EXT_STYPE_H);
 
