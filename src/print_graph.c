20e8e5cadf2f68d6e90aff9f862023c1ad5f0960
Akim Demaille <akim@epita.fr>
2001-08-07 08:18:32 +0000

    * src/print_graph.c (quote): New. (print_core): Use it.

--- src/print_graph.c
+++ src/print_graph.c
@@ -32,9 +32,17 @@
 #include "obstack.h"
 #include "print_graph.h"
 #include "vcg.h"
-#include "quote.h"
+#include "quotearg.h"
 
 static graph_t graph;
+
+/* Return an unambiguous printable representated, allocated in slot 0,
+   for NAME, suitable for C strings.  */
+static char const *
+quote (char const *name)
+{
+  return quotearg_n_style (0, escape_quoting_style, name);
+}
 
 /* This part will construct the label of nodes. */
 static void
@@ -67,7 +75,7 @@ print_core (int state)
 
       rule = -(*sp);
 
-      obstack_fgrow1 (&graph_obstack, _("%d: "), rule);
+      obstack_fgrow1 (&graph_obstack, "%d: ", rule);
       obstack_fgrow1 (&graph_obstack, " %s  ->  ", quote (tags[rlhs[rule]]));
 
       for (sp = ritem + rrhs[rule]; sp < sp1; sp++)
