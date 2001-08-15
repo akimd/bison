e795bd73766b33f46ba1c54dec84654e6afcc59d
Akim Demaille <akim@epita.fr>
2001-08-15 08:53:15 +0000

    (compute_header_macro): Unconstify the result.

--- src/files.c
+++ src/files.c
@@ -90,7 +90,7 @@ stringappend (const char *string1, const char *string2)
 |  header of the parser and store it in header_macro_name.       |
 `---------------------------------------------------------------*/
 
-static const char *
+static char *
 compute_header_macro (void)
 {
   int ite;
