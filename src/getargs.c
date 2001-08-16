78632bbcdc007a0895ca9eb4498b7e042e36ae17
Marc Autret <autret_m@epita.fr>
2001-08-16 18:50:04 +0000

    * src/getargs.c (usage): Update with ``-g, --graph''.

--- src/getargs.c
+++ src/getargs.c
@@ -118,7 +118,9 @@ Output:\n\
   -d, --defines              also produce a header file\n\
   -v, --verbose              also produce an explanation of the automaton\n\
   -b, --file-prefix=PREFIX   specify a PREFIX for output files\n\
-  -o, --output-file=FILE     leave output to FILE\n"), stream);
+  -o, --output-file=FILE     leave output to FILE\n\
+  -g, --graph                also produce a VCG graph description of the \
+automaton\n"), stream);
   putc ('\n', stream);
 
   fputs (_("\
