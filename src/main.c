6f43b11362bfc441d868f163146d4c19a87e47c0
Robert Anisko <robert@lrde.epita.fr>
2001-08-25 14:34:41 +0000

    * src/main.c (main): Initialize the macro table.

--- src/main.c
+++ src/main.c
@@ -51,6 +51,7 @@ main (int argc, char *argv[])
   lineno = 0;
   getargs (argc, argv);
 
+  macro_init ();
   open_files ();
 
   /* Read the input.  Copy some parts of it to FGUARD, FACTION, FTABLE
