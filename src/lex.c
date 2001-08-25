dd3127cf7a8534b1689d19452e310138d8db784f
Robert Anisko <robert@lrde.epita.fr>
2001-08-25 14:27:57 +0000

    * src/lex.c (percent_table): Add tok_define. * src/lex.h: Add tok_define.

--- src/lex.c
+++ src/lex.c
@@ -554,6 +554,7 @@ struct percent_table_struct percent_table[] =
 #endif
   { "header_extension",	NULL,			tok_hdrext},
   { "source_extension",	NULL,			tok_srcext},
+  { "define",		NULL,			tok_define },
   { "verbose",		&verbose_flag,		tok_noop },	/* -v */
   { "debug",		&debug_flag,		tok_noop },	/* -t */
   { "semantic_parser",	&semantic_parser,	tok_noop },
