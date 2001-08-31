9e644e648d544ef79756cfb58098ec6f233bc451
Marc Autret <autret_m@epita.fr>
2001-08-31 18:30:21 +0000

    * src/bison.simple (YYLSP_NEEDED): New definition. * src/output.c (prepare): Add macro insertion of `locations_flag'

--- src/output.c
+++ src/output.c
@@ -1074,6 +1074,8 @@ prepare (void)
   MACRO_INSERT_INT ("nstates", nstates);
   MACRO_INSERT_INT ("ntokens", ntokens);
 
+  MACRO_INSERT_INT ("locations_flag", locations_flag);
+
   if (spec_name_prefix)
     MACRO_INSERT_STRING ("prefix", spec_name_prefix);
 }
