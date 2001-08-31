63c2d5de0fa3237edad53913dca35abda6a36f2d
Marc Autret <autret_m@epita.fr>
2001-08-31 16:01:53 +0000

    * src/reader.c (read_additionnal_code): Rename %%user_code to %%epilogue. * src/output.c (output): Rename %%declarations to %%prologue. * src/bison.simple: Echo modifications.

--- src/output.c
+++ src/output.c
@@ -1114,7 +1114,7 @@ output (void)
 #endif
   prepare ();
   /* Copy definitions in directive.  */
-  macro_insert ("definitions", obstack_finish (&attrs_obstack));
+  macro_insert ("prologue", obstack_finish (&attrs_obstack));
   output_parser ();
 
   obstack_free (&macro_obstack, 0);
--- src/reader.c
+++ src/reader.c
@@ -1666,21 +1666,21 @@ readgram (void)
 }
 
 /* At the end of the grammar file, some C source code must
-   be stored. It is going to be associated to the user_code
+   be stored. It is going to be associated to the epilogue
    directive.  */
 static void
 read_additionnal_code (void)
 {
   char c;
-  struct obstack uc_obstack;
+  struct obstack el_obstack;
   
-  obstack_init (&uc_obstack);
+  obstack_init (&el_obstack);
 
   while ((c = getc (finput)) != EOF)
-    obstack_1grow (&uc_obstack, c);
+    obstack_1grow (&el_obstack, c);
   
-  obstack_1grow (&uc_obstack, 0);
-  macro_insert ("user_code", obstack_finish (&uc_obstack));
+  obstack_1grow (&el_obstack, 0);
+  macro_insert ("epilogue", obstack_finish (&el_obstack));
 }
 
 
