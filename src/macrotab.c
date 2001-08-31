17da6427fe433be974371d1b4f56061c0903776d
Pascal Bart <pascal.bart@epita.fr>
2001-08-31 18:22:54 +0000

    * src/output.c (prepare): Delete insertion of previous muscles, and insert the `prefix' muscles. * src/macrotab.c (macro_init): Likewise. (macro_init): Initialization prefix directive by `yy'. * src/bison.simple: Substitute all %%yylex, %%yychar, %%yylval, %%yydebug, %%yyerror, %%yynerrs and %%yyparse by yylex, yychar, yylval, yydebug, yyerror, yynerrs and yyparse. New directive `#define' to substitute yydebug, ... with option name_prefix.

--- src/macrotab.c
+++ src/macrotab.c
@@ -86,15 +86,7 @@ macro_init (void)
   macro_insert ("maxtok", "0");
   macro_insert ("ntbase", "0");
   macro_insert ("verbose", "0");
-
-  /* Variable prefix names.  */
-  macro_insert ("yyparse", "yyparse");
-  macro_insert ("yylex", "yylex");
-  macro_insert ("yyerror", "yyerror");
-  macro_insert ("yylval", "yylval");
-  macro_insert ("yychar", "yychar");
-  macro_insert ("yydebug", "yydebug");
-  macro_insert ("yynerrs", "yynerrs");
+  macro_insert ("prefix", "yy");
 
   /* No parser macros.  */
   macro_insert ("nnts", "0");
--- src/output.c
+++ src/output.c
@@ -1075,15 +1075,7 @@ prepare (void)
   MACRO_INSERT_INT ("ntokens", ntokens);
 
   if (spec_name_prefix)
-    {
-      MACRO_INSERT_PREFIX ("yylex", "lex");
-      MACRO_INSERT_PREFIX ("yychar", "char");
-      MACRO_INSERT_PREFIX ("yylval", "lval");
-      MACRO_INSERT_PREFIX ("yydebug", "debug");
-      MACRO_INSERT_PREFIX ("yyerror", "error");
-      MACRO_INSERT_PREFIX ("yynerrs", "nerrs");
-      MACRO_INSERT_PREFIX ("yyparse", "parse");
-    }
+    MACRO_INSERT_STRING ("prefix", spec_name_prefix);
 }
 
 /*----------------------------------------------------------.
