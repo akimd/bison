682d48cd092007bea29bcfb37c8c8caea6ee72cb
Pascal Bart <pascal.bart@epita.fr>
2001-09-01 10:40:54 +0000

    * src/reader.c (copy_guard): Use muscle to specify the `#line' filename.

--- src/reader.c
+++ src/reader.c
@@ -1201,7 +1201,8 @@ copy_guard (symbol_list *rule, int stack_offset)
   obstack_fgrow1 (&guard_obstack, "\ncase %d:\n", nrules);
   if (!no_lines_flag)
     obstack_fgrow2 (&guard_obstack, "#line %d %s\n",
-		    lineno, quotearg_style (c_quoting_style, infile));
+		    lineno, quotearg_style (c_quoting_style,
+					    macro_find ("filename")));
   obstack_1grow (&guard_obstack, '{');
 
   count = 0;
