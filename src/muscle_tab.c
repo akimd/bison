f508cb0ac9a43d70b186255021420a27b3225f48
Pascal Bart <pascal.bart@epita.fr>
2001-09-19 19:25:02 +0000

    * src/bison.simple: s/%%filename/%%skeleton. * src/muscle_tab.c (getargs.h): Include it. (muscle_init): Insert new muscle skeleton.

--- src/muscle_tab.c
+++ src/muscle_tab.c
@@ -25,6 +25,7 @@
 #include "hash.h"
 #include "files.h"
 #include "muscle_tab.h"
+#include "getargs.h"
 
 struct hash_table muscle_table;
 
@@ -54,6 +55,9 @@ muscle_init (void)
   /* Version and input file.  */
   muscle_insert ("version", VERSION);
   muscle_insert ("filename", infile);
+
+  /* Skeleton use.  */
+  muscle_insert ("skeleton", skeleton);
 
   /* Types.  */
   muscle_insert ("stype", "int");
