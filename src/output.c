577c6c84990d501ce56415d3207fb3a9379fa9d9
Pascal Bart <pascal.bart@epita.fr>
2001-09-22 21:43:05 +0000

    Fix line report with input_line

--- src/output.c
+++ src/output.c
@@ -992,7 +992,7 @@ output_parser (const char *skel_filename, struct obstack *oout)
 	  else if (!strcmp (muscle_key, "line"))
 	    obstack_fgrow1 (oout, "%d", line + 1);
 	  else if (!strcmp (muscle_key, "input_line"))
-	    obstack_fgrow1 (oout, "%d", lineno + 1);
+	    obstack_fgrow1 (oout, "%d", lineno);
 	  else if (!strcmp (muscle_key, "action"))
 	    {
 	      size_t size = obstack_object_size (&action_obstack);
