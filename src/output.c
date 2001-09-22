6f9344dadf40f1c55328b6d9953a726d156dfb90
Pascal Bart <pascal.bart@epita.fr>
2001-09-22 21:30:53 +0000

    * src/bison.simple: New `#line' directive. * src/output.c (output_parser): Support new dynamic muscle input_line.

--- src/output.c
+++ src/output.c
@@ -991,6 +991,8 @@ output_parser (const char *skel_filename, struct obstack *oout)
 	    obstack_sgrow (oout, muscle_value);
 	  else if (!strcmp (muscle_key, "line"))
 	    obstack_fgrow1 (oout, "%d", line + 1);
+	  else if (!strcmp (muscle_key, "input_line"))
+	    obstack_fgrow1 (oout, "%d", lineno + 1);
 	  else if (!strcmp (muscle_key, "action"))
 	    {
 	      size_t size = obstack_object_size (&action_obstack);
