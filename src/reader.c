f6ec6d13dcd3833254202659ada3541c9dab88a1
Akim Demaille <akim@epita.fr>
2001-10-04 15:07:32 +0000

    * src/reader.c (parse_union_decl): Push the caracters in union_obstack, not attrs_obstack.

--- src/reader.c
+++ src/reader.c
@@ -748,6 +748,8 @@ parse_union_decl (void)
 
   typed = 1;
 
+  /* FIXME: I'm worried: are you sure attrs_obstack is properly
+     filled?  */
   if (no_lines_flag)
     obstack_1grow (&attrs_obstack, '\n');
 
@@ -763,7 +765,7 @@ parse_union_decl (void)
       /* If C contains '/', it is output by copy_comment ().  */
       if (c != '/')
 	{
-	  obstack_1grow (&attrs_obstack, c);
+	  obstack_1grow (&union_obstack, c);
 	  if (defines_flag)
 	    obstack_1grow (&defines_obstack, c);
 	}
