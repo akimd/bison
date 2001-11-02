239e5c1a9adcb5f4ac268e9424c3ae5206740397
Akim Demaille <akim@epita.fr>
2001-11-02 15:26:33 +0000

    * src/symtab.h (SUNDEF): New. * src/symtab.c (bucket_new): Init user_token_number to SUNDEF to stand for `uninitialized', instead of 0. * src/reader.c (packsymbols, parse_thong_decl): Adjust. * src/lex.c (lex): Adjust. * tests/calc.at (_AT_DATA_CALC_Y): Declare a token for EOF. Number it 0. Let yylex return it instead of a plain 0. Reported by Dick Streefland.

--- src/reader.c
+++ src/reader.c
@@ -695,7 +695,6 @@ token_buffer);
 	}
 
       prev = t;
-
     }
 }
 
@@ -821,7 +820,7 @@ parse_thong_decl (void)
   token_t token;
   struct bucket *symbol;
   char *typename = 0;
-  int usrtoknum = 0;
+  int usrtoknum = SUNDEF;
 
   token = lex ();		/* fetch typename or first token */
   if (token == tok_typename)
@@ -1689,6 +1688,9 @@ token_translations_init (void)
       /* A token string alias? */
       if (bp->user_token_number == SALIAS)
 	continue;
+
+      assert (bp->user_token_number != SUNDEF);
+
       /* A token which translation has already been set? */
       if (token_translations[bp->user_token_number] != 2)
 	complain (_("tokens %s and %s both assigned number %d"),
@@ -1775,7 +1777,7 @@ packsymbols (void)
 
       if (bp->class == token_sym)
 	{
-	  if (bp->user_token_number == 0)
+	  if (bp->user_token_number == SUNDEF)
 	    bp->user_token_number = ++last_user_token_number;
 	  if (bp->user_token_number > max_user_token_number)
 	    max_user_token_number = bp->user_token_number;
--- src/symtab.c
+++ src/symtab.c
@@ -1,5 +1,5 @@
 /* Symbol table manager for Bison,
-   Copyright 1984, 1989, 2000 Free Software Foundation, Inc.
+   Copyright 1984, 1989, 2000, 2001 Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
 
@@ -58,7 +58,7 @@ bucket_new (const char *tag, int hashval)
   res->value = 0;
   res->prec = 0;
   res->assoc = right_assoc;
-  res->user_token_number = 0;
+  res->user_token_number = SUNDEF;
   res->alias = NULL;
   res->class = unknown_sym;
 
