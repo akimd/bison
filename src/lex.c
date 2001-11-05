65be086611b2ab1cae806aa3a82beec35ae6ebb9
Akim Demaille <akim@epita.fr>
2001-11-05 10:07:46 +0000

    * src/lex.c (parse_percent_token): s/quotearg/quote/.

--- src/lex.c
+++ src/lex.c
@@ -632,8 +632,8 @@ parse_percent_token (void)
 	&& option_strcmp (token_buffer + 1, tx->name) == 0)
       break;
 
-  if (arg && !tx->ret_val == tok_stropt)
-    fatal (_("`%s' supports no argument: %s"), token_buffer, quotearg (arg));
+  if (arg && tx->ret_val != tok_stropt)
+    fatal (_("`%s' supports no argument: %s"), token_buffer, quote (arg));
 
 
   switch (tx->ret_val)
