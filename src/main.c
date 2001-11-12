0df87bb6096b345a0a25714ee64c3a49596f2778
Akim Demaille <akim@epita.fr>
2001-11-12 09:31:40 +0000

    * src/conflicts.c (count_sr_conflicts, count_rr_conflicts): Return the result instead of using... (src_total, rrc_total, src_count, rrc_count): Remove. (any_conflicts): Remove. (print_conflicts): Split into... (conflicts_print, conflicts_output): New. * src/conflicts.h: Adjust. * src/main.c (main): Invoke both conflicts_output and conflicts_print. * src/print.c (print_grammar): Issue `n' between to rule outputs. * tests/regression.at (Conflicts): New. Reported by Tom Lane.

--- src/main.c
+++ src/main.c
@@ -84,6 +84,7 @@ main (int argc, char *argv[])
      conflicts.  Also resolve s/r conflicts based on precedence
      declarations.  */
   solve_conflicts ();
+  conflicts_print ();
 
   /* Output file names. */
   compute_output_file_names ();
@@ -93,7 +94,7 @@ main (int argc, char *argv[])
   if (complain_message_count)
     exit (1);
 
-  /* Print information about results, if requested.  */
+  /* Output the detailed report on the grammar.  */
   print_results ();
 
   /* Output the VCG graph.  */
