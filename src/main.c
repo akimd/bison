26e1ca45bac046a1f14146e93cb900e211447972
Akim Demaille <akim@epita.fr>
2001-11-14 14:14:48 +0000

    * src/system.h (EXIT_SUCCESS, EXIT_FAILURE): Ensure a proper definition. * src/main.c (main): Use them. Suggested by Hans Aberg.

--- src/main.c
+++ src/main.c
@@ -117,7 +117,7 @@ main (int argc, char *argv[])
 
   output_files ();
 
-  exit (complain_message_count ? 1 : 0);
+  return complain_message_count ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
 /* Abort for an internal error denoted by string S.  */
--- src/system.h
+++ src/system.h
@@ -40,6 +40,18 @@
 # include <stdlib.h>
 #endif
 
+/* The following test is to work around the gross typo in
+   systems like Sony NEWS-OS Release 4.0C, whereby EXIT_FAILURE
+   is defined to 0, not 1.  */
+#if !EXIT_FAILURE
+# undef EXIT_FAILURE
+# define EXIT_FAILURE 1
+#endif
+
+#ifndef EXIT_SUCCESS
+# define EXIT_SUCCESS 0
+#endif
+
 #if HAVE_UNISTD_H
 # include <unistd.h>
 #endif
