0f37a9942fa297c9e45e5224844c1938a45834f2
Akim Demaille <akim@epita.fr>
2001-11-14 14:15:22 +0000

    * src/system.h (EXIT_SUCCESS, EXIT_FAILURE): Ensure a proper definition. * src/main.c (main): Use them. Suggested by Hans Aberg.

--- src/main.c
+++ src/main.c
@@ -119,7 +119,7 @@ main (int argc, char *argv[])
 
   output_files ();
 
-  exit (complain_message_count ? 1 : 0);
+  return complain_message_count ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
 /* Abort for an internal error denoted by string S.  */
