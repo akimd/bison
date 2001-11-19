483642486db865d77e78842eafa9532002f29030
Akim Demaille <akim@epita.fr>
2001-11-19 09:27:11 +0000

    * src/derives.c (print_derives): Fix and enrich. * src/closure.c (print_fderives): Likewise.

--- src/derives.c
+++ src/derives.c
@@ -38,15 +38,21 @@ static void
 print_derives (void)
 {
   int i;
-  short *sp;
 
   fputs ("DERIVES\n", stderr);
 
   for (i = ntokens; i < nsyms; i++)
     {
+      short *sp;
       fprintf (stderr, "\t%s derives\n", tags[i]);
       for (sp = derives[i]; *sp > 0; sp++)
-	fprintf (stderr, "\t\t%d (%s)\n", *sp, tags[*sp]);
+	{
+	  short *rhsp;
+	  fprintf (stderr, "\t\t%d:", *sp);
+	  for (rhsp = ritem + rule_table[*sp].rhs; *rhsp > 0; ++rhsp)
+	    fprintf (stderr, " %s", tags[*rhsp]);
+	  fputc ('\n', stderr);
+	}
     }
 
   fputs ("\n\n", stderr);
