2b7c2ef124509ed17fd27b426db7ec0b69a9ec52
Akim Demaille <akim@epita.fr>
2001-11-19 11:07:50 +0000

    * src/conflicts.c (conflicts_print): Add a missing n.

--- src/conflicts.c
+++ src/conflicts.c
@@ -477,8 +477,8 @@ conflicts_print (void)
       && src_total != expected_conflicts)
     {
       complain_message_count++;
-      fprintf (stderr, ngettext ("expected %d shift/reduce conflict",
-				 "expected %d shift/reduce conflicts",
+      fprintf (stderr, ngettext ("expected %d shift/reduce conflict\n",
+				 "expected %d shift/reduce conflicts\n",
 				 expected_conflicts),
 	       expected_conflicts);
     }
