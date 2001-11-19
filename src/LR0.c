6986fd9e3b0af45d17e709525587877871212bba
Akim Demaille <akim@epita.fr>
2001-11-19 10:28:57 +0000

    * src/LR0.c (new_state): Complete trace code. * src/nullable.c (set_nullable): Don't translate traces.

--- src/LR0.c
+++ src/LR0.c
@@ -200,7 +200,8 @@ new_state (int symbol)
   short *iend;
 
 #if TRACE
-  fprintf (stderr, "Entering new_state, symbol = %d\n", symbol);
+  fprintf (stderr, "Entering new_state, symbol = %d, state = %d\n",
+	   symbol, nstates);
 #endif
 
   if (nstates >= MAXSHORT)
