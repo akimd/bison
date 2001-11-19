bb527fc2715bf579a512b8a4c7787b4f09954bf9
Akim Demaille <akim@epita.fr>
2001-11-19 10:08:54 +0000

    * src/lalr.h (LA): New macro to access to the variable LA. * src/output.c, src/lalr.c, src/print_graph.c, src/conflicts.c: * src/lalr.c: Adjust.

--- src/conflicts.c
+++ src/conflicts.c
@@ -97,7 +97,7 @@ resolve_sr_conflict (int state, int lookaheadnum)
   redprec = rprec[LAruleno[lookaheadnum]];
 
   mask = 1;
-  fp1 = LA + lookaheadnum * tokensetsize;
+  fp1 = LA (lookaheadnum);
   fp2 = lookaheadset;
   for (i = 0; i < ntokens; i++)
     {
@@ -219,7 +219,7 @@ set_conflicts (int state)
   for (i = state_table[state].lookaheads; i < k; i++)
     if (rprec[LAruleno[i]])
       {
-	fp1 = LA + i * tokensetsize;
+	fp1 = LA (i);
 	fp2 = fp1;
 	fp3 = lookaheadset;
 
@@ -238,7 +238,7 @@ set_conflicts (int state)
      for conflicts not resolved above.  */
   for (i = state_table[state].lookaheads; i < k; i++)
     {
-      fp1 = LA + i * tokensetsize;
+      fp1 = LA (i);
       fp2 = fp1;
       fp3 = lookaheadset;
 
@@ -314,7 +314,7 @@ count_sr_conflicts (int state)
 
   for (i = state_table[state].lookaheads; i < k; i++)
     {
-      fp1 = LA + i * tokensetsize;
+      fp1 = LA (i);
       fp2 = lookaheadset;
 
       while (fp2 < fp3)
@@ -366,7 +366,7 @@ count_rr_conflicts (int state)
     return 0;
 
   mask = 1;
-  baseword = LA + m * tokensetsize;
+  baseword = LA (m);
   for (i = 0; i < ntokens; i++)
     {
       unsigned *wordp = baseword;
@@ -572,7 +572,7 @@ print_reductions (FILE *out, int state)
     {
       default_rule = LAruleno[m];
 
-      fp1 = LA + m * tokensetsize;
+      fp1 = LA (m);
       fp2 = shiftset;
       fp3 = lookaheadset;
       fp4 = lookaheadset + tokensetsize;
@@ -609,7 +609,7 @@ print_reductions (FILE *out, int state)
       if (!nodefault)
 	for (i = m; i < n; i++)
 	  {
-	    fp1 = LA + i * tokensetsize;
+	    fp1 = LA (i);
 	    fp2 = shiftset;
 	    fp3 = lookaheadset;
 
@@ -664,7 +664,7 @@ print_reductions (FILE *out, int state)
 	}
 
       mask = 1;
-      fp1 = LA + m * tokensetsize;
+      fp1 = LA (m);
       fp2 = shiftset;
       for (i = 0; i < ntokens; i++)
 	{
--- src/output.c
+++ src/output.c
@@ -354,7 +354,7 @@ action_row (int state)
 	  for (i = n - 1; i >= m; i--)
 	    {
 	      rule = -LAruleno[i];
-	      wordp = LA + i * tokensetsize;
+	      wordp = LA (i);
 	      mask = 1;
 
 	      /* and find each token which the rule finds acceptable
