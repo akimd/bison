4bc30f78dc54b245896dd04724019b9b767a7b33
Akim Demaille <akim@epita.fr>
2001-11-19 10:28:43 +0000

    * src/print_graph.c (print_core): Better locality of variables. * src/print.c (print_core): Likewise.

--- src/print.c
+++ src/print.c
@@ -48,20 +48,17 @@ static void
 print_core (FILE *out, int state)
 {
   int i;
-  int k;
-  int rule;
-  core *statep;
-  short *sp;
-  short *sp1;
+  core *statep = state_table[state].state;
 
-  statep = state_table[state].state;
-  k = statep->nitems;
-
-  if (k == 0)
+  if (!statep->nitems)
     return;
 
-  for (i = 0; i < k; i++)
+  for (i = 0; i < statep->nitems; i++)
     {
+      short *sp;
+      short *sp1;
+      int rule;
+
       sp1 = sp = ritem + statep->items[i];
 
       while (*sp > 0)
@@ -71,17 +68,12 @@ print_core (FILE *out, int state)
       fprintf (out, "    %s  ->  ", tags[rule_table[rule].lhs]);
 
       for (sp = ritem + rule_table[rule].rhs; sp < sp1; sp++)
-	{
-	  fprintf (out, "%s ", tags[*sp]);
-	}
+	fprintf (out, "%s ", tags[*sp]);
 
       fputc ('.', out);
 
-      while (*sp > 0)
-	{
-	  fprintf (out, " %s", tags[*sp]);
-	  sp++;
-	}
+      for (/* Nothing */; *sp > 0; ++sp)
+	fprintf (out, " %s", tags[*sp]);
 
       fprintf (out, _("   (rule %d)"), rule);
       fputc ('\n', out);
