b4a7a230f18c7320bf81340c7225a2806efdca4f
Akim Demaille <akim@epita.fr>
2001-11-19 09:14:26 +0000

    * src/state.h (CORE_ALLOC, SHIFTS_ALLOC, ERRS_ALLOC) (REDUCTIONS_ALLOC):  New. * src/LR0.c, src/conflicts.c: Use them to de-obfuscate memory allocation.

--- src/conflicts.c
+++ src/conflicts.c
@@ -90,7 +90,7 @@ resolve_sr_conflict (int state, int lookaheadnum)
   unsigned *fp1;
   unsigned *fp2;
   int redprec;
-  errs *errp = (errs *) xcalloc (sizeof (errs) + ntokens * sizeof (short), 1);
+  errs *errp = ERRS_ALLOC (ntokens + 1);
   short *errtokens = errp->errs;
 
   /* find the rule to reduce by to get precedence of reduction  */
@@ -170,7 +170,7 @@ resolve_sr_conflict (int state, int lookaheadnum)
       /* Some tokens have been explicitly made errors.  Allocate
          a permanent errs structure for this state, to record them.  */
       i = (char *) errtokens - (char *) errp;
-      err_table[state] = (errs *) xcalloc ((unsigned int) i, 1);
+      err_table[state] = ERRS_ALLOC (i + 1);
       bcopy (errp, err_table[state], i);
     }
   else
