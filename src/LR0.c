fb9087861fd4b7de68fb8e86d08d4f07fd77238d
Akim Demaille <akim@epita.fr>
2001-11-19 10:39:07 +0000

    * src/closure.c (itemsetend): Remove, replaced with... (itemsetsize): new.

--- src/LR0.c
+++ src/LR0.c
@@ -150,7 +150,7 @@ new_itemsets (void)
 
   shiftcount = 0;
 
-  for (i = 0; i < itemsetend - itemset; ++i)
+  for (i = 0; i < itemsetsize; ++i)
     {
       int symbol = ritem[itemset[i]];
       if (symbol > 0)
@@ -544,19 +544,15 @@ augment_automaton (void)
 static void
 save_reductions (void)
 {
-  short *isp;
-  int item;
   int count;
-  reductions *p;
-
-  short *rend;
+  int i;
 
   /* Find and count the active items that represent ends of rules. */
 
   count = 0;
-  for (isp = itemset; isp < itemsetend; isp++)
+  for (i = 0; i < itemsetsize; ++i)
     {
-      item = ritem[*isp];
+      int item = ritem[itemset[i]];
       if (item < 0)
 	redset[count++] = -item;
     }
@@ -565,7 +561,7 @@ save_reductions (void)
 
   if (count)
     {
-      p = REDUCTIONS_ALLOC (count);
+      reductions *p = REDUCTIONS_ALLOC (count);
 
       p->number = this_state->number;
       p->nreds = count;
--- src/closure.c
+++ src/closure.c
@@ -26,8 +26,10 @@
 #include "derives.h"
 #include "warshall.h"
 
+/* ITEMSETSIZE is the size of the array ITEMSET.  */
 short *itemset;
-short *itemsetend;
+size_t itemsetsize;
+
 static unsigned *ruleset;
 
 /* internal data.  See comments before set_fderives and set_firsts.  */
@@ -51,11 +53,10 @@ static int varsetsize;
 static void
 print_closure (int n)
 {
-  short *isp;
-
+  int i;
   fprintf (stderr, "n = %d\n", n);
-  for (isp = itemset; isp < itemsetend; isp++)
-    fprintf (stderr, "   %d\n", *isp);
+  for (i = 0; i < itemsetsize; ++i)
+    fprintf (stderr, "   %d\n", itemset[i]);
   fprintf (stderr, "\n\n");
 }
 
@@ -265,9 +266,9 @@ closure (short *core, int n)
     }
 
   ruleno = 0;
-  itemsetend = itemset;
+  itemsetsize = 0;
   csp = core;
-  for (i= 0; i < rulesetsize; ++i)
+  for (i = 0; i < rulesetsize; ++i)
     {
       int word = ruleset[i];
       if (word == 0)
@@ -283,9 +284,9 @@ closure (short *core, int n)
 	      if (word & (1 << b))
 		{
 		  itemno = rule_table[ruleno].rhs;
-		  while (csp < (core + n ) && *csp < itemno)
-		    *itemsetend++ = *csp++;
-		  *itemsetend++ = itemno;
+		  while (csp < (core + n) && *csp < itemno)
+		    itemset[itemsetsize++] = *csp++;
+		  itemset[itemsetsize++] = itemno;
 		}
 
 	      ruleno++;
@@ -294,7 +295,7 @@ closure (short *core, int n)
     }
 
   while (csp < (core + n))
-    *itemsetend++ = *csp++;
+    itemset[itemsetsize++] = *csp++;
 
   if (trace_flag)
     print_closure (n);
