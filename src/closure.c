720e5c1bc36411c1a0591a525a897f47cfd17987
Akim Demaille <akim@epita.fr>
2001-11-19 10:39:21 +0000

    * src/derives.c (print_derives): Fix and enrich. * src/closure.c (print_fderives): Likewise.

--- src/closure.c
+++ src/closure.c
@@ -100,7 +100,13 @@ print_fderives (void)
 
       for (j = 0; j <= nrules; j++)
 	if (BITISSET (rp, j))
-	  fprintf (stderr, "\t\t%d (%s)\n", j, tags[j]);
+	  {
+	    short *rhsp;
+	    fprintf (stderr, "\t\t%d:", j);
+	    for (rhsp = ritem + rule_table[j].rhs; *rhsp > 0; ++rhsp)
+	      fprintf (stderr, " %s", tags[*rhsp]);
+	    fputc ('\n', stderr);
+	  }
     }
   fprintf (stderr, "\n\n");
 }
