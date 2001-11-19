d80a86e0b53b5fb22e9028ea3dfad990a34cdf5d
Akim Demaille <akim@epita.fr>
2001-11-19 09:10:30 +0000

    * src/lalr.h (consistent): Removed array, which	contents is now member of... (state_t): this structure. * src/output.c, src/lalr.c, src/print_graph.c, src/conflicts.c: Adjust.

--- src/conflicts.c
+++ src/conflicts.c
@@ -191,7 +191,7 @@ set_conflicts (int state)
   unsigned *fp1;
   int symbol;
 
-  if (consistent[state])
+  if (state_table[state].consistent)
     return;
 
   for (i = 0; i < tokensetsize; i++)
--- src/output.c
+++ src/output.c
@@ -620,7 +620,7 @@ action_row (int state)
 
   if (nreds >= 1 && !nodefault)
     {
-      if (consistent[state])
+      if (state_table[state].consistent)
 	default_rule = redp->rules[0];
       else
 	{
