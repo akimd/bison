c1ecb3c1f5d5df75fefc0c6715f623930795fc56
Marc Autret <autret_m@epita.fr>
2001-11-22 21:45:14 +0000

    * src/muscle_tab.c (muscle_init): Remove initialization of skeleton muscle. * src/output.c (output_master_parser): Do it here.

--- src/muscle_tab.c
+++ src/muscle_tab.c
@@ -56,9 +56,6 @@ muscle_init (void)
   muscle_insert ("version", VERSION);
   muscle_insert ("filename", infile);
 
-  /* Skeleton use.  */
-  muscle_insert ("skeleton", skeleton);
-
   /* Types.  */
   muscle_insert ("stype", "int");
   muscle_insert ("ltype", "yyltype");
--- src/output.c
+++ src/output.c
@@ -985,7 +985,6 @@ output_parser (const char *skel_filename, struct obstack *oout)
 	    obstack_fgrow1 (oout, "%d", line + 1);
 	  else if (!strcmp (muscle_key, "input_line"))
 	    obstack_fgrow1 (oout, "%d", lineno);
-	  /* FIXME: Insert the code to recognize %%sub-skeleton for exemple.  */
 	  else
 	    {
 	      obstack_sgrow (oout, "%%");
@@ -1014,6 +1013,7 @@ output_master_parser (void)
       else
 	skeleton = skeleton_find ("BISON_SIMPLE", BISON_SIMPLE);
     }
+  muscle_insert ("skeleton", skeleton);    
   output_parser (skeleton, &table_obstack);
 }
 
