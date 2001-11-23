16c4a35041d6f9bd581134f99e3d6e915e48eb8b
Akim Demaille <akim@epita.fr>
2001-11-23 14:13:19 +0000

    * src/system.h: Include alloca.h. * src/main.c (main) [C_ALLOCA]: Call alloca (0).

--- src/main.c
+++ src/main.c
@@ -117,6 +117,12 @@ main (int argc, char *argv[])
 
   output_files ();
 
+  /* If using alloca.c, flush the alloca'ed memory for the benefit of
+     people running Bison as a library in IDEs.  */
+#if C_ALLOCA
+    alloca (0);
+#endif
+
   return complain_message_count ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
--- src/system.h
+++ src/system.h
@@ -79,6 +79,21 @@
 extern int errno;
 #endif
 
+/* AIX requires this to be the first thing in the file.  */
+#ifndef __GNUC__
+# if HAVE_ALLOCA_H
+#  include <alloca.h>
+# else
+#  ifdef _AIX
+ #pragma alloca
+#  else
+#   ifndef alloca /* predefined by HP cc +Olibcalls */
+char *alloca ();
+#   endif
+#  endif
+# endif
+#endif
+
 #if PROTOTYPES
 # define PARAMS(p) p
 #else
