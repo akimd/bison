cd7f95831ea5c66df7789cbbeb2583c8d59ffa79
Akim Demaille <akim@epita.fr>
2001-11-26 09:19:56 +0000

    * configure.in: Invoke AC_FUNC_OBSTACK and AC_FUNC_ERROR_AT_LINE. * lib/Makefile.am (libbison_a_SOURCES): Adjust.

--- src/files.c
+++ src/files.c
@@ -211,9 +211,9 @@ skeleton_find (const char *envvar, const char *skeleton_name)
   if (!res)
     {
       /* Skeleton file name without path */
-      const char *skel_name = strrchr(skeleton_name, '/');
+      const char *skel_name = strrchr (skeleton_name, '/');
       if (!skel_name)
-        skel_name = strrchr(skeleton_name, '\\');
+        skel_name = strrchr (skeleton_name, '\\');
       if (!skel_name)
         skel_name = skeleton_name;
       else
