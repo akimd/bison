e0c40012bd7c94c88a0bb9886e37de9622729d9a
Akim Demaille <akim@epita.fr>
2001-11-26 09:50:29 +0000

    * src/reader.c (read_declarations): Don't abort on tok_illegal, issue an error message. * tests/regression.at (Invalid %directive): New. Reported by Hans Aberg.

--- src/reader.c
+++ src/reader.c
@@ -1054,10 +1054,10 @@ read_declarations (void)
 	    case tok_stropt:
 	    case tok_intopt:
 	    case tok_obsolete:
-	    case tok_illegal:
 	      abort ();
 	      break;
 
+	    case tok_illegal:
 	    default:
 	      complain (_("unrecognized: %s"), token_buffer);
 	      skip_to_char ('%');
