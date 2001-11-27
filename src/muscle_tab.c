68254a0303c9bb368769b01cd4b9cab28488ab78
Marc Autret <autret_m@epita.fr>
2001-11-27 20:55:16 +0000

    * src/muscle_tab.c (muscle_init): Remove 'verbose' muscle initialization. * src/output.c (prepare): Remove its update.

--- src/muscle_tab.c
+++ src/muscle_tab.c
@@ -86,7 +86,6 @@ muscle_init (void)
   muscle_insert ("final", "0");
   muscle_insert ("maxtok", "0");
   muscle_insert ("ntbase", "0");
-  muscle_insert ("verbose", "0");
   muscle_insert ("error_verbose", "0");
   muscle_insert ("prefix", "yy");
   /* Default #line formatting.  */
--- src/output.c
+++ src/output.c
@@ -1052,7 +1052,6 @@ prepare (void)
   MUSCLE_INSERT_INT ("final", final_state);
   MUSCLE_INSERT_INT ("maxtok", max_user_token_number);
   MUSCLE_INSERT_INT ("ntbase", ntokens);
-  MUSCLE_INSERT_INT ("verbose", 0);
   MUSCLE_INSERT_INT ("error_verbose", error_verbose_flag);
 
   MUSCLE_INSERT_INT ("nnts", nvars);
