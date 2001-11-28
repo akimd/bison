d2b04478d43aafc69426af73667ec017949ea2c5
Akim Demaille <akim@epita.fr>
2001-11-28 14:53:03 +0000

    * src/closure.c (closure): Instead of looping over word in array then bits in words, loop over bits in array.

--- src/closure.c
+++ src/closure.c
@@ -242,7 +242,8 @@ closure (short *core, int n)
   /* Index over RULESET. */
   int r;
 
-  int itemno;
+  /* A bit index over RULESET. */
+  int b;
 
   if (trace_flag)
     {
@@ -271,27 +272,22 @@ closure (short *core, int n)
   ruleno = 0;
   itemsetsize = 0;
   c = 0;
-  for (r = 0; r < rulesetsize; ++r)
+  for (b = 0; b < rulesetsize * BITS_PER_WORD; ++b)
     {
-      int b;
-
-      for (b = 0; b < BITS_PER_WORD; b++)
+      if (BITISSET (ruleset, b))
 	{
-	  if (ruleset[r] & (1 << b))
+	  int itemno = rule_table[ruleno].rhs;
+	  while (c < n && core[c] < itemno)
 	    {
-	      itemno = rule_table[ruleno].rhs;
-	      while (c < n && core[c] < itemno)
-		{
-		  itemset[itemsetsize] = core[c];
-		  itemsetsize++;
-		  c++;
-		}
-	      itemset[itemsetsize] = itemno;
+	      itemset[itemsetsize] = core[c];
 	      itemsetsize++;
+	      c++;
 	    }
-
-	  ruleno++;
+	  itemset[itemsetsize] = itemno;
+	  itemsetsize++;
 	}
+
+      ruleno++;
     }
 
   while (c < n)
