cb4956ee99af6f63b288a6dbb04fc340bb8f8c3b
Akim Demaille <akim@epita.fr>
2001-11-28 11:43:24 +0000

    * tests/reduce.at (Useless Terminals, Useless Nonterminals): New. * src/reduce.c (reduce_grammar_tables): Do not free useless tags, as all tags are free'ed afterwards. From Enrico Scholz.

--- src/reduce.c
+++ src/reduce.c
@@ -352,10 +352,6 @@ reduce_grammar_tables (void)
 	      sprec[n] = sprec[i];
 	      tags[n] = tags[i];
 	    }
-	  else
-	    {
-	      free (tags[i]);
-	    }
 	}
 
       /* Replace all symbol numbers in valid data structures.  */
