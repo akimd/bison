2c4c30aa95ca133b7ba23d4f4977844976975862
Akim Demaille <akim@epita.fr>
2001-11-28 14:52:50 +0000

    * src/closure.c (closure): No longer optimize the special case where all the bits of `ruleset[r]' are set to 0, to make the code clearer.

--- src/closure.c
+++ src/closure.c
@@ -273,31 +273,24 @@ closure (short *core, int n)
   c = 0;
   for (r = 0; r < rulesetsize; ++r)
     {
-      if (ruleset[r] == 0)
-	{
-	  ruleno += BITS_PER_WORD;
-	}
-      else
-	{
-	  int b;
+      int b;
 
-	  for (b = 0; b < BITS_PER_WORD; b++)
+      for (b = 0; b < BITS_PER_WORD; b++)
+	{
+	  if (ruleset[r] & (1 << b))
 	    {
-	      if (ruleset[r] & (1 << b))
+	      itemno = rule_table[ruleno].rhs;
+	      while (c < n && core[c] < itemno)
 		{
-		  itemno = rule_table[ruleno].rhs;
-		  while (c < n && core[c] < itemno)
-		    {
-		      itemset[itemsetsize] = core[c];
-		      itemsetsize++;
-		      c++;
-		    }
-		  itemset[itemsetsize] = itemno;
+		  itemset[itemsetsize] = core[c];
 		  itemsetsize++;
+		  c++;
 		}
-
-	      ruleno++;
+	      itemset[itemsetsize] = itemno;
+	      itemsetsize++;
 	    }
+
+	  ruleno++;
 	}
     }
 
