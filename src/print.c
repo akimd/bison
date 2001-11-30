d287d66a69966117aa8dc1b2d00507ee98e528ab
Akim Demaille <akim@epita.fr>
2001-11-30 10:45:45 +0000

    * src/reduce.c (reduce_output): Formatting changes. * src/print.c (print_results, print_grammar): Likewise. * tests/regression.at (Rule Line Numbers) (Solved SR Conflicts, Unresolved SR Conflicts): Adjust.

--- src/print.c
+++ src/print.c
@@ -181,11 +181,11 @@ print_actions (FILE *out, int state)
 static void
 print_state (FILE *out, int state)
 {
-  fputs ("\n\n", out);
   fprintf (out, _("state %d"), state);
   fputs ("\n\n", out);
   print_core (out, state);
   print_actions (out, state);
+  fputs ("\n\n", out);
 }
 
 /*-----------------------------------------.
@@ -212,7 +212,7 @@ print_grammar (FILE *out)
   int column = 0;
 
   /* rule # : LHS -> RHS */
-  fprintf (out, "\n%s\n\n", _("Grammar"));
+  fprintf (out, "%s\n\n", _("Grammar"));
   fprintf (out, "  %s\n", _("Number, Line, Rule"));
   for (i = 1; i <= nrules; i++)
     /* Don't print rules disabled in reduce_grammar_tables.  */
@@ -228,9 +228,11 @@ print_grammar (FILE *out)
 	  fprintf (out, " /* %s */", _("empty"));
 	fputc ('\n', out);
       }
+  fputs ("\n\n", out);
+
 
   /* TERMINAL (type #) : rule #s terminal is on RHS */
-  fprintf (out, "\n%s\n\n", _("Terminals, with rules where they appear"));
+  fprintf (out, "%s\n\n", _("Terminals, with rules where they appear"));
   fprintf (out, "%s (-1)\n", tags[0]);
 
   for (i = 0; i <= max_user_token_number; i++)
@@ -252,9 +254,10 @@ print_grammar (FILE *out)
 	      }
 	fprintf (out, "%s\n", buffer);
       }
+  fputs ("\n\n", out);
 
-  fprintf (out, "\n%s\n\n",
-	   _("Nonterminals, with rules where they appear"));
+
+  fprintf (out, "%s\n\n", _("Nonterminals, with rules where they appear"));
   for (i = ntokens; i <= nsyms - 1; i++)
     {
       int left_count = 0, right_count = 0;
@@ -309,6 +312,7 @@ print_grammar (FILE *out)
 	}
       fprintf (out, "%s\n", buffer);
     }
+  fputs ("\n\n", out);
 }
 
 void
@@ -324,6 +328,8 @@ print_results (void)
 
       size_t size = obstack_object_size (&output_obstack);
       fwrite (obstack_finish (&output_obstack), 1, size, out);
+      if (size)
+	fputs ("\n\n", out);
 
       reduce_output (out);
       conflicts_output (out);
--- src/reduce.c
+++ src/reduce.c
@@ -407,50 +407,46 @@ nonterminals_reduce (void)
 void
 reduce_output (FILE *out)
 {
-  int i;
-  rule r;
-  bool b;
-
   if (nuseless_nonterminals > 0)
     {
-      fprintf (out, _("Useless nonterminals:"));
-      fprintf (out, "\n\n");
+      int i;
+      fprintf (out, "%s\n\n", _("Useless nonterminals:"));
       for (i = 0; i < nuseless_nonterminals; ++i)
 	fprintf (out, "   %s\n", tags[nsyms + i]);
+      fputs ("\n\n", out);
     }
-  b = FALSE;
-  for (i = 0; i < ntokens; i++)
-    {
+
+  {
+    bool b = FALSE;
+    int i;
+    for (i = 0; i < ntokens; i++)
       if (!BITISSET (V, i) && !BITISSET (V1, i))
 	{
 	  if (!b)
-	    {
-	      fprintf (out, "\n\n");
-	      fprintf (out, _("Terminals which are not used:"));
-	      fprintf (out, "\n\n");
-	      b = TRUE;
-	    }
+	    fprintf (out, "%s\n\n", _("Terminals which are not used:"));
+	  b = TRUE;
 	  fprintf (out, "   %s\n", tags[i]);
 	}
-    }
+    if (b)
+      fputs ("\n\n", out);
+  }
 
   if (nuseless_productions > 0)
     {
-      fprintf (out, "\n\n");
-      fprintf (out, _("Useless rules:"));
-      fprintf (out, "\n\n");
+      int i;
+      fprintf (out, "%s\n\n", _("Useless rules:"));
       for (i = 1; i <= nrules; i++)
 	if (!BITISSET (P, i))
 	  {
+	    rule r;
 	    fprintf (out, "#%-4d  ", i);
 	    fprintf (out, "%s :\t", tags[rule_table[i].lhs]);
 	    for (r = &ritem[rule_table[i].rhs]; *r >= 0; r++)
 	      fprintf (out, " %s", tags[*r]);
-	    fprintf (out, ";\n");
+	    fputs (";\n", out);
 	  }
+      fputs ("\n\n", out);
     }
-  if (nuseless_nonterminals > 0 || nuseless_productions > 0 || b)
-    fprintf (out, "\n\n");
 }
 
 static void
