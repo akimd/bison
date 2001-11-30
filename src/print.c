d39a6b7ca9c0ead60f4dd4bcf237597d40f846d3
Akim Demaille <akim@epita.fr>
2001-11-30 10:46:08 +0000

    Instead of mapping the LHS of unused rules to -1, keep the LHS valid, but flag the rules as invalid. * src/gram.h (rule_t): `useful' is a new member. * src/print.c (print_grammar): Adjust. * src/derives.c (set_derives): Likewise. * src/reader.c (packgram, reduce_output): Likewise. * src/reduce.c (reduce_grammar_tables): Likewise. * tests/reduce.at (Underivable Rules, Useless Rules): New.

--- src/print.c
+++ src/print.c
@@ -216,7 +216,7 @@ print_grammar (FILE *out)
   fprintf (out, "  %s\n", _("Number, Line, Rule"));
   for (i = 1; i <= nrules; i++)
     /* Don't print rules disabled in reduce_grammar_tables.  */
-    if (rule_table[i].lhs >= 0)
+    if (rule_table[i].useful)
       {
 	fprintf (out, _("  %3d %3d %s ->"),
 		 i, rule_table[i].line, tags[rule_table[i].lhs]);
