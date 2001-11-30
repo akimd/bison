002389585a71dfe28525fc0cdf5100c550e40b63
Akim Demaille <akim@epita.fr>
2001-11-30 10:47:56 +0000

    * src/reduce.c: Various comment/formatting changes. (nonterminals_reduce): New, extracted from... (reduce_grammar_tables): here. (reduce_grammar): Call nonterminals_reduce.

--- src/main.c
+++ src/main.c
@@ -64,8 +64,7 @@ main (int argc, char *argv[])
   if (complain_message_count)
     exit (1);
 
-  /* Find useless nonterminals and productions and reduce the grammar.
-     In file reduce.c.  */
+  /* Find useless nonterminals and productions and reduce the grammar. */
   reduce_grammar ();
 
   /* Record other info about the grammar.  In files derives and
