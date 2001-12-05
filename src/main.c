3843c413d06596e343e66e001348b2833249e76c
Akim Demaille <akim@epita.fr>
2001-12-05 09:43:21 +0000

    * src/output.c (pack_vector): Use assert, not berror. * src/main.c (berror): Remove, unused.

--- src/main.c
+++ src/main.c
@@ -41,8 +41,6 @@
 /* The name this program was run with, for messages.  */
 char *program_name;
 
-extern void berror PARAMS((const char *));
-
 int
 main (int argc, char *argv[])
 {
@@ -125,13 +123,4 @@ main (int argc, char *argv[])
 #endif
 
   return complain_message_count ? EXIT_FAILURE : EXIT_SUCCESS;
-}
-
-/* Abort for an internal error denoted by string S.  */
-
-void
-berror (const char *s)
-{
-  fprintf (stderr, _("%s: internal error: %s\n"), program_name, s);
-  abort ();
 }
--- src/output.c
+++ src/output.c
@@ -104,7 +104,6 @@
 #include "conflicts.h"
 #include "muscle_tab.h"
 
-extern void berror PARAMS((const char *));
 
 static int nvectors;
 static int nentries;
@@ -789,8 +788,8 @@ pack_vector (int vector)
 	}
     }
 
-  berror ("pack_vector");
-  return 0;			/* JF keep lint happy */
+  assert (!"pack_vector");
+  return 0;
 }
 
 
@@ -988,7 +987,7 @@ output_master_parser (void)
       else
 	skeleton = skeleton_find ("BISON_SIMPLE", BISON_SIMPLE);
     }
-  muscle_insert ("skeleton", skeleton);    
+  muscle_insert ("skeleton", skeleton);
   output_parser (skeleton, &table_obstack);
 }
 
