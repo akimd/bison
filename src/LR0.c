66d35de779507f43ce3fac42e2a0a7fa8674d598
Akim Demaille <akim@epita.fr>
2001-12-05 09:23:38 +0000

    * src/closure.c (print_closure): Improve. (closure): Use it for printing input and output.

--- src/LR0.c
+++ src/LR0.c
@@ -583,6 +583,9 @@ generate_states (void)
 
   while (this_state)
     {
+      if (trace_flag)
+	fprintf (stderr, "Processing state %d (reached by %s)\n",
+		 this_state->number, tags[this_state->accessing_symbol]);
       /* Set up ruleset and itemset for the transitions out of this
          state.  ruleset gets a 1 bit for each rule that could reduce
          now.  itemset gets a vector of all the items that could be
