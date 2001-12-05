b6a70d99a69f05e2aa7846a8583d78c2b9cd670e
Akim Demaille <akim@epita.fr>
2001-12-05 09:23:52 +0000

    * src/LR0.c (new_itemsets): Use nshifts only, not shiftcount. * src/closure.c, src/closure.h (itemsetsize): Rename as... (nitemset): for consistency with the rest of the project.

--- src/LR0.c
+++ src/LR0.c
@@ -141,7 +141,6 @@ static void
 new_itemsets (void)
 {
   int i;
-  int shiftcount;
 
   if (trace_flag)
     fprintf (stderr, "Entering new_itemsets, state = %d\n",
@@ -151,25 +150,23 @@ new_itemsets (void)
     kernel_size[i] = 0;
 
   shift_symbol = XCALLOC (short, nsyms);
-  shiftcount = 0;
+  nshifts = 0;
 
-  for (i = 0; i < itemsetsize; ++i)
+  for (i = 0; i < nitemset; ++i)
     {
       int symbol = ritem[itemset[i]];
       if (symbol > 0)
 	{
 	  if (!kernel_size[symbol])
 	    {
-	      shift_symbol[shiftcount] = symbol;
-	      shiftcount++;
+	      shift_symbol[nshifts] = symbol;
+	      nshifts++;
 	    }
 
 	  kernel_base[symbol][kernel_size[symbol]] = itemset[i] + 1;
 	  kernel_size[symbol]++;
 	}
     }
-
-  nshifts = shiftcount;
 }
 
 
@@ -542,7 +539,7 @@ save_reductions (void)
   /* Find and count the active items that represent ends of rules. */
 
   count = 0;
-  for (i = 0; i < itemsetsize; ++i)
+  for (i = 0; i < nitemset; ++i)
     {
       int item = ritem[itemset[i]];
       if (item < 0)
--- src/closure.c
+++ src/closure.c
@@ -26,9 +26,9 @@
 #include "derives.h"
 #include "warshall.h"
 
-/* ITEMSETSIZE is the size of the array ITEMSET.  */
+/* NITEMSET is the size of the array ITEMSET.  */
 short *itemset;
-int itemsetsize;
+int nitemset;
 
 static unsigned *ruleset;
 
@@ -218,7 +218,7 @@ closure (short *core, int n)
 	    ruleset[r] |= FDERIVES (ritem[core[c]])[r];
     }
 
-  itemsetsize = 0;
+  nitemset = 0;
   c = 0;
   for (ruleno = 0; ruleno < rulesetsize * BITS_PER_WORD; ++ruleno)
     if (BITISSET (ruleset, ruleno))
@@ -226,23 +226,23 @@ closure (short *core, int n)
 	int itemno = rule_table[ruleno].rhs;
 	while (c < n && core[c] < itemno)
 	  {
-	    itemset[itemsetsize] = core[c];
-	    itemsetsize++;
+	    itemset[nitemset] = core[c];
+	    nitemset++;
 	    c++;
 	  }
-	itemset[itemsetsize] = itemno;
-	itemsetsize++;
+	itemset[nitemset] = itemno;
+	nitemset++;
       }
 
   while (c < n)
     {
-      itemset[itemsetsize] = core[c];
-      itemsetsize++;
+      itemset[nitemset] = core[c];
+      nitemset++;
       c++;
     }
 
   if (trace_flag)
-    print_closure ("output", itemset, itemsetsize);
+    print_closure ("output", itemset, nitemset);
 }
 
 
