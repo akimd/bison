6a1e01f06049af15aafa89f17ecc6c498e73dcc5
Akim Demaille <akim@epita.fr>
2001-12-05 09:18:12 +0000

    * src/lalr.c (maxrhs): Move to... * src/gram.c, src/gram.h (ritem_longest_rhs): here. * src/lalr.c (build_relations): Adjust.

--- src/gram.c
+++ src/gram.c
@@ -53,6 +53,11 @@ int pure_parser;
 
 int error_token_number;
 
+
+/*------------------------.
+| Dump RITEM for traces.  |
+`------------------------*/
+
 void
 ritem_print (FILE *out)
 {
@@ -64,4 +69,33 @@ ritem_print (FILE *out)
     else
       fprintf (out, "  (rule %d)\n", -ritem[i]);
   fputs ("\n\n", out);
+}
+
+
+/*------------------------------------------.
+| Return the size of the longest rule RHS.  |
+`------------------------------------------*/
+
+size_t
+ritem_longest_rhs (void)
+{
+  short *itemp;
+  int length;
+  int max;
+
+  length = 0;
+  max = 0;
+  for (itemp = ritem; *itemp; itemp++)
+    if (*itemp > 0)
+      {
+	length++;
+      }
+    else
+      {
+	if (length > max)
+	  max = length;
+	length = 0;
+      }
+
+  return max;
 }
--- src/lalr.c
+++ src/lalr.c
@@ -194,37 +194,6 @@ set_state_table (void)
 }
 
 
-/*------------------------------------------.
-| Return the size of the longest rule RHS.  |
-`------------------------------------------*/
-
-static size_t
-maxrhs (void)
-{
-  short *itemp;
-  int length;
-  int max;
-
-  length = 0;
-  max = 0;
-  for (itemp = ritem; *itemp; itemp++)
-    {
-      if (*itemp > 0)
-	{
-	  length++;
-	}
-      else
-	{
-	  if (length > max)
-	    max = length;
-	  length = 0;
-	}
-    }
-
-  return max;
-}
-
-
 static void
 initialize_LA (void)
 {
@@ -497,7 +466,7 @@ static void
 build_relations (void)
 {
   short *edge = XCALLOC (short, ngotos + 1);
-  short *states = XCALLOC (short, maxrhs () + 1);
+  short *states = XCALLOC (short, ritem_longest_rhs () + 1);
   int i;
 
   includes = XCALLOC (short *, ngotos);
