5844cd0cfa8cdd592251b380f70cefba3bbb9c4a
Akim Demaille <akim@epita.fr>
2001-12-05 09:24:35 +0000

    * src/reduce.c (reduce_grammar_tables): No longer disable the removal of useless rules via CPP but via `if (0)', so that the compiler still check the code is valid. For instance, it should have noticed `rline' no longer exists: use the `line' member of rule_t. * src/gram.c (dummy, rline): Remove, unused.

--- src/gram.c
+++ src/gram.c
@@ -1,29 +1,27 @@
 /* Allocate input grammar variables for bison,
    Copyright 1984, 1986, 1989, 2001 Free Software Foundation, Inc.
 
-This file is part of Bison, the GNU Compiler Compiler.
+   This file is part of Bison, the GNU Compiler Compiler.
 
-Bison is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
+   Bison is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
-Bison is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   Bison is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with Bison; see the file COPYING.  If not, write to
-the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with Bison; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
 
 
 #include "system.h"
 #include "gram.h"
 #include "reader.h"
-
-void dummy PARAMS((void));
 
 /* comments for these variables are in gram.h  */
 
@@ -41,7 +39,6 @@ short *sprec = NULL;
 short *rassoc = NULL;
 short *sassoc = NULL;
 short *token_translations = NULL;
-short *rline = NULL;
 
 int start_symbol;
 
