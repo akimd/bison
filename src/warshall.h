aaf7be2c4b4fd3bb8cfc17ce6c31c4533fc92048
Akim Demaille <akim@epita.fr>
2001-12-05 09:23:10 +0000

    * src/warshall.c (TC, RTC): De-obsfucate (source reduced to 22% of what it was!). * src/warshall.h: Remove accidental duplication of the content.

--- src/warshall.h
+++ src/warshall.h
@@ -1,30 +1,5 @@
 /* Generate transitive closure of a matrix,
-   Copyright 1984, 1989, 2000 Free Software Foundation, Inc.
-
-   This file is part of Bison, the GNU Compiler Compiler.
-
-   Bison is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-
-   Bison is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with Bison; see the file COPYING.  If not, write to
-   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
-
-#ifndef WARSHALL_H_
-# define WARSHALL_H_
-
-void RTC PARAMS ((unsigned *, int));
-#endif /* !WARSHALL_H_ */
-/* Generate transitive closure of a matrix,
-   Copyright 1984, 1989, 2000 Free Software Foundation, Inc.
+   Copyright 1984, 1989, 2000, 2001 Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
 
