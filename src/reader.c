2dfb4daf9b3032f4740d1f8d4906077ecc72bdef
Akim Demaille <akim@epita.fr>
2001-12-05 09:16:07 +0000

    * src/gram.c, src/gram.h (ritem_print): New. * src/gram.c (dummy): Remove, now there is actual code in gram.c. (This useless function was defined only to work around VMS linkers that can't handle compilation units with variables only). * src/reduce.c (dump_grammar): Use it to trace the construction of ritem.

--- src/reader.c
+++ src/reader.c
@@ -1911,6 +1911,9 @@ packgram (void)
     }
 
   ritem[itemno] = 0;
+
+  if (trace_flag)
+    ritem_print (stderr);
 }
 
 /*-------------------------------------------------------------------.
--- src/reduce.c
+++ src/reduce.c
@@ -455,18 +455,22 @@ dump_grammar (FILE *out)
     fprintf (out, "%5d  %5d   %5d  %s\n", i, sprec[i], sassoc[i], tags[i]);
   fprintf (out, "\n\n");
   fprintf (out, "Rules\n-----\n\n");
-  fprintf (out, "Num (Prec, Assoc) Lhs : (@Rhs) Ritems [Num?]\n");
+  fprintf (out, "Num (Prec, Assoc, Useful, Ritem Range) Lhs -> Rhs (Ritem range) [Num]\n");
   for (i = 1; i <= nrules; i++)
     {
-      fprintf (out, "%-5d(%5d%5d)%5d : (@%-5d)",
+      int rhs_count = 0;
+      /* Find the last RHS index in ritems. */
+      for (r = &ritem[rule_table[i].rhs]; *r > 0; ++r)
+	++rhs_count;
+      fprintf (out, "%3d (%2d, %2d, %2d, %2d-%2d)   %2d ->",
 	       i,
-	       rule_table[i].prec,
-	       rule_table[i].assoc,
-	       rule_table[i].lhs,
-	       rule_table[i].rhs);
+	       rule_table[i].prec, rule_table[i].assoc, rule_table[i].useful,
+	       rule_table[i].rhs, rule_table[i].rhs + rhs_count - 1,
+	       rule_table[i].lhs);
+      /* Dumped the RHS. */
       for (r = &ritem[rule_table[i].rhs]; *r > 0; r++)
-	fprintf (out, "%5d", *r);
-      fprintf (out, " [%d]\n", -(*r));
+	fprintf (out, "%3d", *r);
+      fprintf (out, "  [%d]\n", -(*r));
     }
   fprintf (out, "\n\n");
   fprintf (out, "Rules interpreted\n-----------------\n\n");
