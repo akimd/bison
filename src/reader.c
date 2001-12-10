3735969c7de7c12abfbe0a78e108399196e271e0
Akim Demaille <akim@epita.fr>
2001-12-10 09:07:03 +0000

    * src/reader.c (reader): Calling symbols_output once is enough.

--- src/reader.c
+++ src/reader.c
@@ -2073,7 +2073,6 @@ reader (void)
   /* Assign the symbols their symbol numbers.  Write #defines for the
      token symbols into FDEFINES if requested.  */
   packsymbols ();
-  symbols_output ();
   /* Convert the grammar into the format described in gram.h.  */
   packgram ();
   /* Output the headers. */
