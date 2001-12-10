e83d80b8ed8587fd05437d33f2011403f55f1f9d
Marc Autret <autret_m@epita.fr>
2001-12-10 23:19:09 +0000

    * src/reader.c (copy_definition): Re-use CPP-outed code which replace precedent remove. * src/bison.simple: Remove #line before %%prologue because %%input-line is wrong at this time.

--- src/reader.c
+++ src/reader.c
@@ -437,14 +437,12 @@ copy_definition (void)
   /* -1 while reading a character if prev char was %. */
   int after_percent;
 
-#if 0
   if (!no_lines_flag)
     {
       obstack_fgrow2 (&attrs_obstack, muscle_find ("linef"),
 		      lineno, quotearg_style (c_quoting_style,
 					      muscle_find("filename")));
     }
-#endif
 
   after_percent = 0;
 
