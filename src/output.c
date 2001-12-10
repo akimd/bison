971d51580bc09b94b44f489acdc107cad816c164
Marc Autret <autret_m@epita.fr>
2001-12-10 22:55:06 +0000

    * src/reader.c (symbols_output): Clean up. * src/output.c (output_gram, output): Clean up.

--- src/output.c
+++ src/output.c
@@ -198,11 +198,6 @@ output_gram (void)
 
     XFREE (yyrhs);
   }
-
-#if 0
-  if (!semantic_parser && !no_parser_flag)
-    obstack_sgrow (&table_obstack, "\n#endif\n");
-#endif
 }
 
 
@@ -1068,9 +1063,6 @@ output (void)
   XFREE (user_toknums);
   output_actions ();
 
-#if 0
-  if (!no_parser_flag) */
-#endif
   prepare ();
   /* Copy definitions in directive.  */
   obstack_1grow (&attrs_obstack, 0);
--- src/reader.c
+++ src/reader.c
@@ -1904,11 +1904,6 @@ symbols_output (void)
     obstack_free (&tokendefs, NULL);
   }
 
-#if 0
-  if (!no_parser_flag)
-    output_token_defines (&table_obstack);
-#endif
-
   if (defines_flag)
     {
       output_token_defines (&defines_obstack);
