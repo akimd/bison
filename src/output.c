5449dd0f25f584ec596f165cb6004e1b4d092f10
Akim Demaille <akim@epita.fr>
2001-12-10 09:06:13 +0000

    * src/output.c (output, prepare): Make sure the values of the muscles `action' and `prologue' are 0-terminated.

--- src/output.c
+++ src/output.c
@@ -1037,6 +1037,7 @@ prepare (void)
   MUSCLE_INSERT_INT ("locations-flag", locations_flag);
 
   /* We need to save the actions in the muscle %%action.  */
+  obstack_1grow (&action_obstack, 0);
   muscle_insert ("action", obstack_finish (&action_obstack));
 
   if (spec_name_prefix)
@@ -1067,6 +1068,7 @@ output (void)
 #endif
   prepare ();
   /* Copy definitions in directive.  */
+  obstack_1grow (&attrs_obstack, 0);
   muscle_insert ("prologue", obstack_finish (&attrs_obstack));
 
   output_master_parser ();
