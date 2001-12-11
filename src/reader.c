710ddc4f18039668a7672c8670eb3e010b59292a
Marc Autret <autret_m@epita.fr>
2001-12-11 20:10:22 +0000

    * src/bison.simple: Remove #line just before %%epilogue. It is now handled in ... * src/reader.c (read_additionnal_code): Add the output of a #line for the epilogue.

--- src/reader.c
+++ src/reader.c
@@ -1688,6 +1688,13 @@ read_additionnal_code (void)
 
   obstack_init (&el_obstack);
 
+  if (!no_lines_flag)
+    {
+      obstack_fgrow2 (&el_obstack, muscle_find ("linef"),
+		      lineno, quotearg_style (c_quoting_style,
+					      muscle_find("filename")));
+    }
+
   while ((c = getc (finput)) != EOF)
     obstack_1grow (&el_obstack, c);
 
