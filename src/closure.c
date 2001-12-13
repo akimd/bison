cb581495e689b4875c96eea8135f09d8a72eaacc
Akim Demaille <akim@epita.fr>
2001-12-13 11:02:04 +0000

    * src/closure.c (closure): Use nrules instead of playing tricks with BITS_PER_WORD.

--- src/closure.c
+++ src/closure.c
@@ -220,7 +220,7 @@ closure (short *core, int n)
 
   nitemset = 0;
   c = 0;
-  for (ruleno = 0; ruleno < rulesetsize * BITS_PER_WORD; ++ruleno)
+  for (ruleno = 0; ruleno < nrules + 1; ++ruleno)
     if (BITISSET (ruleset, ruleno))
       {
 	int itemno = rule_table[ruleno].rhs;
