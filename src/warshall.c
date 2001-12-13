ed8e1f68e9743c391f4aba148968ea1e5e960973
Akim Demaille <akim@epita.fr>
2001-12-13 11:02:21 +0000

    The computation of nullable is broken: it doesn't handle empty RHS's properly. * tests/torture.at (GNU AWK Grammar): New. * tests/sets.at (Nullable): New. * src/nullable.c (set_nullable): Instead of blindly looping over `ritems', loop over the rules, and then over their rhs's. Work around Autotest bugs. * src/warshall.c (bitmatrix_print): Don't use `+--+' as table frame, because Autotest understand lines starting with a `+' as traces from the shell.  Then, they are not processed properly. Admittedly an Autotest bug, but we don't have time to wait for Autotest to catch up. * tests/regression.at (Broken Closure): Adjust to the new table frames. Move to... * tests/sets.at: here.

--- src/warshall.c
+++ src/warshall.c
@@ -49,10 +49,10 @@ bitmatrix_print (const char *title, unsigned *matrix, size_t size)
   putc ('\n', stderr);
 
   /* Bar. */
-  fputs ("  +", stderr);
+  fputs ("  .", stderr);
   for (i = 0; i < size; ++i)
     putc ('-', stderr);
-  fputs ("+\n", stderr);
+  fputs (".\n", stderr);
 
   /* Contents. */
   for (i = 0; i < size; ++i)
@@ -64,10 +64,10 @@ bitmatrix_print (const char *title, unsigned *matrix, size_t size)
     }
 
   /* Bar. */
-  fputs ("  +", stderr);
+  fputs ("  `", stderr);
   for (i = 0; i < size; ++i)
     putc ('-', stderr);
-  fputs ("+\n", stderr);
+  fputs ("'\n", stderr);
 
   /* End title. */
   fprintf (stderr, "%s END\n\n", title);
