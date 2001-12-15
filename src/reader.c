51576fb3c661cec97bb58e503351e7ad75053f6d
Akim Demaille <akim@epita.fr>
2001-12-15 11:09:05 +0000

    * src/reader.c (copy_action): When --yacc, don't append a `;' to the user action: let it fail if lacking. Suggested by Aharon Robbins and Tom Tromey.

--- src/reader.c
+++ src/reader.c
@@ -1198,7 +1198,13 @@ copy_action (symbol_list *rule, int stack_offset)
 	}
     }
 
-  obstack_sgrow (&action_obstack, ";\n    break;}");
+  /* As a Bison extension, add the ending semicolon.  Since some Yacc
+     don't do that, help people using bison as a Yacc finding their
+     missing semicolons.  */
+  if (yacc_flag)
+    obstack_sgrow (&action_obstack, "}\n    break;");
+  else
+    obstack_sgrow (&action_obstack, ";\n    break;}");
 }
 
 /*-------------------------------------------------------------------.
