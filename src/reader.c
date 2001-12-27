d945f5cd8e85c0343da52dc720d23624e1c28661
Akim Demaille <akim@epita.fr>
2001-12-27 18:05:42 +0000

    * src/reader.c (symbol_list_new): Be sure to initialize all the fields.

--- src/reader.c
+++ src/reader.c
@@ -44,8 +44,7 @@ typedef struct symbol_list
   const char *action;
   int action_line;
   bucket *ruleprec;
-}
-symbol_list;
+} symbol_list;
 
 int lineno;
 char **tags;
@@ -75,6 +74,8 @@ symbol_list_new (bucket *sym)
   res->next = NULL;
   res->sym = sym;
   res->line = lineno;
+  res->action = NULL;
+  res->action_line = 0;
   res->ruleprec = NULL;
   return res;
 }
