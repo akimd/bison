7742ddebf1f73cc15138d41c11a39f32b43c3ef5
Akim Demaille <akim@epita.fr>
2001-12-27 18:04:28 +0000

    * src/output.c (prepare): Drop the muscle `ntbase' which duplicates ntokens. * src/bison.simple: Formatting/comment changes. Use YYNTOKENS only, which is documented, but not YYNTBASE, which is an undocumented synonym.

--- src/output.c
+++ src/output.c
@@ -1012,7 +1012,6 @@ prepare (void)
   MUSCLE_INSERT_INT ("debug", debug_flag);
   MUSCLE_INSERT_INT ("final", final_state);
   MUSCLE_INSERT_INT ("maxtok", max_user_token_number);
-  MUSCLE_INSERT_INT ("ntbase", ntokens);
   MUSCLE_INSERT_INT ("error-verbose", error_verbose);
   MUSCLE_INSERT_STRING ("prefix", spec_name_prefix);
 
@@ -1057,7 +1056,7 @@ yystype;\n\
       for (i = ntokens; i < nsyms; i++)
 	/* don't make these for dummy nonterminals made by gensym.  */
 	if (*tags[i] != '@')
-	  fprintf (out, "# define\tNT%s\t%d\n", tags[i], i);
+	  fprintf (out, "# define NT%s\t%d\n", tags[i], i);
     }
 
   fprintf (out, "\n#endif /* not %s */\n", macro_name);
--- src/reader.c
+++ src/reader.c
@@ -1738,12 +1738,12 @@ symbols_save (void)
       if (c != '\0')
 	continue;
 
-      obstack_fgrow2 (&tokendefs, "# define\t%s\t%d\n",
+      obstack_fgrow2 (&tokendefs, "# define %s\t%d\n",
 		      symbol, bp->user_token_number);
       if (semantic_parser)
 	/* FIXME: This is probably wrong, and should be just as
 	   above. --akim.  */
-	obstack_fgrow2 (&tokendefs, "# define\tT%s\t%d\n", symbol, bp->value);
+	obstack_fgrow2 (&tokendefs, "# define T%s\t%d\n", symbol, bp->value);
     }
 
   obstack_1grow (&tokendefs, 0);
