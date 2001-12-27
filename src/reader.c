adc8c84856a3b1ad48015552681fe1e9757aa356
Akim Demaille <akim@epita.fr>
2001-12-27 18:06:41 +0000

    * src/reader.c (packgram): Catch nitems overflows.

--- src/reader.c
+++ src/reader.c
@@ -1692,6 +1692,10 @@ packgram (void)
   int ruleno;
   symbol_list *p;
 
+  /* We use short to index items.  */
+  if (nitems >= MAXSHORT)
+    fatal (_("too many items (max %d)"), MAXSHORT);
+
   ritem = XCALLOC (short, nitems + 1);
   rule_table = XCALLOC (rule_t, nrules) - 1;
 
