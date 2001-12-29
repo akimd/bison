ff44279401d71c061f2dc315a381ce2aeeb33a61
Akim Demaille <akim@epita.fr>
2001-12-29 14:17:18 +0000

    * src/reader.c (readgram): Bind the initial rule's lineno to that of the first rule. * tests/regression.at (Rule Line Numbers, Unresolved SR Conflicts): (Solved SR Conflicts): Adjust rule 0's line number.

--- src/reader.c
+++ src/reader.c
@@ -1451,10 +1451,12 @@ readgram (void)
 	t = lex ();
       }
 
-  /* Insert the initial rule:
+  /* Insert the initial rule, which line is that of the first rule
+     (not that of the start symbol):
 
      axiom: %start EOF.  */
   p = symbol_list_new (axiom);
+  p->line = grammar->line;
   p->next = symbol_list_new (startval);
   p->next->next = symbol_list_new (eoftoken);
   p->next->next->next = symbol_list_new (NULL);
