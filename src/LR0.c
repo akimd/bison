375d580671a0e20f43451f72247801048c0d25cb
Akim Demaille <akim@epita.fr>
2001-12-29 14:21:23 +0000

    Memory leak. * src/LR0.c (new_itemsets): Don't allocate `shift_symbol' here, since it allocates it for each state, although only one is needed. (allocate_storage): Do it here.

--- src/LR0.c
+++ src/LR0.c
@@ -113,6 +113,7 @@ allocate_storage (void)
   shiftset = XCALLOC (short, nsyms);
   redset = XCALLOC (short, nrules + 1);
   state_hash = XCALLOC (state_t *, STATE_HASH_SIZE);
+  shift_symbol = XCALLOC (short, nsyms);
 }
 
 
@@ -154,7 +155,6 @@ new_itemsets (void)
   for (i = 0; i < nsyms; i++)
     kernel_size[i] = 0;
 
-  shift_symbol = XCALLOC (short, nsyms);
   nshifts = 0;
 
   for (i = 0; i < nitemset; ++i)
