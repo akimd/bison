8419d367e1c296fa98b42a35820147fe5588b125
Akim Demaille <akim@epita.fr>
2001-12-29 14:24:35 +0000

    Memory leak. * src/reader.c (reader): Free grammar.

--- src/reader.c
+++ src/reader.c
@@ -1867,4 +1867,7 @@ reader (void)
 
   /* Convert the grammar into the format described in gram.h.  */
   packgram ();
+
+  /* The grammar as a symbol_list is no longer needed. */
+  LIST_FREE (symbol_list, grammar);
 }
