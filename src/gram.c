1cca533e2e296a6e54bd6d82c42736f434186df3
Akim Demaille <akim@epita.fr>
2001-12-29 14:19:46 +0000

    * src/gram.c (rprec, rprecsym, rassoc): Remove, unused since long ago.

--- src/gram.c
+++ src/gram.c
@@ -36,9 +36,6 @@ short *ritem = NULL;
 int nritems;
 
 rule_t *rule_table = NULL;
-short *rprec = NULL;
-short *rprecsym = NULL;
-short *rassoc = NULL;
 
 struct bucket **symbols = NULL;
 short *token_translations = NULL;
