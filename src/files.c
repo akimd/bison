b5b61c616b6a42a9f3a9cc0e9aeaf5124e7cffc5
Akim Demaille <akim@epita.fr>
2001-12-29 14:27:45 +0000

    %name-prefix is broken. * src/files.c (spec_name_prefix): Initialize to NULL, not to "yy". Adjust all dependencies. * tests/headers.at (export YYLTYPE): Strengthen this test: use %name-prefix. Renaming yylval but not yylloc is not consistent.  Now we do. * src/bison.simple: Prefix yylloc if used. * doc/bison.texinfo (Decl Summary): Document that.

--- src/files.c
+++ src/files.c
@@ -32,9 +32,16 @@ struct obstack action_obstack;
 struct obstack attrs_obstack;
 struct obstack output_obstack;
 
+/* Initializing some values below (such SPEC_NAME_PREFIX to `yy') is
+   tempting, but don't do that: for the time being our handling of the
+   %directive vs --option leaves precedence to the options by deciding
+   that if a %directive sets a variable which is really set (i.e., not
+   NULL), then the %directive is ignored.  As a result, %name-prefix,
+   for instance, will not be honored.  */
+
 char *spec_outfile = NULL;	/* for -o. */
 char *spec_file_prefix = NULL;	/* for -b. */
-const char *spec_name_prefix = "yy";	/* for -p. */
+const char *spec_name_prefix = NULL;	/* for -p. */
 char *spec_verbose_file = NULL;   /* for --verbose. */
 char *spec_graph_file = NULL;   /* for -g. */
 char *spec_defines_file = NULL; /* for --defines. */
--- src/output.c
+++ src/output.c
@@ -1075,7 +1075,8 @@ output_master_parser (void)
 
 #define MUSCLE_INSERT_PREFIX(Key, Value)	       			\
 {									\
-  obstack_fgrow2 (&muscle_obstack, "%s%s", spec_name_prefix, Value);	\
+  obstack_fgrow2 (&muscle_obstack, "%s%s", 				\
+		  spec_name_prefix ? spec_name_prefix : "yy", Value);	\
   obstack_1grow (&muscle_obstack, 0);					\
   muscle_insert (Key, obstack_finish (&muscle_obstack));	       	\
 }
@@ -1091,7 +1092,7 @@ prepare (void)
   MUSCLE_INSERT_INT ("final", final_state);
   MUSCLE_INSERT_INT ("maxtok", max_user_token_number);
   MUSCLE_INSERT_INT ("error-verbose", error_verbose);
-  MUSCLE_INSERT_STRING ("prefix", spec_name_prefix);
+  MUSCLE_INSERT_STRING ("prefix", spec_name_prefix ? spec_name_prefix : "yy");
 
   MUSCLE_INSERT_INT ("nnts", nvars);
   MUSCLE_INSERT_INT ("nrules", nrules);
@@ -1127,7 +1128,7 @@ yystype;\n\
 
   if (!pure_parser)
     fprintf (out, "\nextern YYSTYPE %slval;\n",
-	     spec_name_prefix);
+	     spec_name_prefix ? spec_name_prefix : "yy");
 
   if (locations_flag)
     {
@@ -1145,7 +1146,7 @@ typedef struct yyltype\n\
 #endif\n");
       if (!pure_parser)
 	fprintf (out, "\nextern YYLTYPE %slloc;\n",
-		 spec_name_prefix);
+		 spec_name_prefix ? spec_name_prefix : "yy");
     }
 
   if (semantic_parser)
