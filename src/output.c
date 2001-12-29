b9cecb91a08fa4baa73a5e12a83611b41507a022
Akim Demaille <akim@epita.fr>
2001-12-29 14:26:49 +0000

    * src/output.c (header_output): Don't forget to export YYLTYPE and yylloc. * tests/headers.at (export YYLTYPE): New, make sure it does. * tests/regression.at (%union and --defines, Invalid CPP headers): Move to... * tests/headers.at: here.

--- src/output.c
+++ src/output.c
@@ -1128,6 +1128,26 @@ yystype;\n\
   if (!pure_parser)
     fprintf (out, "\nextern YYSTYPE %slval;\n",
 	     spec_name_prefix);
+
+  if (locations_flag)
+    {
+      fputs ("\n\n", out);
+      fprintf (out, "\
+#ifndef YYLTYPE\n\
+typedef struct yyltype\n\
+{\n\
+  int first_line;\n\
+  int first_column;\n\
+  int last_line;\n\
+  int last_column;\n\
+} yyltype;\n\
+# define YYLTYPE yyltype\n\
+#endif\n");
+      if (!pure_parser)
+	fprintf (out, "\nextern YYLTYPE %slloc;\n",
+		 spec_name_prefix);
+    }
+
   if (semantic_parser)
     {
       int i;
