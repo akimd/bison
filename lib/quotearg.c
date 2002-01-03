fab5b110fb6f6ce81a10818537f491b0fa524ba5
Akim Demaille <akim@epita.fr>
2002-01-03 09:55:33 +0000

    * lib/quotearg.c: Use `#include "..."' instead of `#include <...>' for non system headers.

--- lib/quotearg.c
+++ lib/quotearg.c
@@ -25,8 +25,8 @@
 # include <stddef.h>  /* For the definition of size_t on windows w/MSVC.  */
 #endif
 #include <sys/types.h>
-#include <quotearg.h>
-#include <xalloc.h>
+#include "quotearg.h"
+#include "xalloc.h"
 
 #include <ctype.h>
 
