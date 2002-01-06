3460813bdbdd33b6b5b92bb16bd79cecad8a5e51
Akim Demaille <akim@epita.fr>
2002-01-06 20:49:53 +0000

    Bison is dead wrong in its RR conflict reports. * tests/torture.at (GNU Cim Grammar): New. * src/conflicts.c (count_rr_conflicts): Fix.

--- src/conflicts.c
+++ src/conflicts.c
@@ -265,7 +265,7 @@ count_rr_conflicts (state_t *state)
       int count = 0;
       int j;
       for (j = 0; j < state->nlookaheads; ++j)
-	if (BITISSET (LA (state->lookaheadsp), state->lookaheadsp + j))
+	if (BITISSET (LA (state->lookaheadsp + j), i))
 	  count++;
 
       if (count >= 2)
