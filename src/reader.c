3f2f7848cc416b7fd6b452187956b1b99672e6a3
Akim Demaille <akim@epita.fr>
2002-01-06 20:59:30 +0000

    * src/reader.c (parse_braces): Don't fetch the next char, the convention is to fetch on entry. * tests/torture.at (GNU Cim Grammar): Reintroduce their weird 'switch' without a following semicolon. * tests/regression.at (braces parsing): New.

--- src/reader.c
+++ src/reader.c
@@ -1119,10 +1119,7 @@ copy_action (symbol_list *rule, int stack_offset)
       /* above loop exits when c is '}' */
 
       if (--count)
-	{
-	  obstack_1grow (&action_obstack, c);
-	  c = getc (finput);
-	}
+	obstack_1grow (&action_obstack, c);
     }
 
   /* As a Bison extension, add the ending semicolon.  Since some Yacc
