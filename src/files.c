5e5d541531bda9507136d3c578315a2882d67412
Marc Autret <autret_m@epita.fr>
2002-01-20 15:46:19 +0000

    * tests/output.at: New test. * src/files.c (compute_base_names): Don't map extensions when the YACC flag is set, use defaults. Reported by Evgeny Stambulchik.

--- src/files.c
+++ src/files.c
@@ -403,7 +403,7 @@ compute_base_names (void)
 
       /* Computes the extensions from the grammar file name.  */
       filename_split (infile, &base, &tab, &ext);
-      if (ext)
+      if (ext && !yacc_flag)
 	compute_exts_from_gf (ext);
     }
 }
@@ -425,7 +425,7 @@ compute_output_file_names (void)
     src_extension = ".c";
   if (!header_extension)
     header_extension = ".h";
-
+  
   /* It the defines filename if not given, we create it.  */
   if (!spec_defines_file)
     spec_defines_file = stringappend (full_base_name, header_extension);
