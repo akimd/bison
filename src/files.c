fc6edc45c09f4fa32e18f8df7258c7f7be6a24e9
Marc Autret <autret_m@epita.fr>
2002-01-20 15:59:34 +0000

    * src/files.c (compute_output_file_names): Fix

--- src/files.c
+++ src/files.c
@@ -417,14 +417,14 @@ compute_output_file_names (void)
 {
   compute_base_names ();
 
-  parser_file_name =
-    spec_outfile ? spec_outfile : stringappend (full_base_name, src_extension);
-
   /* If not yet done. */
   if (!src_extension)
     src_extension = ".c";
   if (!header_extension)
     header_extension = ".h";
+  
+  parser_file_name =
+    spec_outfile ? spec_outfile : stringappend (full_base_name, src_extension);
   
   /* It the defines filename if not given, we create it.  */
   if (!spec_defines_file)
