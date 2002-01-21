bec30531f782a6f4fe1466fb62b61352d873cb58
Akim Demaille <akim@epita.fr>
2002-01-21 15:50:39 +0000

    * src/files.c (skeleton_find) [MSDOS]: Fix cp definition.

--- src/files.c
+++ src/files.c
@@ -192,6 +192,7 @@ skeleton_find (const char *envvar, const char *skeleton_name)
   const char *res = getenv (envvar);
 
 #if defined (MSDOS) || defined (_WIN32)
+  const char *cp = getenv ("INIT");
   if (!res)
     {
       /* Skeleton file name without path */
@@ -204,7 +205,6 @@ skeleton_find (const char *envvar, const char *skeleton_name)
         ++skel_name;
 
       /* File doesn't exist in current directory; try in INIT directory.  */
-      const char *cp = getenv ("INIT");
       if (cp)
 	{
 	  res = XMALLOC (char, strlen (cp) + strlen (skel_name) + 2);
