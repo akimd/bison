82841af7d05fd59204a55dd3c3669b7154a4d2b5
Akim Demaille <akim@epita.fr>
2002-01-27 13:57:17 +0000

    Fix `%nonassoc and eof'. * src/state.c (errs_dup): Aaaah!  The failure was due to bytes which were not properly copied!  Replace memcpy (res->errs, src->errs, src->nerrs); with memcpy (res->errs, src->errs, src->nerrs * sizeof (src->errs[0])); !!! * tests/regression.at (%nonassoc and eof): Adjust to newest Autotest: `.' is not in the PATH.

--- src/LR0.c
+++ src/LR0.c
@@ -364,9 +364,9 @@ save_reductions (void)
 }
 
 
-/*--------------------.
+/*---------------.
 | Build STATES.  |
-`--------------------*/
+`---------------*/
 
 static void
 set_states (void)
--- src/state.c
+++ src/state.c
@@ -61,7 +61,7 @@ errs *
 errs_dup (errs *src)
 {
   errs *res = errs_new (src->nerrs);
-  memcpy (res->errs, src->errs, src->nerrs);
+  memcpy (res->errs, src->errs, src->nerrs * sizeof (src->errs[0]));
   return res;
 }
 
