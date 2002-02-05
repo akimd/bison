5bb18f9a197ee1898fdefe85de99e76ebd357845
Akim Demaille <akim@epita.fr>
2002-02-05 09:57:52 +0000

    * src/bison.simple, src/bison.hairy, src/bison.c++: Move to... * data/: here.

--- src/output.c
+++ src/output.c
@@ -1,5 +1,5 @@
 /* Output the generated parsing program for bison,
-   Copyright 1984, 1986, 1989, 1992, 2000, 2001
+   Copyright (C) 1984, 1986, 1989, 1992, 2000, 2001, 2002
    Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
