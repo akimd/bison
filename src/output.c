4bda3f10cade960947bfd075a6a9d91e4ef448e6
Akim Demaille <akim@epita.fr>
2002-02-13 18:35:03 +0000

    * src/output.c (output_rule_data): Don't output NULL, it might not be defined yet.

--- src/output.c
+++ src/output.c
@@ -275,8 +275,9 @@ output_rule_data (void)
       obstack_sgrow (&format_obstack, ", ");
       j += strsize;
     }
-  /* add a NULL entry to list of tokens */
-  obstack_sgrow (&format_obstack, "NULL");
+  /* Add a NULL entry to list of tokens (well, 0, as NULL might not be
+     defined).  */
+  obstack_sgrow (&format_obstack, "0");
 
   /* Finish table and store. */
   obstack_1grow (&format_obstack, 0);
