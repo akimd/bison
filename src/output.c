abe017f60d93dfc22f28e79006f58df97a0761e1
Akim Demaille <akim@epita.fr>
2002-02-25 14:00:57 +0000

    * m4/m4.m4: New, from CVS Autoconf. * configure.in: Invoke it. * src/output.c (output_skeleton): Use its result instead of the hard coded name.

--- src/output.c
+++ src/output.c
@@ -1001,15 +1001,17 @@ output_skeleton (void)
   /* Invoke m4 on the definition of the muscles, and the skeleton. */
   {
     const char *bison_pkgdatadir = getenv ("BISON_PKGDATADIR");
+    const char *m4 = getenv ("M4");
+    if (!m4)
+      m4 = M4;
     if (!bison_pkgdatadir)
       bison_pkgdatadir = PKGDATADIR;
     if (trace_flag)
       fprintf (stderr,
-	       "running: m4 -I %s m4sugar/m4sugar.m4 %s %s\n",
-	       bison_pkgdatadir, tempfile, skeleton);
-    skel_in = readpipe ("m4",
-			"-I",
-			bison_pkgdatadir,
+	       "running: %s -I %s m4sugar/m4sugar.m4 %s %s\n",
+	       m4, bison_pkgdatadir, tempfile, skeleton);
+    skel_in = readpipe (m4,
+			"-I", bison_pkgdatadir,
 			"m4sugar/m4sugar.m4",
 			tempfile,
 			skeleton,
