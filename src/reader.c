bfcf1f3af044b8aa2193a4271a065085f7868777
Akim Demaille <akim@epita.fr>
2002-03-04 16:23:35 +0000

    * tests/input.at (Invalid $n, Invalid @n): Add the ending `;'. * tests/output.at (AT_CHECK_OUTPUT): Likewise. * tests/headers.at (AT_TEST_CPP_GUARD_H): Ditto. * tests/semantic.at (Parsing Guards): Similarly. * src/reader.at (readgram): Complain if the last rule is not ended with a semi-colon.

--- src/reader.c
+++ src/reader.c
@@ -1445,6 +1445,8 @@ readgram (void)
 	/* Warn if there is no default for $$ but we need one.  */
 	else if (!xactions && !first_rhs && lhs->type_name != 0)
 	  complain (_("empty rule for typed nonterminal, and no action"));
+	if (t == tok_two_percents || t == tok_eof)
+	  complain (_("previous rule lacks an ending `;'"));
 	if (t == tok_semicolon)
 	  t = lex ();
       }
