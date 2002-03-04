5d08b24fbaf0169e3641c944e53a29a48f8558c9
Akim Demaille <akim@epita.fr>
2002-03-04 17:00:45 +0000

    * src/reader.c (readgram): A missing `;' is a warning, not an error!

--- src/reader.c
+++ src/reader.c
@@ -1441,7 +1441,7 @@ readgram (void)
 		  symval = ssave;
 		  if (t1 == tok_colon)
 		    {
-		      complain (_("previous rule lacks an ending `;'"));
+		      warn (_("previous rule lacks an ending `;'"));
 		      break;
 		    }
 
@@ -1553,7 +1553,7 @@ readgram (void)
 	  else if (!xactions && !first_rhs && lhs->type_name != 0)
 	    complain (_("empty rule for typed nonterminal, and no action"));
 	  if (t == tok_two_percents || t == tok_eof)
-	    complain (_("previous rule lacks an ending `;'"));
+	    warn (_("previous rule lacks an ending `;'"));
 	  if (t == tok_semicolon)
 	    t = lex ();
 	}
