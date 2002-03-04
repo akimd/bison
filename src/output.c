d0039cbcf8c932634407b40dca7747c537200e27
Akim Demaille <akim@epita.fr>
2002-03-04 14:17:30 +0000

    * src/output.c (output_skeleton): tempdir is const. bytes_read is unused.

--- src/output.c
+++ src/output.c
@@ -958,10 +958,9 @@ static void
 output_skeleton (void)
 {
   /* Store the definition of all the muscles. */
-  char *tempdir = getenv ("TMPDIR");
+  const char *tempdir = getenv ("TMPDIR");
   char *tempfile = NULL;
   FILE *out = NULL;
-  ssize_t bytes_read;
   int fd;
 
   if (tempdir == NULL)
