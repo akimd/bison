e5352bc7bfd45be91568d0c7bcf6417d127f7a83
Akim Demaille <akim@epita.fr>
2002-03-04 13:58:20 +0000

    * tests/sets.at (Broken Closure): Add the ending `;'. * src/reader.at (readgram): Complain if a rule is not ended with a semi-colon.

--- src/reader.c
+++ src/reader.c
@@ -1326,7 +1326,10 @@ readgram (void)
 		unlex (t1);
 		symval = ssave;
 		if (t1 == tok_colon)
-		  break;
+		  {
+		    complain (_("previous rule lacks an ending `;'"));
+		    break;
+		  }
 
 		if (!first_rhs)	/* JF */
 		  first_rhs = symval;
