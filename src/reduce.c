11652ab3dc44bf12d5fa9095d1495b5cd3102ff7
Akim Demaille <akim@epita.fr>
2002-04-07 15:30:42 +0000

    * src/reduce.c (inaccessable_symbols): Fix a buglet: because of a lacking `+ 1' to nrules, Bison reported as useless a token if it was used solely to set the precedence of the last rule...

--- src/reduce.c
+++ src/reduce.c
@@ -215,7 +215,7 @@ inaccessable_symbols (void)
   nuseless_nonterminals = nvars - nuseful_nonterminals;
 
   /* A token that was used in %prec should not be warned about.  */
-  for (i = 1; i < nrules; i++)
+  for (i = 1; i < nrules + 1; i++)
     if (rules[i].precsym != 0)
       bitset_set (V1, rules[i].precsym);
 }
