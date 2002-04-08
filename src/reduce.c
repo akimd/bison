bb88b0fc9e1f8ff9f2f5e672078a81d13288e0d5
Akim Demaille <akim@epita.fr>
2002-04-08 11:45:29 +0000

    * src/reduce.c (reduce_grammar): First reduce the nonterminals, and then the grammar.

--- src/reduce.c
+++ src/reduce.c
@@ -487,10 +487,13 @@ reduce_grammar (void)
     fatal (_("Start symbol %s does not derive any sentence"),
 	   quotearg_style (escape_quoting_style, symbols[start_symbol]->tag));
 
-  if (nuseless_productions > 0)
-    reduce_grammar_tables ();
+  /* First reduce the nonterminals, as they renumber themselves in the
+     whole grammar.  If you change the order, nonterms would be
+     renumbered only in the reduced grammar.  */
   if (nuseless_nonterminals > 0)
     nonterminals_reduce ();
+  if (nuseless_productions > 0)
+    reduce_grammar_tables ();
 
   if (trace_flag)
     {
