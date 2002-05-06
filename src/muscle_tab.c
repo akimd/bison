cfaee611d8383e1f0f5180a248abd52b513aa26f
Akim Demaille <akim@epita.fr>
2002-05-06 08:23:54 +0000

    * src/muscle_tab.c (muscle_m4_output): Must return TRUE for hash_do_for_each.

--- src/muscle_tab.c
+++ src/muscle_tab.c
@@ -97,11 +97,12 @@ muscle_find (const char *key)
 /* Output the definition of all the current muscles into a list of
    m4_defines.  */
 
-static void
+static int
 muscle_m4_output (muscle_entry_t *entry, FILE *out)
 {
   fprintf (out, "m4_define([b4_%s],\n", entry->key);
   fprintf (out, "          [[%s]])\n\n\n", entry->value);
+  return 1;
 }
 
 
