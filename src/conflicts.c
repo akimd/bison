4a713ec2e3f236999a20ab9c04004624ec63a557
Paul Hilfinger <Hilfinger@CS.Berkeley.EDU>
2002-05-27 20:55:43 +0000

    * src/conflicts.c (log_resolution): Correct typo: obstack_printf should be obstack_fgrow1.

--- src/conflicts.c
+++ src/conflicts.c
@@ -104,7 +104,7 @@ log_resolution (int lookahead, int token,
 	  break;
 
 	case left_resolution:
-	  obstack_printf (&solved_conflicts_obstack,
+	  obstack_fgrow1 (&solved_conflicts_obstack,
 			  " (%%left %s)",
 			  symbols[token]->tag);
 	  break;
