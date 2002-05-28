670ddffd5ba88d6d5baf6aa51803592687ae48b0
Akim Demaille <akim@epita.fr>
2002-05-28 10:02:36 +0000

    * src/output.c (output_skeleton): Be sure to allocate enough room for `/' _and_ for `0' in full_skeleton.

--- src/output.c
+++ src/output.c
@@ -1002,7 +1002,7 @@ output_skeleton (void)
     if (!bison_pkgdatadir)
       bison_pkgdatadir = PKGDATADIR;
     pkg_data_len = strlen (bison_pkgdatadir);
-    full_skeleton = XMALLOC (char, pkg_data_len + strlen (skeleton) + 1);
+    full_skeleton = XMALLOC (char, pkg_data_len + strlen (skeleton) + 2);
     if (bison_pkgdatadir[pkg_data_len-1] == '/')
       sprintf (full_skeleton, "%s%s", bison_pkgdatadir, skeleton);
     else
