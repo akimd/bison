4515534cd2024f8930bf8fa09745f3fdb46f0009
Akim Demaille <akim@epita.fr>
2002-06-14 17:37:12 +0000

    * src/main.c (main): Invoke scanner_free.

--- src/main.c
+++ src/main.c
@@ -110,6 +110,10 @@ main (int argc, char *argv[])
   free_nullable ();
   free_derives ();
   grammar_free ();
+
+  /* The scanner memory cannot be released right after parsing, as it
+     contains things such as user actions, prologue, epilogue etc.  */
+  scanner_free ();
   muscle_free ();
   /* If using alloca.c, flush the alloca'ed memory for the benefit of
      people running Bison as a library in IDEs.  */
