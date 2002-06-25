9be0c25bf054b026304e6ec480c3893817625607
Akim Demaille <akim@epita.fr>
2002-06-25 12:05:38 +0000

    * src/getargs.c (report_argmatch): Initialize strtok().

--- src/getargs.c
+++ src/getargs.c
@@ -75,6 +75,7 @@ static void
 report_argmatch (char *args)
 {
   ARGMATCH_ASSERT (report_args, report_types);
+  args = strtok (args, ",");
   do
     {
       int report = XARGMATCH ("--report", args,
