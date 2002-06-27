60491a948b9339a6f68fafb63b51e9899d4e6aad
Akim Demaille <akim@epita.fr>
2002-06-27 12:08:20 +0000

    * data/bison.simple, data/bison.c++: Rename as... * data/yacc.c, data/lalr1.cc: these. * doc/bison.texinfo (Environment Variables): Remove.

--- src/output.c
+++ src/output.c
@@ -1140,7 +1140,7 @@ prepare (void)
 
   /* Find the right skeleton file.  */
   if (!skeleton)
-    skeleton = "bison.simple";
+    skeleton = "yacc.c";
 
   /* Parse the skeleton file and output the needed parsers.  */
   muscle_insert ("skeleton", skeleton);
