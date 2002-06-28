720623af846838b895298fed24f6d5d4cb666742
Paul Hilfinger <Hilfinger@CS.Berkeley.EDU>
2002-06-28 22:42:58 +0000

    * data/glr.c: Correct typo in Emacs-mode directive.  Slightly   reorganize first lines parallel to yacc.c.

--- data/glr.c
+++ data/glr.c
@@ -1,4 +1,5 @@
-m4_divert(-1)                                                       -*- YYC -*-
+m4_divert(-1)                                                       -*- C -*-
+m4_include([c.m4])
 
 # GLR skeleton for Bison
 # Copyright (C) 2002 Free Software Foundation, Inc.
@@ -17,10 +18,6 @@ m4_divert(-1)                                                       -*- YYC -*-
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 # 02111-1307  USA
-
-m4_include([c.m4])
-
-
 
 # b4_lhs_value([TYPE])
 # --------------------
