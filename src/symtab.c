260008e546ee1ef9b2c9946f3e97726c015222ef
Akim Demaille <akim@epita.fr>
2002-06-30 17:30:58 +0000

    * src/symtab.c (symbol_new): Initialize the `printer' member.

--- src/symtab.c
+++ src/symtab.c
@@ -51,11 +51,13 @@ symbol_new (const char *tag, location_t location)
 
   res->type_name = NULL;
   res->destructor = NULL;
+  res->printer = NULL;
 
   res->number = NUMBER_UNDEFINED;
   res->prec = 0;
   res->assoc = right_assoc;
   res->user_token_number = USER_NUMBER_UNDEFINED;
+
   res->alias = NULL;
   res->class = unknown_sym;
 
