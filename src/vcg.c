e5fb6710321bf3b412e34028ba0b934ab123970b
Akim Demaille <akim@epita.fr>
2002-06-30 17:30:09 +0000

    * src/vcg.c (quote): Use slot 2, since we often pass symbol tag escaped in slot 0.

--- src/vcg.c
+++ src/vcg.c
@@ -1,5 +1,5 @@
 /* VCG description handler for Bison.
-   Copyright 2001 Free Software Foundation, Inc.
+   Copyright 2001, 2002 Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
 
@@ -23,12 +23,14 @@
 #include "vcg_defaults.h"
 #include "quotearg.h"
 
-/* Return an unambiguous printable representated, allocated in slot 0,
-   for NAME, suitable for C strings.  */
+/* Return an unambiguous printable representated, for NAME, suitable
+   for C strings.  Use slot 2 since the user may use slots 0 and 1.
+   */
+
 static char const *
 quote (char const *name)
 {
-  return quotearg_n_style (0, c_quoting_style, name);
+  return quotearg_n_style (2, c_quoting_style, name);
 }
 
 
