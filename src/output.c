3650b4b8dddf39f6d6f5b2514d1f036548c5fe2e
Akim Demaille <akim@epita.fr>
2002-07-26 06:24:11 +0000

    * src/output.c (prepare_tokens): Go up to ntokens, not ntokens + 1. * doc/bison.texinfo (Decl Summary): s/$illegal/$undefined./. * tests/regression.at (Web2c Actions): Adjust.

--- src/output.c
+++ src/output.c
@@ -344,11 +344,11 @@ prepare_tokens (void)
   /* Output YYTOKNUM. */
   {
     int i;
-    int *values = XCALLOC (int, ntokens + 1);
-    for (i = 0; i < ntokens + 1; ++i)
+    int *values = XCALLOC (int, ntokens);
+    for (i = 0; i < ntokens; ++i)
       values[i] = symbols[i]->user_token_number;
     muscle_insert_int_table ("toknum", values,
-			     0, 1, ntokens + 1);
+			     values[0], 1, ntokens);
     free (values);
   }
 }
