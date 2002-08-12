d7163c0a58fad051d90a6f83107c646c4408971c
Paul Eggert <eggert@cs.ucla.edu>
2002-08-12 14:53:26 +0000

    (symbol_get): Don't cast LHS of an assignment; this is a GCC extension and is not portable to other compilers.

--- src/symtab.c
+++ src/symtab.c
@@ -421,7 +421,7 @@ symbol_get (const char *key, location_t location)
 
   /* Keep the symbol in a printable form.  */
   key = quotearg_style (escape_quoting_style, key);
-  (const char *) probe.tag = key;
+  *(char const **) &probe.tag = key;
   entry = hash_lookup (symbol_table, &probe);
 
   if (!entry)
