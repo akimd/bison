91e1aa523d1d08d903f42055c3021e2006b9d20a
Paul Eggert <eggert@cs.ucla.edu>
2002-08-12 14:34:51 +0000

    (assoc_to_string): Wrap strings in _() if they need translation.

--- src/assoc.c
+++ src/assoc.c
@@ -28,7 +28,7 @@ assoc_to_string (assoc_t assoc)
   switch (assoc)
     {
     case undef_assoc:
-      return "undefined associativity";
+      return _("undefined associativity");
 
     case right_assoc:
       return "%right";
