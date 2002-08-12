c81bad8950aa100e78e9cadb569e1c1a39a83fe3
Paul Eggert <eggert@cs.ucla.edu>
2002-08-12 14:41:48 +0000

    Cast result of bindtextdomain and textdomain to void, to avoid a GCC warning when --disable-nls is in effect.

--- src/main.c
+++ src/main.c
@@ -51,8 +51,8 @@ main (int argc, char *argv[])
 {
   program_name = argv[0];
   setlocale (LC_ALL, "");
-  bindtextdomain (PACKAGE, LOCALEDIR);
-  textdomain (PACKAGE);
+  (void) bindtextdomain (PACKAGE, LOCALEDIR);
+  (void) textdomain (PACKAGE);
 
   getargs (argc, argv);
 
