c0ad8bf3c2fb46f001bb24ac98a72b8bfb3a77ce
Akim Demaille <akim@epita.fr>
2002-09-04 12:03:01 +0000

    * data/yacc.c: Guard the declaration of yytoknum also with `#ifdef YYPRINT', so it is declared only when used.

--- data/yacc.c
+++ data/yacc.c
@@ -358,12 +358,14 @@ static const char *const yytname[[]] =
 };
 #endif
 
+# ifdef YYPRINT
 /* YYTOKNUM[[YYLEX-NUM]] -- Internal token number corresponding to
    token YYLEX-NUM.  */
 static const b4_int_type_for([b4_toknum]) yytoknum[[]] =
 {
   b4_toknum
 };
+# endif
 
 /* YYR1[[YYN]] -- Symbol number of symbol that rule YYN derives.  */
 static const b4_int_type_for([b4_r1]) yyr1[[]] =
