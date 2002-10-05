242a6e484ee19060ef7b59fe1b14ab67cd153341
Paul Eggert <eggert@cs.ucla.edu>
2002-10-05 05:47:51 +0000

    (reader): SHRT_MAX -> SYMBOL_NUMBER_MAX.

--- src/reader.c
+++ src/reader.c
@@ -541,9 +541,9 @@ reader (void)
     grammar = p;
   }
 
-  if (nsyms > SHRT_MAX)
+  if (SYMBOL_NUMBER_MAX < nsyms)
     fatal (_("too many symbols (tokens plus nonterminals); maximum %d"),
-          SHRT_MAX);
+	   SYMBOL_NUMBER_MAX);
 
   assert (nsyms == ntokens + nvars);
 
