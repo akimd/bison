09147be095972d16f5c1d89dfe99bdb401b2685c
Paul Eggert <eggert@cs.ucla.edu>
2002-10-07 05:41:35 +0000

    (debug_lbitset): Do not assume that bitset_word is the same width as int.

--- lib/lbitset.c
+++ lib/lbitset.c
@@ -1439,7 +1439,7 @@ debug_lbitset (bset)
 	  
 	  fprintf (stderr, "  Word %u:", i);
 	  for (j = 0; j < LBITSET_WORD_BITS; j++)
-	    if ((word & (1 << j)))
+	    if ((word & ((bitset_word) 1 << j)))
 	      fprintf (stderr, " %u", j);
 	  fprintf (stderr, "\n");
 	}
