303e6da2654d7a8e578829d709f938297f5bea4c
Paul Eggert <eggert@cs.ucla.edu>
2002-10-10 07:35:19 +0000

    Include bitset.h, not bbitset.h.

--- lib/abitset.h
+++ lib/abitset.h
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #ifndef _ABITSET_H
 #define _ABITSET_H 
 
-#include "bbitset.h"
+#include "bitset.h"
 
 extern size_t abitset_bytes PARAMS ((bitset_bindex));
 
