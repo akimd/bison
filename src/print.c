7ea9a33f3b12c4fc2c5907dd22a74c1441d2855d
Akim Demaille <akim@epita.fr>
2002-10-13 19:35:59 +0000

    * src/print.c (print_state): Separate the list of solved conflicts from the other items. * tests/conflicts.at (Resolved SR Conflicts): Adjust.

--- src/print.c
+++ src/print.c
@@ -422,7 +422,10 @@ print_state (FILE *out, state_t *state)
   print_actions (out, state);
   if ((report_flag & report_solved_conflicts)
       && state->solved_conflicts)
-    fputs (state->solved_conflicts, out);
+    {
+      fputc ('\n', out);
+      fputs (state->solved_conflicts, out);
+    }
 }
 
 /*-----------------------------------------.
