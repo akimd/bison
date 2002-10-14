05846dae07d2113181fb791fc9c5f9382edc35ce
Akim Demaille <akim@epita.fr>
2002-10-14 11:38:21 +0000

    * src/tables.c (table_ninf_remap): base -> tab. Reported by Matt Rosing.

--- src/tables.c
+++ src/tables.c
@@ -774,7 +774,7 @@ table_ninf_remap (base_t tab[], size_t size, base_t ninf)
 
   for (i = 0; i < size; i++)
     if (tab[i] < res && tab[i] != ninf)
-      res = base[i];
+      res = tab[i];
 
   --res;
 
