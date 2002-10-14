768eb43fe816c1617e13833df3b6dfd22f632fc4
Paul Eggert <eggert@cs.ucla.edu>
2002-10-14 08:06:06 +0000

    (yysigned_char): New type.

--- data/glr.c
+++ data/glr.c
@@ -198,6 +198,12 @@ static YYLTYPE yyloc_default;
 #  define yyfalse 0
 #endif
 
+#if defined (__STDC__) || defined (__cplusplus)
+   typedef signed char yysigned_char;
+#else
+   typedef short yysigned_char;
+#endif
+
 /*-----------------.
 | GCC extensions.  |
 `-----------------*/
--- data/yacc.c
+++ data/yacc.c
@@ -307,6 +307,12 @@ b4_location_if(
 
 #endif
 
+#if defined (__STDC__) || defined (__cplusplus)
+   typedef signed char yysigned_char;
+#else
+   typedef short yysigned_char;
+#endif
+
 /* YYFINAL -- State number of the termination state. */
 #define YYFINAL  b4_final_state_number
 #define YYLAST   b4_last
