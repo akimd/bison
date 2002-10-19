c19988b766d542b4d4c83b1b27579ee83cb5f930
Akim Demaille <akim@epita.fr>
2002-10-19 14:39:07 +0000

    * tests/calc.at (_AT_DATA_CALC_Y): Define VAL, LOC, LEX_FORMALS, LEX_PRE_FORMALS, LEX_ARGS, LEX_PRE_ARGS, USE_LEX_ARGS. Use them to have `calc.y' ready for %pure-parser. * data/yacc.c (YYLEX): Pass a yylex return type to b4_c_function_call.

--- data/yacc.c
+++ data/yacc.c
@@ -509,7 +509,7 @@ while (0)
 #ifdef YYLEX_PARAM
 # define YYLEX yylex (b4_pure_if([&yylval[]b4_location_if([, &yylloc]), ])YYLEX_PARAM)
 #else
-# define YYLEX b4_c_function_call([yylex],
+# define YYLEX b4_c_function_call([yylex], [int],
                    b4_pure_if([[[[]], [[&yylval]]],
                                b4_location_if([[[], [&yylloc]],])])
                    m4_fst(b4_lex_param))
