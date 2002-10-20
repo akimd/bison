9e2c5ef112d118b7714d91b60f2b13cb1a51a7cf
Paul Eggert <eggert@cs.ucla.edu>
2002-10-20 05:34:55 +0000

    (TICKS_TO_MSEC, CLOCKS_TO_MSEC): Do not cast to float, as this generates a warning on Solaris 8 + GCC 3.2 with --enable-gcc-warnings.  Instead, divide into 1.0 rather than 1; this generates a more-accurate value anyway. (timervar_accumulate): Rename local variables to avoid confusion with similarly-named global 'start_time'.

--- lib/timevar.c
+++ lib/timevar.c
@@ -118,12 +118,12 @@ extern clock_t clock PARAMS ((void));
    _constants_ variable at run time, anyway?  */
 #ifdef USE_TIMES
 static float ticks_to_msec;
-#define TICKS_TO_MSEC (1 / (float)TICKS_PER_SECOND)
+#define TICKS_TO_MSEC (1.0 / TICKS_PER_SECOND)
 #endif
 
 #ifdef USE_CLOCK
 static float clocks_to_msec;
-#define CLOCKS_TO_MSEC (1 / (float)CLOCKS_PER_SEC)
+#define CLOCKS_TO_MSEC (1.0 / CLOCKS_PER_SEC)
 #endif
 
 #if IN_GCC
@@ -236,17 +236,17 @@ get_time (now)
   }
 }
 
-/* Add the difference between STOP_TIME and START_TIME to TIMER.  */
+/* Add the difference between STOP and START to TIMER.  */
 
 static void
-timevar_accumulate (timer, start_time, stop_time)
+timevar_accumulate (timer, start, stop)
      struct timevar_time_def *timer;
-     struct timevar_time_def *start_time;
-     struct timevar_time_def *stop_time;
+     struct timevar_time_def *start;
+     struct timevar_time_def *stop;
 {
-  timer->user += stop_time->user - start_time->user;
-  timer->sys += stop_time->sys - start_time->sys;
-  timer->wall += stop_time->wall - start_time->wall;
+  timer->user += stop->user - start->user;
+  timer->sys += stop->sys - start->sys;
+  timer->wall += stop->wall - start->wall;
 }
 
 /* Initialize timing variables.  */
