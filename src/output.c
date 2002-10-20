bb33f19a4b26ac602083587bfe9aa632edb75275
Paul Eggert <eggert@cs.ucla.edu>
2002-10-20 06:01:07 +0000

    (prepare): Use xstrdup to convert char const * to char *, to avoid GCC warning.

--- src/output.c
+++ src/output.c
@@ -306,9 +306,9 @@ prepare_states (void)
 
 
 
-/*----------------------------------.
-| Output the user actions to OOUT.  |
-`----------------------------------*/
+/*---------------------------------.
+| Output the user actions to OUT.  |
+`---------------------------------*/
 
 static void
 user_actions_output (FILE *out)
@@ -620,7 +620,7 @@ prepare (void)
     }
 
   /* Parse the skeleton file and output the needed parsers.  */
-  muscle_insert ("skeleton", skeleton);
+  muscle_insert ("skeleton", xstrdup (skeleton));
 }
 
 
