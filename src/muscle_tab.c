d9d363b1728c1ee338ee686ca2a0e1b1175da6f3
Paul Eggert <eggert@cs.ucla.edu>
2002-10-20 05:47:09 +0000

    (muscle_pair_list_grow): Rename local to avoid confusion with similarly-named more-global.

--- src/muscle_tab.c
+++ src/muscle_tab.c
@@ -173,12 +173,12 @@ muscle_grow (const char *key, const char *val, const char *separator)
 void muscle_pair_list_grow (const char *muscle,
 			    const char *a1, const char *a2)
 {
-  char *value;
+  char *val;
   obstack_fgrow2 (&muscle_obstack, "[[[%s]], [[%s]]]", a1, a2);
   obstack_1grow (&muscle_obstack, 0);
-  value = obstack_finish (&muscle_obstack);
-  muscle_grow (muscle, value, ",\n");
-  obstack_free (&muscle_obstack, value);
+  val = obstack_finish (&muscle_obstack);
+  muscle_grow (muscle, val, ",\n");
+  obstack_free (&muscle_obstack, val);
 }
 
 /*-------------------------------.
