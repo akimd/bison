b4afb6bbe51c7355a2e3efb19c16f72cc36a23d3
Paul Eggert <eggert@cs.ucla.edu>
2002-10-22 04:43:50 +0000

    Use C99-style bool instead of our own style.
    (reader): Use EXIT_FAILURE and EXIT_SUCCESS rather than 1 and 0.

--- src/reader.c
+++ src/reader.c
@@ -422,7 +422,7 @@ packgram (void)
       rules[ruleno].lhs = p->sym;
       rules[ruleno].rhs = ritem + itemno;
       rules[ruleno].location = p->location;
-      rules[ruleno].useful = TRUE;
+      rules[ruleno].useful = true;
       rules[ruleno].action = p->action;
       rules[ruleno].action_location = p->action_location;
       rules[ruleno].dprec = p->dprec;
@@ -507,8 +507,8 @@ reader (void)
 
   /* If something went wrong during the parsing, don't try to
      continue.  */
-  if (complain_message_count)
-    exit (1);
+  if (complaint_issued)
+    exit (EXIT_FAILURE);
 
   /* Grammar has been read.  Do some checking */
   if (nrules == 0)
