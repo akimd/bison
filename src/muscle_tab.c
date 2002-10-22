5dd5fd4a56ee569247f92d50def20d6476bb4691
Paul Eggert <eggert@cs.ucla.edu>
2002-10-22 04:39:05 +0000

    (hash_compare_muscles): (a ? FALSE : TRUE) -> (! a)

--- src/muscle_tab.c
+++ src/muscle_tab.c
@@ -38,7 +38,7 @@ hash_compare_muscles (void const *x, void const *y)
 {
   const muscle_entry_t *m1 = x;
   const muscle_entry_t *m2 = y;
-  return strcmp (m1->key, m2->key) ? FALSE : TRUE;
+  return strcmp (m1->key, m2->key) == 0;
 }
 
 static unsigned int
