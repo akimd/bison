901ec1537a0ceb980398a5395751ea0e445c4ce5
Paul Eggert <eggert@cs.ucla.edu>
2002-10-22 05:01:46 +0000

    (xfopen, xfclose): Use EXIT_FAILURE rathern than 2. What was that "2" for?

--- src/files.c
+++ src/files.c
@@ -111,7 +111,7 @@ xfopen (const char *name, const char *mode)
 
   ptr = fopen (name, mode);
   if (!ptr)
-    error (2, errno, _("cannot open file `%s'"), name);
+    error (EXIT_FAILURE, errno, _("cannot open file `%s'"), name);
 
   return ptr;
 }
@@ -130,7 +130,7 @@ xfclose (FILE *ptr)
 
   result = fclose (ptr);
   if (result == EOF)
-    error (2, errno, _("cannot close file"));
+    error (EXIT_FAILURE, errno, _("cannot close file"));
 
   return result;
 }
