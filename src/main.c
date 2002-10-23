f6d0c2394c64bdd89ccd86a2d41ca70d02777886
Paul Eggert <eggert@cs.ucla.edu>
2002-10-23 18:41:43 +0000

    (main): With --trace=time, report times even if a non-fatal error occurs.  Formerly, the times were reported in some such cases but not in others.

--- src/main.c
+++ src/main.c
@@ -74,7 +74,7 @@ main (int argc, char *argv[])
   timevar_pop (TV_READER);
 
   if (complaint_issued)
-    return EXIT_FAILURE;
+    goto finish;
 
   /* Find useless nonterminals and productions and reduce the grammar. */
   timevar_push (TV_REDUCE);
@@ -138,7 +138,7 @@ main (int argc, char *argv[])
   /* Stop if there were errors, to avoid trashing previous output
      files.  */
   if (complaint_issued)
-    return EXIT_FAILURE;
+    goto finish;
 
   /* Lookaheads are no longer needed. */
   timevar_push (TV_FREE);
@@ -172,6 +172,8 @@ main (int argc, char *argv[])
 
   if (trace_flag & trace_bitsets)
     bitset_stats_dump (stderr);
+
+ finish:
 
   /* Stop timing and print the times.  */
   timevar_stop (TV_TOTAL);
