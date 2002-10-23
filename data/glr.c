ffa4c58acb6909d00cc1093ff836c434fec06f1b
Paul Eggert <eggert@cs.ucla.edu>
2002-10-23 06:30:49 +0000

    (struct yyltype): Define members even when not doing locations.  This is more consistent with yacc.c, and it works around the following bug reports: http://mail.gnu.org/pipermail/bug-bison/2002-October/001748.html http://mail.gnu.org/pipermail/bug-bison/2002-October/001764.html http://mail.gnu.org/pipermail/bug-bison/2002-October/001769.html

--- data/glr.c
+++ data/glr.c
@@ -178,11 +178,10 @@ typedef union b4_stype yystype;
 #ifndef YYLTYPE
 typedef struct yyltype
 {
-]b4_location_if([
   int first_line;
   int first_column;
   int last_line;
-  int last_column;])[
+  int last_column;
 } yyltype;
 # define YYLTYPE ]b4_location_type[
 # define YYLTYPE_IS_TRIVIAL 1
