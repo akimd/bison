f956c304c521049006067456acb28bb3518ac07f
Paul Eggert <eggert@cs.ucla.edu>
2002-10-23 18:41:04 +0000

    (reader): Just return if a complaint has been issued, instead of exiting, so that 'main' can report times.

--- src/reader.c
+++ src/reader.c
@@ -508,7 +508,7 @@ reader (void)
   /* If something went wrong during the parsing, don't try to
      continue.  */
   if (complaint_issued)
-    exit (EXIT_FAILURE);
+    return;
 
   /* Grammar has been read.  Do some checking */
   if (nrules == 0)
