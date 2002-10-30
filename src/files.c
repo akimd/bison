e63ee1f16c6b8633ed7385d1e7f2531b5dfda8f5
Paul Eggert <eggert@cs.ucla.edu>
2002-10-30 06:19:00 +0000

    (xfclose): Return void, not int, since it always returned zero. Report I/O error if ferror indicates one.

--- src/files.c
+++ src/files.c
@@ -120,19 +120,17 @@ xfopen (const char *name, const char *mode)
 | Try to close file PTR, and print an error message if fails.  |
 `-------------------------------------------------------------*/
 
-int
+void
 xfclose (FILE *ptr)
 {
-  int result;
-
   if (ptr == NULL)
-    return 0;
+    return;
 
-  result = fclose (ptr);
-  if (result == EOF)
+  if (ferror (ptr))
+    error (EXIT_FAILURE, 0, _("I/O error"));
+
+  if (fclose (ptr) != 0)
     error (EXIT_FAILURE, errno, _("cannot close file"));
-
-  return result;
 }
 
 
