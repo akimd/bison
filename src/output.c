01cfa697919c2a7563c61d7a07863445317efa13
Paul Eggert <eggert@cs.ucla.edu>
2002-11-03 06:41:59 +0000

    (prepare_symbols): When printing token names, escape "[" as "@<:@" and likewise for "]".

--- src/output.c
+++ src/output.c
@@ -136,7 +136,7 @@ prepare_symbols (void)
 	const char *cp =
 	  quotearg_n_style (1, c_quoting_style,
 			    symbols[i]->tag);
-	/* Width of the next token, including the two quotes, the coma
+	/* Width of the next token, including the two quotes, the comma
 	   and the space.  */
 	int strsize = strlen (cp) + 2;
 
@@ -146,7 +146,14 @@ prepare_symbols (void)
 	    j = 2;
 	  }
 
-	obstack_sgrow (&format_obstack, cp);
+	for (; *cp; cp++)
+	  switch (*cp)
+	    {
+	    case '[': obstack_sgrow (&format_obstack, "@<:@"); break;
+	    case ']': obstack_sgrow (&format_obstack, "@:>@"); break;
+	    default: obstack_1grow (&format_obstack, *cp); break;
+	    }
+
 	obstack_sgrow (&format_obstack, ", ");
 	j += strsize;
       }
