9e4c5cb833494f69288aedd8059d03bfb30e5020
Paul Eggert <eggert@cs.ucla.edu>
2002-11-03 07:03:53 +0000

    (quotearg_buffer_restyled): Fix off-by-two bug in trigraph handling.

--- lib/quotearg.c
+++ lib/quotearg.c
@@ -330,8 +330,8 @@ quotearg_buffer_restyled (char *buffer, size_t buffersize,
 		  case '<': case '=': case '>':
 		    /* Escape the second '?' in what would otherwise be
 		       a trigraph.  */
-		    i += 2;
 		    c = arg[i + 2];
+		    i += 2;
 		    STORE ('?');
 		    STORE ('\\');
 		    STORE ('?');
