6e40b4ebd43da977e1399f3b4678f8346d8e3f2c
Akim Demaille <akim@epita.fr>
2002-11-06 16:22:04 +0000

    #line should have quoted strings. Ideally, this should be done by m4_quotearg. * src/scan-skel.l: Include quotearg.h. Quote __ofile__. * src/output.c (symbol_printers_output) (symbol_destructors_output): Quote the file name.

--- src/output.c
+++ src/output.c
@@ -380,7 +380,8 @@ symbol_destructors_output (FILE *out)
 	   destructor, typename. */
 	fprintf (out, "%s[[[%s]], [[%d]], [[%s]], [[%d]], [[%s]], [[%s]]]",
 		 first ? "" : ",\n",
-		 symbol->destructor_location.file,
+		 quotearg_style (escape_quoting_style,
+				 symbol->destructor_location.file),
 		 symbol->destructor_location.first_line,
 		 symbol->tag,
 		 symbol->number,
@@ -414,7 +415,8 @@ symbol_printers_output (FILE *out)
 	   printer, typename. */
 	fprintf (out, "%s[[[%s]], [[%d]], [[%s]], [[%d]], [[%s]], [[%s]]]",
 		 first ? "" : ",\n",
-		 symbol->printer_location.file,
+		 quotearg_style (escape_quoting_style,
+				 symbol->printer_location.file),
 		 symbol->printer_location.first_line,
 		 symbol->tag,
 		 symbol->number,
