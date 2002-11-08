09d353a08f1fe9ec1a219a077e7dc82cafa4685e
Paul Eggert <eggert@cs.ucla.edu>
2002-11-08 06:14:29 +0000

    (scanner_last_string_free): Remove decl.

--- src/reader.h
+++ src/reader.h
@@ -40,7 +40,6 @@ typedef struct gram_control_s
 /* From the scanner.  */
 extern FILE *gram_in;
 extern int gram__flex_debug;
-void scanner_last_string_free (void);
 void scanner_initialize (void);
 void scanner_free (void);
 
