6c239755dc5b62e0bd71682a62a33d013ace5553
Paul Eggert <eggert@cs.ucla.edu>
2002-11-12 07:26:27 +0000

    (prologue_augment, epilogue_set): Quote filenames for C and M4.

--- src/reader.c
+++ src/reader.c
@@ -69,9 +69,10 @@ prologue_augment (const char *prologue, location_t location)
   struct obstack *oout =
     !typed ? &pre_prologue_obstack : &post_prologue_obstack;
 
-  obstack_fgrow2 (oout, "]b4_syncline([[%d]], [[%s]])[\n",
-	   location.first_line,
-	   quotearg_style (escape_quoting_style, location.file));
+  obstack_fgrow1 (oout, "]b4_syncline([[%d]], [[",
+		  location.first_line);
+  MUSCLE_OBSTACK_SGROW (oout, quotearg_style (c_quoting_style, location.file));
+  obstack_sgrow (oout, "]])[\n");
   obstack_sgrow (oout, prologue);
 }
 
@@ -85,9 +86,11 @@ prologue_augment (const char *prologue, location_t location)
 void
 epilogue_set (const char *epilogue, location_t location)
 {
-  obstack_fgrow2 (&muscle_obstack, "]b4_syncline([[%d]], [[%s]])[\n",
-		  location.first_line,
-		  quotearg_style (escape_quoting_style, location.file));
+  obstack_fgrow1 (&muscle_obstack, "]b4_syncline([[%d]], [[",
+		  location.first_line);
+  MUSCLE_OBSTACK_SGROW (&muscle_obstack,
+			quotearg_style (c_quoting_style, location.file));
+  obstack_sgrow (&muscle_obstack, "]])[\n");
   obstack_sgrow (&muscle_obstack, epilogue);
   obstack_1grow (&muscle_obstack, 0);
   muscle_insert ("epilogue", obstack_finish (&muscle_obstack));
