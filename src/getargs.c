573a6cd3568911ec7a698daea60fa24f36088cd2
Akim Demaille <akim@epita.fr>
2002-11-12 08:13:10 +0000

    * src/getargs.c (long_options): Remove duplicates. * src/vmsgetargs.c, src/build.com, src/bison.cld, src/vmshlp.mar: Remove. * doc/bison.rnh: Remove. * doc/bison.texinfo (VMS Invocation): Remove.

--- src/getargs.c
+++ src/getargs.c
@@ -284,18 +284,6 @@ static struct option const long_options[] =
   /* Hidden. */
   { "trace",         optional_argument,   0,     'T' },
 
-  /* FIXME: semantic parsers will output an `include' of an
-     output file: be sure that the naem included is indeed the name of
-     the output file.  */ /* FIXME Should we activate this options ?
-     */
-  { "output",      required_argument,     0, 'o' },
-  { "file-prefix", required_argument,     0, 'b' },
-  { "name-prefix", required_argument,     0, 'p' },
-
-  /*
-   * Percent and command line declarations.
-   */
-
   /* Output.  */
   { "defines",     optional_argument,   0,   'd' },
 
--- src/output.c
+++ src/output.c
@@ -591,8 +591,7 @@ prepare (void)
   MUSCLE_INSERT_INT ("pure", pure_parser);
   MUSCLE_INSERT_INT ("synclines_flag", !no_lines_flag);
 
-  /* FIXME: This is wrong: the muscles should decide whether they hold
-     a copy or not, but the situation is too obscure currently.  */
+  /* File names.  */
   MUSCLE_INSERT_STRING ("prefix", spec_name_prefix ? spec_name_prefix : "yy");
 
   /* User Code.  */
