4e956a7d9e38f9a4aea59c9603c26e22d74989c5
Paul Eggert <eggert@cs.ucla.edu>
2002-11-12 07:23:52 +0000

    Include quotearg.h, since we need to quote C strings now. (muscle_init): Quote filename as a C string.

--- src/muscle_tab.c
+++ src/muscle_tab.c
@@ -20,6 +20,7 @@
 
 #include "system.h"
 #include "hash.h"
+#include "quotearg.h"
 #include "files.h"
 #include "muscle_tab.h"
 #include "getargs.h"
@@ -64,7 +65,7 @@ muscle_init (void)
 
   /* Version and input file.  */
   MUSCLE_INSERT_STRING ("version", VERSION);
-  MUSCLE_INSERT_STRING ("filename", infile);
+  MUSCLE_INSERT_C_STRING ("filename", infile);
 }
 
 
