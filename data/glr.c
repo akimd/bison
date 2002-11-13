53b779905639965220efde7e2c06de1151c45d0c
Paul Eggert <eggert@cs.ucla.edu>
2002-11-13 06:48:18 +0000

    (inline): Remove #define.  It's the user's responsibility to #define it away, just like 'const'.  This fixes one of the bugs reported by Nelson H.F. Beebe in <http://mail.gnu.org/pipermail/bug-bison/2002-November/001873.html>.

--- data/glr.c
+++ data/glr.c
@@ -243,10 +243,6 @@ b4_syncline([@oline@], [@ofile@])
 # define ATTRIBUTE_UNUSED __attribute__ ((__unused__))
 #endif
 
-#if ! defined (__GNUC__)
-# define inline
-#endif
-
 /* YYFINAL -- State number of the termination state. */
 #define YYFINAL  ]b4_final_state_number[
 /* YYLAST -- Last index in YYTABLE.  */
