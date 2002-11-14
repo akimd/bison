970785f1a049a7f5cd24dcba280b407b2765d19a
Paul Hilfinger <Hilfinger@CS.Berkeley.EDU>
2002-11-14 23:18:55 +0000

    * data/glr.c (yyFail): Always set yyerrflag.  Corrects regression  on input.at test.

--- data/glr.c
+++ data/glr.c
@@ -594,12 +594,12 @@ static void yyfreeGLRStack (yyGLRStack* yystack);
 static void
 yyFail (yyGLRStack* yystack]b4_pure_formals[, const char* yyformat, ...)
 {
+  yystack->yyerrflag = 1;
   if (yyformat != NULL)
     {
       char yymsg[256];
       va_list yyap;
       va_start (yyap, yyformat);
-      yystack->yyerrflag = 1;
       vsprintf (yymsg, yyformat, yyap);
       yyerror (]b4_yyerror_args[yymsg);
     }
