93ba6bf344389e00b3f8b5a0cb06226d32b8b275
Paul Eggert <eggert@cs.ucla.edu>
2002-11-15 01:28:40 +0000

    (output_skeleton): Call xfopen instead of duplicating xfopen's body.

--- src/output.c
+++ src/output.c
@@ -526,10 +526,7 @@ output_skeleton (void)
   strcpy (full_path, pkgdatadir);
   full_path[pkgdatadirlen] = '/';
   strcpy (full_path + pkgdatadirlen + 1, m4sugar);
-  in = fopen (full_path, "r");
-  if (! in)
-    error (EXIT_FAILURE, errno, "%s", full_path);
-  xfclose (in);
+  xfclose (xfopen (full_path, "r"));
   strcpy (full_path + pkgdatadirlen + 1, skeleton);
 
   /* Create an m4 subprocess connected to us via two pipes.  */
