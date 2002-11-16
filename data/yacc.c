ee41006f01b4a1968c7181b2fde5d6a2f5830f36
Paul Eggert <eggert@cs.ucla.edu>
2002-11-16 08:13:40 +0000

    Avoid over-quoting of __line__ and __file.

--- data/yacc.c
+++ data/yacc.c
@@ -210,10 +210,10 @@ typedef struct yyltype
 #endif
 
 /* Copy the second part of user declarations.  */
-]b4_post_prologue[
+]b4_post_prologue
 
 /* Line __line__ of __file__.  */
-]b4_syncline([@oline@], [@ofile@])[
+b4_syncline([@oline@], [@ofile@])[
 
 #if ! defined (yyoverflow) || YYERROR_VERBOSE
 
