b475e0cce5411d3eeb149f81d299a248412986ad
Paul Eggert <eggert@cs.ucla.edu>
2002-11-21 05:18:57 +0000

    (nullable_compute): Use abort rather than assert.

--- src/nullable.c
+++ src/nullable.c
@@ -99,7 +99,8 @@ nullable_compute (void)
 	else
 	  {
 	    /* This rule has an empty RHS. */
-	    assert (item_number_as_rule_number (rule->rhs[0]) == ruleno);
+	    if (item_number_as_rule_number (rule->rhs[0]) != ruleno)
+	      abort ();
 	    if (rule->useful && !nullable[rule->lhs->number])
 	      {
 		nullable[rule->lhs->number] = 1;
