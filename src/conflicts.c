b9a0104860cfa2c2c75d9acbf93dc8f6532712ad
Paul Eggert <eggert@cs.ucla.edu>
2002-11-21 05:16:36 +0000

    (resolve_sr_conflict): Use abort rather than assert.

--- src/conflicts.c
+++ src/conflicts.c
@@ -231,8 +231,7 @@ resolve_sr_conflict (state_t *state, int ruleno,
 	      break;
 
 	    case undef_assoc:
-	      assert (symbols[i]->assoc != undef_assoc);
-	      break;
+	      abort ();
 	    }
       }
 
