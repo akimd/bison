78c3da9e09e60da5d41aba898dd3adffc1cfd4d0
Paul Eggert <eggert@cs.ucla.edu>
2002-11-29 05:59:59 +0000

    (reader): Remove gram_control; it wasn't being used.

--- src/reader.c
+++ src/reader.c
@@ -469,8 +469,6 @@ packgram (void)
 void
 reader (void)
 {
-  gram_control_t gram_control;
-
   /* Initialize the symbol table.  */
   symbols_new ();
 
@@ -500,7 +498,7 @@ reader (void)
   gram__flex_debug = trace_flag & trace_scan;
   gram_debug = trace_flag & trace_parse;
   scanner_initialize ();
-  gram_parse (&gram_control);
+  gram_parse ();
 
   /* If something went wrong during the parsing, don't try to
      continue.  */
