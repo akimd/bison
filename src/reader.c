e927351156e5964091a1d086ac782d3ce0b87982
Paul Eggert <eggert@cs.ucla.edu>
2002-11-30 09:52:11 +0000

    (grammar_current_rule_check): Warn about type clashes instead of complaining.

--- src/reader.c
+++ src/reader.c
@@ -260,14 +260,14 @@ grammar_current_rule_check (void)
     {
       const char *rhs_type = first_rhs->type_name ? first_rhs->type_name : "";
       if (!STRUNIQ_EQ (lhs_type, rhs_type))
-	complain_at (current_rule->location,
-		     _("type clash on default action: <%s> != <%s>"),
-		     lhs_type, rhs_type);
+	warn_at (current_rule->location,
+		 _("type clash on default action: <%s> != <%s>"),
+		 lhs_type, rhs_type);
     }
   /* Warn if there is no default for $$ but we need one.  */
   else
-    complain_at (current_rule->location,
-		 _("empty rule for typed nonterminal, and no action"));
+    warn_at (current_rule->location,
+	     _("empty rule for typed nonterminal, and no action"));
 }
 
 
