c5d128ec1ae0141984b205183fecda0c734fcf97
Paul Eggert <eggert@cs.ucla.edu>
2002-12-07 05:45:46 +0000

    (yy_reduce_print): Don't assume that yyrline[yyrule] promotes to int; it might be unsigned int.

--- data/glr.c
+++ data/glr.c
@@ -1103,8 +1103,9 @@ static inline void
 yy_reduce_print (size_t yyk, yyRuleNum yyrule)
 {
   int yyi;
-  YYFPRINTF (stderr, "Reducing stack %d by rule %d (line %d), ",
-	     yyk, yyrule - 1, yyrline[yyrule]);
+  unsigned int yylineno = yyrline[yyrule];
+  YYFPRINTF (stderr, "Reducing stack %d by rule %d (line %u), ",
+	     yyk, yyrule - 1, yylineno);
   /* Print the symbols being reduced, and their result.  */
   for (yyi = yyprhs[yyrule]; 0 <= yyrhs[yyi]; yyi++)
     YYFPRINTF (stderr, "%s ", yytokenName (yyrhs[yyi]));
