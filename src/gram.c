5fcdb07b709f345b1efeea2956618d7d7359f67b
Paul Eggert <eggert@cs.ucla.edu>
2002-12-07 05:55:28 +0000

    LOCATION_PRINT -> location_print

--- src/gram.c
+++ src/gram.c
@@ -316,7 +316,7 @@ grammar_rules_never_reduced_report (const char *message)
   for (r = 0; r < nrules ; ++r)
     if (!rules[r].useful)
       {
-	LOCATION_PRINT (stderr, rules[r].location);
+	location_print (stderr, rules[r].location);
 	fprintf (stderr, ": %s: %s: ",
 		 _("warning"), message);
 	rule_print (&rules[r], stderr);
