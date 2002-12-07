4f21fffe7e1e948cf2ad9fcf06c8a6716d48fc10
Paul Eggert <eggert@cs.ucla.edu>
2002-12-07 05:46:30 +0000

    (yy_reduce_print): Don't assume that yyrline[yyrule] promotes to int; it might be unsigned int.

--- data/yacc.c
+++ data/yacc.c
@@ -577,8 +577,9 @@ do {								\
                    [[int yyrule], [yyrule]])[
 {
   int yyi;
-  YYFPRINTF (stderr, "Reducing stack by rule %d (line %d), ",
-             yyrule - 1, yyrline[yyrule]);
+  unsigned int yylineno = yyrline[yyrule];
+  YYFPRINTF (stderr, "Reducing stack by rule %d (line %u), ",
+             yyrule - 1, yylineno);
   /* Print the symbols being reduced, and their result.  */
   for (yyi = yyprhs[yyrule]; 0 <= yyrhs[yyi]; yyi++)
     YYFPRINTF (stderr, "%s ", yytname [yyrhs[yyi]]);
