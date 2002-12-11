c134bfbeeef01f4004df6a8ef53777554449502b
Paul Eggert <eggert@cs.ucla.edu>
2002-12-11 05:31:12 +0000

    Adjust to type renamings.

--- src/assoc.c
+++ src/assoc.c
@@ -19,13 +19,14 @@
    Boston, MA 02111-1307, USA.  */
 
 #include "system.h"
+
 #include "assoc.h"
 
 
 const char *
-assoc_to_string (assoc_t assoc)
+assoc_to_string (assoc a)
 {
-  switch (assoc)
+  switch (a)
     {
     case undef_assoc:
       return "undefined associativity";
