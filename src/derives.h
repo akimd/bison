fbfa091d0504b3634a9609ab0a4d1e778dbbc2c0
Paul Eggert <eggert@cs.ucla.edu>
2002-12-11 06:26:42 +0000

    Include "gram.h".

--- src/derives.h
+++ src/derives.h
@@ -1,5 +1,7 @@
 /* Match rules with nonterminals for bison,
-   Copyright (C) 1984, 1989, 2000, 2001, 2002  Free Software Foundation, Inc.
+
+   Copyright (C) 1984, 1989, 2000, 2001, 2002 Free Software
+   Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
 
@@ -21,9 +23,11 @@
 #ifndef DERIVES_H_
 # define DERIVES_H_
 
+# include "gram.h"
+
 /* DERIVES[SYMBOL - NTOKENS] points to a vector of the rules that
    SYMBOL derives, terminated with NULL.  */
-extern rule_t ***derives;
+extern rule ***derives;
 
 /* Compute DERIVES.  */
 
