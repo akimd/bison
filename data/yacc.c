1915f1331ee5866cbd9f7fdd2b5a5b78e5652d56
Paul Eggert <eggert@cs.ucla.edu>
2002-12-13 09:33:23 +0000

    Fix a bad interaction with flex 2.5.23 reported by Bruce Lilly in <http://mail.gnu.org/pipermail/bug-bison/2002-November/001929.html>.
    (YYLTYPE, struct yyltype): Do not define unless
    locations are requested.
    (union yyalloc): Do not depend on YYLTYPE_IS_TRIVIAL unless
    locations are requested.

--- data/yacc.c
+++ data/yacc.c
@@ -197,7 +197,7 @@ b4_syncline([@oline@], [@ofile@])],
 # define YYSTYPE_IS_TRIVIAL 1
 #endif
 
-#ifndef YYLTYPE
+]b4_location_if([#ifndef YYLTYPE
 typedef struct yyltype
 {
   int first_line;
@@ -208,6 +208,7 @@ typedef struct yyltype
 # define YYLTYPE ]b4_location_type[
 # define YYLTYPE_IS_TRIVIAL 1
 #endif
+])[
 
 /* Copy the second part of user declarations.  */
 ]b4_post_prologue
@@ -249,7 +250,7 @@ b4_syncline([@oline@], [@ofile@])[
 
 #if (! defined (yyoverflow) \
      && (! defined (__cplusplus) \
-	 || (YYLTYPE_IS_TRIVIAL && YYSTYPE_IS_TRIVIAL)))
+	 || (]b4_location_if([YYLTYPE_IS_TRIVIAL && ])[YYSTYPE_IS_TRIVIAL)))
 
 /* A type that is properly aligned for any stack member.  */
 union yyalloc
