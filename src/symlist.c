4613f49b90c0c440910da55581f95183b9bb1272
Paul Eggert <eggert@cs.ucla.edu>
2002-12-13 08:49:08 +0000

    XMALLOC -> MALLOC

--- src/symlist.c
+++ src/symlist.c
@@ -32,7 +32,7 @@
 symbol_list *
 symbol_list_new (symbol *sym, location loc)
 {
-  symbol_list *res = XMALLOC (symbol_list, 1);
+  symbol_list *res = MALLOC (res, 1);
   res->next = NULL;
   res->sym = sym;
   res->location = loc;
--- src/vcg.c
+++ src/vcg.c
@@ -1,4 +1,5 @@
 /* VCG description handler for Bison.
+
    Copyright (C) 2001, 2002 Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
@@ -420,7 +421,7 @@ add_classname (graph *g, int val, const char *name)
 {
   struct classname *classname;
 
-  classname = XMALLOC (struct classname, 1);
+  MALLOC (classname, 1);
   classname->no = val;
   classname->name = name;
   classname->next = g->classname;
@@ -432,7 +433,7 @@ add_infoname (graph *g, int integer, const char *str)
 {
   struct infoname *infoname;
 
-  infoname = XMALLOC (struct infoname, 1);
+  MALLOC (infoname, 1);
   infoname->integer = integer;
   infoname->chars = str;
   infoname->next = g->infoname;
@@ -446,7 +447,7 @@ add_colorentry (graph *g, int color_idx, int red_cp,
 {
   struct colorentry *ce;
 
-  ce = XMALLOC (struct colorentry, 1);
+  MALLOC (ce, 1);
   ce->color_index = color_idx;
   ce->red_cp = red_cp;
   ce->green_cp = green_cp;
