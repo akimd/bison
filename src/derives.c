e59a68718b2e60db619058070f02bedc51cba304
Paul Eggert <eggert@cs.ucla.edu>
2002-12-13 08:22:26 +0000

    Undo XFREE change; we assume C89 compiler, not C89 library.

--- src/derives.c
+++ src/derives.c
@@ -116,6 +116,6 @@ derives_compute (void)
 void
 derives_free (void)
 {
-  free (derives[0]);
-  free (derives);
+  XFREE (derives[0]);
+  XFREE (derives);
 }
--- src/gram.c
+++ src/gram.c
@@ -329,9 +329,9 @@ grammar_rules_never_reduced_report (const char *message)
 void
 grammar_free (void)
 {
-  free (ritem);
+  XFREE (ritem);
   free (rules);
-  free (token_translations);
+  XFREE (token_translations);
   /* Free the symbol table data structure.  */
   symbols_free ();
   free_merger_functions ();
