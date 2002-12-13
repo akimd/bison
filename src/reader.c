9c4637fa8c5a0bbfe31293f7550a41ed79cab730
Paul Eggert <eggert@cs.ucla.edu>
2002-12-13 08:32:12 +0000

    XCALLOC -> CALLOC, XMALLOC -> MALLOC.

--- src/reader.c
+++ src/reader.c
@@ -1,4 +1,5 @@
 /* Input parser for Bison
+
    Copyright (C) 1984, 1986, 1989, 1992, 1998, 2000, 2001, 2002
    Free Software Foundation, Inc.
 
@@ -127,7 +128,7 @@ get_merge_function (uniqstr name, uniqstr type, location loc)
       break;
   if (syms->next == NULL)
     {
-      syms->next = XMALLOC (merger_list, 1);
+      MALLOC (syms->next, 1);
       syms->next->name = uniqstr_new (name);
       syms->next->type = uniqstr_new (type);
       syms->next->next = NULL;
@@ -406,8 +407,8 @@ packgram (void)
   rule_number ruleno = 0;
   symbol_list *p = grammar;
 
-  ritem = XCALLOC (item_number, nritems);
-  rules = XCALLOC (rule, nrules);
+  CALLOC (ritem, nritems);
+  CALLOC (rules, nrules);
 
   while (p)
     {
