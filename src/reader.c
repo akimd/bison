52328c6e9941e9edde89fff880fc5dfdcc9429ee
Paul Eggert <eggert@cs.ucla.edu>
2002-12-21 12:56:44 +0000

    Comment fix.

--- src/reader.c
+++ src/reader.c
@@ -181,7 +181,7 @@ free_merger_functions (void)
 /* The (currently) last symbol of GRAMMAR. */
 symbol_list *grammar_end = NULL;
 
-/* Append S to the GRAMMAR. */
+/* Append SYM to the grammar.  */
 void
 grammar_symbol_append (symbol *sym, location loc)
 {
