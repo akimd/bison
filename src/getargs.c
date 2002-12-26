fcbfa6b01c0222b01254730c66d539ed2c841a4e
Paul Eggert <eggert@cs.ucla.edu>
2002-12-26 06:55:45 +0000

    (getargs) [MSDOS]: Don't assume optarg != NULL with -g option.

--- src/getargs.c
+++ src/getargs.c
@@ -355,7 +355,8 @@ getargs (int argc, char *argv[])
       case 'g':
 	/* Here, the -g and --graph=FILE options are differentiated.  */
 	graph_flag = 1;
-	spec_graph_file = AS_FILE_NAME (optarg);
+	if (optarg)
+	  spec_graph_file = AS_FILE_NAME (optarg);
 	break;
 
       case 'v':
