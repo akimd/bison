f6abc26e38ad488fa7ce59e22efcc2130bea310f
Paul Eggert <eggert@cs.ucla.edu>
2002-12-26 06:58:35 +0000

    Comment fix: don't mention obsolete bison.simple.

--- data/glr.c
+++ data/glr.c
@@ -204,7 +204,7 @@ typedef struct YYLTYPE
 #endif
 
 /* Default (constant) values used for initialization for null
-   right-hand sides.  Unlike the standard bison.simple template,
+   right-hand sides.  Unlike the standard yacc.c template,
    here we set the default values of $$ and $@@ to zeroed-out
    values.  Since the default value of these quantities is undefined,
    this behavior is technically correct. */
