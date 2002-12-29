527203e98e6c57e185ac4308631cc6286edb13d1
Paul Eggert <eggert@cs.ucla.edu>
2002-12-29 04:30:18 +0000

    (symbol_make_alias): Set type of SYMVAL to be that of SYM's type. This fixes Debian bug 168069, reported by Thomas Olsson.

--- src/symtab.c
+++ src/symtab.c
@@ -221,7 +221,7 @@ symbol_check_defined_processor (void *sym, void *null ATTRIBUTE_UNUSED)
 
 /*------------------------------------------------------------------.
 | Declare the new symbol SYM.  Make it an alias of SYMVAL, and type |
-| them with TYPENAME.                                               |
+| SYMVAL with SYM's type.                                           |
 `------------------------------------------------------------------*/
 
 void
@@ -247,6 +247,7 @@ symbol_make_alias (symbol *sym, symbol *symval, location loc)
 	abort ();
       sym->number = symval->number =
 	(symval->number < sym->number) ? symval->number : sym->number;
+      symbol_type_set (symval, sym->type_name, loc);
     }
 }
 
