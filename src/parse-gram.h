8e4900ef405e04a30ca46cbcabafe4bfe3c0bf79
Paul Eggert <eggert@cs.ucla.edu>
2003-01-01 10:09:55 +0000

    Regenerate.

--- src/parse-gram.h
+++ src/parse-gram.h
@@ -1,4 +1,4 @@
-/* A Bison parser, made by GNU Bison 1.75f.  */
+/* A Bison parser, made by GNU Bison 1.875.  */
 
 /* Skeleton parser for Yacc-like parsing with Bison,
    Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002 Free Software Foundation, Inc.
