93b716f4aa3d9429acde721e040dccdea525b0ae
Paul Eggert <eggert@cs.ucla.edu>
2003-01-05 01:11:03 +0000

    Update copyright date.

--- lib/bitset_stats.c
+++ lib/bitset_stats.c
@@ -1,5 +1,5 @@
 /* Bitset statistics.
-   Copyright (C) 2002 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2003 Free Software Foundation, Inc.
    Contributed by Michael Hayes (m.hayes@elec.canterbury.ac.nz).
 
    This program is free software; you can redistribute it and/or modify
