69363a9e4d0b8d3b51536434d3395d38519f617b
Paul Eggert <eggert@cs.ucla.edu>
2003-01-13 06:41:29 +0000

    * NEWS: %expect-violations are now just warnings, reverting to Bison 1.30 and 1.75 behavior.  This fixes the GCC 3.2 bootstrapping problem reported by Matthias Klose; see <http://mail.gnu.org/archive/html/bug-bison/2003-01/msg00053.html>. * src/conflicts.c (conflicts_print): Likewise. * tests/conflicts.at (%expect not enough, %expect too much, %expect with reduce conflicts): Likewise. * doc/bison.texinfo (Expect Decl): Document this.  Also mention that the warning is enabled if the number of conflicts changes (not necessarily increases).

--- src/conflicts.c
+++ src/conflicts.c
@@ -1,6 +1,6 @@
 /* Find and resolve or report look-ahead conflicts for bison,
 
-   Copyright (C) 1984, 1989, 1992, 2000, 2001, 2002
+   Copyright (C) 1984, 1989, 1992, 2000, 2001, 2002, 2003
    Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
@@ -495,12 +495,12 @@ conflicts_print (void)
   if (expected_conflicts != -1)
     {
       if (! src_ok)
-	complain (ngettext ("expected %d shift/reduce conflict",
-			    "expected %d shift/reduce conflicts",
-			    expected_conflicts),
-		  expected_conflicts);
+	warn (ngettext ("expected %d shift/reduce conflict",
+			"expected %d shift/reduce conflicts",
+			expected_conflicts),
+	      expected_conflicts);
       if (rrc_total)
-	complain (_("expected 0 reduce/reduce conflicts"));
+	warn (_("expected 0 reduce/reduce conflicts"));
     }
 }
 
