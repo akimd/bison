34e1d160971ac6856af13d8bfcc10ea5a5249ff3
Paul Eggert <eggert@cs.ucla.edu>
2003-01-17 08:43:08 +0000

    (symbol_printers_output): Fix typo that led to core dump.  Problem reported by Antonio Rus in <http://mail.gnu.org/archive/html/bug-bison/2003-01/msg00058.html>.

--- src/output.c
+++ src/output.c
@@ -432,7 +432,7 @@ symbol_printers_output (FILE *out)
 
   fputs ("m4_define([b4_symbol_printers], \n[", out);
   for (i = 0; i < nsyms; ++i)
-    if (symbols[i]->destructor)
+    if (symbols[i]->printer)
       {
 	symbol *sym = symbols[i];
 
