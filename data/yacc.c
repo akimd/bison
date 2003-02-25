52f32190aa3e60eb079f8e374cf683094c5529ad
Paul Eggert <eggert@cs.ucla.edu>
2003-02-25 07:57:48 +0000

    (yy_reduce_print): yylineno -> yylno.

--- data/yacc.c
+++ data/yacc.c
@@ -583,9 +583,9 @@ do {								\
                    [[int yyrule], [yyrule]])[
 {
   int yyi;
-  unsigned int yylineno = yyrline[yyrule];
+  unsigned int yylno = yyrline[yyrule];
   YYFPRINTF (stderr, "Reducing stack by rule %d (line %u), ",
-             yyrule - 1, yylineno);
+             yyrule - 1, yylno);
   /* Print the symbols being reduced, and their result.  */
   for (yyi = yyprhs[yyrule]; 0 <= yyrhs[yyi]; yyi++)
     YYFPRINTF (stderr, "%s ", yytname [yyrhs[yyi]]);
