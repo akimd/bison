47ee29cf60549531e8400e808681c4dcc74694d4
Paul Eggert <eggert@cs.ucla.edu>
2003-02-25 07:56:34 +0000

    (yy_reduce_print): yylineno -> yylno, to avoid collision with flex use of yylineno. Problem reported by Bruce Lilly in <http://mail.gnu.org/archive/html/bug-bison/2003-02/msg00016.html>.

--- data/glr.c
+++ data/glr.c
@@ -1123,9 +1123,9 @@ static inline void
 yy_reduce_print (size_t yyk, yyRuleNum yyrule)
 {
   int yyi;
-  unsigned int yylineno = yyrline[yyrule];
+  unsigned int yylno = yyrline[yyrule];
   YYFPRINTF (stderr, "Reducing stack %d by rule %d (line %u), ",
-	     yyk, yyrule - 1, yylineno);
+	     yyk, yyrule - 1, yylno);
   /* Print the symbols being reduced, and their result.  */
   for (yyi = yyprhs[yyrule]; 0 <= yyrhs[yyi]; yyi++)
     YYFPRINTF (stderr, "%s ", yytokenName (yyrhs[yyi]));
