1d2d7eddf73d4244900ea00fe1949c56db07a2f7
Paul Eggert <eggert@cs.ucla.edu>
2003-04-21 19:37:09 +0000

    (yyexpandGLRStack) [!YYSTACKEXPANDABLE]: Fix obvious typo that results in uncompilable GLR parsers when both %pure-parser and %locations are used.  (trivial change from Yakov Markovitch <Markovitch@iso.ru>)

--- data/glr.c
+++ data/glr.c
@@ -926,7 +926,7 @@ yyexpandGLRStack (yyGLRStack* yystack]b4_pure_formals[)
 
 #else
 
-  yyFail (yystack][]b4_lpure_args[, "parser stack overflow");
+  yyFail (yystack][]b4_pure_args[, "parser stack overflow");
 #endif
 }
 
