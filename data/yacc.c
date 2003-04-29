68cdf74747d8094d31c7f3aa7757a0f3a08ed444
Paul Eggert <eggert@cs.ucla.edu>
2003-04-29 06:04:04 +0000

    (b4_lex_param): Corrected for the case where %lex-param is provided and %pure-parser isn't.
    From Tim Van Holder in
    <http://mail.gnu.org/archive/html/bug-bison/2003-04/msg00045.html>.

--- data/yacc.c
+++ data/yacc.c
@@ -58,8 +58,8 @@ m4_ifset([b4_parse_param], [b4_c_args(b4_parse_param), ])])
 # b4_lex_param arrives quoted twice, but we want to keep only one level.
 m4_define([b4_lex_param],
 m4_dquote(b4_pure_if([[[[YYSTYPE *]], [[&yylval]]][]dnl
-b4_location_if([, [[YYLTYPE *], [&yylloc]]])])dnl
-m4_ifdef([b4_lex_param], [, ]b4_lex_param)))
+b4_location_if([, [[YYLTYPE *], [&yylloc]]])m4_ifdef([b4_lex_param], [, ])])dnl
+m4_ifdef([b4_lex_param], b4_lex_param)))
 
 
 
