f54526254746d9a19dacd9d1c0cac798776501d2
Paul Eggert <eggert@cs.ucla.edu>
2003-05-25 06:43:19 +0000

    (YYSTACKEXPANDABLE): Do not evaluate YYSTYPE_IS_TRIVIAL or YYSTYPE_IS_TRIVIAL unless they are defined. This fixes a problem reported by John Bowman when the Compaq/HP Alpha cxx compiler happy (e.g. using cxx -D__USE_STD_IOSTREAM -ansi -Wall -gall).

--- data/glr.c
+++ data/glr.c
@@ -510,7 +510,8 @@ int yydebug;
 
 #if (! defined (YYSTACKEXPANDABLE) \
      && (! defined (__cplusplus) \
-	 || (]b4_location_if([YYLTYPE_IS_TRIVIAL && ])[YYSTYPE_IS_TRIVIAL)))
+	 || (]b4_location_if([[defined (YYLTYPE_IS_TRIVIAL) && YYLTYPE_IS_TRIVIAL \
+	     && ]])[defined (YYSTYPE_IS_TRIVIAL) && YYSTYPE_IS_TRIVIAL)))
 #define YYSTACKEXPANDABLE 1
 #else
 #define YYSTACKEXPANDABLE 0
