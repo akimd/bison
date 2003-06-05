692be7fef0688572ba933a1d616005bd2bfb445e
Paul Eggert <eggert@cs.ucla.edu>
2003-06-05 10:40:45 +0000

    Import of 2003-06-08 libbitset <http://mail.gnu.org/archive/html/bison-patches/2003-06/msg00005.html>

--- lib/libiberty.h
+++ lib/libiberty.h
@@ -1,35 +1,12 @@
-/* Fake libiberty.h for Bison.
-   Copyright (C) 2002, 2003, 2004 Free Software Foundation, Inc.
+/* Dummy libiberty header file.  */
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+#define PARAMS(X) X
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+#define ATTRIBUTE_UNUSED __attribute__ ((__unused__))
 
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+#define xmalloc malloc
+#define xcalloc calloc
+#define xrealloc realloc
 
+#define xalloc_die()
 
-/* Bison depends on libiberty's implementation of bitsets, which
-   requires a `libiberty.h' file.  This file provides the minimum
-   services.  */
-
-#ifndef BISON_LIBIBERTY_H_
-# define BISON_LIBIBERTY_H_ 1
-
-# ifndef __attribute__
-#  if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 8) || __STRICT_ANSI__
-#   define __attribute__(x)
-#  endif
-# endif
-
-# define ATTRIBUTE_UNUSED __attribute__ ((__unused__))
-
-# include "xalloc.h"
-
-#endif /* ! BISON_LIBIBERTY_H_ */
