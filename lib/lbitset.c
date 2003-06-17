28ee846711e18ec802c44c9d7896523162f177eb
Paul Eggert <eggert@cs.ucla.edu>
2003-06-17 07:24:40 +0000

    Upgrade to 2003-06-08 libbitset, then: lbitset_unused_clear): Use prototypes, not old-style definitions.

--- lib/lbitset.c
+++ lib/lbitset.c
@@ -883,8 +883,7 @@ lbitset_empty_p (bitset dst)
 
 /* Ensure that any unused bits within the last element are clear.  */
 static inline void
-lbitset_unused_clear (dst)
-     bitset dst;
+lbitset_unused_clear (bitset dst)
 {
   unsigned int last_bit;
   bitset_bindex n_bits;
