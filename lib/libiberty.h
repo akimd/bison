6f2d9f5854c3fb7e51d36f3a266428a9a2f49deb
Paul Eggert <eggert@cs.ucla.edu>
2003-06-17 07:25:48 +0000

    Do not include config.h; that's the includer's job. Do not include <stdlib.h>. (PARAMS): Define unconditionally for C89. (ATTRIBUTE_NORETURN): Remove. (ATTRIBUTE_UNUSED): Define unconditionally.

--- lib/libiberty.h
+++ lib/libiberty.h
@@ -1,12 +1,38 @@
-/* Dummy libiberty header file.  */
+/* Fake libiberty.h for Bison.
+   Copyright (C) 2002, 2003 Free Software Foundation, Inc.
 
-#define PARAMS(X) X
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
-#define ATTRIBUTE_UNUSED __attribute__ ((__unused__))
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-#define xmalloc malloc
-#define xcalloc calloc
-#define xrealloc realloc
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
-#define xalloc_die()
 
+/* Bison depends on libiberty's implementation of bitsets, which
+   requires a `libiberty.h' file.  This file provides the minimum
+   services.  */
+
+#ifndef BISON_LIBIBERTY_H_
+# define BISON_LIBIBERTY_H_ 1
+
+# define PARAMS(X) X
+
+# ifndef __attribute__
+#  if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 8) || __STRICT_ANSI__
+#   define __attribute__(x)
+#  endif
+# endif
+
+# define ATTRIBUTE_UNUSED __attribute__ ((__unused__))
+
+# include "xalloc.h"
+
+#endif /* ! BISON_LIBIBERTY_H_ */
