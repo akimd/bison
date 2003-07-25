dfc8a22010ec0dbbcc361ab308e7e83ae917ef0f
Paul Eggert <eggert@cs.ucla.edu>
2003-07-25 07:42:25 +0000

    (yyglrReduce): Don't use C89 string concatenation.

--- data/glr.c
+++ data/glr.c
@@ -1200,8 +1200,7 @@ yyglrReduce (yyGLRStack* yystack, size_t yyk, yyRuleNum yyrule,
       yyupdateSplit (yystack, yys);
       yynewLRState = yyLRgotoState (yys->yylrState, yylhsNonterm (yyrule));
       YYDPRINTF ((stderr,
-		  "Reduced stack %lu by rule #%d; action deferred. "
-		  "Now in state %d.\n",
+		  "Reduced stack %lu by rule #%d; action deferred. Now in state %d.\n",
 		  (unsigned long int) yyk, yyrule - 1, yynewLRState));
       for (yyi = 0; yyi < yystack->yytops.yysize; yyi += 1)
 	if (yyi != yyk && yystack->yytops.yystates[yyi] != NULL)
