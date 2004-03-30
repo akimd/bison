2ae64eed2720a7bd03febd7207a62049448f04c1
Paul Eggert <eggert@cs.ucla.edu>
2004-03-30 08:11:20 +0000

    (version): Update copyright year to 2004.

--- src/getargs.c
+++ src/getargs.c
@@ -1,6 +1,6 @@
 /* Parse command line arguments for Bison.
 
-   Copyright (C) 1984, 1986, 1989, 1992, 2000, 2001, 2002, 2003
+   Copyright (C) 1984, 1986, 1989, 1992, 2000, 2001, 2002, 2003, 2004
    Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
@@ -267,7 +267,7 @@ version (void)
   putc ('\n', stdout);
 
   fprintf (stdout,
-	   _("Copyright (C) %d Free Software Foundation, Inc.\n"), 2003);
+	   _("Copyright (C) %d Free Software Foundation, Inc.\n"), 2004);
 
   fputs (_("\
 This is free software; see the source for copying conditions.  There is NO\n\
