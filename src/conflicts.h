1f916a78e6af829d8c2a8ec982f45aa7a30a9af5
Paul Eggert <eggert@cs.ucla.edu>
2004-04-13 17:43:15 +0000

    Update copyright to 2004.

--- src/conflicts.h
+++ src/conflicts.h
@@ -1,5 +1,5 @@
 /* Find and resolve or report look-ahead conflicts for bison,
-   Copyright (C) 2000, 2001, 2002 Free Software Foundation, Inc.
+   Copyright (C) 2000, 2001, 2002, 2004 Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
 
