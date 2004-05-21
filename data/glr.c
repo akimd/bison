383e69dc798510a66d7bf59b237c987285f79a21
Paul Eggert <eggert@cs.ucla.edu>
2004-05-21 22:23:04 +0000

    Output yylloc macro defn in glr.c even if locations are not being generated.

--- data/glr.c
+++ data/glr.c
@@ -158,7 +158,7 @@ m4_if(b4_prefix[], [yy], [],
 #define yychar  b4_prefix[]char
 #define yydebug b4_prefix[]debug
 #define yynerrs b4_prefix[]nerrs
-b4_location_if([#define yylloc b4_prefix[]lloc])])
+#define yylloc b4_prefix[]lloc])
 
 b4_token_defines(b4_tokens)
 
