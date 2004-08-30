417141dd6134bdb32a1a7d64b2bac4f8bcca0461
Akim Demaille <akim@epita.fr>
2004-08-30 07:50:08 +0000

    * data/glr.c: Guard the declarations of yypstack and yypdumpstack with #ifdef YYDEBUG.

--- data/glr.c
+++ data/glr.c
@@ -1931,6 +1931,7 @@ yyrecoverSyntaxError (yyGLRStack* yystack,
 }
 
 /* DEBUGGING ONLY */
+#ifdef YYDEBUG
 static void yypstack (yyGLRStack* yystack, int yyk) ATTRIBUTE_UNUSED;
 static void yypdumpstack (yyGLRStack* yystack) ATTRIBUTE_UNUSED;
 
@@ -1998,6 +1999,7 @@ yypdumpstack (yyGLRStack* yystack)
 	     (long int) YYINDEX (yystack->yytops.yystates[yyi]));
   fprintf (stderr, "\n");
 }
+#endif
 ]
 
 b4_epilogue
