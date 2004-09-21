0092f063af8393d1be1cb6aa5668f959c884c2d5
Akim Demaille <akim@epita.fr>
2004-09-21 06:59:19 +0000

    * data/yacc.c, data/glr.c (b4_at_dollar, b4_dollar_dollar): Remove the useless parentheses. Suggested by Paul Eggert.

--- data/glr.c
+++ data/glr.c
@@ -1833,8 +1833,8 @@ yyrecoverSyntaxError (yyGLRStack* yystack,
 #endif
 ])
 m4_ifdef([b4_initial_action], [
-m4_pushdef([b4_at_dollar],     [(yylval)])dnl
-m4_pushdef([b4_dollar_dollar], [(yylloc)])dnl
+m4_pushdef([b4_at_dollar],     [yylval])dnl
+m4_pushdef([b4_dollar_dollar], [yylloc])dnl
   /* User initialization code. */
   b4_initial_action
 m4_popdef([b4_dollar_dollar])dnl
--- data/yacc.c
+++ data/yacc.c
@@ -826,8 +826,8 @@ b4_c_function_def([yyparse], [int], b4_parse_param)
 #endif
 ]])
 m4_ifdef([b4_initial_action], [
-m4_pushdef([b4_at_dollar],     [(yylloc)])dnl
-m4_pushdef([b4_dollar_dollar], [(yylval)])dnl
+m4_pushdef([b4_at_dollar],     [yylloc])dnl
+m4_pushdef([b4_dollar_dollar], [yylval])dnl
   /* User initialization code. */
   b4_initial_action
 m4_popdef([b4_dollar_dollar])dnl
