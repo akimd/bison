0dcca5c22f8a1f2969b48b704b68c91e3e184942
Akim Demaille <akim@epita.fr>
2004-09-27 07:47:47 +0000

    * data/yacc.c (YY_LOCATION_PRINT): Fix its default declaration.

--- data/yacc.c
+++ data/yacc.c
@@ -527,9 +527,9 @@ while (0)
      fprintf (File, "%d.%d-%d.%d",			\
               (Loc).first_line, (Loc).first_column,	\
               (Loc).last_line,  (Loc).last_column)
+# else
+#  define YY_LOCATION_PRINT(File, Loc) ((void) 0)
 # endif
-#else
-# define YY_LOCATION_PRINT(File, Loc) ((void) 0)
 #endif
 
 
