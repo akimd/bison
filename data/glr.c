b9b51bf05a1984dd839da6941cfc460c3cadfdcf
Paul Eggert <eggert@cs.ucla.edu>
2004-10-27 22:22:51 +0000

    Fix wording problem in comment.

--- data/glr.c
+++ data/glr.c
@@ -208,8 +208,8 @@ typedef struct YYLTYPE
 /* Default (constant) value used for initialization for null
    right-hand sides.  Unlike the standard yacc.c template,
    here we set the default value of $$ to a zeroed-out value.
-   Since the default value of these quantities is undefined,
-   this behavior is technically correct. */
+   Since the default value is undefined, this behavior is
+   technically correct. */
 static YYSTYPE yyval_default;
 
 /* Copy the second part of user declarations.  */
