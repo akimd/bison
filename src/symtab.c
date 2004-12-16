3239db74b1cdeb3393c6c6ae839167d9f14f2251
Paul Eggert <eggert@cs.ucla.edu>
2004-12-16 00:08:21 +0000

    (symbol_new): Report an error if the input grammar contains too many symbols.

--- src/symtab.c
+++ src/symtab.c
@@ -66,6 +66,9 @@ symbol_new (uniqstr tag, location loc)
   res->alias = NULL;
   res->class = unknown_sym;
 
+  if (nsyms == SYMBOL_NUMBER_MAXIMUM)
+    fatal (_("too many symbols in input grammar (limit is %d)"),
+	   SYMBOL_NUMBER_MAXIMUM);
   nsyms++;
   return res;
 }
