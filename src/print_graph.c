10454ea42fefb0a4efe337c1ef54cd99a4e2001a
Akim Demaille <akim@epita.fr>
2004-12-16 09:09:56 +0000

    * src/print_graph.c (print_graph): Remove layoutalgorithm uses for real.

--- src/print_graph.c
+++ src/print_graph.c
@@ -202,13 +202,7 @@ print_graph (void)
 
   new_graph (&static_graph);
 
-#if 0
-  static_graph.smanhattan_edges = yes;
-  static_graph.manhattan_edges = yes;
-#endif
-
   static_graph.display_edge_labels = yes;
-  static_graph.layoutalgorithm = normal;
 
   static_graph.port_sharing = no;
   static_graph.finetuning = yes;
