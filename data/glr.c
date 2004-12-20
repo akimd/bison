9bec482e399ce3cfa2d395a294f4758726c13e40
Paul Eggert <eggert@cs.ucla.edu>
2004-12-20 03:44:10 +0000

    (YYLLOC_DEFAULT): Use GNU spacing conventions.

--- data/glr.c
+++ data/glr.c
@@ -429,17 +429,17 @@ static const ]b4_int_type_for([b4_stos])[ yystos[] =
     do									\
       if (N)								\
 	{								\
-	  (Current).first_line   = YYRHSLOC(Rhs, 1).first_line;		\
-	  (Current).first_column = YYRHSLOC(Rhs, 1).first_column;	\
-	  (Current).last_line    = YYRHSLOC(Rhs, N).last_line;		\
-	  (Current).last_column  = YYRHSLOC(Rhs, N).last_column;	\
+	  (Current).first_line   = YYRHSLOC (Rhs, 1).first_line;	\
+	  (Current).first_column = YYRHSLOC (Rhs, 1).first_column;	\
+	  (Current).last_line    = YYRHSLOC (Rhs, N).last_line;		\
+	  (Current).last_column  = YYRHSLOC (Rhs, N).last_column;	\
 	}								\
       else								\
 	{								\
 	  (Current).first_line   = (Current).last_line   =		\
-	    YYRHSLOC(Rhs, 0).last_line;					\
+	    YYRHSLOC (Rhs, 0).last_line;				\
 	  (Current).first_column = (Current).last_column =		\
-	    YYRHSLOC(Rhs, 0).last_column;				\
+	    YYRHSLOC (Rhs, 0).last_column;				\
 	}								\
     while (0)
 
--- data/yacc.c
+++ data/yacc.c
@@ -515,17 +515,17 @@ while (0)
     do									\
       if (N)								\
 	{								\
-	  (Current).first_line   = YYRHSLOC(Rhs, 1).first_line;		\
-	  (Current).first_column = YYRHSLOC(Rhs, 1).first_column;	\
-	  (Current).last_line    = YYRHSLOC(Rhs, N).last_line;		\
-	  (Current).last_column  = YYRHSLOC(Rhs, N).last_column;	\
+	  (Current).first_line   = YYRHSLOC (Rhs, 1).first_line;	\
+	  (Current).first_column = YYRHSLOC (Rhs, 1).first_column;	\
+	  (Current).last_line    = YYRHSLOC (Rhs, N).last_line;		\
+	  (Current).last_column  = YYRHSLOC (Rhs, N).last_column;	\
 	}								\
       else								\
 	{								\
 	  (Current).first_line   = (Current).last_line   =		\
-	    YYRHSLOC(Rhs, 0).last_line;					\
+	    YYRHSLOC (Rhs, 0).last_line;				\
 	  (Current).first_column = (Current).last_column =		\
-	    YYRHSLOC(Rhs, 0).last_column;				\
+	    YYRHSLOC (Rhs, 0).last_column;				\
 	}								\
     while (0)
 #endif
