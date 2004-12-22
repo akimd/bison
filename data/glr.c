d7e14fc000458de046cfcded04c2fe8438f63919
Paul Eggert <eggert@cs.ucla.edu>
2004-12-22 21:19:47 +0000

    * NEWS: Bison-generated parsers no longer default to using the alloca function (when available) to extend the parser stack, due to widespread problems in unchecked stack-overflow detection. * data/glr.c (YYMAXDEPTH): Remove undef when zero.  It's the user's responsibility to set it to a positive value.  This lets the user specify a value that is not a preprocessor constant. * data/yacc.c (YYMAXDEPTH): Likewise. (YYSTACK_ALLOC): Define only if YYSTACK_USE_ALLOCA is nonzero. * doc/bison.texinfo (Stack Overflow): YYMAXDEPTH no longer needs to be a compile-time constant.  However, explain the constraints on it. Also, explain the constraints on YYINITDEPTH. (Table of Symbols): Explain that alloca is no longer the default. Explain the user's responsibility if they define YYSTACK_USE_ALLOCA to 1.

--- data/glr.c
+++ data/glr.c
@@ -536,10 +536,6 @@ int yydebug;
    SIZE_MAX < YYMAXDEPTH * sizeof (GLRStackItem)
    evaluated with infinite-precision integer arithmetic.  */
 
-#if YYMAXDEPTH == 0
-# undef YYMAXDEPTH
-#endif
-
 #ifndef YYMAXDEPTH
 # define YYMAXDEPTH ]b4_stack_depth_max[
 #endif
--- data/yacc.c
+++ data/yacc.c
@@ -226,14 +226,10 @@ b4_syncline([@oline@], [@ofile@])[
 
 # ifdef YYSTACK_USE_ALLOCA
 #  if YYSTACK_USE_ALLOCA
-#   define YYSTACK_ALLOC alloca
-#  endif
-# else
-#  if defined (alloca) || defined (_ALLOCA_H)
-#   define YYSTACK_ALLOC alloca
-#  else
 #   ifdef __GNUC__
 #    define YYSTACK_ALLOC __builtin_alloca
+#   else
+#    define YYSTACK_ALLOC alloca
 #   endif
 #  endif
 # endif
@@ -647,10 +643,6 @@ int yydebug;
    Do not make this value too large; the results are undefined if
    SIZE_MAX < YYSTACK_BYTES (YYMAXDEPTH)
    evaluated with infinite-precision integer arithmetic.  */
-
-#if defined (YYMAXDEPTH) && YYMAXDEPTH == 0
-# undef YYMAXDEPTH
-#endif
 
 #ifndef YYMAXDEPTH
 # define YYMAXDEPTH ]b4_stack_depth_max[
