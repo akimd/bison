55a91a8242d818dd4869e0de4333d7cf4d557beb
Paul Eggert <eggert@cs.ucla.edu>
2004-12-26 05:26:30 +0000

    (new_itemsets): Use memset rather than zeroing by hand.  This avoids a warning about comparing int to size_t when GCC warnings are enabled.

--- src/LR0.c
+++ src/LR0.c
@@ -183,8 +183,7 @@ new_itemsets (state *s)
   if (trace_flag & trace_automaton)
     fprintf (stderr, "Entering new_itemsets, state = %d\n", s->number);
 
-  for (i = 0; i < nsyms; i++)
-    kernel_size[i] = 0;
+  memset (kernel_size, 0, nsyms * sizeof *kernel_size);
 
   nshifts = 0;
 
