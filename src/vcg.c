f8b30d02ec86c7de69d5ef14aa4a97c69c359cf9
Paul Eggert <eggert@cs.ucla.edu>
2005-01-28 22:06:55 +0000

    (output_graph): G_VIEW -> normal_view in case someone wants to redefine G_VIEW.

--- src/vcg.c
+++ src/vcg.c
@@ -809,7 +809,7 @@ output_graph (graph *g, FILE *fout)
     fprintf (fout, "\tcrossing_optimization:\t%s\n",
 	     get_decision_str (g->crossing_optimization));
 
-  if (g->view != G_VIEW)
+  if (g->view != normal_view)
     fprintf (fout, "\tview:\t%s\n", get_view_str (g->view));
 
   if (g->edges != G_EDGES)
