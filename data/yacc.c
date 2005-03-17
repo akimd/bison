baf073c6c5d2ded338dd5c3c6b68db1f5fb71d11
Paul Eggert <eggert@cs.ucla.edu>
2005-03-17 19:19:44 +0000

    (yyparse): Omit spaces before #line. Problem reported by twlevo@xs4all.nl.

--- data/yacc.c
+++ data/yacc.c
@@ -836,7 +836,7 @@ m4_ifdef([b4_initial_action], [
 m4_pushdef([b4_at_dollar],     [yylloc])dnl
 m4_pushdef([b4_dollar_dollar], [yylval])dnl
   /* User initialization code. */
-  b4_initial_action
+b4_initial_action
 m4_popdef([b4_dollar_dollar])dnl
 m4_popdef([b4_at_dollar])dnl
 /* Line __line__ of yacc.c.  */
