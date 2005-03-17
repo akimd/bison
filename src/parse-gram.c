4b3017faf2b78e09bdfbf9f03a9fd60cc7eb5d0c
Paul Eggert <eggert@cs.ucla.edu>
2005-03-17 19:22:00 +0000

    Regenerate.

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -1139,7 +1139,7 @@ YYLTYPE yylloc;
 
 
   /* User initialization code. */
-  #line 71 "parse-gram.y"
+#line 71 "parse-gram.y"
 {
   /* Bison's grammar can initial empty locations, hence a default
      location is needed. */
