72f000b088367bd18a20b3e063bc1ab6a3e0bc15
Paul Eggert <eggert@cs.ucla.edu>
2005-04-17 08:07:15 +0000

    * NEWS: Bison-generated C parsers no longer quote literal strings associated with tokens. * src/output.c (prepare_symbols): Don't escape strings, since users don't want to see C escapes. * tests/calc.at (AT_CHECK_CALC): Adjust to lack of quotes in diagnostics. * tests/regression.at (Token definitions, Web2c Actions): Likewise.

--- src/output.c
+++ src/output.c
@@ -162,7 +162,10 @@ prepare_symbols (void)
     int j = 2;
     for (i = 0; i < nsyms; i++)
       {
-	const char *cp = quotearg_style (c_quoting_style, symbols[i]->tag);
+	char const *tag = symbols[i]->tag;
+	char const *cp = (*tag == '"'
+			  ? tag
+			  : quotearg_style (c_quoting_style, tag));
 	/* Width of the next token, including the two quotes, the
 	   comma and the space.  */
 	int width = strlen (cp) + 2;
