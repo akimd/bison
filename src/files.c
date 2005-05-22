f2a97c6294e9b1e47255a88609d76e4527350b8f
Paul Eggert <eggert@cs.ucla.edu>
2005-05-22 17:52:12 +0000

    * src/files.c: Include "stdio-safer.h"; this fixes a typo in the previous change.

--- src/files.c
+++ src/files.c
@@ -32,6 +32,7 @@
 #include "files.h"
 #include "getargs.h"
 #include "gram.h"
+#include "stdio-safer.h"
 
 /* From basename.c.  Almost a lie, as it returns a char *. */
 const char *base_name (char const *name);
