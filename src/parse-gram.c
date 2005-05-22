bd134904da53e28185c7eff23d7ef1cf6b8389f2
Paul Eggert <eggert@cs.ucla.edu>
2005-05-22 05:20:48 +0000

    Regenerate.

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -1,7 +1,7 @@
 /* A Bison parser, made by GNU Bison 2.0a.  */
 
 /* Skeleton parser for Yacc-like parsing with Bison,
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -1990,9 +1990,6 @@ yyacceptlab:
 | yyabortlab -- YYABORT comes here.  |
 `-----------------------------------*/
 yyabortlab:
-  yydestruct (_("Error: discarding lookahead"),
-              yytoken, &yylval, &yylloc);
-  yychar = YYEMPTY;
   yyresult = 1;
   goto yyreturn;
 
@@ -2002,13 +1999,14 @@ yyabortlab:
 `----------------------------------------------*/
 yyoverflowlab:
   yyerror (_("parser stack overflow"));
-  yydestruct (_("Error: discarding lookahead"),
-	      yytoken, &yylval, &yylloc);
   yyresult = 2;
   /* Fall through.  */
 #endif
 
 yyreturn:
+  if (yychar != YYEOF && yychar != YYEMPTY)
+     yydestruct (_("Error: discarding lookahead"),
+		 yytoken, &yylval, &yylloc);
   if (yyssp != yyss)
     for (;;)
       {
--- src/parse-gram.h
+++ src/parse-gram.h
@@ -1,7 +1,7 @@
 /* A Bison parser, made by GNU Bison 2.0a.  */
 
 /* Skeleton parser for Yacc-like parsing with Bison,
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -143,7 +143,7 @@ typedef union YYSTYPE {
   assoc assoc;
   uniqstr uniqstr;
 } YYSTYPE;
-/* Line 1381 of yacc.c.  */
+/* Line 1379 of yacc.c.  */
 #line 148 "parse-gram.h"
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
