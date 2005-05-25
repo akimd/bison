4d1801f1a888fc5687ae103fb76935ef0d24cbaf
Paul Eggert <eggert@cs.ucla.edu>
2005-05-25 19:48:00 +0000

    Fix BeOS, FreeBSD, MacOS porting problems reported by Bruno Haible. * lib/bitset.c (bitset_print): Don't assume size_t can be printed with %d format. * lib/ebitset.c (min, max): Undef before defining. * lib/vbitset.c (min, max): Likewise. * lib/subpipe.c (create_subpipe): Save local variables in case vfork clobbers them. * tests/synclines.at (AT_SYNCLINES_COMPILE): Add support for the error message syntax used by gcc-4.0.

--- lib/bitset.c
+++ lib/bitset.c
@@ -1,5 +1,5 @@
 /* General bitsets.
-   Copyright (C) 2002, 2003, 2004 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2003, 2004, 2005 Free Software Foundation, Inc.
    Contributed by Michael Hayes (m.hayes@elec.canterbury.ac.nz).
 
    This program is free software; you can redistribute it and/or modify
@@ -111,7 +111,7 @@ bitset_type_choose (bitset_bindex n_bits ATTRIBUTE_UNUSED, unsigned int attr)
   /* Choose the type of bitset.  Note that sometimes we will be asked
   for a zero length fixed size bitset.  */
 
-  
+
   /* If no attributes selected, choose a good compromise.  */
   if (!attr)
     return BITSET_VARRAY;
@@ -307,7 +307,7 @@ bitset_print (FILE *file, bitset bset, bool verbose)
 	pos = 0;
       }
 
-    fprintf (file, "%d ", i);
+    fprintf (file, "%lu ", (unsigned long int) i);
     pos += 1 + (i >= 10) + (i >= 100);
   };
 
--- lib/subpipe.c
+++ lib/subpipe.c
@@ -114,6 +114,13 @@ create_subpipe (char const * const *argv, int fd[2])
       || (from_out_fd = fd_safer (pipe_fd[1])) < 0)
     error (EXIT_FAILURE, errno, "pipe");
 
+  /* Save the local variables in the parent now, in case vfork
+     clobbers them.  */
+  fd[0] = to_out_fd;
+  fd[1] = from_in_fd;
+  pipe_fd[0] = to_in_fd;
+  pipe_fd[1] = from_out_fd;
+
   pid = vfork ();
   if (pid < 0)
     error (EXIT_FAILURE, errno, "fork");
@@ -137,10 +144,8 @@ create_subpipe (char const * const *argv, int fd[2])
     }
 
   /* Parent.  */
-  close (to_in_fd);
-  close (from_out_fd);
-  fd[0] = to_out_fd;
-  fd[1] = from_in_fd;
+  close (pipe_fd[0]);
+  close (pipe_fd[1]);
   return pid;
 }
 
