73e561d51a8f0ff3e28005a5e642556b8b14c347
Paul Eggert <eggert@cs.ucla.edu>
2005-05-30 17:25:18 +0000

    Regenerate.

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -1851,7 +1851,7 @@ yyerrlab:
 	      }
 
 	  yyf = _(yyformat);
-	  yysize1 = yysize + strlen (yyf);
+	  yysize1 = yysize + yystrlen (yyf);
 	  yysize_overflow |= yysize1 < yysize;
 	  yysize = yysize1;
 
