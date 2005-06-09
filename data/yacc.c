0925ebb4fa72edab483ba025aa2e998339117599
Paul Eggert <eggert@cs.ucla.edu>
2005-06-09 20:44:28 +0000

    * data/yacc.c (malloc, free) [defined __cplusplus]: Wrap inside extern "C" {}.

--- data/yacc.c
+++ data/yacc.c
@@ -262,6 +262,9 @@ b4_syncline([@oline@], [@ofile@])[
 #  ifndef YYSTACK_ALLOC_MAXIMUM
 #   define YYSTACK_ALLOC_MAXIMUM ((YYSIZE_T) -1)
 #  endif
+#  ifdef __cplusplus
+extern "C" {
+#  endif
 #  ifndef YYMALLOC
 #   define YYMALLOC malloc
 #   if (! defined (malloc) && ! defined (YYINCLUDED_STDLIB_H) \
@@ -275,6 +278,9 @@ void *malloc (YYSIZE_T); /* INFRINGES ON USER NAME SPACE */
 	&& (defined (__STDC__) || defined (__cplusplus)))
 void free (void *); /* INFRINGES ON USER NAME SPACE */
 #   endif
+#  endif
+#  ifdef __cplusplus
+}
 #  endif
 # endif
 #endif /* ! defined (yyoverflow) || YYERROR_VERBOSE */
--- src/parse-gram.c
+++ src/parse-gram.c
@@ -327,6 +327,9 @@ typedef struct YYLTYPE
 #  ifndef YYSTACK_ALLOC_MAXIMUM
 #   define YYSTACK_ALLOC_MAXIMUM ((YYSIZE_T) -1)
 #  endif
+#  ifdef __cplusplus
+extern "C" {
+#  endif
 #  ifndef YYMALLOC
 #   define YYMALLOC malloc
 #   if (! defined (malloc) && ! defined (YYINCLUDED_STDLIB_H) \
@@ -340,6 +343,9 @@ void *malloc (YYSIZE_T); /* INFRINGES ON USER NAME SPACE */
 	&& (defined (__STDC__) || defined (__cplusplus)))
 void free (void *); /* INFRINGES ON USER NAME SPACE */
 #   endif
+#  endif
+#  ifdef __cplusplus
+}
 #  endif
 # endif
 #endif /* ! defined (yyoverflow) || YYERROR_VERBOSE */
@@ -1165,8 +1171,8 @@ YYLTYPE yylloc;
   yylloc.start.line   = yylloc.end.line   = 1;
   yylloc.start.column = yylloc.end.column = 0;
 }
-/* Line 863 of yacc.c.  */
-#line 1170 "parse-gram.c"
+/* Line 869 of yacc.c.  */
+#line 1176 "parse-gram.c"
   yyvsp[0] = yylval;
     yylsp[0] = yylloc;
 
@@ -1750,8 +1756,8 @@ yyreduce:
       default: break;
     }
 
-/* Line 1059 of yacc.c.  */
-#line 1755 "parse-gram.c"
+/* Line 1065 of yacc.c.  */
+#line 1761 "parse-gram.c"
 
   yyvsp -= yylen;
   yyssp -= yylen;
