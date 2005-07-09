5f6da1c0f87fce6498af745949507b5991641251
Paul Eggert <eggert@cs.ucla.edu>
2005-07-09 08:27:02 +0000

    Regenerate.

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -1171,11 +1171,9 @@ YYLTYPE yylloc;
   yylloc.start.line   = yylloc.end.line   = 1;
   yylloc.start.column = yylloc.end.column = 0;
 }
-/* Line 869 of yacc.c.  */
+/* Line 870 of yacc.c.  */
 #line 1176 "parse-gram.c"
-  yyvsp[0] = yylval;
-    yylsp[0] = yylloc;
-
+  yylsp[0] = yylloc;
   goto yysetstate;
 
 /*------------------------------------------------------------.
@@ -1756,8 +1754,8 @@ yyreduce:
       default: break;
     }
 
-/* Line 1065 of yacc.c.  */
-#line 1761 "parse-gram.c"
+/* Line 1066 of yacc.c.  */
+#line 1759 "parse-gram.c"
 
   yyvsp -= yylen;
   yyssp -= yylen;
--- src/parse-gram.h
+++ src/parse-gram.h
@@ -143,7 +143,7 @@ typedef union YYSTYPE {
   assoc assoc;
   uniqstr uniqstr;
 } YYSTYPE;
-/* Line 1388 of yacc.c.  */
+/* Line 1389 of yacc.c.  */
 #line 148 "parse-gram.h"
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
