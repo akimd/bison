716f248dcd69ff9c1adbf6140a7028b0dc19641c
Akim Demaille <akim@epita.fr>
2005-07-12 13:43:00 +0000

    Trailing trace.

--- src/symlist.c
+++ src/symlist.c
@@ -118,7 +118,6 @@ symbol_list_n_type_name_get (symbol_list *rp, location loc, int n)
       if (rp == NULL || rp->sym == NULL)
 	{
 	  complain_at (loc, _("invalid $ value: $%d"), n);
-	  fprintf (stderr, "\n");
 	  return NULL;
 	}
       ++i;
