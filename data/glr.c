d741bd1bfd186e7ce7f74377a01f5eeb206324e0
Paul Eggert <eggert@cs.ucla.edu>
2005-07-14 05:08:03 +0000

    * data/glr.c (yyuserAction): Fix uninitialized variable that caused a valgrind failure.  Problem reported by twlevo@xs4all.nl.

--- data/glr.c
+++ data/glr.c
@@ -781,7 +781,8 @@ yyuserAction (yyRuleNum yyn, int yyrhslen, yyGLRStackItem* yyvsp,
   else
     *yyvalp = yyvsp[YYFILL (1-yyrhslen)].yystate.yysemantics.yysval;
   YYLLOC_DEFAULT (*yylocp, yyvsp - yyrhslen, yyrhslen);
-]
+]b4_location_if([[  yystack->yyerror_range[1].yystate.yyloc = *yylocp;
+]])
   switch (yyn)
     {
       b4_actions
