ab703f2c9b0e79a6abd6d587a4df2381643cb24c
Paul Eggert <eggert@cs.ucla.edu>
2005-07-20 21:17:04 +0000

    (SYMBOL_ATTR_PRINT, symbol_print): Direct output to FILE * parameter, not to stderr.

--- src/symtab.c
+++ src/symtab.c
@@ -80,12 +80,12 @@ symbol_new (uniqstr tag, location loc)
 
 #define SYMBOL_ATTR_PRINT(Attr)				\
   if (s->Attr)						\
-    fprintf (stderr, " %s { %s }", #Attr, s->Attr)
+    fprintf (f, " %s { %s }", #Attr, s->Attr)
 
 void
 symbol_print (symbol *s, FILE *f)
 {
-  fprintf (stderr, "\"%s\"", s->tag);
+  fprintf (f, "\"%s\"", s->tag);
   SYMBOL_ATTR_PRINT (type_name);
   SYMBOL_ATTR_PRINT (destructor);
   SYMBOL_ATTR_PRINT (printer);
