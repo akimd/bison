aacbb77a75367eb9857494f5f0057ae7d548471e
Paul Eggert <eggert@cs.ucla.edu>
2005-07-22 22:32:03 +0000

    Update copyright.

--- lib/bitsetv.c
+++ lib/bitsetv.c
@@ -1,5 +1,5 @@
 /* Bitset vectors.
-   Copyright (C) 2001, 2002, 2004 Free Software Foundation, Inc.
+   Copyright (C) 2001, 2002, 2004, 2005 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
