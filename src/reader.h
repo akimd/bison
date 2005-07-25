57beea57768aeaae6eee4bedbecae7d295018065
Paul Eggert <eggert@cs.ucla.edu>
2005-07-25 03:12:53 +0000

    Update copyright.

--- src/reader.h
+++ src/reader.h
@@ -1,6 +1,6 @@
 /* Input parser for Bison
 
-   Copyright (C) 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
+   Copyright (C) 2000, 2001, 2002, 2003, 2005 Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
 
