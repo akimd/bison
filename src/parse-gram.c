ec5d1a8a24f4724c8de98ce70bdc5037f6577cf5
Paul Eggert <eggert@cs.ucla.edu>
2005-08-22 02:33:18 +0000

    Regenerate.

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -2163,11 +2163,11 @@ yyexhaustedlab:
 
 yyreturn:
   if (yychar != YYEOF && yychar != YYEMPTY)
-     yydestruct ("Error: discarding lookahead",
+     yydestruct ("Cleanup: discarding lookahead",
 		 yytoken, &yylval, &yylloc);
   while (yyssp != yyss)
     {
-      yydestruct ("Error: popping",
+      yydestruct ("Cleanup: popping",
 		  yystos[*yyssp], yyvsp, yylsp);
       YYPOPSTACK;
     }
