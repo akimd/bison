127287e9c478d3b29f6671cab599131443443f95
Paul Eggert <eggert@cs.ucla.edu>
2005-09-09 22:59:46 +0000

    * data/glr.c (YYCHK1): Do not assume YYE is in range. This avoids a diagnostic from gcc -Wswitch-enum. Problem reported by twlevo@xs4all.nl.

--- data/glr.c
+++ data/glr.c
@@ -2024,7 +2024,7 @@ yyrecoverSyntaxError (yyGLRStack* yystack,
 #define YYCHK1(YYE)							     \
   do {									     \
     switch (YYE) {							     \
-    default:								     \
+    case yyok:								     \
       break;								     \
     case yyabort:							     \
       goto yyabortlab;							     \
@@ -2032,6 +2032,8 @@ yyrecoverSyntaxError (yyGLRStack* yystack,
       goto yyacceptlab;							     \
     case yyerr:								     \
       goto yyuser_error;						     \
+    default:								     \
+      goto yybuglab;							     \
     }									     \
   } while (0)
 
@@ -2186,6 +2188,10 @@ b4_syncline([@oline@], [@ofile@])])dnl
  yyacceptlab:
   yyresult = 0;
   goto yyreturn;
+
+ yybuglab:
+  YYASSERT (yyfalse);
+  /* Fall through.  */
 
  yyabortlab:
   yyresult = 1;
