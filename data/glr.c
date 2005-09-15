d110145422cc59a8d588735a4671fc4cae9aad76
Paul Eggert <eggert@cs.ucla.edu>
2005-09-15 18:02:03 +0000

    false -> yyfalse

--- data/glr.c
+++ data/glr.c
@@ -1675,7 +1675,7 @@ yyresolveValue (yySemanticOption* yyoptionList, yyGLRStack* yystack,
 	      yymerge = yyfalse;
 	      break;
 	    default:
-	      /* This cannot happen so it is not worth a YYASSERT (false),
+	      /* This cannot happen so it is not worth a YYASSERT (yyfalse),
 	         but some compilers complain if the default case is
 		 omitted.  */
 	      break;
