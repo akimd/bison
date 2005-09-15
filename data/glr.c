ae199bf10fc7f201c07593f4e4f85ec7f12d5764
Paul Eggert <eggert@cs.ucla.edu>
2005-09-15 14:16:54 +0000

    * data/glr.c (yyresolveValue): Add default case to pacify gcc -Wswitch-default.  Problem reported by twlevo@xs4all.nl.

--- data/glr.c
+++ data/glr.c
@@ -1674,6 +1674,11 @@ yyresolveValue (yySemanticOption* yyoptionList, yyGLRStack* yystack,
 	      yybest = yyp;
 	      yymerge = yyfalse;
 	      break;
+	    default:
+	      /* This cannot happen so it is not worth a YYASSERT (false),
+	         but some compilers complain if the default case is
+		 omitted.  */
+	      break;
 	    }
 	  yypp = &yyp->yynext;
 	}
