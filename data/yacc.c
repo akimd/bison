62c4328e008dd6dd68fb5ac2a7ed1b8790f81279
Paul Eggert <eggert@cs.ucla.edu>
2005-09-21 20:08:38 +0000

    Don't rely on whether alloc is #defined; that is in the user's name spac.

--- data/yacc.c
+++ data/yacc.c
@@ -263,7 +263,7 @@ b4_syncline([@oline@], [@ofile@])[
 #    define alloca _alloca
 #   else
 #    define YYSTACK_ALLOC alloca
-#    if (! defined (alloca) && ! defined (_ALLOCA_H) && ! defined (_STDLIB_H) \
+#    if (! defined (_ALLOCA_H) && ! defined (_STDLIB_H) \
 	 && (defined (__STDC__) || defined (__cplusplus)))
 #     include <stdlib.h> /* INFRINGES ON USER NAME SPACE */
 #     ifndef _STDLIB_H
