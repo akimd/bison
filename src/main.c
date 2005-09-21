0d50976f3c7b4e74e8cbe32dbeb0312a7845ca14
Paul Eggert <eggert@cs.ucla.edu>
2005-09-21 19:54:48 +0000

    More alloca cleanups.
    * bootstrap (gnulib_modules): Remove alloca.  Bison doesn't need
    it itself.
    * src/main.c (main) [C_ALLOCA]: Don't flush alloca'ed memory; we
    don't use alloca any more.
    
    * data/yacc.c [YYSTACK_USE_ALLOCA && !defined __GNUC__ && ! defined
    __BUILTIN_VA_ARG_INCR && ! defined _AIX && ! defined _MSC_VER &&
    (defined alloca || defined _ALLOCA_H)]: Don't include <stdlib.h>;
    not needed in this case.
    * tests/torture.at (Exploding the Stack Size with Alloca): Adjust
    to match yacc.c, to test more hosts.

--- src/main.c
+++ src/main.c
@@ -172,14 +172,6 @@ main (int argc, char *argv[])
   scanner_free ();
   muscle_free ();
   uniqstrs_free ();
-  /* If using alloca.c, flush the alloca'ed memory for the benefit of
-     people running Bison as a library in IDEs.  */
-#if C_ALLOCA
-  {
-    extern void *alloca (size_t);
-    alloca (0);
-  }
-#endif
   timevar_pop (TV_FREE);
 
   if (trace_flag & trace_bitsets)
