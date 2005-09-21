58b90cc2d82b068770bd38b5dff41e24a4bfeb37
Paul Eggert <eggert@cs.ucla.edu>
2005-09-21 20:08:58 +0000

    Regenerate.

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -328,7 +328,7 @@ typedef struct YYLTYPE
 #    define alloca _alloca
 #   else
 #    define YYSTACK_ALLOC alloca
-#    if (! defined (alloca) && ! defined (_ALLOCA_H) && ! defined (_STDLIB_H) \
+#    if (! defined (_ALLOCA_H) && ! defined (_STDLIB_H) \
 	 && (defined (__STDC__) || defined (__cplusplus)))
 #     include <stdlib.h> /* INFRINGES ON USER NAME SPACE */
 #     ifndef _STDLIB_H
