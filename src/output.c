ec4d88f4965d67eda9bff4a7772cb25a33c3fdf5
Juan Manuel Guerrero <juan.guerrero@gmx.de>
2005-10-02 16:59:42 +0000

    (output_skeleton): Use end_of_output_subpipe() to handle the lack of pipe/fork functionality on non-posix systems.

--- src/output.c
+++ src/output.c
@@ -577,6 +577,7 @@ output_skeleton (void)
 
   /* Read and process m4's output.  */
   timevar_push (TV_M4);
+  end_of_output_subpipe (pid, filter_fd);
   in = fdopen (filter_fd[1], "r");
   if (! in)
     error (EXIT_FAILURE, get_errno (),
