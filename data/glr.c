a1373f559a4365b9c03d2bacd6961317d0f8393b
Akim Demaille <akim@epita.fr>
2005-11-14 15:33:18 +0000

    * data/glr.c (yy_reduce_print): Fix the $ number.

--- data/glr.c
+++ data/glr.c
@@ -1386,7 +1386,7 @@ yy_reduce_print (yyGLRStack* yystack, size_t yyk, yyRuleNum yyrule,
   //for (yyi = yyprhs[yyrule]; 0 <= yyrhs[yyi]; yyi++)
   for (yyi = 0; yyi < yynrhs; yyi++)
     {
-      fprintf (stderr, "   $%d = ", yyi);
+      fprintf (stderr, "   $%d = ", yyi + 1);
       yysymprint (stderr, yyrhs[yyprhs[yyrule] + yyi],
                   &]b4_rhs_value(yynrhs, yyi)[
                   ]b4_location_if([, &]b4_rhs_location(yynrhs, yyi))[]dnl
