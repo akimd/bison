b2741627b109a934f745d7d83af756b18a29a39b
Akim Demaille <akim@epita.fr>
2005-11-14 14:34:38 +0000

    * data/glr.c (yyexpandGLRStack): Declare only if defined.

--- data/glr.c
+++ data/glr.c
@@ -783,7 +783,9 @@ struct yyGLRStack {
   yyGLRStateSet yytops;
 };
 
+#if YYSTACKEXPANDABLE
 static void yyexpandGLRStack (yyGLRStack* yystack);
+#endif
 
 static void yyFail (yyGLRStack* yystack]b4_pure_formals[, const char* yymsg)
   __attribute__ ((__noreturn__));
