d9963c85979e4d2adecd639d4b226b13f9e26385
Paul Eggert <eggert@cs.ucla.edu>
2005-11-18 19:37:13 +0000

    * data/yacc.c (yy_reduce_print, YY_REDUCE_PRINT): Fix typos in previous change that broke 'make check'. YY_REDUCE_PRINT cannot be a pseudo-varargs macro; that isn't supported in C. * tests/calc.at (_AT_CHECK_CALC,_AT_CHECK_CALC_ERROR): Don't check NUM-STDERR-LINES, since the output format is fluctuating. We can revert this once things settle down.

--- data/yacc.c
+++ data/yacc.c
@@ -668,9 +668,9 @@ do {								\
 `------------------------------------------------*/
 
 ]b4_c_function_def([yy_reduce_print], [static void],
-                   [[YYSTYPE *yyvsp], [yyvsvp]],
-   b4_location_if([[[YYLTYPE *yylsp], [yyvlvp]],])
-                   [[int yyrule],     [yyrule]]
+                   [[YYSTYPE *yyvsp], [yyvsp]],
+    b4_location_if([[[YYLTYPE *yylsp], [yylsp]],])
+		   [[int yyrule], [yyrule]]m4_ifset([b4_parse_param], [,])
                    b4_parse_param)[
 {
   int yynrhs = yyr2[yyrule];
@@ -690,10 +690,10 @@ do {								\
     }
 }
 
-# define YY_REDUCE_PRINT(Args)					\
-do {								\
-  if (yydebug)							\
-    yy_reduce_print (yyvsp, ]b4_location_if([yylsp, ])[Args);	\
+# define YY_REDUCE_PRINT(Rule)		\
+do {					\
+  if (yydebug)				\
+    yy_reduce_print (yyvsp, ]b4_location_if([yylsp, ])[Rule]b4_user_args[); \
 } while (/*CONSTCOND*/ 0)
 
 /* Nonzero means print parse trace.  It is left uninitialized so that
@@ -703,7 +703,7 @@ int yydebug;
 # define YYDPRINTF(Args)
 # define YY_SYMBOL_PRINT(Title, Type, Value, Location)
 # define YY_STACK_PRINT(Bottom, Top)
-# define YY_REDUCE_PRINT(Args)
+# define YY_REDUCE_PRINT(Rule)
 #endif /* !YYDEBUG */
 
 
@@ -1261,7 +1261,7 @@ yyreduce:
 ]b4_location_if(
 [[  /* Default location. */
   YYLLOC_DEFAULT (yyloc, yylsp - yylen, yylen);]])[
-  YY_REDUCE_PRINT ((yyn]b4_user_args[));
+  YY_REDUCE_PRINT (yyn);
   switch (yyn)
     {
       ]b4_actions[
