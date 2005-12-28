a51d426a63f276249acccb50cd7bc930d34ebb6c
Paul Eggert <eggert@cs.ucla.edu>
2005-12-28 08:51:34 +0000

    Update copyright.

--- src/location.h
+++ src/location.h
@@ -1,5 +1,5 @@
 /* Locations for Bison
-   Copyright (C) 2002, 2004 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2004, 2005 Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
 
