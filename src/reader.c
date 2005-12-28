668c5d192776326107790e8d622dfa7ddd015ea3
Paul Eggert <eggert@cs.ucla.edu>
2005-12-28 08:31:22 +0000

    (grammar_rule_check): Rewrite slightly to avoid GCC warnings.

--- src/reader.c
+++ src/reader.c
@@ -247,17 +247,19 @@ grammar_rule_check (const symbol_list *r)
 
   /* Check that typed symbol values are used.  */
   {
-    symbol_list *l = r;
+    symbol_list const *l = r;
     int n = 0;
     for (; l && l->sym; l = l->next, ++n)
       if (! (l->used
 	     || !l->sym->type_name
 	     /* The default action, $$ = $1, `uses' both.  */
-	     || !r->action && (n == 0 || n == 1)))
-	if (n)
-	  warn_at (r->location, _("unused value: $%d"), n);
-	else
-	  warn_at (r->location, _("unset value: $$"));
+	     || (!r->action && (n == 0 || n == 1))))
+	{
+	  if (n)
+	    warn_at (r->location, _("unused value: $%d"), n);
+	  else
+	    warn_at (r->location, _("unset value: $$"));
+	}
   }
 }
 
