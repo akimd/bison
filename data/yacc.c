0a96ba819fb75b14465445336719dd44f9ecb8f7
Akim Demaille <akim@epita.fr>
2006-01-02 16:26:17 +0000

    * src/output.c (output_skeleton): Don't hard wire the inclusion of c.m4. * data/c++.m4, data/glr.c, data/yacc.c: Include c.m4. * data/glr.cc: Do not include stack.hh.

--- data/yacc.c
+++ data/yacc.c
@@ -20,7 +20,7 @@ m4_divert(-1)                                                -*- C -*-
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301  USA
 
-
+m4_include(b4_pkgdatadir/[c.m4])
 
 ## ---------------- ##
 ## Default values.  ##
