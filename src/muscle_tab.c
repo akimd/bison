ff5150d9a43b1bf59866a57f724a4bd1e9b60ad1
Paul Eggert <eggert@cs.ucla.edu>
2006-01-03 20:19:41 +0000

    (muscle_grow): Fix comment.

--- src/muscle_tab.c
+++ src/muscle_tab.c
@@ -118,9 +118,9 @@ muscle_insert (const char *key, char *value)
 
 
 /*-------------------------------------------------------------------.
-| Insert (KEY, VALUE).  If KEY already existed, overwrite the        |
-| previous value.  Uses MUSCLE_OBSTACK.  De-allocates the previously |
-| associated value.  VALUE and SEPARATOR are copied.                 |
+| Append VALUE to the current value of KEY.  If KEY did not already  |
+| exist, create it.  Use MUSCLE_OBSTACK.  De-allocate the previously |
+| associated value.  Copy VALUE and SEPARATOR.                       |
 `-------------------------------------------------------------------*/
 
 void
@@ -198,7 +198,7 @@ void muscle_pair_list_grow (const char *muscle,
 | Find the value of muscle KEY.  |
 `-------------------------------*/
 
-char*
+char *
 muscle_find (const char *key)
 {
   muscle_entry probe;
