9ae1f0c6a9cfdb18910d0bf2b31c566a16826bda
Paul Eggert <eggert@cs.ucla.edu>
2006-01-03 22:25:58 +0000

    Update copyright.

--- src/reduce.c
+++ src/reduce.c
@@ -1,6 +1,6 @@
 /* Grammar reduction for Bison.
 
-   Copyright (C) 1988, 1989, 2000, 2001, 2002, 2003, 2005 Free
+   Copyright (C) 1988, 1989, 2000, 2001, 2002, 2003, 2005, 2006 Free
    Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
