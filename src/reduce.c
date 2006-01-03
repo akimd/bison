4d56beff96de67083b357bffa76f110166a9b67c
Paul Eggert <eggert@cs.ucla.edu>
2006-01-03 22:24:43 +0000

    (reduce_grammar): Remove unnecessary cast to bool.

--- src/reduce.c
+++ src/reduce.c
@@ -426,7 +426,7 @@ reduce_grammar (void)
   useless_nonterminals ();
   inaccessable_symbols ();
 
-  reduced = (bool) (nuseless_nonterminals + nuseless_productions > 0);
+  reduced = (nuseless_nonterminals + nuseless_productions > 0);
   if (!reduced)
     return;
 
