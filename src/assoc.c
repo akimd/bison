06f01bc4964795ec030c756e292f52b6f439a3d4
Paul Eggert <eggert@cs.ucla.edu>
2006-01-22 08:17:18 +0000

    * src/assoc.c (assoc_to_string): Use a default: abort (); case to pacify gcc -Wswitch-default. * src/scan-gram.l (adjust_location): Use a default: break; case to pacify gcc -Wswitch-default. * src/scan-skel.h (skel_get_lineno, skel_get_in, skel_get_out): (skel_get_leng, skel_get_text, skel_set_lineno, skel_set_in): (skel_set_out, skel_get_debug, skel_set_debug, skel_lex_destroy): Move these decls to scan-skel.l, since they don't need to be visible elsewhere. * src/scan-skel.l: Accept the above decls. (skel_lex): New decl, to pacify GCC when unpatched flex 2.5.31 is used.

--- src/assoc.c
+++ src/assoc.c
@@ -1,5 +1,5 @@
 /* Associativity information.
-   Copyright (C) 2002, 2005 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2005, 2006 Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
 
@@ -29,6 +29,9 @@ assoc_to_string (assoc a)
 {
   switch (a)
     {
+    default:
+      abort ();
+
     case undef_assoc:
       return "undefined associativity";
 
@@ -41,5 +44,4 @@ assoc_to_string (assoc a)
     case non_assoc:
       return "%nonassoc";
     }
-  return NULL;
 }
