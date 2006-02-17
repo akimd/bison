fc3f467ff12133962ef69f58e60cb24fc29cc929
Paul Eggert <eggert@cs.ucla.edu>
2006-02-17 00:08:17 +0000

    * data/yacc.c [(! defined yyoverflow || YYERROR_VERBOSE) && ! YYSTACK_USE_ALLOCA && ! defined YYSTACK_ALLOC && defined __cplusplus && ! defined _STDLIB_H && ! ((defined YYMALLOC || defined malloc) && (defined YYFREE || defined free))]: Include <stdlib.h> rather than rolling our own declarations of malloc and free, to avoid problems with incompatible declarations (using 'throw') C++'s stdlib.h.  This should fix Debian bug 340012 <http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=340012>, reported by Guillaume Melquiond.

--- data/yacc.c
+++ data/yacc.c
@@ -338,8 +338,13 @@ typedef short int yytype_int16;
 #  ifndef YYSTACK_ALLOC_MAXIMUM
 #   define YYSTACK_ALLOC_MAXIMUM YYSIZE_MAXIMUM
 #  endif
-#  ifdef __cplusplus
-extern "C" {
+#  if (defined __cplusplus && ! defined _STDLIB_H \
+       && ! ((defined YYMALLOC || defined malloc) \
+	     && (defined YYFREE || defined free)))
+#   include <stdlib.h> /* INFRINGES ON USER NAME SPACE */
+#   ifndef _STDLIB_H
+#    define _STDLIB_H 1
+#   endif
 #  endif
 #  ifndef YYMALLOC
 #   define YYMALLOC malloc
@@ -352,9 +357,6 @@ void *malloc (YYSIZE_T); /* INFRINGES ON USER NAME SPACE */
 #   if ! defined free && ! defined _STDLIB_H && ]b4_c_modern[
 void free (void *); /* INFRINGES ON USER NAME SPACE */
 #   endif
-#  endif
-#  ifdef __cplusplus
-}
 #  endif
 # endif
 #endif /* ! defined yyoverflow || YYERROR_VERBOSE */
