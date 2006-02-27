18d9185cae4c43684e4a2a134dd3090b55307586
Paul Eggert <eggert@cs.ucla.edu>
2006-02-27 19:50:59 +0000

    * data/glr.c (yyexpandGLRStack): Catch an off-by-one error that led to a segmentation fault in GNU Pascal.  Problem reported by Waldek Hebisch.

--- data/glr.c
+++ data/glr.c
@@ -1190,7 +1190,7 @@ yyexpandGLRStack (yyGLRStack* yystackp)
   size_t yysize, yynewSize;
   size_t yyn;
   yysize = yystackp->yynextFree - yystackp->yyitems;
-  if (YYMAXDEPTH <= yysize)
+  if (YYMAXDEPTH - YYHEADROOM < yysize)
     yyMemoryExhausted (yystackp);
   yynewSize = 2*yysize;
   if (YYMAXDEPTH < yynewSize)
