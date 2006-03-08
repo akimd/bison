d6b771c345a389742bfcd73ff13dac1d4f016c88
Paul Eggert <eggert@cs.ucla.edu>
2006-03-08 19:45:10 +0000

    Fix some nits reported by twlevo. * doc/FAQ: Remove ancient Y2K FAQ, replacing it with "secure" and "POSIX".  Use more-modern syntax for URLs.  Mention C++ and ask for Java.  Don't hardwire OS version numbers.  Add copyright notice. * m4/.cvsignore: Add unistd_h.m4, for latest gnulib. * src/conflicts.c (solved_conflicts_obstack): Now static.

--- src/conflicts.c
+++ src/conflicts.c
@@ -40,7 +40,7 @@
 int expected_sr_conflicts = -1;
 int expected_rr_conflicts = -1;
 static char *conflicts;
-struct obstack solved_conflicts_obstack;
+static struct obstack solved_conflicts_obstack;
 
 static bitset shift_set;
 static bitset look_ahead_set;
