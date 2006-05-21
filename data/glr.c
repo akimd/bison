7b5cdcbd176bf71ef4a16977d557cb56d1602511
Joel E. Denny <jdenny@ces.clemson.edu>
2006-05-21 08:11:30 +0000

    * data/yacc.c (yyparse): Wrap the final return from yyparse inside YYID to make sure that YYID will never be unused.  This fixes a 'make maintainer-check' failure caused by the recent changes to the 'Trivial grammars' test case, which caused g++ 4.1.0 to complain that YYID was not used. * data/glr.c (yyparse): Wrap yyparse's return inside YYID just in case.

--- data/glr.c
+++ data/glr.c
@@ -2523,7 +2523,8 @@ b4_syncline([@oline@], [@ofile@])])dnl
       yyfreeGLRStack (&yystack);
     }
 
-  return yyresult;
+  /* Make sure YYID is used.  */
+  return YYID (yyresult);
 }
 
 /* DEBUGGING ONLY */
--- data/yacc.c
+++ data/yacc.c
@@ -1480,7 +1480,8 @@ yyreturn:
   if (yymsg != yymsgbuf)
     YYSTACK_FREE (yymsg);
 #endif
-  return yyresult;
+  /* Make sure YYID is used.  */
+  return YYID (yyresult);
 ]}
 
 
