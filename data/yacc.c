aefef0d6cf40ee09d6ffb6b24547adc718de6327
Paul Eggert <eggert@cs.ucla.edu>
2006-05-27 00:28:17 +0000

    * data/yacc.c (yy_reduce_print): Omit trailing white space in generated source code.

--- data/yacc.c
+++ data/yacc.c
@@ -693,9 +693,9 @@ do {								\
 
 ]b4_c_function_def([yy_reduce_print], [static void],
 		   [[YYSTYPE *yyvsp], [yyvsp]],
-    b4_locations_if([[[YYLTYPE *yylsp], [yylsp]],])
-		   [[int yyrule], [yyrule]]m4_ifset([b4_parse_param], [,])
-		   b4_parse_param)[
+    b4_locations_if([[[YYLTYPE *yylsp], [yylsp]],
+		   ])[[int yyrule], [yyrule]]m4_ifset([b4_parse_param], [,
+		   ])b4_parse_param)[
 {
   int yynrhs = yyr2[yyrule];
   int yyi;
