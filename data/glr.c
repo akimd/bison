021039846636833e7a38887c9b28f1a4a7f0687c
Paul Eggert <eggert@cs.ucla.edu>
2006-05-30 23:45:04 +0000

    * data/glr.c (YYRECOVERING): Define to be a function-like macro with no arguments, not as an object-like macro.  This is for compatibility with data/yacc.c.  Problem reported by John P. Hartmann in <http://lists.gnu.org/archive/html/bug-bison/2006-05/msg00059.html>. * doc/bison.texinfo (Action Features, Error Recovery, Table of Symbols): Document this.

--- data/glr.c
+++ data/glr.c
@@ -906,7 +906,7 @@ yyuserAction (yyRuleNum yyn, int yyrhslen, yyGLRStackItem* yyvsp,
 # undef YYERROR
 # define YYERROR return yyerrok, yyerr
 # undef YYRECOVERING
-# define YYRECOVERING (yystackp->yyerrState != 0)
+# define YYRECOVERING() (yystackp->yyerrState != 0)
 # undef yyclearin
 # define yyclearin (yychar = YYEMPTY)
 # undef YYFILL
