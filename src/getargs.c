184e42f06525f9076bfa76900dd40e7533a4c46b
Joel E. Denny <jdenny@ces.clemson.edu>
2006-06-06 06:00:55 +0000

    * src/getargs.c (usage): Mention GLR not just LALR(1) in --help output. Use Akim's wording from <http://lists.gnu.org/archive/html/bison-patches/2006-05/msg00056.html>.

--- src/getargs.c
+++ src/getargs.c
@@ -199,7 +199,7 @@ usage (int status)
       /* Some efforts were made to ease the translators' task, please
 	 continue.  */
       fputs (_("\
-GNU bison generates parsers for LALR(1) grammars.\n"), stdout);
+GNU bison generates LALR(1) and GLR parsers.\n"), stdout);
       putc ('\n', stdout);
 
       fprintf (stdout, _("\
