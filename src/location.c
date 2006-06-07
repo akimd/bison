0c8e079fa62ba9bbae1b6766e1082b482d7dab07
Joel E. Denny <jdenny@ces.clemson.edu>
2006-06-07 20:06:27 +0000

    Get Bison to build again when configured with --enable-gcc-warnings. * src/location.c, src/location.h, src/main.c, src/scan-code.l: Add some missing #include's. * src/scan-code.l (handle_action_dollar, handle_action_at): Rename loc argument as it shadows a global. * src/scan-gram.l: Remove stray comma that prevents boundary_set invocation.
    * src/.cvsignore: Add scan-code.c.

--- src/location.c
+++ src/location.c
@@ -21,8 +21,10 @@
 #include <config.h>
 #include "system.h"
 
+#include <mbswidth.h>
 #include <quotearg.h>
 
+#include "complain.h"
 #include "location.h"
 
 location const empty_location;
--- src/main.c
+++ src/main.c
@@ -43,6 +43,7 @@
 #include "print_graph.h"
 #include "reader.h"
 #include "reduce.h"
+#include "scan-gram.h"
 #include "symtab.h"
 #include "tables.h"
 #include "uniqstr.h"
