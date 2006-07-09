80ac75bcb69c3396f1a69bf9e68c48dfd041b42e
Paul Eggert <eggert@cs.ucla.edu>
2006-07-09 19:48:45 +0000

    (flags_argmatch): Return void, not int, to pacify ./configure --enable-gcc-warnings.

--- src/getargs.c
+++ src/getargs.c
@@ -81,8 +81,8 @@ extern char *program_name;
  *
  *  The special value 0 resets the flags to 0.
  */
-static int
-flags_argmatch (const char *option, 
+static void
+flags_argmatch (const char *option,
 		const char * const keys[], const int values[],
 		int *flags, char *args)
 {
@@ -103,7 +103,7 @@ flags_argmatch (const char *option,
     *flags = ~0;
 }
 
-/** Decode a set of sub arguments. 
+/** Decode a set of sub arguments.
  *
  *  \param FlagName  the flag familly to update.
  *  \param args      the effective sub arguments to decode.
