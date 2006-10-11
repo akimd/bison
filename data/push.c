f317f99734750ac092b7622ce1e1084f31fa3a5a
Paul Eggert <eggert@cs.ucla.edu>
2006-10-11 19:13:48 +0000

    Fix missing bracket.

--- data/push.c
+++ data/push.c
@@ -1391,7 +1391,7 @@ yybackup:
 	pv->yyval = yyval;
 	]b4_locations_if([pv->yyloc = yyloc;])[
 	goto yypushreturn;
-gottoken:])
+gottoken:])[
       YYDPRINTF ((stderr, "Reading a token: "));
       ]b4_push_if([], [yychar = YYLEX;])[
     }
