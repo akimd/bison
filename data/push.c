31283fc38a9168a3d191f1db5ff3a76bc3c4ccbf
Joel E. Denny <jdenny@ces.clemson.edu>
2006-10-22 00:28:16 +0000

    * data/push.c, data/yacc.c: Make sure there's a newline at the end of the parser header file so that gcc doesn't warn.

--- data/push.c
+++ data/push.c
@@ -1754,5 +1754,5 @@ enum { YYPUSH_MORE = 4 };])[
 
 m4_ifdef([b4_provides],
 [[/* Copy the %provides blocks.  */
-]b4_user_provides])[]dnl
+]b4_user_provides])[]
 ])dnl b4_defines_if
--- data/yacc.c
+++ data/yacc.c
@@ -1531,5 +1531,5 @@ typedef struct YYLTYPE
 
 m4_ifdef([b4_provides],
 [[/* Copy the %provides blocks.  */
-]b4_user_provides])[]dnl
+]b4_user_provides])[]
 ])dnl b4_defines_if
