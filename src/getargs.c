ba7560e26bfa2f5a340545830d46e0437fcb701a
Joel E. Denny <jdenny@ces.clemson.edu>
2006-10-31 22:24:57 +0000

    * src/getargs.c (flags_argmatch): Don't cause segmentation fault for empty subargument list.  For example: `bison  --warnings=  parser.y'.

--- src/getargs.c
+++ src/getargs.c
@@ -91,15 +91,15 @@ flags_argmatch (const char *option,
   if (args)
     {
       args = strtok (args, ",");
-      do
+      while (args)
 	{
 	  int value = XARGMATCH (option, args, keys, values);
 	  if (value == 0)
 	    *flags = 0;
 	  else
 	    *flags |= value;
+          args = strtok (NULL, ",");
 	}
-      while ((args = strtok (NULL, ",")));
     }
   else
     *flags = ~0;
