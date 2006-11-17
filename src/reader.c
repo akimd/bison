26b8a43805a6cfebeea1b74d1263e978eb837ec2
Joel E. Denny <jdenny@ces.clemson.edu>
2006-11-17 20:07:08 +0000

    Don't let %prec take a nonterminal. * src/reader.c (grammar_current_rule_prec_set): Make the %prec symbol a token. * tests/input.at (%prec takes a token): New test checking that %prec won't take a nonterminal.

--- src/reader.c
+++ src/reader.c
@@ -387,6 +387,7 @@ grammar_midrule_action (void)
 void
 grammar_current_rule_prec_set (symbol *precsym, location loc)
 {
+  symbol_class_set (precsym, token_sym, loc, false);
   if (current_rule->ruleprec)
     complain_at (loc, _("only one %s allowed per rule"), "%prec");
   current_rule->ruleprec = precsym;
