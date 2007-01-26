665f0c2454e4d935a2bb1f869f917a7e58003d28
Paul Eggert <eggert@cs.ucla.edu>
2007-01-26 23:11:59 +0000

    Adjust to recent gnulib changes. * lib/.cvsignore: Remove stpcpy.h, strndup.h, strnlen.h. Add string.h, string_.h, unistd_.h, wchar_.h. * m4/.cvsignore: Add gnulib-common.m4, string_h.m4, wchar.m4. * src/system.h: Don't include <stpcpy.h>; this is now done by <string.h>.

--- src/system.h
+++ src/system.h
@@ -1,6 +1,6 @@
 /* System-dependent definitions for Bison.
 
-   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006 Free
+   Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007 Free
    Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
@@ -59,7 +59,6 @@ typedef size_t uintptr_t;
 | Gnulib.  |
 `---------*/
 
-#include <stpcpy.h>
 #include <unlocked-io.h>
 #include <verify.h>
 #include <xalloc.h>
