2c2b72205a56abe1d4a9be3633fc1529188b35bc
Paul Eggert <eggert@cs.ucla.edu>
2007-02-04 07:19:10 +0000

    * .cvsignore: Add javacomp.sh, javaexec.sh.  Is this really the right spot for these files? * bootstrap.conf (gnulib_modules): Add c-strcase. * lib/.cvsignore: Add c-ctype.c c-ctype.h, c-strcasecomp.c, c-strncasecmp.c. * src/getargs.c: Include c-strcase.h. (language_argmatch): Use c_strcasecmp rather than strcasecmp, to avoid unspecified behavior.

--- src/getargs.c
+++ src/getargs.c
@@ -25,6 +25,7 @@
 #include "revision.h"
 
 #include <argmatch.h>
+#include <c-strcase.h>
 #include <configmake.h>
 #include <error.h>
 
@@ -366,7 +367,7 @@ language_argmatch (char const *arg, int prio, location const *loc)
     {
       int i;
       for (i = 0; valid_languages[i].language[0]; i++)
-	if (strcasecmp (arg, valid_languages[i].language) == 0)
+	if (c_strcasecmp (arg, valid_languages[i].language) == 0)
 	  {
 	    language_prio = prio;
 	    language = &valid_languages[i];
