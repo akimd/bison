1cfe1ed7c8c41cf0a1c8a3d5c8b22da2559759a2
Paul Eggert <eggert@cs.ucla.edu>
2007-02-07 20:37:29 +0000

    Undo most of the 2007-02-03 change, switching to the strcase module now that gnulib strcase has been fixed. * bootstrap.conf (gnulib_modules): Remove c-strcase.  Add strcase. * lib/.cvsignore: Remove c-ctype.c, c-ctype.h, c-strcase.h, c-strcasecomp.c, c-strncasecmp.c.  Add strcasecmp.c, strncasecmp.c * m4/.cvsignore: Add strcase.m4. * src/getargs.c: Revert 2007-02-03 change, as follows. Don't include c-strcase.h. (language_argmatch): Use strcasecmp rather than c_strcasecmp. strcasecmp has "unspecified behavior" outside the POSIX locale, but it works fine in practice if at least one argument is ASCII, as is the case in Bison.

--- src/getargs.c
+++ src/getargs.c
@@ -25,7 +25,6 @@
 #include "revision.h"
 
 #include <argmatch.h>
-#include <c-strcase.h>
 #include <configmake.h>
 #include <error.h>
 
@@ -367,7 +366,7 @@ language_argmatch (char const *arg, int prio, location const *loc)
     {
       int i;
       for (i = 0; valid_languages[i].language[0]; i++)
-	if (c_strcasecmp (arg, valid_languages[i].language) == 0)
+	if (strcasecmp (arg, valid_languages[i].language) == 0)
 	  {
 	    language_prio = prio;
 	    language = &valid_languages[i];
