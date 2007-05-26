0c650a2027a0d664e1e4f40b3d34e4c1b410277b
Joel E. Denny <jdenny@ces.clemson.edu>
2007-05-26 19:10:43 +0000

    * data/glr.c (yylval): As in yacc.c, don't extern in the header for the case of pure parsers.  Reported by Frans Englich at <http://lists.gnu.org/archive/html/help-bison/2007-05/msg00018.html>. * THANKS: Add Frans Englich.

--- data/glr.c
+++ data/glr.c
@@ -2638,7 +2638,8 @@ b4_copyright([Skeleton interface for Bison GLR parsers in C],
 
 b4_shared_declarations
 
-extern YYSTYPE b4_prefix[]lval;
+b4_pure_if([],
+[[extern YYSTYPE b4_prefix][lval;]])
 
 b4_locations_if([b4_pure_if([],
 [extern YYLTYPE b4_prefix[]lloc;])
