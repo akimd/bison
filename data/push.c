eb1b07409f0ccad9970c8a60ded9f2a56d205b9c
Joel E. Denny <jdenny@ces.clemson.edu>
2007-07-28 04:27:32 +0000

    Fix push parsing memory leak reported by Brandon Lucia at <http://lists.gnu.org/archive/html/bug-bison/2007-07/msg00032.html>. * THANKS: Add Brandon Lucia. * data/push.c (yypstate_delete): Free the stack if it was reallocated but the parse never completed and thus freed it. * tests/Makefile.am (TESTSUITE_AT): Add push.at. * tests/testsuite.at: Include push.at. * test/push.at: New. (Push Parsing: Memory Leak for Early Deletion): New test case.

--- data/push.c
+++ data/push.c
@@ -1124,6 +1124,12 @@ b4_push_if(
 ]b4_c_function_def([[yypstate_delete]], [[void]],
                    [[[yypstate *yyps]], [[yyps]]])[
 {
+#ifndef yyoverflow
+  /* If the stack was reallocated but the parse did not complete, then the
+     stack still needs to be freed.  */
+  if (!yyps->yynew && yyps->yyss != yyps->yyssa)
+    YYSTACK_FREE (yyps->yyss);
+#endif
   free (yyps);
 }
 
