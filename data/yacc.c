a2ea208d82fdd67257d16e11b93ab17a8c48d452
Joel E. Denny <jdenny@ces.clemson.edu>
2007-08-03 04:51:23 +0000

    * data/yacc.c (yyexhaustedlab): Define it when YYERROR_VERBOSE is true since it's then always used regardless of whether yyoverflow is defined.  Reported by Christian Burger at <http://lists.gnu.org/archive/html/bug-bison/2007-07/msg00031.html>. * THANKS: Add Christian Burger.

--- data/yacc.c
+++ data/yacc.c
@@ -1448,7 +1448,7 @@ yyabortlab:
   yyresult = 1;
   goto yyreturn;
 
-#ifndef yyoverflow
+#if !defined(yyoverflow) || YYERROR_VERBOSE
 /*-------------------------------------------------.
 | yyexhaustedlab -- memory exhaustion comes here.  |
 `-------------------------------------------------*/
