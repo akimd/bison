a4f75309f2e467a701ebd8bfb0faf79c011226a6
Paul Eggert <eggert@cs.ucla.edu>
2007-09-24 06:34:44 +0000

    * src/conflicts.c (log_resolution): Fix indenting bugs I introduced. Problem reported by Wojciech Polak. * src/print-xml.c (xml_puts): Work even if LEVEL exceeds INT_MAX/2. (xml_printf): Undo change I made on 21 September; that is, indent 2 spaces, not 1.

--- src/conflicts.c
+++ src/conflicts.c
@@ -145,7 +145,7 @@ log_resolution (rule *r, symbol_number token,
 	    case shift_resolution:
 	    case right_resolution:
 	      obstack_fgrow2 (&solved_conflicts_xml_obstack,
-			      "<resolution rule=\"%d\" symbol=\"%s\""
+			      "        <resolution rule=\"%d\" symbol=\"%s\""
 			      " type=\"shift\">",
 			      r->number,
 			      xml_escape (symbols[token]->tag));
@@ -154,7 +154,7 @@ log_resolution (rule *r, symbol_number token,
 	    case reduce_resolution:
 	    case left_resolution:
 	      obstack_fgrow2 (&solved_conflicts_xml_obstack,
-			      "<resolution rule=\"%d\" symbol=\"%s\""
+			      "        <resolution rule=\"%d\" symbol=\"%s\""
 			      " type=\"reduce\">",
 			      r->number,
 			      xml_escape (symbols[token]->tag));
@@ -162,7 +162,7 @@ log_resolution (rule *r, symbol_number token,
 
 	    case nonassoc_resolution:
 	      obstack_fgrow2 (&solved_conflicts_xml_obstack,
-			      "<resolution rule=\"%d\" symbol=\"%s\""
+			      "        <resolution rule=\"%d\" symbol=\"%s\""
 			      " type=\"error\">",
 			      r->number,
 			      xml_escape (symbols[token]->tag));
--- src/print-xml.c
+++ src/print-xml.c
@@ -494,9 +494,8 @@ void
 xml_puts (FILE *out, int level, char const *s)
 {
   int i;
-  level *= 2;
   for (i = 0; i < level; i++)
-    fputc (' ', out);
+    fputs ("  ", out);
   fputs (s, out);
   fputc ('\n', out);
 }
@@ -508,7 +507,7 @@ xml_printf (FILE *out, int level, char const *fmt, ...)
   va_list arglist;
 
   for (i = 0; i < level; i++)
-    fputc (' ', out);
+    fputs ("  ", out);
 
   va_start (arglist, fmt);
   vfprintf (out, fmt, arglist);
