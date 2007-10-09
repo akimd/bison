f506ad1cbf909e2ebce3c84f6e057c7231191897
Joel E. Denny <jdenny@ces.clemson.edu>
2007-10-09 08:57:14 +0000

    * src/getargs.c (version): Update copyright year.

--- src/getargs.c
+++ src/getargs.c
@@ -323,7 +323,7 @@ version (void)
   putc ('\n', stdout);
 
   fprintf (stdout,
-	   _("Copyright (C) %d Free Software Foundation, Inc.\n"), 2006);
+	   _("Copyright (C) %d Free Software Foundation, Inc.\n"), 2007);
 
   fputs (_("\
 This is free software; see the source for copying conditions.  There is NO\n\
