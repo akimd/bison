cd4a9a598f4ae2cbaf4503b40d7f7f1b3931fd53
Wojciech Polak <polak@gnu.org>
2007-10-17 18:11:03 +0000

    src/print-xml.c (print_grammar): Renamed <terminal> and <nonterminal> attributes: "type" to "number" and "symbol" to "name".

--- src/print-xml.c
+++ src/print-xml.c
@@ -429,7 +429,7 @@ print_grammar (FILE *out, int level)
 	item_number *rhsp;
 
 	xml_printf (out, level + 2,
-		    "<terminal type=\"%d\" symbol=\"%s\">",
+		    "<terminal number=\"%d\" name=\"%s\">",
 		    i, xml_escape (tag));
 
 	for (r = 0; r < nrules; r++)
@@ -465,7 +465,7 @@ print_grammar (FILE *out, int level)
 	}
 
       xml_printf (out, level + 2,
-		  "<nonterminal type=\"%d\" symbol=\"%s\">",
+		  "<nonterminal number=\"%d\" name=\"%s\">",
 		  i, xml_escape (tag));
 
       if (left_count > 0)
