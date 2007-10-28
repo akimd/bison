c373bf8bb8da68ed12b1e73aaa2d777f6c701e46
Joel E. Denny <jdenny@ces.clemson.edu>
2007-10-28 20:11:35 +0000

    %define push_pull -> %define api.push_pull.  Discussed starting at <http://lists.gnu.org/archive/html/bison-patches/2007-09/msg00005.html>. * data/push.c: Expect the new name. * data/yacc.c: Likewise. * doc/bison.texinfo (Push Decl): Update. (Decl Summary): Update %define entry. (Push Parser Function): Update. (Pull Parser Function): Update. (Parser Create Function): Update. (Parser Delete Function): Update. * tests/calc.at (Simple LALR Calculator): Update. * tests/input.at (%define enum variables): Update. * tests/push.at (Push Parsing: Memory Leak for Early Deletion): Update. (Push Parsing: Multiple impure instances): Update. (Push Parsing: Unsupported Skeletons): Update. * tests/torture.at (Exploding the Stack Size with Alloca): Update. (Exploding the Stack Size with Malloc): Update.
    * NEWS (2.3a+): Add an entry for the push parser, and clean up the
    other entries some.

--- data/push.c
+++ data/push.c
@@ -18,12 +18,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Check the value of %define push_pull.
-b4_percent_define_default([[push_pull]], [[pull]])
-b4_percent_define_check_values([[[[push_pull]], [[pull]], [[push]], [[both]]]])
+# Check the value of %define api.push_pull.
+b4_percent_define_default([[api.push_pull]], [[pull]])
+b4_percent_define_check_values([[[[api.push_pull]],
+                               [[pull]], [[push]], [[both]]]])
 b4_define_flag_if([pull]) m4_define([b4_pull_flag], [[1]])
 b4_define_flag_if([push]) m4_define([b4_push_flag], [[1]])
-m4_case(b4_percent_define_get([[push_pull]]),
+m4_case(b4_percent_define_get([[api.push_pull]]),
         [pull], [m4_define([b4_push_flag], [[0]])],
         [push], [m4_define([b4_pull_flag], [[0]])])
 
--- data/yacc.c
+++ data/yacc.c
@@ -19,7 +19,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # This hack will go away when we mv push.c yacc.c.
-m4_ifndef([b4_percent_define(push_pull)],
+m4_ifndef([b4_percent_define(api.push_pull)],
 [m4_if(b4_use_push_for_pull_flag, [0], [
 
 m4_include(b4_pkgdatadir/[c.m4])
