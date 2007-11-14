a005a9c4dc5588fc017a1cb1d19c1560e0e68943
Akim Demaille <akim@epita.fr>
2007-11-14 13:18:44 +0000

    * doc/yacc.1.in: New. * configure.ac, doc/Makefile.am: Adjust. * configure.ac (PACKAGE_COPYRIGHT_YEAR): New substitution, and new config.h symbol. Use AC_SUBST for assignments too. * src/getargs.c (version): Use PACKAGE_COPYRIGHT_YEAR.

--- src/getargs.c
+++ src/getargs.c
@@ -334,7 +334,8 @@ version (void)
   putc ('\n', stdout);
 
   fprintf (stdout,
-	   _("Copyright (C) %d Free Software Foundation, Inc.\n"), 2007);
+	   _("Copyright (C) %d Free Software Foundation, Inc.\n"),
+	   PACKAGE_COPYRIGHT_YEAR);
 
   fputs (_("\
 This is free software; see the source for copying conditions.  There is NO\n\
