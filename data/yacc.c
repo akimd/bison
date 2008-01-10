5d1cfef46d61877d29ee9d638317f4ca3c3915c9
Paul Eggert <eggert@cs.ucla.edu>
2008-01-10 00:58:14 +0000

    * data/yacc.c (yyparse): Correct the comment when locations aren't used.
    Problem reported by Claudio Saavedra in
    <http://lists.gnu.org/archive/html/bug-bison/2008-01/msg00003.html>.

--- data/yacc.c
+++ data/yacc.c
@@ -1018,10 +1018,10 @@ b4_c_function_def([yyparse], [int], b4_parse_param)
   YYSIZE_T yymsg_alloc = sizeof yymsgbuf;
 #endif
 
-  /* Three stacks and their tools:
-     `yyss': related to states,
-     `yyvs': related to semantic values,
-     `yyls': related to locations.
+  /* The stacks and their tools:
+     `yyss' is related to states.
+     `yyvs' is related to semantic values.]b4_locations_if([[
+     `yyls' is related to locations.]])[
 
      Refer to the stacks thru separate pointers, to allow yyoverflow
      to reallocate them elsewhere.  */
