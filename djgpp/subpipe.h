691972493bfba4fc784549dfd0c4c6fb41af2dc7
Juan Manuel Guerrero <juan.guerrero@gmx.de>
2008-02-06 09:59:13 +0000

    * subpipe.h: Copyright timestamp adjusted.

--- djgpp/subpipe.h
+++ djgpp/subpipe.h
@@ -1,5 +1,5 @@
 /* Subprocesses with pipes.
-   Copyright (C) 2005 Free Software Foundation, Inc.
+   Copyright (C) 2005, 2006, 2007, 2008 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
