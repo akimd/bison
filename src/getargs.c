59da312bc7a98d2cea04dceb53fc9e7b6c9c1ee3
Joel E. Denny <jdenny@ces.clemson.edu>
2008-02-23 04:26:14 +0000

    * NEWS (2.3a+): Don't say %language is experimental.  Mention Java and say its interface is experimental. * doc/bison.texinfo (Decl Summary): In the %language entry, mention Java. (Bison Options): In the -L and --language entry, mention Java. (Java Bison Interface): Say the interface is experimental. * src/getargs.c (usage): Mention -L and --language.
    * NEWS (2.3a+): Say the push parsing interface is experimental.
    * doc/bison.texinfo (Push Decl): Likewise.
    (Decl Summary): Likewise in the "%define api.push_pull" entry.
    (Push Parser Function): Likewise.
    (Pull Parser Function): Likewise.
    (Parser Create Function): Likewise.
    (Parser Delete Function): Likewise.
    (Table of Symbols): Likewise in the yypstate_delete, yypstate_new,
    yypull_parse, and yypush_parse entries.
    
    * NEWS (2.3a+): Mention XML support, and say the schema is
    experimental.
    * doc/bison.texinfo (Bison Options): Mention -x and --xml.
    * src/getargs.c (usage): Say the XML schema is experimental.
    
    * NEWS (2.3a+): Say option instead of flag.

--- src/getargs.c
+++ src/getargs.c
@@ -1,7 +1,7 @@
 /* Parse command line arguments for Bison.
 
    Copyright (C) 1984, 1986, 1989, 1992, 2000, 2001, 2002, 2003, 2004,
-   2005, 2006, 2007 Free Software Foundation, Inc.
+   2005, 2006, 2007, 2008 Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
 
@@ -269,6 +269,7 @@ Operation modes:\n\
 
       fputs (_("\
 Parser:\n\
+  -L, --language=LANGUAGE    specify the output programming language\n\
   -S, --skeleton=FILE        specify the skeleton to use\n\
   -t, --debug                instrument the parser for debugging\n\
       --locations            enable locations computation\n\
@@ -288,6 +289,7 @@ Output:\n\
   -o, --output=FILE          leave output to FILE\n\
   -g, --graph[=FILE]         also output a graph of the automaton\n\
   -x, --xml[=FILE]           also output an XML report of the automaton\n\
+                             (the XML schema is experimental)\n\
 \n\
 "), stdout);
 
