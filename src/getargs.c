8fa369111fcc3d06a0c9fc278f063b8033dac094
Joel E. Denny <jdenny@ces.clemson.edu>
2008-07-18 11:26:15 -0400

    Use git-version-gen for version names between releases.
    * .cvsignore (.tarball-version, GNUmakefile, *~): Add.
    * .gitignore (/.tarball-version, /GNUmakefile, /*~): Add.
    * .prev-version: New.
    * .version.in: Remove.
    * ChangeLog: Remove the $Id$ previously used for capturing the CVS
    revision.
    * GNUmakefile: Remove, now copied from Gnulib.
    * Makefile.am: Add code suggested by comments in
    build-aux/git-version-gen.
    (EXTRA_DIST): Remove GNUmakefile, handled by Gnulib.  Add maint.mk,
    .prev-version, and .version.
    * NEWS (2.3b+): Rename to...
    (?.?): ... this because we're dropping the "+" version naming scheme,
    but, in general, we still can't be sure of our next release name.
    * bootstrap: Add a quick hack to remove from .gitignore the
    GNUmakefile entry that gnulib adds.  We already have a /GNUmakefile
    entry.  This should really be fixed in gnulib instead.
    * bootstrap.conf (gnulib_modules): Add gnumakefile.
    * configure.ac (AC_INIT): Set version name by invoking
    build-aux/git-version-gen.
    (AC_CONFIG_FILES): Remove .version, now generated by
    build-aux/git-version-gen.
    * maint.mk: New, copied from coreutils.
    * doc/.cvsignore (bison.1): Add.
    * doc/.gitignore (/bison.1): Add.
    * doc/bison.1: Remove, generated.
    * src/.cvsignore (revision.c): Remove.
    * src/.gitignore (/revision.c): Remove.
    * src/Makefile.am (bison_SOURCES): Remove revision.c and revision.h.
    (BUILT_SOURCES): Remove revision.c.
    (revision.c): Remove.
    * src/getargs.c (version): Don't print revision after the VERSION.
    * src/revision.h: Remove.

--- src/getargs.c
+++ src/getargs.c
@@ -20,7 +20,6 @@
 
 #include <config.h>
 #include "system.h"
-#include "revision.h"
 #include "output.h"
 
 #include <argmatch.h>
@@ -338,7 +337,6 @@ version (void)
      continue.  */
   printf (_("bison (GNU Bison) %s"), VERSION);
   putc ('\n', stdout);
-  printf ("%s", revision);
   fputs (_("Written by Robert Corbett and Richard Stallman.\n"), stdout);
   putc ('\n', stdout);
 
