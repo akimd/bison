9b9e0a7d46123c50e51f6742eba1bfa0d091b8aa
Akim Demaille <demaille@gostai.com>
2008-07-29 12:44:28 +0200

    Fix --help.
    	* src/getargs.c (usage): Fix help string for -W.

--- src/getargs.c
+++ src/getargs.c
@@ -265,7 +265,7 @@ Operation modes:\n\
       --print-localedir      output directory containing locale-dependent data\n\
       --print-datadir        output directory containing skeletons and XSLT\n\
   -y, --yacc                 emulate POSIX Yacc\n\
-  -W, --warnings=[CATEGORY]  report the warnings falling in CATEGORY\n\
+  -W, --warnings[=CATEGORY]  report the warnings falling in CATEGORY\n\
 \n\
 "), stdout);
 
