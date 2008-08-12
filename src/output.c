4175f6bba2da4050e268523d396ee446dc4d7655
Akim Demaille <demaille@gostai.com>
2008-08-12 21:11:53 +0200

    muscles_output.
    	* src/output.c (muscles_output): New, extracted from...
    	(output_skeleton): here.
    	Adjust.

--- src/output.c
+++ src/output.c
@@ -485,6 +485,26 @@ prepare_actions (void)
   muscle_insert_unsigned_int_table ("conflicting_rules", conflict_list,
 				    0, 1, conflict_list_cnt);
 }
+
+
+/*--------------------------------------------.
+| Output the definitions of all the muscles.  |
+`--------------------------------------------*/
+
+static void
+muscles_output (FILE *out)
+{
+  fputs ("m4_init()\n", out);
+
+  type_names_output (out);
+  user_actions_output (out);
+  merger_output (out);
+  token_definitions_output (out);
+  symbol_code_props_output (out, "destructors", &symbol_destructor_get);
+  symbol_code_props_output (out, "printers", &symbol_printer_get);
+
+  muscles_m4_output (out);
+}
 
 /*---------------------------.
 | Call the skeleton parser.  |
@@ -586,18 +606,7 @@ output_skeleton (void)
   if (! out)
     error (EXIT_FAILURE, get_errno (),
 	   "fdopen");
-
-  /* Output the definitions of all the muscles.  */
-  fputs ("m4_init()\n", out);
-
-  type_names_output (out);
-  user_actions_output (out);
-  merger_output (out);
-  token_definitions_output (out);
-  symbol_code_props_output (out, "destructors", &symbol_destructor_get);
-  symbol_code_props_output (out, "printers", &symbol_printer_get);
-
-  muscles_m4_output (out);
+  muscles_output (out);
   xfclose (out);
 
   /* Read and process m4's output.  */
