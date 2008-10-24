4b4da9fb83f9e77bae0f507938db76e523032de5
Akim Demaille <demaille@gostai.com>
2008-10-23 20:02:52 -0500

    Remove duplicate header inclusion.
    	* src/LR0.c: here.

--- src/LR0.c
+++ src/LR0.c
@@ -1,6 +1,6 @@
 /* Generate the nondeterministic finite state machine for Bison.
 
-   Copyright (C) 1984, 1986, 1989, 2000, 2001, 2002, 2004, 2005, 2006, 2007
+   Copyright (C) 1984, 1986, 1989, 2000, 2001, 2002, 2004, 2005, 2006, 2007, 2008
    Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
@@ -32,7 +32,6 @@
 #include "closure.h"
 #include "complain.h"
 #include "getargs.h"
-#include "gram.h"
 #include "gram.h"
 #include "lalr.h"
 #include "reader.h"
