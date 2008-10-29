c0ee9e21157b86eeb151debcc1b8f4b1ca7e9eb8
Di-an Jan <dianj@freeshell.org>
2008-10-28 21:38:32 -0400

    Don't overwrite the input file.
    * src/files.c (output_file_name_check): Fatal error if using input file
    for output.
    * tests/output.at: (AT_CHECK_CONFLICTING_OUTPUT): Add return status
    argument.
    (Conflicting output files): Add test.

--- src/files.c
+++ src/files.c
@@ -351,6 +351,8 @@ compute_output_file_names (void)
 void
 output_file_name_check (char const *file_name)
 {
+  if (0 == strcmp (file_name, grammar_file))
+    fatal (_("refusing to overwrite the input file %s"), quote (file_name));
   {
     int i;
     for (i = 0; i < file_names_count; i++)
