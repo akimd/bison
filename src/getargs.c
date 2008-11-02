ed4d67dcbe00ca687a6de988aada4ab90a6e03fa
Joel E. Denny <jdenny@ces.clemson.edu>
2008-11-02 16:54:45 -0500

    Say %language is experimental.
    We're thinking of extending it's effect on output file naming.  See the
    thread at
    <http://lists.gnu.org/archive/html/bison-patches/2008-10/msg00003.html>.
    * NEWS: Say it's experimental.
    * doc/bison.texinfo (Decl Summary): Say it's experimental, and so don't
    recommend it over %skeleton for now.
    (Bison Options): Likewise.
    (C++ Bison Interface): Use %skeleton not %language.
    (Calc++ Parser): Use %skeleton not %language.
    * src/getargs.c (usage): Say it's experimental.

--- src/getargs.c
+++ src/getargs.c
@@ -272,6 +272,7 @@ Operation modes:\n\
       fputs (_("\
 Parser:\n\
   -L, --language=LANGUAGE    specify the output programming language\n\
+                             (this is an experimental feature)\n\
   -S, --skeleton=FILE        specify the skeleton to use\n\
   -t, --debug                instrument the parser for debugging\n\
       --locations            enable locations computation\n\
