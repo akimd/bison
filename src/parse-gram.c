086fd1137de9b07647ef85ad1bbd5c2af661a462
Akim Demaille <demaille@gostai.com>
2008-11-11 16:41:28 +0100

    Regen.
    	* src/parse-gram.h, src/parse-gram.c: Regen.

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -1,4 +1,4 @@
-/* A Bison parser, made by GNU Bison 2.4.79-c85be.  */
+/* A Bison parser, made by GNU Bison 2.4.149-dcb5-dirty.  */
 
 /* Implementation for Bison's Yacc-like parsers in C
 
@@ -45,7 +45,7 @@
 #define YYBISON 1
 
 /* Bison version.  */
-#define YYBISON_VERSION "2.4.79-c85be"
+#define YYBISON_VERSION "2.4.149-dcb5-dirty"
 
 /* Skeleton name.  */
 #define YYSKELETON_NAME "yacc.c"
@@ -565,7 +565,7 @@ union yyalloc
 #define YYNNTS  33
 /* YYNRULES -- Number of rules.  */
 #define YYNRULES  106
-/* YYNRULES -- Number of states.  */
+/* YYNSTATES -- Number of states.  */
 #define YYNSTATES  144
 
 /* YYTRANSLATE(YYLEX) -- Bison symbol number corresponding to YYLEX.  */
@@ -3238,3 +3238,4 @@ char_name (char c)
       return quotearg_style (escape_quoting_style, buf);
     }
 }
+
