a8beef7e6a9f6b75fa249d59b4c79585190540b6
Joel E. Denny <jdenny@ces.clemson.edu>
2008-11-17 10:36:28 -0500

    Fix last warning from --enable-gcc-warnings.
    * src/getargs.c (getargs): Don't assign const address to non-const
    pointer.

--- src/getargs.c
+++ src/getargs.c
@@ -529,11 +529,14 @@ getargs (int argc, char *argv[])
         {
           char* name = optarg;
           char* value = strchr (optarg, '=');
+          char const * muscle_value = "";
           if (value)
-            *value++ = 0;
-          else
-            value = "";
-          muscle_percent_define_insert (name, command_line_location (), value);
+            {
+              *value++ = 0;
+              muscle_value = value;
+            }
+          muscle_percent_define_insert (name, command_line_location (),
+                                        muscle_value);
         }
 	break;
 
