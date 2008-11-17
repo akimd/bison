c4eb1e841e579e79e861007994e3902bb6cf5fb2
Joel E. Denny <jdenny@ces.clemson.edu>
2008-11-17 10:51:17 -0500

    Simplify last patch slightly.
    * src/getargs.c (getargs): Here.
    (cherry picked from commit 9ce405ce1dd2ff7d495dafe6201b87669549f98b)

--- src/getargs.c
+++ src/getargs.c
@@ -527,14 +527,10 @@ getargs (int argc, char *argv[])
         {
           char* name = optarg;
           char* value = strchr (optarg, '=');
-          char const * muscle_value = "";
           if (value)
-            {
-              *value++ = 0;
-              muscle_value = value;
-            }
+            *value++ = 0;
           muscle_percent_define_insert (name, command_line_location (),
-                                        muscle_value);
+                                        value ? value : "");
         }
 	break;
 
