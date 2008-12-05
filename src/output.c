d333175f63f402dbadb647175e40ad88bf1defb5
Eric Blake <ebb9@byu.net>
2008-12-05 16:16:15 -0700

    Avoid compiler warning.
    * src/output.c (muscle_insert_item_number_table): Delete unused
    function.
    
    Signed-off-by: Eric Blake <ebb9@byu.net>

--- src/output.c
+++ src/output.c
@@ -104,7 +104,6 @@ GENERATE_MUSCLE_INSERT_TABLE(muscle_insert_int_table, int)
 GENERATE_MUSCLE_INSERT_TABLE(muscle_insert_base_table, base_number)
 GENERATE_MUSCLE_INSERT_TABLE(muscle_insert_rule_number_table, rule_number)
 GENERATE_MUSCLE_INSERT_TABLE(muscle_insert_symbol_number_table, symbol_number)
-GENERATE_MUSCLE_INSERT_TABLE(muscle_insert_item_number_table, item_number)
 GENERATE_MUSCLE_INSERT_TABLE(muscle_insert_state_number_table, state_number)
 
 
