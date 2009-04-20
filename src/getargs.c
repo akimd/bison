44c124a3896b149561b3b9cee411b4a479ab04d7
Akim Demaille <demaille@gostai.com>
2009-04-20 12:53:46 +0200

    Consistently refer to Yacc, not YACC.
    	* src/getargs.c (usage, warnings_args): s/YACC/Yacc/.

--- src/getargs.c
+++ src/getargs.c
@@ -216,7 +216,7 @@ static const char * const warnings_args[] =
      that argmatch_valid be more readable.  */
   "none            - no warnings",
   "midrule-values  - unset or unused midrule values",
-  "yacc            - incompatibilities with POSIX YACC",
+  "yacc            - incompatibilities with POSIX Yacc",
   "all             - all of the above",
   "error           - warnings are errors",
   0
@@ -312,7 +312,7 @@ Output:\n\
       fputs (_("\
 Warning categories include:\n\
   `midrule-values'  unset or unused midrule values\n\
-  `yacc'            incompatibilities with POSIX YACC\n\
+  `yacc'            incompatibilities with POSIX Yacc\n\
   `all'             all the warnings\n\
   `no-CATEGORY'     turn off warnings in CATEGORY\n\
   `none'            turn off all the warnings\n\
