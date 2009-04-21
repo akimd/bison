eb45ef3ba6fff47b9d037da56170f795766c9423
Joel E. Denny <jdenny@ces.clemson.edu>
2009-04-21 05:12:01 -0400

    Document %define lr.type and lr.default_rules.
    * NEWS (2.5): Add an entry.
    * src/getargs.c (usage): Mention IELR(1) and canonical LR(1)
    besides just LALR(1) and GLR(1).
    * doc/bison.texinfo (Introduction): Likewise.
    (Language and Grammar): Bison is no longer limited to LALR(1)
    restrictions.
    (GLR parsing): Say deterministic or LR(1) rather than LALR(1)
    when trying to distinguish from GLR.  Talk about LR(1) grammars
    rather than LALR(1) grammars.
    (Decl Summary): In %define api.push_pull entry, say it applies
    to deterministic parsers in C rather than LALR(1) parsers in C.
    Add lr.default_rules entry.
    Add lr.type entry.
    (Mystery Conflicts): Bison is no longer limited to LALR(1)
    restrictions.
    (Generalized LR Parsing): Same changes as for the previous GLR
    section.
    (Memory Management): Say deterministic rather than LALR(1).
    (Understanding): Correct some bison output.
    Index discussion of "accepting state".
    Say deterministic rather than LALR(1).
    (Bison Options): In --yacc entry, say deterministic rather than
    LALR(1).
    In --report, --graph, and --xml entries, just don't mention
    LALR(1).
    (C++ Parsers): Say deterministic rather than LALR(1).
    (Table of Symbols): Likewise in YYSTACK_USE_ALLOCA entry.
    (Glossary): Add Accepting State, Consistent State, Default Rule,
    and IELR(1) definitions.
    In Generalized LR (GLR) definition, make same changes as in
    previous GLR sections.
    In LALR(1) definition, say Bison uses LALR(1) by default rather
    than implying Bison is limited to LALR(1).
    (LocalWords): Add IELR.
    (cherry picked from commit 34a6c2d19b85d4650b2d90f60b76d44071d167d1)

--- src/getargs.c
+++ src/getargs.c
@@ -262,7 +262,8 @@ usage (int status)
 
       printf (_("Usage: %s [OPTION]... FILE\n"), program_name);
       fputs (_("\
-Generate LALR(1) and GLR parsers.\n\
+Generate a deterministic or GLR parser employing LALR(1), IELR(1), or\n\
+canonical LR(1) parser tables.\n\
 \n\
 "), stdout);
 
