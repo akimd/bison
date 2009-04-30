1d0f55cc843d6dfca01e2c13fd49ee2c119c6631
Joel E. Denny <jdenny@ces.clemson.edu>
2009-04-29 21:48:21 -0400

    Rename lr.default_reductions to lr.default-reductions.
    * NEWS (2.5): Here.
    * doc/bison.texinfo: Here.
    * src/lalr.c (initialize_LA): Here.
    * src/print.c (print_reductions): Here.
    * src/reader.c (reader): Here.
    * src/tables.c (action_row): Here.
    * tests/input.at (%define enum variables): Here.
    * tests/reduce.at (AT_TEST_LR_DEFAULT_REDUCTIONS): Here.

--- src/print.c
+++ src/print.c
@@ -335,7 +335,7 @@ print_reductions (FILE *out, state *s)
   if (default_reduction)
     {
       char *default_reductions =
-        muscle_percent_define_get ("lr.default_reductions");
+        muscle_percent_define_get ("lr.default-reductions");
       print_reduction (out, width, _("$default"), default_reduction, true);
       aver (0 == strcmp (default_reductions, "all")
             || (0 == strcmp (default_reductions, "consistent")
