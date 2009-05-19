9b04dad70ab2304d86175b002db6594ec653ae47
Akim Demaille <demaille@gostai.com>
2009-05-19 11:29:40 +0200

    remove useless variable.
    	* src/getargs.c (skeleton_arg): Remove now useless variable.
    	Should help the compiler see that this printf-like call is sane.

--- src/getargs.c
+++ src/getargs.c
@@ -385,11 +385,7 @@ skeleton_arg (char const *arg, int prio, location loc)
       skeleton = arg;
     }
   else if (prio == skeleton_prio)
-    {
-      char const *msg =
-	_("multiple skeleton declarations are invalid");
-      complain_at (loc, msg);
-    }
+    complain_at (loc, _("multiple skeleton declarations are invalid"));
 }
 
 void
