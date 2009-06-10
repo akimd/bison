94505fb7e2f6a043d83db4bcd0d64ca675bce71e
Akim Demaille <demaille@gostai.com>
2009-06-10 10:40:55 +0200

    gnulib: update.
    	* gnulib: Update to latest.
    	* lib/.cvsignore, lib/.gitignore, m4/.cvsignore,
    	* m4/.gitignore: Regen.
    	* src/symtab.c (symbol_from_uniqstr, semantic_type_from_uniqstr):
    	Call xalloc_die on hash_insert failures.
    	Requested by the new __warn_unused_result__ attribute of
    	hash_insert.

--- src/symtab.c
+++ src/symtab.c
@@ -672,7 +672,8 @@ symbol_from_uniqstr (const uniqstr key, location loc)
     {
       /* First insertion in the hash. */
       entry = symbol_new (key, loc);
-      hash_insert (symbol_table, entry);
+      if (!hash_insert (symbol_table, entry))
+        xalloc_die ();
     }
   return entry;
 }
@@ -696,7 +697,8 @@ semantic_type_from_uniqstr (const uniqstr key)
     {
       /* First insertion in the hash. */
       entry = semantic_type_new (key);
-      hash_insert (semantic_type_table, entry);
+      if (!hash_insert (semantic_type_table, entry))
+        xalloc_die ();
     }
   return entry;
 }
