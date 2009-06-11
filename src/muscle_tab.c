914e713d0787d4db80e29b1aa0a26e0fd5c929fa
Akim Demaille <demaille@gostai.com>
2009-06-11 14:42:12 +0200

    hash: check insertion for memory exhaustion.
    	* src/muscle-tab.c (muscle_insert, muscle_grow)
    	* src/state.c (state_hash_insert): Check the return value of
    	hash_insert.

--- src/muscle_tab.c
+++ src/muscle_tab.c
@@ -121,7 +121,8 @@ muscle_insert (char const *key, char const *value)
       /* First insertion in the hash. */
       entry = xmalloc (sizeof *entry);
       entry->key = key;
-      hash_insert (muscle_table, entry);
+      if (!hash_insert (muscle_table, entry))
+        xalloc_die ();
     }
   else
     free (entry->storage);
@@ -150,7 +151,8 @@ muscle_grow (const char *key, const char *val, const char *separator)
       /* First insertion in the hash. */
       entry = xmalloc (sizeof *entry);
       entry->key = key;
-      hash_insert (muscle_table, entry);
+      if (!hash_insert (muscle_table, entry))
+        xalloc_die ();
       entry->value = entry->storage = xstrdup (val);
     }
   else
--- src/state.c
+++ src/state.c
@@ -352,7 +352,8 @@ state_hash_free (void)
 void
 state_hash_insert (state *s)
 {
-  hash_insert (state_table, s);
+  if (!hash_insert (state_table, s))
+    xalloc_die ();
 }
 
 
