13cdf208ed0f643dd00262ef87fcc7b83dab27a6
Alex Rozenman <rozenman@gmail.com>
2009-07-24 21:04:16 +0300

    Fix some memory leaks. 	* src/named-ref.c: Add a pointer check (named_ref_free). 	* src/scan-code.l: New function (variant_table_free). Called in 	code_scanner_free. 	* src/symlist.c: Call to named_ref_free (symbol_list_free).

--- src/named-ref.c
+++ src/named-ref.c
@@ -36,6 +36,7 @@ named_ref_new (uniqstr id, location loc)
 void
 named_ref_free (named_ref *r)
 {
-  free (r);
+  if (r)
+    free (r);
 }
 
--- src/symlist.c
+++ src/symlist.c
@@ -146,7 +146,14 @@ symbol_list_prepend (symbol_list *list, symbol_list *node)
 void
 symbol_list_free (symbol_list *list)
 {
-  LIST_FREE (symbol_list, list);
+  symbol_list *node, *next;
+  for (node = list; node; node = next)
+    {
+      next = node->next;
+      if (node->named_ref)
+        named_ref_free (node->named_ref);
+      free (node);
+    }
 }
 
 
