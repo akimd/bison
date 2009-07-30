dbbb64f09180b8749e3a91d3559e13b933cd15c8
Joel E. Denny <jdenny@clemson.edu>
2009-07-30 17:12:10 -0400

    maint: automate annual package-wide copyright-year update.
    * .x-update-copyright: New.
    * Makefile.am (EXTRA_DIST): Remove maint.mk.
    * bootstrap.conf (gnulib_modules): Add maintainer-makefile and
    update-copyright.  Remove gnumakefile, which is implied by
    maintainer-makefile.
    * cfg.mk (bootstrap-tools): Copy from old maint.mk.
    * gnulib: Update.
    * maint.mk: Remove, now copied from gnulib.
    * examples/extexi: Add missing "(C)" in copyright statement so
    update-copyright can recognize it.
    * src/LR0.h: Likewise.
    * src/print.h: Likewise.
    * src/print_graph.h: Likewise.
    * src/named-ref.c: Likewise.
    * src/named-ref.h: Likewise.
    * src/gram.c: Add missing comma in copyright statement.
    * src/gram.h: Likewise.
    (cherry picked from commit 50bcb22c4cda30353f44930b0a29519609527408)
    
    Conflicts:
    
    	gnulib
    	src/gram.c

--- src/print.h
+++ src/print.h
@@ -1,5 +1,5 @@
 /* Print information on generated parser, for bison,
-   Copyright 2000 Free Software Foundation, Inc.
+   Copyright (C) 2000, 2009 Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
 
