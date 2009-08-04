ac2def5602c14b52869a761bc0309ab55c1cc869
Joel E. Denny <jdenny@clemson.edu>
2009-08-04 18:06:20 -0400

    maint: automate b4_copyright updates.
    * Makefile.am (update-b4-copyright): New target rule.
    * build-aux/Makefile.am (EXTRA_DIST): Add update-b4-copyright.
    * build-aux/update-b4-copyright: New.
    * data/yacc.c: Remove stray characters around b4_copyright
    invocations.
    (cherry picked from commit 269e222e24b03ccc4ab7881d960750ddeb131b05)
    
    Conflicts:
    
    	build-aux/local.mk
    	data/yacc.c

--- data/yacc.c
+++ data/yacc.c
@@ -154,7 +154,7 @@ m4_define([b4_rhs_location],
 m4_changecom()
 m4_divert_push(0)dnl
 @output(b4_parser_file_name@)@
-b4_copyright([Skeleton implementation for Bison's Yacc-like parsers in C],dnl '
+b4_copyright([Skeleton implementation for Bison's Yacc-like parsers in C],
   [1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
    2009])[
 
@@ -1676,7 +1676,7 @@ yypushreturn:
 ]b4_epilogue
 b4_defines_if(
 [@output(b4_spec_defines_file@)@
-b4_copyright([Skeleton interface for Bison's Yacc-like parsers in C],dnl '
+b4_copyright([Skeleton interface for Bison's Yacc-like parsers in C],
   [1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
    2009])
 
