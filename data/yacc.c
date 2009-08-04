c9e0a8807940d59bdd3920b178fea0eae246c885
Joel E. Denny <jdenny@clemson.edu>
2009-08-04 18:06:20 -0400

    maint: automate b4_copyright updates.
    * Makefile.am (update-b4-copyright): New target rule.
    * build-aux/Makefile.am (EXTRA_DIST): Add update-b4-copyright.
    * build-aux/update-b4-copyright: New.
    * data/yacc.c: Remove stray characters around b4_copyright
    invocations.
    (cherry picked from commit ac2def5602c14b52869a761bc0309ab55c1cc869)
    
    Conflicts:
    
    	data/yacc.c

--- data/yacc.c
+++ data/yacc.c
@@ -3,7 +3,7 @@
 # Yacc compatible skeleton for Bison
 
 # Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
-# 2007, 2008 Free Software Foundation, Inc.
+# 2007, 2008-2009 Free Software Foundation, Inc.
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -154,8 +154,8 @@ m4_define([b4_rhs_location],
 m4_changecom()
 m4_divert_push(0)dnl
 @output(b4_parser_file_name@)@
-b4_copyright([Skeleton implementation for Bison's Yacc-like parsers in C],dnl '
-  [1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006])[
+b4_copyright([Skeleton implementation for Bison's Yacc-like parsers in C],
+  [1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2009])[
 
 /* C LALR(1) parser skeleton written by Richard Stallman, by
    simplifying the original so-called "semantic" parser.  */
@@ -1675,8 +1675,8 @@ yypushreturn:
 ]b4_epilogue
 b4_defines_if(
 [@output(b4_spec_defines_file@)@
-b4_copyright([Skeleton interface for Bison's Yacc-like parsers in C],dnl '
-  [1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006])
+b4_copyright([Skeleton interface for Bison's Yacc-like parsers in C],
+  [1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2009])
 
 b4_percent_code_get([[requires]])[]dnl
 
