269e222e24b03ccc4ab7881d960750ddeb131b05
Joel E. Denny <jdenny@clemson.edu>
2009-08-04 18:06:20 -0400

    maint: automate b4_copyright updates.
    * Makefile.am (update-b4-copyright): New target rule.
    * build-aux/local.mk (EXTRA_DIST): Add update-b4-copyright.
    * build-aux/update-b4-copyright: New.
    * data/yacc.c: Remove stray characters around b4_copyright
    invocations.

--- build-aux/local.mk
+++ build-aux/local.mk
@@ -1,3 +1,4 @@
 EXTRA_DIST +=					\
+build-aux/update-b4-copyright			\
 build-aux/prev-version.txt			\
 build-aux/cross-options.pl
--- data/yacc.c
+++ data/yacc.c
@@ -156,8 +156,7 @@ m4_define([b4_rhs_location],
 m4_changecom()
 m4_divert_push(0)dnl
 @output(b4_parser_file_name@)@
-b4_copyright([Implementation for Bison's Yacc-like parsers in C])dnl'
-[
+b4_copyright([Implementation for Bison's Yacc-like parsers in C])[
 
 /* C LALR(1) parser skeleton written by Richard Stallman, by
    simplifying the original so-called "semantic" parser.  */
@@ -1599,7 +1598,7 @@ yypushreturn:
 ]b4_epilogue[]dnl
 b4_defines_if(
 [@output(b4_spec_defines_file@)@
-b4_copyright([Interface for Bison's Yacc-like parsers in C])dnl'
+b4_copyright([Interface for Bison's Yacc-like parsers in C])dnl
 
 b4_percent_code_get([[requires]])[]dnl
 
