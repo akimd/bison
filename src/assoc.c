38609c34559fb86f2fb827dfed929ba9c396dc6f
Joel E. Denny <jdenny@clemson.edu>
2009-08-06 19:15:28 -0400

    maint: run "make update-copyright"

--- src/assoc.c
+++ src/assoc.c
@@ -1,5 +1,5 @@
 /* Associativity information.
-   Copyright (C) 2002, 2005, 2006 Free Software Foundation, Inc.
+   Copyright (C) 2002, 2005, 2006, 2009 Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
 
--- src/nullable.h
+++ src/nullable.h
@@ -1,5 +1,5 @@
 /* Part of the bison parser generator,
-   Copyright (C) 2000, 2002 Free Software Foundation, Inc.
+   Copyright (C) 2000, 2002, 2009 Free Software Foundation, Inc.
 
    This file is part of Bison, the GNU Compiler Compiler.
 
