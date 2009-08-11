07c0db18b4a795a3a35a4d98b3d001bfc7d4e1c1
Joel E. Denny <jdenny@clemson.edu>
2009-08-10 21:43:07 -0400

    Miscellaneous code readability improvements.
    * src/reader.c (reader): Move %define front-end variable
    defaults and checking into...
    (prepare_percent_define_front_end_variables): ... this new
    function.
    
    * src/scan-gram.l (INITIAL): For consistency with string
    literals, don't store open quote on character literal.  It's
    discarded before returning anyway.
    (SC_ESCAPED_CHARACTER): Similarly, don't store close quote.
    Make length test more readable, and make the character stored
    for an empty literal more obvious while consistent with the
    previous behavior.
    
    * src/symtab.c, src/symtab.h: Rename USER_NUMBER_ALIAS to
    USER_NUMBER_HAS_STRING_ALIAS throughout.
    * src/symtab.c (symbol_make_alias): Remove comment from symtab.c
    that is repeated in symtab.h.  Improve argument names to make it
    clear which side of the symbol-string alias pair is which.
    (symbol_check_alias_consistency): Improve local variable names
    for the same purpose.
    * src/symtab.h (struct symbol): Make comments about aliases
    clearer.
    (symbol_make_alias): Improve comments and argument name.
    * src/output.c (token_definitions_output): Update for rename to
    USER_NUMBER_HAS_STRING_ALIAS and improve comments about aliases.
    (cherry picked from commit dfaa48602d4e6d329e3ae9063f44929490928b3d)
    
    Conflicts:
    
    	src/symtab.c
    	src/symtab.h

--- src/output.c
+++ src/output.c
@@ -343,10 +343,10 @@ token_definitions_output (FILE *out)
       symbol *sym = symbols[i];
       int number = sym->user_token_number;
 
-      /* At this stage, if there are literal aliases, they are part of
-	 SYMBOLS, so we should not find symbols which are the aliases
-	 here.  */
-      aver (number != USER_NUMBER_ALIAS);
+      /* At this stage, if there are literal string aliases, they are
+         part of SYMBOLS, so we should not find their aliased symbols
+         here.  */
+      aver (number != USER_NUMBER_HAS_STRING_ALIAS);
 
       /* Skip error token.  */
       if (sym == errtoken)
--- src/reader.c
+++ src/reader.c
@@ -36,6 +36,7 @@
 #include "scan-gram.h"
 #include "scan-code.h"
 
+static void prepare_percent_define_front_end_variables (void);
 static void check_and_convert_grammar (void);
 
 static symbol_list *grammar = NULL;
@@ -590,8 +591,18 @@ reader (void)
   gram_debug = trace_flag & trace_parse;
   gram_scanner_initialize ();
   gram_parse ();
+  prepare_percent_define_front_end_variables ();
 
-  /* Set front-end %define variable defaults.  */
+  if (! complaint_issued)
+    check_and_convert_grammar ();
+
+  xfclose (gram_in);
+}
+
+static void
+prepare_percent_define_front_end_variables (void)
+{
+  /* Set %define front-end variable defaults.  */
   muscle_percent_define_default ("lr.keep-unreachable-states", "false");
   {
     char *lr_type;
@@ -606,7 +617,7 @@ reader (void)
     free (lr_type);
   }
 
-  /* Check front-end %define variables.  */
+  /* Check %define front-end variables.  */
   {
     static char const * const values[] = {
       "lr.type", "LALR", "IELR", "canonical LR", NULL,
@@ -615,11 +626,6 @@ reader (void)
     };
     muscle_percent_define_check_values (values);
   }
-
-  if (! complaint_issued)
-    check_and_convert_grammar ();
-
-  xfclose (gram_in);
 }
 
 
