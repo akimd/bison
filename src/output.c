1266b636740f0c6719d8cc11a5d569084fa37009
Eric Blake <ebb9@byu.net>
2009-08-20 16:43:07 -0600

    Make previous patch more robust.
    * src/output.c (ARRAY_CARDINALITY): New macro, copied from
    argmatch.h.
    (output_skeleton): Use it.
    Suggested by Akim Demaille.
    
    Signed-off-by: Eric Blake <ebb9@byu.net>

--- src/output.c
+++ src/output.c
@@ -21,6 +21,7 @@
 #include <config.h>
 #include "system.h"
 
+#include <assert.h>
 #include <configmake.h>
 #include <error.h>
 #include <get-errno.h>
@@ -40,6 +41,7 @@
 #include "symtab.h"
 #include "tables.h"
 
+# define ARRAY_CARDINALITY(Array) (sizeof (Array) / sizeof *(Array))
 
 static struct obstack format_obstack;
 
@@ -546,6 +548,7 @@ output_skeleton (void)
     argv[i++] = full_m4bison;
     argv[i++] = full_skeleton;
     argv[i++] = NULL;
+    assert (i <= ARRAY_CARDINALITY (argv));
   }
 
   init_subpipe ();
