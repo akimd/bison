47eced3099712180364f4e01b839242027d9a9d8
Joel E. Denny <jdenny@clemson.edu>
2009-09-29 06:54:38 -0400

    Use the correct conversion specifier for size_t.
    Reported by Jim Meyering.
    * src/Sbitset.h (SBITSET__INDEX__CONVERSION_SPEC): New, "zu"
    because Sbitset__Index is size_t.
    * src/Sbitset.c (Sbitset__fprint): Use it instead of %d.

--- src/Sbitset.c
+++ src/Sbitset.c
@@ -65,14 +65,16 @@ Sbitset__fprint(Sbitset self, Sbitset__Index nbits, FILE *file)
   Sbitset__Index i;
   Sbitset itr;
   bool first = true;
-  fprintf (file, "nbits = %d, set = {", nbits);
+  fprintf (file,
+           "nbits = %" SBITSET__INDEX__CONVERSION_SPEC ", set = {",
+           nbits);
   SBITSET__FOR_EACH (self, nbits, itr, i)
     {
       if (first)
         first = false;
       else
         fprintf (file, ",");
-      fprintf (file, " %d", i);
+      fprintf (file, " %" SBITSET__INDEX__CONVERSION_SPEC, i);
     }
   fprintf (file, " }");
 }
