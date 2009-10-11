786578e394700615f6a841614e1280bf6284ec8c
Joel E. Denny <jdenny@clemson.edu>
2009-10-11 16:15:39 -0400

    portability: use va_start and va_end in the same function.
    * src/complain.c (error_message): Move va_end from here...
    (ERROR_MESSAGE): ... to here.

--- src/complain.c
+++ src/complain.c
@@ -68,7 +68,6 @@ error_message (location *loc,
     fprintf (stderr, "%s: ", prefix);
 
   vfprintf (stderr, message, args);
-  va_end (args);
   putc ('\n', stderr);
   fflush (stderr);
 }
@@ -79,6 +78,7 @@ error_message (location *loc,
   va_list args;					\
   va_start (args, Message);			\
   error_message (Loc, Prefix, Message, args);	\
+  va_end (args);				\
 }
 
 
