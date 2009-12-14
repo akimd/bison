51c7ca01e2c9cfb5c7a274bed0e14899adbd005c
Joel E. Denny <jdenny@clemson.edu>
2009-12-14 16:04:00 -0500

    Expand GLR acronym in summary of Bison.
    Based on discussion with Akim Demaille starting at
    <http://lists.gnu.org/archive/html/bison-patches/2009-04/msg00087.html>.
    * doc/bison.texinfo (Introduction): Here.
    * src/getargs.c (usage): Here.

--- src/getargs.c
+++ src/getargs.c
@@ -257,8 +257,8 @@ usage (int status)
 
       printf (_("Usage: %s [OPTION]... FILE\n"), program_name);
       fputs (_("\
-Generate a deterministic or GLR parser employing LALR(1), IELR(1), or\n\
-canonical LR(1) parser tables.\n\
+Generate a deterministic LR or generalized LR (GLR) parser employing\n\
+LALR(1), IELR(1), or canonical LR(1) parser tables.\n\
 \n\
 "), stdout);
 
