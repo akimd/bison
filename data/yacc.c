874ac415b95e9c98ab33dec4b33755d989986261
Jonathan Nieder <jrnieder@gmail.com>
2009-12-15 22:07:46 -0600

    Simplify y.tab.c when location tracking is disabled.
    * data/yacc.c: Do not check YYLTYPE_IS_TRIVIAL if location
    tracking is not enabled.  Instead, unconditionally define
    YY_LOCATION_PRINT as a no-op for backward compatibility.
    (cherry picked from commit fcd32abd3a4b68ef525d7766dbed1c98465e23b9)

--- data/yacc.c
+++ data/yacc.c
@@ -730,7 +730,7 @@ while (YYID (0))
     while (YYID (0))
 #endif
 
-
+]b4_locations_if([[
 /* YY_LOCATION_PRINT -- Print the location on the stream.
    This macro was not mandated originally: define only if we know
    we won't break user code: when these are the locations we know.  */
@@ -745,7 +745,13 @@ while (YYID (0))
 #  define YY_LOCATION_PRINT(File, Loc) ((void) 0)
 # endif
 #endif
+]], [[
+/* This macro is provided for backward compatibility. */
 
+#ifndef YY_LOCATION_PRINT
+# define YY_LOCATION_PRINT(File, Loc) ((void) 0)
+#endif
+]])[
 
 /* YYLEX -- calling `yylex' with the right arguments.  */
 
