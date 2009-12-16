3bba2dadae2c76440018345bfd73f56452e6932d
Joel E. Denny <jdenny@clemson.edu>
2009-12-16 01:41:17 -0500

    * data/yacc.c: Reformat m4 a little.
    (cherry picked from commit 19750d310c1f21b72fc26d0b8905fac08d175130)

--- data/yacc.c
+++ data/yacc.c
@@ -728,9 +728,9 @@ while (YYID (0))
 	    YYRHSLOC (Rhs, 0).last_column;				\
 	}								\
     while (YYID (0))
-#endif
+#endif]b4_locations_if([[
 
-]b4_locations_if([[
+
 /* YY_LOCATION_PRINT -- Print the location on the stream.
    This macro was not mandated originally: define only if we know
    we won't break user code: when these are the locations we know.  */
@@ -744,14 +744,15 @@ while (YYID (0))
 # else
 #  define YY_LOCATION_PRINT(File, Loc) ((void) 0)
 # endif
-#endif
-]], [[
+#endif]], [[
+
+
 /* This macro is provided for backward compatibility. */
 
 #ifndef YY_LOCATION_PRINT
 # define YY_LOCATION_PRINT(File, Loc) ((void) 0)
-#endif
-]])[
+#endif]])[
+
 
 /* YYLEX -- calling `yylex' with the right arguments.  */
 
