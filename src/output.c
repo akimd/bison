22cc8d813ee57c9631e527a31010ab138f9b7e06
Joel E. Denny <jdenny@clemson.edu>
2010-02-22 18:09:19 -0500

    portability: fix several issues with M4 subprocess.
    M4's output pipe was not being drained upon fatal errors during
    scan_skel.  As a result, broken-pipe messages from M4 were seen
    on at least AIX, HP-UX, Solaris, and RHEL4, and this caused a
    failure in the test suite.  The problem was that, on platforms
    where the default disposition for SIGPIPE is ignore instead of
    terminate, M4 sometimes saw fwrite fail with errno=EPIPE and
    then reported it.  However, there's some sort of race condition,
    because the new test group occasionally succeeded.
    Reported by Albert Chin at
    <http://lists.gnu.org/archive/html/bug-bison/2010-02/msg00004.html>.
    
    There were also problems with the test suite livelocking on
    Tru64 5.1b.  Reported by Didier Godefroy at
    <http://lists.gnu.org/archive/html/bug-bison/2009-05/msg00005.html>.
    Switching to create_pipe_bidi suggested by Akim Demaille.
    
    To attempt to solve both of these problems, switch to gnulib's
    create_pipe_bidi and register M4 process as a slave.  Along the
    way, clean up file name conflict handling, which was affected by
    the broken-pipe problem before the switch.
    * NEWS (2.4.2): Document.
    * THANKS (Didier Godefroy): Add.
    * bootstrap.conf (gnulib_modules): Add pipe.
    * gnulib: Update to latest to make sure we have all the latest
    fixes.
    * lib/Makefile.am (libbison_a_SOURCES): Remove subpipe.h and
    subpipe.c.
    * po/POTFILES.in (lib/subpipe.c): Remove.
    * src/files.c (compute_output_file_names): Update invocations
    of output_file_name_check.
    (output_file_name_check): In the case that the grammar file
    would be overwritten, use complain instead of fatal, but replace
    the output file name with /dev/null.  Use the /dev/null solution
    for the case of two conflicting output files as well because it
    seems safer in case Bison one day tries to open both files at
    the same time.
    * src/files.h (output_file_name_check): Update prototype.
    * src/output.c (output_skeleton): Use create_pipe_bidi and
    wait_subprocess.  Assert that scan_skel completely drains the
    pipe.
    * src/scan-skel.l (at_directive_perform): Update
    output_file_name_check invocation.
    * tests/output.at (AT_CHECK_CONFLICTING_OUTPUT): Check that the
    grammar file actually isn't overwritten.
    (Conflicting output files: -o foo.y): Update expected output.
    * tests/skeletons.at (Fatal errors but M4 continues producing
    output): New test group.

--- src/output.c
+++ src/output.c
@@ -25,9 +25,10 @@
 #include <configmake.h>
 #include <error.h>
 #include <get-errno.h>
+#include <pipe.h>
 #include <quotearg.h>
-#include <subpipe.h>
 #include <timevar.h>
+#include <wait-process.h>
 
 #include "complain.h"
 #include "files.h"
@@ -551,13 +552,14 @@ output_skeleton (void)
     assert (i <= ARRAY_CARDINALITY (argv));
   }
 
-  init_subpipe ();
-  pid = create_subpipe (argv, filter_fd);
+  /* The ugly cast is because gnulib gets the const-ness wrong.  */
+  pid = create_pipe_bidi ("m4", m4, (char **)(void*)argv, false, true,
+                          true, filter_fd);
   free (full_m4sugar);
   free (full_m4bison);
   free (full_skeleton);
 
-  out = fdopen (filter_fd[0], "w");
+  out = fdopen (filter_fd[1], "w");
   if (! out)
     error (EXIT_FAILURE, get_errno (),
 	   "fdopen");
@@ -576,14 +578,17 @@ output_skeleton (void)
 
   /* Read and process m4's output.  */
   timevar_push (TV_M4);
-  end_of_output_subpipe (pid, filter_fd);
-  in = fdopen (filter_fd[1], "r");
+  in = fdopen (filter_fd[0], "r");
   if (! in)
     error (EXIT_FAILURE, get_errno (),
 	   "fdopen");
   scan_skel (in);
+  /* scan_skel should have read all of M4's output.  Otherwise, when we
+     close the pipe, we risk letting M4 report a broken-pipe to the
+     Bison user.  */
+  aver (feof (in));
   xfclose (in);
-  reap_subpipe (pid, m4);
+  wait_subprocess (pid, "m4", false, false, true, true, NULL);
   timevar_pop (TV_M4);
 }
 
