e0fda26c8dda7f03443f69fb8c20b7cdeb1b37fd
Joel E. Denny <joeldenny@joeldenny.org>
2010-04-25 17:05:18 -0400

    i18n: fix untranslatable string.
    Reported by Goran Uddeborg at
    <http://lists.gnu.org/archive/html/bug-bison/2010-03/msg00000.html>.
    * src/muscle-tab.c (muscle_percent_define_insert): Here.

--- src/muscle-tab.c
+++ src/muscle-tab.c
@@ -425,8 +425,8 @@ muscle_percent_define_insert (char const *variable, location variable_loc,
           free (variable_tr);
           return;
         }
-      complain_at (variable_loc, _("%s `%s' redefined"),
-                   "%define variable", variable);
+      complain_at (variable_loc, _("%%define variable `%s' redefined"),
+                   variable);
       complain_at (muscle_percent_define_get_loc (variable),
                    _("previous definition"));
     }
