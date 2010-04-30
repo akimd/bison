02354690ee02dfa05564137aafc6721eb54d21ab
Joel E. Denny <joeldenny@joeldenny.org>
2010-04-30 14:41:07 -0400

    Revert 2009-12-30 change for undefined %prec token complaints.
    That is, keep them as warnings because that should be sufficient
    to satisfy POSIX without creating backward compatibility issues.
    Suggested by Richard Stallman at
    <http://lists.gnu.org/archive/html/bison-patches/2010-03/msg00033.html>.
    * NEWS (2.5): Remove mention of complaint.
    * src/reader.c (grammar_rule_check): Convert warning back to
    complaint.
    * tests/input.at (%prec's token must be defined): Update.

--- src/reader.c
+++ src/reader.c
@@ -327,8 +327,8 @@ grammar_rule_check (const symbol_list *r)
   if (r->ruleprec
       && r->ruleprec->tag[0] != '\'' && r->ruleprec->tag[0] != '"'
       && !r->ruleprec->declared && !r->ruleprec->prec)
-    complain_at (r->location, _("token for %%prec is not defined: %s"),
-                 r->ruleprec->tag);
+    warn_at (r->location, _("token for %%prec is not defined: %s"),
+             r->ruleprec->tag);
 }
 
 
