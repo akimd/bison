ca7a59e838ee278ced86ea0918487e5a7c69f8c3
Joel E. Denny <joeldenny@joeldenny.org>
2010-04-30 19:50:52 -0400

    tests: fix maintainer-xml-check.
    * data/xslt/xml2dot.xsl (xsl:template match="bison-xml-report"):
    Update output to include comments now produced by --graph.
    (xsl:template match="automaton"): As for --graph, name the
    digraph after the grammar file.
    * src/print-xml.c (escape_bufs): Enlarge array.
    (print_xml): Add bug-report and url attributes to
    bison-xml-report element.
    (cherry picked from commit bc81de36b7ef78ff280e14a173e5cc5d5a3191eb)

--- src/print-xml.c
+++ src/print-xml.c
@@ -48,7 +48,7 @@ struct escape_buf
   char *ptr;
   size_t size;
 };
-static struct escape_buf escape_bufs[2];
+static struct escape_buf escape_bufs[3];
 
 
 /*--------------------------------.
@@ -505,8 +505,12 @@ print_xml (void)
   FILE *out = xfopen (spec_xml_file, "w");
 
   fputs ("<?xml version=\"1.0\"?>\n\n", out);
-  xml_printf (out, level, "<bison-xml-report version=\"%s\">",
-	      xml_escape (VERSION));
+  xml_printf (out, level,
+              "<bison-xml-report version=\"%s\" bug-report=\"%s\""
+              " url=\"%s\">",
+              xml_escape_n (0, VERSION),
+              xml_escape_n (1, PACKAGE_BUGREPORT),
+              xml_escape_n (2, PACKAGE_URL));
 
   fputc ('\n', out);
   xml_printf (out, level + 1, "<filename>%s</filename>",
