20be2f9227151063a9e201bc3aad93362a0cebbc
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2010-07-29 01:43:37 -0700

    Remove restrictions on expanding GLR stacks in C++.
        * data/glr.c:  Remove tests for __cplusplus related to definition of
        YYSTACKEXPANDABLE.
        * doc/bison.texinfo: Rewrite paragraph saying C++ stacks are not
        expandable to instead indicate conditions that prevent their expansion
        in C++.  Reorganize section on GLR semantic values a bit. Remove
        discussion of YYLLOC_DEFAULT. Mention restrictions to POD data.

--- data/glr.c
+++ data/glr.c
@@ -576,13 +576,7 @@ int yydebug;
 #define YYHEADROOM 2
 
 #ifndef YYSTACKEXPANDABLE
-# if (! defined __cplusplus \
-      || (]b4_locations_if([[defined YYLTYPE_IS_TRIVIAL && YYLTYPE_IS_TRIVIAL \
-	  && ]])[defined YYSTYPE_IS_TRIVIAL && YYSTYPE_IS_TRIVIAL))
 #  define YYSTACKEXPANDABLE 1
-# else
-#  define YYSTACKEXPANDABLE 0
-# endif
 #endif
 
 #if YYSTACKEXPANDABLE
