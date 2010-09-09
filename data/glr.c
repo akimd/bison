39f5757a95b687b334165c129ec3c95ce5fec8b5
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2010-09-09 00:46:50 -0700

    Remove compiler warnings in GLR parsers.
    	* data/glr.c (yySymbol): Define as int to avoid compiler warnings
    	about possible change of value.

--- data/glr.c
+++ data/glr.c
@@ -673,7 +673,7 @@ typedef int yyStateNum;
 typedef int yyRuleNum;
 
 /** Grammar symbol */
-typedef short int yySymbol;
+typedef int yySymbol;
 
 /** Item references, as in LALR(1) machine */
 typedef short int yyItemNum;
