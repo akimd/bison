d89e48b3c9e857aa6d34f0bc66c9d9ac305f1b0c
Joel E. Denny <joeldenny@joeldenny.org>
2011-02-06 08:52:09 -0500

    doc: document experimental features better.
    * doc/bison.texinfo (Introduction): Say that IELR(1) and canonical
    LR(1) are experimental.  Mention Java.  Normally experimental
    features probably shouldn't be mentioned in the introduction.
    However, if Bison's limitations to LALR(1), C, and C++ are so
    important that they should be mentioned here, then it's important
    to point out that Bison is beginning to escape those limitations.
    Moreover, these particular experimental features have very little
    chance of being removed.
    * src/getargs.c (usage): Say that IELR(1) and canonical LR(1) are
    experimental.

--- src/getargs.c
+++ src/getargs.c
@@ -258,7 +258,8 @@ usage (int status)
       printf (_("Usage: %s [OPTION]... FILE\n"), program_name);
       fputs (_("\
 Generate a deterministic LR or generalized LR (GLR) parser employing\n\
-LALR(1), IELR(1), or canonical LR(1) parser tables.\n\
+LALR(1), IELR(1), or canonical LR(1) parser tables.  IELR(1) and\n\
+canonical LR(1) support is experimental.\n\
 \n\
 "), stdout);
 
