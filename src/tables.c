32493bc84db0c69e6e1d9d5c5c7505ab4f698f7b
Joel E. Denny <joeldenny@joeldenny.org>
2011-03-06 12:46:27 -0500

    lr.default-reductions: rename "all" value to "full".
    States that shift the error token do not have default reductions,
    and GLR disables some default reductions, so "all" was a misnomer.
    * doc/bison.texinfo (%define Summary): Update.
    (Default Reductions): Update.
    * src/print.c (print_reductions): Update.
    * src/reader.c (prepare_percent_define_front_end_variables):
    Update.
    * src/tables.c (action_row): Update.
    * tests/input.at (%define enum variables): Update.
    * tests/reduce.at (%define lr.default-reductions): Update.
    (cherry picked from commit d815ec4a6290e18fac9220438622f6dd27b3227f)

--- src/tables.c
+++ src/tables.c
@@ -310,7 +310,7 @@ action_row (state *s)
   {
     char *default_reductions =
       muscle_percent_define_get ("lr.default-reductions");
-    if (0 != strcmp (default_reductions, "all") && !s->consistent)
+    if (0 != strcmp (default_reductions, "full") && !s->consistent)
       nodefault = true;
     free (default_reductions);
   }
