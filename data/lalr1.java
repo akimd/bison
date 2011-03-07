1a33f4f60872281c70c2aea3f8529d8660609609
Joel E. Denny <jdenny@clemson.edu>
2011-03-06 22:48:46 -0500

    java: finish fixing parser stack popping bug.
    * NEWS (2.5): Document.
    * data/lalr1.java (YYParser::YYStack::pop): Fix off-by-one error
    in clearing the location stack.  Also fix pop function that
    accepts no arguments.
    (cherry picked from commit 4c2a6e42ba8b6bc4e04985f5ef3ec8926048d4b1)
    
    Conflicts:
    
    	data/lalr1.java

--- data/lalr1.java
+++ data/lalr1.java
@@ -297,14 +297,14 @@ b4_lexer_if([[
     }
 
     public final void pop () {
-      height--;
+      pop (1);
     }
 
     public final void pop (int num) {
       // Avoid memory leaks... garbage collection is a white lie!
       if (num > 0) {
         java.util.Arrays.fill (valueStack, height - num + 1, height + 1, null);
-        ]b4_locations_if([[java.util.Arrays.fill (locStack, height - num + 1, height, null);]])[
+        ]b4_locations_if([[java.util.Arrays.fill (locStack, height - num + 1, height + 1, null);]])[
       }
       height -= num;
     }
