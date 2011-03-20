a6e5a28079865c4924194685723962f198fd33ff
Joel E. Denny <joeldenny@joeldenny.org>
2011-03-20 17:35:10 -0400

    lr.default-reductions: rename "full" value to "most".
    Unlike "consistent" and "accepting", "full" doesn't answer the
    question of "which states".
    * doc/bison.texinfo (%define Summary): Update.
    (Default Reductions): Update.
    * src/print.c (print_reductions): Update.
    * src/reader.c (prepare_percent_define_front_end_variables):
    Update.
    * src/tables.c (action_row): Update.
    * tests/input.at (%define enum variables): Update.
    * tests/reduce.at (%define lr.default-reductions): Update.

--- src/tables.c
+++ src/tables.c
@@ -310,7 +310,7 @@ action_row (state *s)
   {
     char *default_reductions =
       muscle_percent_define_get ("lr.default-reductions");
-    if (0 != strcmp (default_reductions, "full") && !s->consistent)
+    if (0 != strcmp (default_reductions, "most") && !s->consistent)
       nodefault = true;
     free (default_reductions);
   }
