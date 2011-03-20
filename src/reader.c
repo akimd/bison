f0ad1b2fb28da66a3fadd9890cf621f55eb90986
Joel E. Denny <joeldenny@joeldenny.org>
2011-03-20 17:35:10 -0400

    lr.default-reductions: rename "full" value to "most".
    Unlike "consistent" and "accepting", "full" doesn't answer the
    question of "which states".
    * doc/bison.texinfo (%define Summary): Update.
    (Default Reductions): Update.
    * src/print.c (print_reductions): Update.
    * src/reader.c (prepare_percent_define_front_end_variables):
    Update.
    * src/tables.c (action_row): Update.
    * tests/input.at (%define enum variables): Update.
    * tests/reduce.at (%define lr.default-reductions): Update.
    (cherry picked from commit a6e5a28079865c4924194685723962f198fd33ff)

--- src/reader.c
+++ src/reader.c
@@ -632,7 +632,7 @@ prepare_percent_define_front_end_variables (void)
     muscle_percent_define_default ("lr.type", "lalr");
     lr_type = muscle_percent_define_get ("lr.type");
     if (0 != strcmp (lr_type, "canonical-lr"))
-      muscle_percent_define_default ("lr.default-reductions", "full");
+      muscle_percent_define_default ("lr.default-reductions", "most");
     else
       muscle_percent_define_default ("lr.default-reductions", "accepting");
     free (lr_type);
@@ -642,7 +642,7 @@ prepare_percent_define_front_end_variables (void)
   {
     static char const * const values[] = {
       "lr.type", "lalr", "ielr", "canonical-lr", NULL,
-      "lr.default-reductions", "full", "consistent", "accepting", NULL,
+      "lr.default-reductions", "most", "consistent", "accepting", NULL,
       NULL
     };
     muscle_percent_define_check_values (values);
