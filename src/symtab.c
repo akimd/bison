1f36f54446248b8984b9c378b0c756a6b51b34cb
Joel E. Denny <jdenny@clemson.edu>
2011-05-01 21:53:35 -0400

    Fix precedence for end token.
    Since Bison 2.3b, which restored the ability of precedence
    directives to assign user token numbers, doing so for user token
    number 0 has produced an assertion failure.
    * NEWS (2.5): Document fix.
    * src/symtab.c (symbol_user_token_number_set): In the case of the
    end token, don't decrement ntokens if it was never incremented.
    * tests/regression.at (Token number in precedence declaration):
    Extend.
    (cherry picked from commit 9d6af153184eea964fef7f87d76a60fe29f715b5)

--- src/symtab.c
+++ src/symtab.c
@@ -403,10 +403,11 @@ symbol_user_token_number_set (symbol *sym, int user_token_number, location loc)
   if (user_token_number == 0)
     {
       endtoken = sym;
-      endtoken->number = 0;
       /* It is always mapped to 0, so it was already counted in
 	 NTOKENS.  */
-      --ntokens;
+      if (endtoken->number != NUMBER_UNDEFINED)
+        --ntokens;
+      endtoken->number = 0;
     }
 }
 
