e6062fb6da2e6271222882b20093d5af63405e17
Jim Meyering <meyering@redhat.com>
2012-01-13 12:19:08 +0100

    build: avoid warning from coverity about lbitset_elt_find
    * lib/lbitset.c (lbitset_elt_find): Remove unnecessary test of "elt",
    at a point where we know it is non-NULL, due to prior dereference.

--- lib/lbitset.c
+++ lib/lbitset.c
@@ -352,7 +352,7 @@ lbitset_elt_find (bitset bset, bitset_windex windex,
 
       /* ELT is the nearest to the one we want.  If it's not the one
 	 we want, the one we want does not exist.  */
-      if (elt && (windex - elt->index) < LBITSET_ELT_WORDS)
+      if (windex - elt->index < LBITSET_ELT_WORDS)
 	{
 	  bset->b.cindex = elt->index;
 	  bset->b.csize = LBITSET_ELT_WORDS;
