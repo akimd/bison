ae93e4e4b8e78396a7707de3dcdd47aca7a46dec
Jim Meyering <meyering@redhat.com>
2012-01-16 19:20:50 +0100

    quote consistently and make tests pass with new quoting from gnulib
    Updating to gnulib pulled in new quote and quotarg modules,
    by which quoting is now done like 'this' rather than `this'.
    That change induces many "make check" test failures.  This change
    adapts code and tests so that "make check" passes once again.
    * src/scan-code.l: Quote like 'this', not like `this'.
    * src/scan-gram.l: Likewise.
    * src/symtab.c: Likewise.
    * tests/actions.at: Adjust tests to match.
    * tests/input.at: Likewise.
    * tests/named-refs.at: Likewise.
    * tests/output.at: Likewise.
    * tests/regression.at: Likewise.
    * lib/.gitignore: Regenerate.
    * m4/.gitignore: Likewise.

--- src/symtab.c
+++ src/symtab.c
@@ -444,10 +444,10 @@ void
 symbol_make_alias (symbol *sym, symbol *str, location loc)
 {
   if (str->alias)
-    warn_at (loc, _("symbol `%s' used more than once as a literal string"),
+    warn_at (loc, _("symbol '%s' used more than once as a literal string"),
              str->tag);
   else if (sym->alias)
-    warn_at (loc, _("symbol `%s' given more than one literal string"),
+    warn_at (loc, _("symbol '%s' given more than one literal string"),
              sym->tag);
   else
     {
