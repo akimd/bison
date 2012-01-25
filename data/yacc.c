d115aad9112fb4e2fe1b268c9db7390732d39539
Akim Demaille <demaille@gostai.com>
2012-01-25 16:57:58 +0100

    yacc: fix YYBACKUP.
    Reported by David Kastrup:
    https://lists.gnu.org/archive/html/bug-bison/2011-10/msg00002.html.
    
    	* data/yacc.c (YYBACKUP): Accept rhs size.
    	Restore the proper state value.
    	* TODO (YYBACKUP): Make it...
    	* tests/actions.at: a new test case.
    	* NEWS, THANKS: Update.

--- data/yacc.c
+++ data/yacc.c
@@ -646,11 +646,12 @@ static const ]b4_int_type_for([b4_toknum])[ yytoknum[] =
 
 #define YYBACKUP(Token, Value)                                  \
 do                                                              \
-  if (yychar == YYEMPTY && yylen == 1)                          \
+  if (yychar == YYEMPTY)                                        \
     {                                                           \
       yychar = (Token);                                         \
       yylval = (Value);                                         \
-      YYPOPSTACK (1);                                           \]b4_lac_if([[
+      YYPOPSTACK (yylen);                                       \
+      yystate = *yyssp;                                         \]b4_lac_if([[
       YY_LAC_DISCARD ("YYBACKUP");                              \]])[
       goto yybackup;                                            \
     }                                                           \
