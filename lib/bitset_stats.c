bd65f25570a90fe248b7b9ddb59a0d05ea7c79d6
Akim Demaille <demaille@gostai.com>
2012-02-19 10:18:29 +0100

    bitset: fix an incorrect error message.
    	* lib/bitset_stats.c: here.
    	Reported by Stefano Lattarini.

--- lib/bitset_stats.c
+++ lib/bitset_stats.c
@@ -284,7 +284,7 @@ bitset_stats_write (const char *file_name)
     {
       if (fwrite (&bitset_stats_info_data, sizeof (bitset_stats_info_data),
                   1, file) != 1)
-        perror (_("cannot not write stats file"));
+        perror (_("cannot write stats file"));
       if (fclose (file) != 0)
         perror (_("cannot write stats file"));
     }
