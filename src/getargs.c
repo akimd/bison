a055b2f07dbbc37cf15b5d8cfb8b0df226d3f65e
Akim Demaille <demaille@gostai.com>
2012-02-19 11:13:04 +0100

    maint: address sc_bindtextdomain, sc_program_name and sc_prohibit_HAVE_MBRTOWC.
    	* bootstrap.conf (gnulib_modules): Require progname.
    	* src/complain.c, src/getargs.c, src/getargs.h, src/main.c: Use it.
    	* cfg.mk (exclude): New.
    	Use it.
    	Skip lib/main.c for bindtextdomain and set_program_name.
    (cherry picked from commit 4d699f44e65d29b4d49cf68d7a3b4bf856eb5d1b)
    
    Conflicts:
    
    	cfg.mk

--- src/getargs.c
+++ src/getargs.c
@@ -40,6 +40,8 @@
 # undef HACK_FOR___GNU_LIBRARY___PROTOTYPE
 #endif
 
+#include <progname.h>
+
 #include "complain.h"
 #include "files.h"
 #include "getargs.h"
@@ -78,8 +80,6 @@ const char *skeleton = NULL;
 int language_prio = default_prio;
 struct bison_language const *language = &valid_languages[0];
 const char *include = NULL;
-
-char *program_name;
 
 
 /** Decode an option's set of keys.
--- src/main.c
+++ src/main.c
@@ -24,6 +24,7 @@
 #include <bitset_stats.h>
 #include <bitset.h>
 #include <configmake.h>
+#include <progname.h>
 #include <quotearg.h>
 #include <timevar.h>
 
@@ -57,7 +58,7 @@
 int
 main (int argc, char *argv[])
 {
-  program_name = argv[0];
+  set_program_name (argv[0]);
   setlocale (LC_ALL, "");
   (void) bindtextdomain (PACKAGE, LOCALEDIR);
   (void) bindtextdomain ("bison-runtime", LOCALEDIR);
