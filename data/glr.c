a6b2f4fc020fe6e464f4a445a7851825d3d57e4d
Akim Demaille <demaille@gostai.com>
2012-02-21 11:51:37 +0100

    glr: fix ambiguity reports.
    Fix a regression introduced in commit
    783aa653f4ca70a75919c8516b950494c612cbfe.
    
    	* tests/glr-regression.at (Ambiguity reports): New.
    	* data/glr.c (yyreportTree): Fix an offset error.

--- data/glr.c
+++ data/glr.c
@@ -1727,10 +1727,10 @@ yyreportTree (yySemanticOption* yyx, int yyindent)
         {
           if (yystates[yyi-1]->yyposn+1 > yystates[yyi]->yyposn)
             YYFPRINTF (stderr, "%*s%s <empty>\n", yyindent+2, "",
-                       yytokenName (yystos[yystates[yyi-1]->yylrState]));
+                       yytokenName (yystos[yystates[yyi]->yylrState]));
           else
             YYFPRINTF (stderr, "%*s%s <tokens %lu .. %lu>\n", yyindent+2, "",
-                       yytokenName (yystos[yystates[yyi-1]->yylrState]),
+                       yytokenName (yystos[yystates[yyi]->yylrState]),
                        (unsigned long int) (yystates[yyi-1]->yyposn + 1),
                        (unsigned long int) yystates[yyi]->yyposn);
         }
