a37131cc63607b8fa59ba58296dd0b1682ec54d5
Theophile Ranquet <ranquet@lrde.epita.fr>
2012-03-12 10:01:09 +0100

    getargs: fix the locations of command-line input
    * src/getargs.c (command_line_location): Here.
    * tests/input.at: Adjust.

--- src/getargs.c
+++ src/getargs.c
@@ -556,7 +556,7 @@ command_line_location (void)
 {
   location res;
   /* "<command line>" is used in GCC's messages about -D. */
-  boundary_set (&res.start, uniqstr_new ("<command line>"), optind, -1);
+  boundary_set (&res.start, uniqstr_new ("<command line>"), optind - 1, -1);
   res.end = res.start;
   return res;
 }
