37e884cd9a249660f39b5b7290e1f4b3dcc851b0
Akim Demaille <akim@lrde.epita.fr>
2012-03-29 14:57:25 +0200

    maint: formatting changes.
    * src/system.h: Indent CPP directives using cppi.

--- src/system.h
+++ src/system.h
@@ -16,52 +16,52 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #ifndef BISON_SYSTEM_H
-#define BISON_SYSTEM_H
+# define BISON_SYSTEM_H
 
 /* flex 2.5.31 gratutiously defines macros like INT8_MIN.  But this
    runs afoul of pre-C99 compilers that have <inttypes.h> or
    <stdint.h>, which are included below if available.  It also runs
    afoul of pre-C99 compilers that define these macros in <limits.h>.  */
-#if ! defined __STDC_VERSION__ || __STDC_VERSION__ < 199901
-# undef INT8_MIN
-# undef INT16_MIN
-# undef INT32_MIN
-# undef INT8_MAX
-# undef INT16_MAX
-# undef UINT8_MAX
-# undef INT32_MAX
-# undef UINT16_MAX
-# undef UINT32_MAX
-#endif
+# if ! defined __STDC_VERSION__ || __STDC_VERSION__ < 199901
+#  undef INT8_MIN
+#  undef INT16_MIN
+#  undef INT32_MIN
+#  undef INT8_MAX
+#  undef INT16_MAX
+#  undef UINT8_MAX
+#  undef INT32_MAX
+#  undef UINT16_MAX
+#  undef UINT32_MAX
+# endif
 
-#include <limits.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
+# include <limits.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <string.h>
 
-#if HAVE_SYS_TYPES_H
-# include <sys/types.h>
-#endif
+# if HAVE_SYS_TYPES_H
+#  include <sys/types.h>
+# endif
 
-#include <unistd.h>
-#include <inttypes.h>
+# include <unistd.h>
+# include <inttypes.h>
 
-#ifndef UINTPTR_MAX
+# ifndef UINTPTR_MAX
 /* This isn't perfect, but it's good enough for Bison, which needs
    only to hash pointers.  */
 typedef size_t uintptr_t;
-#endif
+# endif
 
 // Version mismatch.
-#define EX_MISMATCH 63
+# define EX_MISMATCH 63
 
 /*---------.
 | Gnulib.  |
 `---------*/
 
-#include <unlocked-io.h>
-#include <verify.h>
-#include <xalloc.h>
+# include <unlocked-io.h>
+# include <verify.h>
+# include <xalloc.h>
 
 
 /*-----------------.
@@ -80,49 +80,49 @@ typedef size_t uintptr_t;
    and safer logic than it would for users.  Due to the overhead of M4,
    suppressing Code is unlikely to offer any significant improvement in
    Bison's performance anyway.  */
-#define PACIFY_CC(Code) Code
+# define PACIFY_CC(Code) Code
 
-#ifndef __attribute__
+# ifndef __attribute__
 /* This feature is available in gcc versions 2.5 and later.  */
-# if (! defined __GNUC__ || __GNUC__ < 2 \
+#  if (! defined __GNUC__ || __GNUC__ < 2 \
       || (__GNUC__ == 2 && __GNUC_MINOR__ < 5) || __STRICT_ANSI__)
-#  define __attribute__(Spec) /* empty */
+#   define __attribute__(Spec) /* empty */
+#  endif
 # endif
-#endif
 
 /* The __-protected variants of `format' and `printf' attributes
    are accepted by gcc versions 2.6.4 (effectively 2.7) and later.  */
-#if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 7)
-# define __format__ format
-# define __printf__ printf
-#endif
+# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 7)
+#  define __format__ format
+#  define __printf__ printf
+# endif
 
-#ifndef ATTRIBUTE_NORETURN
-# define ATTRIBUTE_NORETURN __attribute__ ((__noreturn__))
-#endif
+# ifndef ATTRIBUTE_NORETURN
+#  define ATTRIBUTE_NORETURN __attribute__ ((__noreturn__))
+# endif
 
-#ifndef ATTRIBUTE_UNUSED
-# define ATTRIBUTE_UNUSED __attribute__ ((__unused__))
-#endif
+# ifndef ATTRIBUTE_UNUSED
+#  define ATTRIBUTE_UNUSED __attribute__ ((__unused__))
+# endif
 
-#define FUNCTION_PRINT() fprintf (stderr, "%s: ", __func__)
+# define FUNCTION_PRINT() fprintf (stderr, "%s: ", __func__)
 
 /*------.
 | NLS.  |
 `------*/
 
-#include <locale.h>
+# include <locale.h>
 
-#include <gettext.h>
-#define _(Msgid)  gettext (Msgid)
-#define N_(Msgid) (Msgid)
+# include <gettext.h>
+# define _(Msgid)  gettext (Msgid)
+# define N_(Msgid) (Msgid)
 
 
 /*-----------.
 | Booleans.  |
 `-----------*/
 
-#include <stdbool.h>
+# include <stdbool.h>
 
 
 
@@ -148,43 +148,43 @@ typedef size_t uintptr_t;
    For now, we use assert but we call it aver throughout Bison in case
    we later wish to try another scheme.
 */
-#include <assert.h>
-#define aver assert
+# include <assert.h>
+# define aver assert
 
 
 /*-----------.
 | Obstacks.  |
 `-----------*/
 
-#define obstack_chunk_alloc xmalloc
-#define obstack_chunk_free  free
-#include <obstack.h>
+# define obstack_chunk_alloc xmalloc
+# define obstack_chunk_free  free
+# include <obstack.h>
 
-#define obstack_sgrow(Obs, Str) \
+# define obstack_sgrow(Obs, Str) \
   obstack_grow (Obs, Str, strlen (Str))
 
-#define obstack_fgrow1(Obs, Format, Arg1)	\
+# define obstack_fgrow1(Obs, Format, Arg1)	\
 do {						\
   char buf[4096];				\
   sprintf (buf, Format, Arg1);			\
   obstack_grow (Obs, buf, strlen (buf));	\
 } while (0)
 
-#define obstack_fgrow2(Obs, Format, Arg1, Arg2)	\
+# define obstack_fgrow2(Obs, Format, Arg1, Arg2)	\
 do {						\
   char buf[4096];				\
   sprintf (buf, Format, Arg1, Arg2);		\
   obstack_grow (Obs, buf, strlen (buf));	\
 } while (0)
 
-#define obstack_fgrow3(Obs, Format, Arg1, Arg2, Arg3)	\
+# define obstack_fgrow3(Obs, Format, Arg1, Arg2, Arg3)	\
 do {							\
   char buf[4096];					\
   sprintf (buf, Format, Arg1, Arg2, Arg3);		\
   obstack_grow (Obs, buf, strlen (buf));		\
 } while (0)
 
-#define obstack_fgrow4(Obs, Format, Arg1, Arg2, Arg3, Arg4)	\
+# define obstack_fgrow4(Obs, Format, Arg1, Arg2, Arg3, Arg4)	\
 do {								\
   char buf[4096];						\
   sprintf (buf, Format, Arg1, Arg2, Arg3, Arg4);		\
@@ -197,17 +197,17 @@ do {								\
 | Extensions to use for the output files.  |
 `-----------------------------------------*/
 
-#ifndef OUTPUT_EXT
-# define OUTPUT_EXT ".output"
-#endif
+# ifndef OUTPUT_EXT
+#  define OUTPUT_EXT ".output"
+# endif
 
-#ifndef TAB_EXT
-# define TAB_EXT ".tab"
-#endif
+# ifndef TAB_EXT
+#  define TAB_EXT ".tab"
+# endif
 
-#ifndef DEFAULT_TMPDIR
-# define DEFAULT_TMPDIR "/tmp"
-#endif
+# ifndef DEFAULT_TMPDIR
+#  define DEFAULT_TMPDIR "/tmp"
+# endif
 
 
 
@@ -215,7 +215,7 @@ do {								\
 | Free a linked list.  |
 `---------------------*/
 
-#define LIST_FREE(Type, List)			\
+# define LIST_FREE(Type, List)			\
 do {						\
   Type *_node, *_next;				\
   for (_node = List; _node; _node = _next)	\
