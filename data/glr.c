775673717fc9b7e5174418f7420ba21040a8e675
Akim Demaille <akim@lrde.epita.fr>
2012-05-04 17:33:12 +0200

    glr.c: formatting changes.
    * data/glr.c: Fix indentation.

--- data/glr.c
+++ data/glr.c
@@ -544,7 +544,7 @@ typedef enum { yyok, yyaccept, yyabort, yyerr } YYRESULTTAG;
     if (yydebug)                                                        \
       {                                                                 \
         YYFPRINTF (stderr, "%s ", Title);                               \
-      yy_symbol_print (stderr, Type, Value]b4_locuser_args([Location])[);        \
+        yy_symbol_print (stderr, Type, Value]b4_locuser_args([Location])[);        \
         YYFPRINTF (stderr, "\n");                                       \
       }                                                                 \
   } while (YYID (0))
