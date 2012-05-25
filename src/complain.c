aed41cf9a0dde045bf93c9241dfdd62d75fe3d18
Akim Demaille <akim@lrde.epita.fr>
2012-05-25 14:36:32 +0200

    maint: space changes
    * configure.ac, src/complain.c: space changes.

--- src/complain.c
+++ src/complain.c
@@ -74,10 +74,11 @@ error_message (location *loc,
   vfprintf (stderr, message, args);
   {
     size_t l = strlen (message);
-    if (l < 2 || message[l-2] != ':' || message[l-1] != ' ') {
-      putc ('\n', stderr);
-      fflush (stderr);
-    }
+    if (l < 2 || message[l-2] != ':' || message[l-1] != ' ')
+      {
+        putc ('\n', stderr);
+        fflush (stderr);
+      }
   }
 }
 
