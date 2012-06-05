28126202a52cda40411bc33ed795ceb4cfdd302d
Akim Demaille <akim@lrde.epita.fr>
2012-06-05 17:26:35 +0200

    maint: minor simplification
    * src/output.c (prepare): Assign use_push_for_pull_flag's value at its
    declaration.

--- src/output.c
+++ src/output.c
@@ -685,12 +685,8 @@ prepare (void)
 {
   /* BISON_USE_PUSH_FOR_PULL is for the test suite and should not be documented
      for the user.  */
-  char const *use_push_for_pull_env = getenv ("BISON_USE_PUSH_FOR_PULL");
-  bool use_push_for_pull_flag = false;
-  if (use_push_for_pull_env != NULL
-      && use_push_for_pull_env[0] != '\0'
-      && STRNEQ (use_push_for_pull_env, "0"))
-    use_push_for_pull_flag = true;
+  char const *cp = getenv ("BISON_USE_PUSH_FOR_PULL");
+  bool use_push_for_pull_flag = cp && *cp && STRNEQ (cp, "0");
 
   /* Flags. */
   MUSCLE_INSERT_BOOL ("defines_flag", defines_flag);
