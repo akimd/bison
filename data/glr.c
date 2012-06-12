0f11eec272453d5646e5aeda135650b4fb8ac33d
Akim Demaille <akim@lrde.epita.fr>
2012-06-12 16:15:14 +0200

    yacc.c: instead of duplicating y.tab.h inside y.tac.c, include it.
    This is already what glr.c and lalr1.cc do.
    
    * data/yacc.c: here.

--- data/glr.c
+++ data/glr.c
@@ -203,7 +203,7 @@ m4_if(b4_prefix, [yy], [],
 ]b4_null_define[
 
 ]b4_defines_if([[#include "@basename(]b4_spec_defines_file[@)"]],
-              [b4_shared_declarations])[
+               [b4_shared_declarations])[
 
 /* Enabling traces.  */
 #ifndef YYDEBUG
--- data/yacc.c
+++ data/yacc.c
@@ -319,7 +319,8 @@ m4_if(b4_prefix, [yy], [],
 # define YYTOKEN_TABLE ]b4_token_table[
 #endif
 
-]b4_shared_declarations[
+]b4_defines_if([[#include "@basename(]b4_spec_defines_file[@)"]],
+               [b4_shared_declarations])[
 
 /* Copy the second part of user declarations.  */
 ]b4_user_post_prologue
