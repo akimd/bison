e97965c0221bbc71160448e059cedcf176a7aad5
Akim Demaille <akim@lrde.epita.fr>
2012-06-20 11:16:47 +0200

    maint: style changes.
    * src/reduce.c (reduce_grammar_tables): Define variables with their
    initial value.

--- src/reduce.c
+++ src/reduce.c
@@ -285,13 +285,12 @@ reduce_grammar_tables (void)
 static void
 nonterminals_reduce (void)
 {
-  symbol_number i, n;
-
   /* Map the nonterminals to their new index: useful first, useless
      afterwards.  Kept for later report.  */
 
   symbol_number *nontermmap = xnmalloc (nvars, sizeof *nontermmap);
-  n = ntokens;
+  symbol_number n = ntokens;
+  symbol_number i;
   for (i = ntokens; i < nsyms; i++)
     if (bitset_test (V, i))
       nontermmap[i - ntokens] = n++;
