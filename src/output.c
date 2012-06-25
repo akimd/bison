423ca9b50e4321c078cc6fe7903b10ed4907d269
Akim Demaille <akim@lrde.epita.fr>
2012-06-25 09:49:04 +0200

    fix for printers and destructors.
    The previous "code_props: factor more" patch sends has_%printer
    etc. to m4, instead of has_printer.
    
    * src/output.c (prepare_symbol_definitions): Fix value of pname.

--- src/output.c
+++ src/output.c
@@ -447,7 +447,8 @@ prepare_symbol_definitions (void)
         int j;
         for (j = 0; j < CODE_PROPS_SIZE; ++j)
           {
-            char const *pname = code_props_type_string (j);
+            /* "printer", not "%printer".  */
+            char const *pname = code_props_type_string (j) + 1;
             code_props const *p = symbol_code_props_get (sym, j);
             SET_KEY2("has", pname);
             MUSCLE_INSERT_INT (key, !!p->code);
