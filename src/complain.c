73370a9daab5c9fe1395240e2aeccd14b8e901c7
Victor Santet <victor.santet@epita.fr>
2012-06-29 16:23:42 +0200

    warnings: display warnings categories
    * src/complain.c (error_message): Call 'warnings_print_categories'.
    * src/gram.c (grammar_rules_useless_report): Display itself warning
    category.
    * tests/actions.at, tests/conflicts.at, tests/existing.at,
    tests/input.at, tests/named-refs.at, tests/output.at, tests/reduce.at,
    tests/regression.at, tests/skeletons.at: Adjust.
    * NEWS: Document this.
    
    Signed-off-by: Akim Demaille <akim@lrde.epita.fr>

--- src/complain.c
+++ src/complain.c
@@ -100,6 +100,7 @@ error_message (const location *loc, warnings flags, const char *prefix,
     fprintf (stderr, "%s: ", prefix);
 
   vfprintf (stderr, message, args);
+  warnings_print_categories (flags);
   {
     size_t l = strlen (message);
     if (l < 2 || message[l-2] != ':' || message[l-1] != ' ')
--- src/gram.c
+++ src/gram.c
@@ -312,10 +312,10 @@ grammar_rules_useless_report (const char *message)
       for (r = 0; r < nrules ; ++r)
         if (!rules[r].useful)
           {
-            complain_at (rules[r].location, w, "%s: ", message);
+            complain_at (rules[r].location, w | silent, "%s: ", message);
             rule_print (&rules[r], stderr);
+            warnings_print_categories (w);
             fprintf (stderr, "\n");
-            fflush (stderr);
           }
     }
 }
