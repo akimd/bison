457bf91968a87a01dfb5317b008497ac4dcde8a4
Jim Meyering <meyering@redhat.com>
2012-08-05 13:28:01 +0200

    use locale-indep. c_is* functions for parsing, not isspace, isprint etc
    * src/parse-gram.y: Include "c-ctype.h".
    (add_param): Parse with c_isspace, not isspace.
    * src/parse-gram.c: Likewise.
    * src/scan-gram.l: Include c-ctype.h, not ctype.h.
    (SC_ESCAPED_STRING,SC_ESCAPED_CHARACTER): Use c_isspace and c_isprint,
    not ctype.h's locale-dependent functions.

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -94,6 +94,7 @@
 #include <config.h>
 #include "system.h"
 
+#include "c-ctype.h"
 #include "complain.h"
 #include "conflicts.h"
 #include "files.h"
@@ -3170,11 +3171,11 @@ add_param (char const *type, char *decl, location loc)
   /* Strip the surrounding '{' and '}', and any blanks just inside
      the braces.  */
   --p;
-  while (isspace ((unsigned char) *p))
+  while (c_isspace ((unsigned char) *p))
     --p;
   p[1] = '\0';
   ++decl;
-  while (isspace ((unsigned char) *decl))
+  while (c_isspace ((unsigned char) *decl))
     ++decl;
 
   if (! name_start)
--- src/parse-gram.y
+++ src/parse-gram.y
@@ -20,6 +20,7 @@
 #include <config.h>
 #include "system.h"
 
+#include "c-ctype.h"
 #include "complain.h"
 #include "conflicts.h"
 #include "files.h"
@@ -735,11 +736,11 @@ add_param (char const *type, char *decl, location loc)
   /* Strip the surrounding '{' and '}', and any blanks just inside
      the braces.  */
   --p;
-  while (isspace ((unsigned char) *p))
+  while (c_isspace ((unsigned char) *p))
     --p;
   p[1] = '\0';
   ++decl;
-  while (isspace ((unsigned char) *decl))
+  while (c_isspace ((unsigned char) *decl))
     ++decl;
 
   if (! name_start)
