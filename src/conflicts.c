d140056923f4942c645cf56e0f6940e28113cfdd
Akim Demaille <akim@lrde.epita.fr>
2012-09-21 15:19:12 +0200

    %expect-rr is for GLR only
    * src/conflicts.c (conflicts_print): Complain about %expect-rr if not
    in GLR mode, regardless of the number of reduce/reduce conflicts.
    * tests/conflicts.at (%expect-rr non GLR): New test.
    * NEWS: Update.

--- src/conflicts.c
+++ src/conflicts.c
@@ -576,7 +576,7 @@ conflicts_print (void)
         }
   }
 
-  if (! glr_parser && rrc_total > 0 && expected_rr_conflicts != -1)
+  if (! glr_parser && expected_rr_conflicts != -1)
     {
       complain (Wother, _("%%expect-rr applies only to GLR parsers"));
       expected_rr_conflicts = -1;
