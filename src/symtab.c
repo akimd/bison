b506d9bfcb9e96d07264a43da7bdf136329dbc86
Theophile Ranquet <theophile.ranquet@gmail.com>
2012-09-28 12:12:59 +0000

    errors: indent "user token number redeclaration" context
    This is the continuation of the work on the readability of errors
    context.
    
    * src/symtab.c (user_token_number_redeclaration): Use
    complain_at_indent to output with increased indentation level.
    * tests/input:at: Apply this change.
    
    Signed-off-by: Akim Demaille <akim@lrde.epita.fr>

--- src/symtab.c
+++ src/symtab.c
@@ -534,6 +534,7 @@ user_token_number_redeclaration (int num, symbol *first, symbol *second)
   complain_at_indent (second->location, &i,
                       _("user token number %d redeclaration for %s"),
                       num, second->tag);
+  i += SUB_INDENT;
   complain_at_indent (first->location, &i,
                       _("previous declaration for %s"),
                       first->tag);
