a49f4904c36d585fa057b681c38af4d48de7c23b
Theophile Ranquet <theophile.ranquet@gmail.com>
2012-10-01 13:44:19 +0000

    warnings: remove spurious suffixes on context
    Rectify a bug that introduced suffixes out of place.
    
    * src/complainc.c (complains): Handle all three special warning bits.
    * src/scan-code.l (show_sub_message): Remove useless argument.
    * tests/named-refs.at: Adjust.
    
    Signed-off-by: Akim Demaille <akim@lrde.epita.fr>

--- src/complain.c
+++ src/complain.c
@@ -130,7 +130,7 @@ complains (const location *loc, warnings flags, const char *message,
        error_message (loc, fatal, _("fatal error"), message, args);
        exit (EXIT_FAILURE);
     }
-  else if (flags & (complaint | warnings_flag))
+  else if (flags & (complaint | warnings_flag | silent))
     {
       const char* prefix =
         flags & (errors_flag | complaint) ? _("error") : _("warning");
