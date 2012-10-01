46b7d74cb5d2f4ccefdf92f46d25cd8761324f18
Theophile Ranquet <theophile.ranquet@gmail.com>
2012-10-01 15:01:01 +0000

    formatting changes
    * src/complain.c: Here.
    
    Signed-off-by: Akim Demaille <akim@lrde.epita.fr>

--- src/complain.c
+++ src/complain.c
@@ -90,8 +90,8 @@ error_message (const location *loc, warnings flags, const char *prefix,
   if (loc)
     pos += location_print (stderr, *loc);
   else
-    pos += fprintf(stderr, "%s", current_file ? current_file : program_name);
-  pos += fprintf(stderr, ": ");
+    pos += fprintf (stderr, "%s", current_file ? current_file : program_name);
+  pos += fprintf (stderr, ": ");
 
   if (indent_ptr)
     {
