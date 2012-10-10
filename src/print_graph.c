9fc99ca35009c1be76ce1c73133833ad82562a8b
Theophile Ranquet <theophile.ranquet@gmail.com>
2012-10-10 17:14:04 +0000

    graphs: style: prefix state number with "state"
    * src/print_graph.c (print_core): Here.
    
    Signed-off-by: Akim Demaille <akim@lrde.epita.fr>

--- src/print_graph.c
+++ src/print_graph.c
@@ -55,7 +55,7 @@ print_core (struct obstack *oout, state *s)
       snritems = nitemset;
     }
 
-  obstack_printf (oout, "%d\\n", s->number);
+  obstack_printf (oout, "state %d\\n", s->number);
   for (i = 0; i < snritems; i++)
     {
       item_number *sp;
