2be37f19fe2165fcfd8752a30b55308a5bdb9b84
Theophile Ranquet <theophile.ranquet@gmail.com>
2012-10-10 17:14:02 +0000

    graphs: style: prefix rules and change shapes
    * src/graphviz.c (start_graph): Use box rather than ellipsis.
    * src/print_graph.c (print_core): Prefix rules with their number.
    
    Signed-off-by: Akim Demaille <akim@lrde.epita.fr>

--- src/graphviz.c
+++ src/graphviz.c
@@ -51,6 +51,7 @@ start_graph (FILE *fout)
            "digraph %s\n"
            "{\n",
            quote (grammar_file));
+  fprintf (fout, "node [shape=box]\n");
 }
 
 void
--- src/print_graph.c
+++ src/print_graph.c
@@ -68,7 +68,7 @@ print_core (struct obstack *oout, state *s)
 
       r = item_number_as_rule_number (*sp);
 
-      obstack_printf (oout, "\n%s -> ", rules[r].lhs->tag);
+      obstack_printf (oout, "\n%d: %s -> ", r, rules[r].lhs->tag);
 
       for (sp = rules[r].rhs; sp < sp1; sp++)
 	obstack_printf (oout, "%s ", symbols[*sp]->tag);
