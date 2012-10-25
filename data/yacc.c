4f4a54ab92aeea1b6ea7ebced11ef58a35e090c8
Akim Demaille <akim@lrde.epita.fr>
2012-10-25 14:45:36 +0200

    yacc.c: don't use _Pragma GCC diagnostic with 4.6
    Reported by Peter Simons.
    http://lists.gnu.org/archive/html/bug-bison/2012-10/msg00033.html
    
    * data/yacc.c (b4_declare_scanner_communication_variables): 4.7
    seems fine though.

--- data/yacc.c
+++ data/yacc.c
@@ -171,7 +171,7 @@ m4_define([b4_declare_scanner_communication_variables], [[
 int yychar;
 
 ]b4_pure_if([[
-#if defined __GNUC__ && (4 < __GNUC__ + (6 <= __GNUC_MINOR__))
+#if defined __GNUC__ && 407 <= __GNUC__ * 100 + __GNUC_MINOR__
 /* Suppress an incorrect diagnostic about yylval being uninitialized.  */
 # define YY_IGNORE_MAYBE_UNINITIALIZED_BEGIN \
     _Pragma ("GCC diagnostic push") \
