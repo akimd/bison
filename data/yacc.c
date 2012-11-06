63e8426ac652bd10f45d0e0b576bfae41d779158
Akim Demaille <akim@lrde.epita.fr>
2012-11-06 15:34:56 +0100

    yacc.c: also disable -Wuninitialized.
    * data/yacc.c (YY_IGNORE_MAYBE_UNINITIALIZED_BEGIN): For some versions
    of GCC, -Wmaybe-uninitialized alone does not suffice.

--- data/yacc.c
+++ data/yacc.c
@@ -175,6 +175,7 @@ int yychar;
 /* Suppress an incorrect diagnostic about yylval being uninitialized.  */
 # define YY_IGNORE_MAYBE_UNINITIALIZED_BEGIN \
     _Pragma ("GCC diagnostic push") \
+    _Pragma ("GCC diagnostic ignored \"-Wuninitialized\"")\
     _Pragma ("GCC diagnostic ignored \"-Wmaybe-uninitialized\"")
 # define YY_IGNORE_MAYBE_UNINITIALIZED_END \
     _Pragma ("GCC diagnostic pop")
