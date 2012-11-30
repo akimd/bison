effd30c08d3f9d936a67c6b7a9a4253915711c74
Theophile Ranquet <ranquet@lrde.epita.fr>
2012-11-30 15:27:54 +0100

    getargs: don't label --language/-l as experimental
    * NEWS: Announce it.
    * doc/bison.texi, src/getargs.c (usage): Here.

--- src/getargs.c
+++ src/getargs.c
@@ -307,7 +307,6 @@ Operation modes:\n\
       fputs (_("\
 Parser:\n\
   -L, --language=LANGUAGE          specify the output programming language\n\
-                                   (this is an experimental feature)\n\
   -S, --skeleton=FILE              specify the skeleton to use\n\
   -t, --debug                      instrument the parser for debugging\n\
       --locations                  enable location support\n\
