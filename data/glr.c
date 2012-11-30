5215919366833a0a37bb321189ad9d22398ab260
Akim Demaille <akim@lrde.epita.fr>
2012-11-30 11:15:12 +0100

    glr.c: remove stray macro
    * data/glr.c (YYOPTIONAL_LOC): Remove, unused since commit
    769a8ef9bcb5e14d0be9d0869f5dca20ab093930.

--- data/glr.c
+++ data/glr.c
@@ -306,13 +306,6 @@ b4_percent_code_get[]dnl
 # endif
 #endif
 
-]b4_locations_if([#define YYOPTIONAL_LOC(Name) Name],[
-#ifdef __cplusplus
-# define YYOPTIONAL_LOC(Name) /* empty */
-#else
-# define YYOPTIONAL_LOC(Name) Name __attribute__ ((__unused__))
-#endif])[
-
 #ifndef YYASSERT
 # define YYASSERT(Condition) ((void) ((Condition) || (abort (), 0)))
 #endif
