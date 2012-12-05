e35cd6def7a19423a5f0fc566d844f6019df111a
Theophile Ranquet <ranquet@lrde.epita.fr>
2012-12-05 15:27:25 +0100

    misc: require getline
    * bootstrap.conf: Here, used by src/location.c.
    * src/getargs.c (long_options): Rename --flags to --feature.

--- src/getargs.c
+++ src/getargs.c
@@ -552,7 +552,7 @@ static struct option const long_options[] =
 
   /* Output.  */
   { "defines",     optional_argument,   0,   'd' },
-  { "flag",        optional_argument,   0,   'f' },
+  { "feature",     optional_argument,   0,   'f' },
 
   /* Operation modes.  */
   { "fixed-output-files", no_argument,  0,   'y' },
