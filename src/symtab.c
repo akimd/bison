e96b1b2c452d62d0fe9cef4338c882db0b7d0691
Theophile Ranquet <ranquet@lrde.epita.fr>
2012-12-11 13:23:44 +0100

    symtab: add missing initializations
    * src/symtab.c (semantic_type_new): Here.

--- src/symtab.c
+++ src/symtab.c
@@ -111,8 +111,8 @@ semantic_type_new (uniqstr tag, const location *loc)
 
   uniqstr_assert (tag);
   res->tag = tag;
-  if (loc)
-    res->location = *loc;
+  res->location = loc ? *loc : empty_location;
+  res->status = undeclared;
   for (int i = 0; i < CODE_PROPS_SIZE; ++i)
     code_props_none_init (&res->props[i]);
 
