56441943d0c6e6685368626b31342a81b129f143
Akim Demaille <akim@lrde.epita.fr>
2012-12-19 11:05:56 +0100

    yacc.c: style changes
    * data/yacc.c (b4_lex_param): Provide arguments with a name.

--- data/yacc.c
+++ data/yacc.c
@@ -105,8 +105,8 @@ m4_ifset([b4_parse_param], [b4_args(b4_parse_param), ])])
 # Accumulate in b4_lex_param all the yylex arguments.
 # b4_lex_param arrives quoted twice, but we want to keep only one level.
 m4_define([b4_lex_param],
-m4_dquote(b4_pure_if([[[[YYSTYPE *]], [[&yylval]]][]dnl
-b4_locations_if([, [[YYLTYPE *], [&yylloc]]])m4_ifdef([b4_lex_param], [, ])])dnl
+m4_dquote(b4_pure_if([[[[YYSTYPE *yylval]], [[&yylval]]][]dnl
+b4_locations_if([, [[YYLTYPE *yylloc], [&yylloc]]])m4_ifdef([b4_lex_param], [, ])])dnl
 m4_ifdef([b4_lex_param], b4_lex_param)))
 
 
