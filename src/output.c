55439a1cea2206bde793cbe1a8a0091207cc02dd
Akim Demaille <akim@lrde.epita.fr>
2012-12-22 10:16:49 +0100

    variant: produce stable results
    Improve the output by ensuring a well defined order for type switches.
    
    * src/uniqstr.h: Style changes for macro arguments.
    (UNIQSTR_CMP): Replace by...
    (uniqstr_cmp): this.
    * src/uniqstr.c (uniqstr_cmp): New.
    Produce well defined results.
    * src/output.c: Use it.

--- src/output.c
+++ src/output.c
@@ -284,10 +284,10 @@ prepare_states (void)
 static int
 symbol_type_name_cmp (const symbol **lhs, const symbol **rhs)
 {
-  int res = UNIQSTR_CMP((*lhs)->type_name, (*rhs)->type_name);
-  if (res)
-    return res;
-  return (*lhs)->number - (*rhs)->number;
+  int res = uniqstr_cmp ((*lhs)->type_name, (*rhs)->type_name);
+  if (!res)
+    res = (*lhs)->number - (*rhs)->number;
+  return res;
 }
 
 
--- src/uniqstr.c
+++ src/uniqstr.c
@@ -103,7 +103,17 @@ uniqstr_print_processor (void *ustr, void *null ATTRIBUTE_UNUSED)
   return uniqstr_print (ustr);
 }
 
-
+
+int
+uniqstr_cmp(uniqstr l, uniqstr r)
+{
+  return (l == r ? 0
+          : !l ? -1
+          : !r ? +1
+          : strcmp (l, r));
+}
+
+
 /*-----------------------.
 | A uniqstr hash table.  |
 `-----------------------*/
@@ -119,6 +129,7 @@ hash_uniqstr (void const *m, size_t tablesize)
 {
   return hash_string (m, tablesize);
 }
+
 
 /*----------------------------.
 | Create the uniqstrs table.  |
