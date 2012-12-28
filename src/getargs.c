9c4788b7ee33cf142775a827f67bb9747ba1d2ca
Theophile Ranquet <ranquet@lrde.epita.fr>
2012-12-28 13:32:14 +0100

    carets: activate by default
    * src/getargs.c (feature_flag): Here.
    * tests/local.at (AT_BISON_CHECK_, AT_BISON_CHECK_NO_XML): Deactivate carets
    for the testsuite, by default.
    * tests/input.at: Adjust the locations for command line definitions.

--- src/getargs.c
+++ src/getargs.c
@@ -46,7 +46,7 @@ bool yacc_flag; /* for -y */
 bool nondeterministic_parser = false;
 bool glr_parser = false;
 
-int feature_flag = feature_none;
+int feature_flag = feature_caret;
 int report_flag = report_none;
 int trace_flag = trace_none;
 
