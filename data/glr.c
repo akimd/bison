39ad6cc6acde293499c9501a3bfcb6ac64bdad22
Akim Demaille <akim@lrde.epita.fr>
2013-01-04 16:01:07 +0100

    glr.c: fix an unused argument issue
    * data/glr.c (yyuserAction): "Use" yyrhslen, as in variant mode, we might
    not use it.

--- data/glr.c
+++ data/glr.c
@@ -771,7 +771,8 @@ yyuserAction (yyRuleNum yyn, size_t yyrhslen, yyGLRStackItem* yyvsp,
     (yystackp->yysplitPoint == YY_NULL);
   int yylow;
 ]b4_parse_param_use([yyvalp], [yylocp])dnl
-[# undef yyerrok
+[  YYUSE (yyrhslen);
+# undef yyerrok
 # define yyerrok (yystackp->yyerrState = 0)
 # undef YYACCEPT
 # define YYACCEPT return yyaccept
