bc19eadd65652ce52f85dcd6fb815a0e6f0541e4
Akim Demaille <akim@lrde.epita.fr>
2013-01-04 16:00:59 +0100

    glr.c: style changes
    * data/glr.c (yyuserAction): Use a size_t for sizes.

--- data/glr.c
+++ data/glr.c
@@ -763,7 +763,7 @@ yyfill (yyGLRStackItem *yyvsp, int *yylow, int yylow1, yybool yynormal)
  *  (@@$).  Returns yyok for normal return, yyaccept for YYACCEPT,
  *  yyerr for YYERROR, yyabort for YYABORT.  */
 static YYRESULTTAG
-yyuserAction (yyRuleNum yyn, int yyrhslen, yyGLRStackItem* yyvsp,
+yyuserAction (yyRuleNum yyn, size_t yyrhslen, yyGLRStackItem* yyvsp,
               yyGLRStack* yystackp,
               YYSTYPE* yyvalp]b4_locuser_formals[)
 {
