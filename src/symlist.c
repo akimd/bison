e6c25014bb566595d7de92696c8f67d63dbcf4a9
Akim Demaille <akim@lrde.epita.fr>
2013-02-01 14:23:49 +0100

    symlist: use the right stream
    * src/symlist.c (symbol_list_syms_print): Use "f", not stderr.

--- src/symlist.c
+++ src/symlist.c
@@ -88,7 +88,7 @@ symbol_list_syms_print (const symbol_list *l, FILE *f)
   for (/* Nothing. */; l && l->content.sym; l = l->next)
     {
       symbol_print (l->content.sym, f);
-      fprintf (stderr, l->action_props.is_value_used ? " used" : " unused");
+      fprintf (f, l->action_props.is_value_used ? " used" : " unused");
       if (l && l->content.sym)
         fprintf (f, ", ");
     }
