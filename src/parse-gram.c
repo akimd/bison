66c209cf435b5bfc5cf982d8df9cc3ef05b4546b
Akim Demaille <akim@lrde.epita.fr>
2013-02-05 14:37:55 +0100

    build: restore C90 compatibility
    * src/parse-gram.y, src/parse-gram.c: Don't use // comments.

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -119,7 +119,7 @@ static void version_check (location const *loc, char const *version);
 
 static void gram_error (location const *, char const *);
 
-/// A string that describes a char (e.g., 'a' -> "'a'").
+/* A string that describes a char (e.g., 'a' -> "'a'").  */
 static char const *char_name (char);
 
 #line 126 "src/parse-gram.c" /* yacc.c:357  */
--- src/parse-gram.y
+++ src/parse-gram.y
@@ -46,7 +46,7 @@ static void version_check (location const *loc, char const *version);
 
 static void gram_error (location const *, char const *);
 
-/// A string that describes a char (e.g., 'a' -> "'a'").
+/* A string that describes a char (e.g., 'a' -> "'a'").  */
 static char const *char_name (char);
 %}
 
