848bcc288d88f5d89242cf5c1a75a8da5e03c736
Akim Demaille <akim@lrde.epita.fr>
2013-02-09 16:07:09 +0100

    debug: improve the display of symbol lists
    * src/symtab.c (symbol_print): Remove useless quotes (the symbol already
    has quotes).
    Prefer fputs.
    * src/symlist.c (symbol_list_syms_print): Likewise.
    Fix separators.

--- src/symlist.c
+++ src/symlist.c
@@ -84,12 +84,17 @@ symbol_list_type_new (uniqstr type_name, location loc)
 void
 symbol_list_syms_print (const symbol_list *l, FILE *f)
 {
+  char const *sep = "";
   for (/* Nothing. */; l && l->content.sym; l = l->next)
     {
+      fputs (sep, f);
+      fputs (l->content_type == SYMLIST_SYMBOL ? "symbol: "
+             : l->content_type == SYMLIST_TYPE ? "type: "
+             : "invalid content_type: ",
+             f);
       symbol_print (l->content.sym, f);
-      fprintf (f, l->action_props.is_value_used ? " used" : " unused");
-      if (l && l->content.sym)
-        fprintf (f, ", ");
+      fputs (l->action_props.is_value_used ? " used" : " unused", f);
+      sep = ", ";
     }
 }
 
--- src/symtab.c
+++ src/symtab.c
@@ -153,13 +153,13 @@ symbol_print (symbol const *s, FILE *f)
 {
   if (s)
     {
-      fprintf (f, "\"%s\"", s->tag);
+      fputs (s->tag, f);
       SYMBOL_ATTR_PRINT (type_name);
       SYMBOL_CODE_PRINT (destructor);
       SYMBOL_CODE_PRINT (printer);
     }
   else
-    fprintf (f, "<NULL>");
+    fputs ("<NULL>", f);
 }
 
 #undef SYMBOL_ATTR_PRINT
