8b807f1177634400b307b32f120f16adfd0cead0
Akim Demaille <akim@lrde.epita.fr>
2013-02-16 12:46:09 +0100

    report: use %empty to denote empty rules
    * src/gram.c (rule_rhs_print): Use %empty for empty rules.
    * tests/conflicts.at, tests/regression.at, tests/sets.at: Adjust.

--- src/gram.c
+++ src/gram.c
@@ -93,16 +93,14 @@ rule_rhs_length (rule const *r)
 void
 rule_rhs_print (rule const *r, FILE *out)
 {
-  if (*r->rhs >= 0)
+  if (0 <= *r->rhs)
     {
       item_number *rp;
       for (rp = r->rhs; *rp >= 0; rp++)
         fprintf (out, " %s", symbols[*rp]->tag);
     }
   else
-    {
-      fprintf (out, " /* %s */", _("empty"));
-    }
+    fputs (" %empty", out);
 }
 
 static void
