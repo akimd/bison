bbed3fdef6c4db866f3fe951a2709c2cbea18e04
Akim Demaille <akim@lrde.epita.fr>
2013-02-19 10:07:37 +0100

    regen

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -1,4 +1,4 @@
-/* A Bison parser, made by GNU Bison 2.7.1087-c21e-dirty.  */
+/* A Bison parser, made by GNU Bison 2.7.1116-42fef.  */
 
 /* Bison implementation for Yacc-like parsers in C
 
@@ -44,7 +44,7 @@
 #define YYBISON 1
 
 /* Bison version.  */
-#define YYBISON_VERSION "2.7.1087-c21e-dirty"
+#define YYBISON_VERSION "2.7.1116-42fef"
 
 /* Skeleton name.  */
 #define YYSKELETON_NAME "yacc.c"
--- src/parse-gram.h
+++ src/parse-gram.h
@@ -1,4 +1,4 @@
-/* A Bison parser, made by GNU Bison 2.7.1087-c21e-dirty.  */
+/* A Bison parser, made by GNU Bison 2.7.1116-42fef.  */
 
 /* Bison interface for Yacc-like parsers in C
 
