bb4b189b9c94f66acfe0239b22f295d1314e173e
Akim Demaille <akim@lrde.epita.fr>
2013-04-14 10:28:26 +0200

    muscle: minor simplification which uncovers a missing warning
    * src/muscle-tab.c (muscle_percent_define_ensure): Discover the virtues
    of || to factor conditionals.
    * NEWS: As api.pure is no longer flagged as "used" by accident,
    we now have warnings for useless definitions.
    * tests/calc.at: So remove api.pure settings when running C++ tests,
    since C++ skeletons use a pure interface.

--- src/muscle-tab.c
+++ src/muscle-tab.c
@@ -541,10 +541,8 @@ muscle_percent_define_ensure (char const *variable, location loc,
 
   /* Don't complain is VARIABLE is already defined, but be sure to set
      its value to VAL.  */
-  if (!muscle_find_const (name))
-    muscle_percent_define_insert (variable, loc, muscle_keyword, val,
-                                  MUSCLE_PERCENT_DEFINE_GRAMMAR_FILE);
-  if (muscle_percent_define_flag_if (variable) != value)
+  if (!muscle_find_const (name)
+      || muscle_percent_define_flag_if (variable) != value)
     muscle_percent_define_insert (variable, loc, muscle_keyword, val,
                                   MUSCLE_PERCENT_DEFINE_GRAMMAR_FILE);
 }
