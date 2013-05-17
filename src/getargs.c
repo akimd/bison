30465cb7786a7ded6ac04499204bacee503c3aa3
Petr Machata <pmachata@redhat.com>
2013-05-17 17:23:14 +0200

    drop unused options --raw, -n, -e, --include and -I
    * --raw appears to be ignored.  It was marked as obsolete in the
      commit ec3bc39, and documented as no longer supported as of 1.29
      (2001-09-07).  Support for %raw appears to have been dropped in
      e9955c83 on 2002-06-11, but --raw was kept around.  Maybe it's time
      to drop it as well?
    
    * Commit e9955c83 dropped support for %no-parser as well, and
      converted it to option.  --no-parser was later dropped in 728c4be2
      on 2007-08-12, but -n was kept around, probably as an omission.  All
      three are documented as removed since 2.3b (2008-05-27).
    
    * -e existed for a single day in 2001.  It was introduced in eeeb962b
      on 2001-11-27.  The handling was removed in c7925b99 on 2001-11-28,
      but "e" was kept in the list of short options.  Probably an
      omission.
    
    * --include appears to be dead code.  The option sets a variable, but
      that variable is not used anywhere.  It was added in f6bd5427 on
      2001-11-26 as a %-directive, with comments that it's not yet
      implemented.  It was converted to a command-line option later, but
      doesn't seem to ever have been actually implemented.
    
    Signed-off-by: Petr Machata <pmachata@redhat.com>
    Signed-off-by: Akim Demaille <akim@lrde.epita.fr>

--- src/getargs.c
+++ src/getargs.c
@@ -61,7 +61,6 @@ int skeleton_prio = default_prio;
 const char *skeleton = NULL;
 int language_prio = default_prio;
 struct bison_language const *language = &valid_languages[0];
-const char *include = NULL;
 
 /** Decode an option's key.
  *
@@ -465,12 +464,10 @@ static char const short_options[] =
   "b:"
   "d"
   "f::"
-  "e"
   "g::"
   "h"
   "k"
   "l"
-  "n"
   "o:"
   "p:"
   "r:"
@@ -500,7 +497,6 @@ static struct option const long_options[] =
 
   /* Parser. */
   { "name-prefix",   required_argument,   0,   'p' },
-  { "include",       required_argument,   0,   'I' },
 
   /* Output. */
   { "file-prefix", required_argument,   0,   'b' },
@@ -529,7 +525,6 @@ static struct option const long_options[] =
   { "force-define",   required_argument,         0,   'F' },
   { "locations",      no_argument,               0, LOCATIONS_OPTION },
   { "no-lines",       no_argument,               0,   'l' },
-  { "raw",            no_argument,               0,     0 },
   { "skeleton",       required_argument,         0,   'S' },
   { "language",       required_argument,         0,   'L' },
   { "token-table",    no_argument,               0,   'k' },
@@ -602,10 +597,6 @@ getargs (int argc, char *argv[])
                                         c == 'D' ? MUSCLE_PERCENT_DEFINE_D
                                                  : MUSCLE_PERCENT_DEFINE_F);
         }
-        break;
-
-      case 'I':
-        include = AS_FILE_NAME (optarg);
         break;
 
       case 'L':
