8e13c5c03b26aa518c7ccee997868aa8650e1613
Akim Demaille <akim@lrde.epita.fr>
2013-06-03 16:40:30 +0200

    build: fix a warning from clang
    * src/muscle-tab.c: Declare local functions static.

--- src/muscle-tab.c
+++ src/muscle-tab.c
@@ -557,6 +557,7 @@ muscle_percent_define_use (char const *variable)
 /* The value of %define variable VARIABLE (corresponding to FIELD, if
    defined).  Do not register as used, but diagnose unset variables.  */
 
+static
 char const *
 muscle_percent_define_get_raw (char const *variable, char const *field)
 {
