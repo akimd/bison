9e87ff54729e4d16d61b00b8338a516124ff094d
Akim Demaille <akim@lrde.epita.fr>
2013-09-19 15:00:32 +0200

    glr: formatting changes
    * data/glr.c: here.

--- data/glr.c
+++ data/glr.c
@@ -1080,7 +1080,7 @@ yyexpandGLRStack (yyGLRStack* yystackp)
     }
   if (yystackp->yysplitPoint != YY_NULL)
     yystackp->yysplitPoint = YYRELOC (yystackp->yyitems, yynewItems,
-                                 yystackp->yysplitPoint, yystate);
+                                      yystackp->yysplitPoint, yystate);
 
   for (yyn = 0; yyn < yystackp->yytops.yysize; yyn += 1)
     if (yystackp->yytops.yystates[yyn] != YY_NULL)
