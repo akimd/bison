4c4191cec2b30632a31993a6e46fb241ab9c3434
Andreas Schwab <schwab@linux-m68k.org>
2013-09-28 00:00:34 +0200

    location: fix EOF check
    * location.c (location_caret): Use int, not char, for values from
    getc.

--- src/location.c
+++ src/location.c
@@ -188,7 +188,7 @@ location_caret (location loc, FILE *out)
   /* Read the actual line.  Don't update the offset, so that we keep a pointer
      to the start of the line.  */
   {
-    char c = getc (caret_info.source);
+    int c = getc (caret_info.source);
     if (c != EOF)
       {
         /* Quote the file, indent by a single column.  */
