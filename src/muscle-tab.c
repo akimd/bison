ab96bb245789d1c24a68d637d48ffab889be3152
Akim Demaille <akim@lrde.epita.fr>
2014-02-03 15:01:44 +0100

    build: use abort to pacify compiler errors
    clang, with -DNDEBUG and -Werror fails on some functions that might
    lack a return.  This is because aver is just another assert, discarded
    with -DNDEBUG.  So use abort.
    
    * src/muscle-tab.c, src/scan-skel.l: here.

--- src/muscle-tab.c
+++ src/muscle-tab.c
@@ -37,7 +37,7 @@ muscle_kind_new (char const *k)
     return muscle_keyword;
   else if (STREQ (k, "string"))
     return muscle_string;
-  aver (0);
+  abort ();
 }
 
 char const *
@@ -49,7 +49,7 @@ muscle_kind_string (muscle_kind k)
     case muscle_keyword: return "keyword";
     case muscle_string:  return "string";
     }
-  aver (0);
+  abort ();
 }
 
 
