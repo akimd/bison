8347aabe14765c1a55d2ee40ae574c3be1fceca5
Josh Soref <jsoref@users.noreply.github.com>
2017-07-25 23:12:16 -0400

    spelling: family

--- src/getargs.c
+++ src/getargs.c
@@ -127,7 +127,7 @@ flags_argmatch (const char *opt,
 
 /** Decode a set of sub arguments.
  *
- *  \param FlagName  the flag familly to update.
+ *  \param FlagName  the flag family to update.
  *  \param Args      the effective sub arguments to decode.
  *  \param All       the "all" value.
  *
