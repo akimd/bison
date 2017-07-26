f6120712986d59df5315f1bbb0c40d5b8a951abd
Josh Soref <jsoref@users.noreply.github.com>
2017-07-26 00:28:49 -0400

    spelling: transitions

--- src/graphviz.c
+++ src/graphviz.c
@@ -178,8 +178,8 @@ output_red (state const *s, reductions const *reds, FILE *fout)
 
       bool defaulted = default_reduction && default_reduction == reds->rules[j];
 
-      /* Build the lookahead tokens lists, one for enabled transitions and one
-         for disabled transistions. */
+      /* Build the lookahead tokens lists, one for enabled transitions
+         and one for disabled transitions. */
       bool firstd = true;
       bool firste = true;
       rule_number ruleno = reds->rules[j]->number;
