2e9e5918892b068e46e2b28cb0df8bfe8e822a79
Akim Demaille <akim.demaille@gmail.com>
2018-05-12 18:18:41 +0200

    Update copyright years
    Run `make update-copyright`.

--- lib/ebitset.c
+++ lib/ebitset.c
@@ -1,6 +1,7 @@
 /* Functions to support expandable bitsets.
 
-   Copyright (C) 2002-2006, 2009-2015 Free Software Foundation, Inc.
+   Copyright (C) 2002-2006, 2009-2015, 2018 Free Software Foundation,
+   Inc.
 
    Contributed by Michael Hayes (m.hayes@elec.canterbury.ac.nz).
 
