1e7a8315f6c870f8cf5af66a5cefea027ebb8214
Akim Demaille <akim.demaille@gmail.com>
2018-05-27 10:57:52 +0200

    bison: style: indentation fixes
    * src/parse-gram.y: here.

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -3057,7 +3057,7 @@ add_param (param_type type, char *decl, location loc)
       --p;
     p[1] = '\0';
     ++decl;
-  while (c_isspace ((unsigned char) *decl))
+    while (c_isspace ((unsigned char) *decl))
       ++decl;
   }
 
