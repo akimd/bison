27767290da059416b4c19ae50c9ccda0528f0686
Akim Demaille <akim.demaille@gmail.com>
2018-06-17 18:34:31 +0200

    report: display the type of the symbols
    * src/print.c (print_nonterminal_symbols, print_terminal_symbols):
    Also should the type of the symbols.

--- src/print.c
+++ src/print.c
@@ -420,6 +420,12 @@ print_terminal_symbols (FILE *out)
         buffer[0] = 0;
         fputs (tag, out);
         END_TEST (65);
+        if (symbols[token_translations[i]]->content->type_name)
+          {
+            column += fprintf (out, " <%s>",
+                               symbols[token_translations[i]]->content->type_name);
+            END_TEST (65);
+          }
         sprintf (buffer, " (%d)", i);
 
         for (r = 0; r < nrules; r++)
@@ -465,6 +471,9 @@ print_nonterminal_symbols (FILE *out)
         }
 
       fputs (tag, out);
+      if (symbols[i]->content->type_name)
+        column += fprintf (out, " <%s>",
+                           symbols[i]->content->type_name);
       sprintf (buffer, " (%d)", i);
       END_TEST (0);
 
