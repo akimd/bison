0def17c3d1d008f660845556f5456d505ef73747
Akim Demaille <akim.demaille@gmail.com>
2018-07-09 13:26:56 +0200

    print: remove unused function
    This function was unused since 1991's original import by
    rms (e06f0c34427faedc7afbec9554adbffc4c87312e).
    
    * src/print.c (print_token): Remove.

--- src/print.c
+++ src/print.c
@@ -40,14 +40,6 @@
 
 static bitset no_reduce_set;
 
-#if 0
-static void
-print_token (int extnum, int token)
-{
-  fprintf (out, _(" type %d is %s\n"), extnum, tags[token]);
-}
-#endif
-
 
 
 /*---------------------------------------.
