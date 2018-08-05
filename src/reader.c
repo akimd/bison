da8f4a2f5f69f094949c8bf0c7f9eca4493735ff
Akim Demaille <akim.demaille@gmail.com>
2018-08-05 10:42:15 +0200

    rule actions cannot be typed
    Make sure that we cannot apply a type to the (main) action of a rule.
    
    * src/reader.c (grammar_rule_check): Issue the warning.
    * tests/input.at (Cannot type action): Check the warning.

--- src/reader.c
+++ src/reader.c
@@ -351,6 +351,11 @@ grammar_rule_check (const symbol_list *r)
       && !r->ruleprec->content->prec)
     complain (&r->location, Wother,
               _("token for %%prec is not defined: %s"), r->ruleprec->tag);
+
+  /* Check that the (main) action was not typed.  */
+  if (r->action_props.type)
+    complain (&r->location, Wother,
+              _("only midrule actions can be typed: %s"), r->action_props.type);
 }
 
 
