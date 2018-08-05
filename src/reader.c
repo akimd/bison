f18f71cfb077d594b0ef3e9d1ec39aedead38c7b
Akim Demaille <akim.demaille@gmail.com>
2018-08-05 10:18:52 +0200

    warn about typed mid-rule actions in Yacc mode
    * src/reader.c (grammar_current_rule_action_append): Warn.
    * tests/input.at (AT_CHECK_UNUSED_VALUES): Check.

--- src/reader.c
+++ src/reader.c
@@ -528,6 +528,9 @@ grammar_current_rule_action_append (const char *action, location loc,
 {
   if (current_rule->action_props.code)
     grammar_midrule_action ();
+  if (type)
+    complain (&loc, Wyacc,
+              _("POSIX Yacc does not support typed midrule actions"));
   /* After all symbol declarations have been parsed, packgram invokes
      code_props_translate_code.  */
   code_props_rule_action_init (&current_rule->action_props, action, loc,
