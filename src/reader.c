akimd/bison

commit 3df32101e7978eaafa63bce8908de3dcae4d9cda

warnings: address -Wnull-dereference in reader.c
Based on a patch by David Michael.
http://lists.gnu.org/archive/html/bison-patches/2018-07/msg00000.html

* src/reader.c (find_start): New, extracted from...
(check_and_convert_grammar): here.

src/reader.c
@@ -709,6 +709,26 @@ prepare_percent_define_front_end_variables (void)
   }
 }
 
+/* Find the first LHS which is not a dummy.  */
+
+static symbol *
+find_start_symbol ()
+{
+  symbol_list *res = grammar;
+  for (;
+       res != NULL && symbol_is_dummy (res->content.sym);
+       res = res->next)
+    {
+      for (res = res->next;
+           res != NULL && res->content.sym != NULL;
+           res = res->next)
+        continue;
+      aver (res != NULL);
+    }
+  aver (res != NULL);
+  return res->content.sym;
+}
+
 
 /*-------------------------------------------------------------.
 | Check the grammar that has just been read, and convert it to |
@@ -738,19 +758,8 @@ check_and_convert_grammar (void)
   /* Find the start symbol if no %start.  */
   if (!start_flag)
     {
-      symbol_list *node;
-      for (node = grammar;
-           node != NULL && symbol_is_dummy (node->content.sym);
-           node = node->next)
-        {
-          for (node = node->next;
-               node != NULL && node->content.sym != NULL;
-               node = node->next)
-            continue;
-        }
-      aver (node != NULL);
-      grammar_start_symbol_set (node->content.sym,
-                                node->content.sym->location);
+      symbol *start = find_start_symbol ();
+      grammar_start_symbol_set (start, start->location);
     }
 
   /* Insert the initial rule, whose line is that of the first rule

THANKS
@@ -39,6 +39,7 @@ Daniel Galloway           dg1751@att.com
 Daniel Hagerty            hag@gnu.org
 David J. MacKenzie        djm@gnu.org
 David Kastrup             dak@gnu.org
+David Michael             fedora.dm0@gmail.com
 Dennis Clarke             dclarke@blastwave.org
 Derek Clegg               derek@me.com
 Derek M. Jones            derek@knosof.co.uk

commit 7783ba2d4f73d6e71067a79b4becc5a48d92db16

fix incorrect C code
Commit 3df32101e7978eaafa63bce8908de3dcae4d9cda introduced invalid C
code.  Caught by GCC 7.3.0.

* bootstrap.conf (gnulib_modules): We need assume.
* src/reader.c (find_start_symbol): Fix the signature (too much C++,
sorry...).
Prefer 'assume' to 'assert', so that we don't have these warnings even
when NDEBUG is defined.

bootstrap.conf
@@ -17,7 +17,8 @@
 
 # gnulib modules used by this package.
 gnulib_modules='
-  argmatch assert calloc-posix close closeout config-h c-strcase
+  argmatch assert assume
+  calloc-posix close closeout config-h c-strcase
   configmake
   dirname
   error extensions fdl fopen-safer

src/reader.c
@@ -726,20 +726,20 @@ prepare_percent_define_front_end_variables (void)
 /* Find the first LHS which is not a dummy.  */
 
 static symbol *
-find_start_symbol ()
+find_start_symbol (void)
 {
   symbol_list *res = grammar;
   for (;
-       res != NULL && symbol_is_dummy (res->content.sym);
+       res && symbol_is_dummy (res->content.sym);
        res = res->next)
     {
       for (res = res->next;
-           res != NULL && res->content.sym != NULL;
+           res && res->content.sym;
            res = res->next)
         continue;
-      aver (res != NULL);
+      assume (res);
     }
-  aver (res != NULL);
+  assume (res);
   return res->content.sym;
 }
 
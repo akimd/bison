7783ba2d4f73d6e71067a79b4becc5a48d92db16
Akim Demaille <akim.demaille@gmail.com>
2018-08-15 13:51:36 +0200

    fix incorrect C code
    Commit 3df32101e7978eaafa63bce8908de3dcae4d9cda introduced invalid C
    code.  Caught by GCC 7.3.0.
    
    * bootstrap.conf (gnulib_modules): We need assume.
    * src/reader.c (find_start_symbol): Fix the signature (too much C++,
    sorry...).
    Prefer 'assume' to 'assert', so that we don't have these warnings even
    when NDEBUG is defined.

--- src/reader.c
+++ src/reader.c
@@ -726,20 +726,20 @@ prepare_percent_define_front_end_variables (void)
 /* Find the first LHS which is not a dummy.  */
 
 static symbol *
-find_start_symbol ()
+find_start_symbol (void)
 {
   symbol_list *res = grammar;
   for (;
-       res != NULL && symbol_is_dummy (res->content.sym);
+       res && symbol_is_dummy (res->content.sym);
        res = res->next)
     {
       for (res = res->next;
-           res != NULL && res->content.sym != NULL;
+           res && res->content.sym;
            res = res->next)
         continue;
-      aver (res != NULL);
+      assume (res);
     }
-  aver (res != NULL);
+  assume (res);
   return res->content.sym;
 }
 
