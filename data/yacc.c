b01b80836d02d6f10cac5a0a9f3a03889ad02fa0
Akim Demaille <akim.demaille@gmail.com>
2018-08-15 18:58:03 +0200

    tests: fix warnings in push mode
    Fix warning with GCC 8, -DNDEBUG.
    
        422. push.at:83: testing Multiple impure instances ...
        input.y: In function 'main':
        input.c:1022:12: error: potential null pointer dereference [-Werror=null-dereference]
           if (!yyps->yynew && yyps->yyss != yyps->yyssa)
                ~~~~^~~~~~~
    
    * data/yacc.c (pstate_delete): Do nothing if called on null pointer.

--- data/yacc.c
+++ data/yacc.c
@@ -1326,16 +1326,19 @@ b4_function_define([[yyparse]], [[int]], b4_parse_param)[
 ]b4_function_define([[yypstate_delete]], [[void]],
                    [[[yypstate *yyps]], [[yyps]]])[
 {
+  if (yyps)
+    {
 #ifndef yyoverflow
-  /* If the stack was reallocated but the parse did not complete, then the
-     stack still needs to be freed.  */
-  if (!yyps->yynew && yyps->yyss != yyps->yyssa)
-    YYSTACK_FREE (yyps->yyss);
+      /* If the stack was reallocated but the parse did not complete, then the
+         stack still needs to be freed.  */
+      if (!yyps->yynew && yyps->yyss != yyps->yyssa)
+        YYSTACK_FREE (yyps->yyss);
 #endif]b4_lac_if([[
-  if (!yyps->yynew && yyps->yyes != yyps->yyesa)
-    YYSTACK_FREE (yyps->yyes);]])[
-  free (yyps);]b4_pure_if([], [[
-  yypstate_allocated = 0;]])[
+      if (!yyps->yynew && yyps->yyes != yyps->yyesa)
+        YYSTACK_FREE (yyps->yyes);]])[
+      free (yyps);]b4_pure_if([], [[
+      yypstate_allocated = 0;]])[
+    }
 }
 ]b4_pure_if([[
 #define ]b4_prefix[nerrs yyps->]b4_prefix[nerrs]])[
