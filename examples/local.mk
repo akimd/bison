32e0fd1a99308d5311ec8508b558cb2401863d23
Akim Demaille <akim.demaille@gmail.com>
2018-08-19 17:34:27 +0200

    examples: add empty lines
    Currently the examples are too dense, let's put empty lines where
    '#line' would be issued.  And also remove some spurious empty
    lines (remains from @group, @end group, etc.).
    
    * examples/extexi: Do that.
    * examples/local.mk (extexiFLAGS): Rename as...
    (EXTEXIFLAGS): this.

--- examples/local.mk
+++ examples/local.mk
@@ -26,9 +26,9 @@ AM_CXXFLAGS =							\
 doc = $(top_srcdir)/doc/bison.texi
 extexi = $(top_srcdir)/%D%/extexi
 if ENABLE_GCC_WARNINGS
-extexiFLAGS = --synclines
+EXTEXIFLAGS = --synclines
 endif
-extract = VERSION="$(VERSION)" $(PERL) $(extexi) $(extexiFLAGS) $(doc) --
+extract = VERSION="$(VERSION)" $(PERL) $(extexi) $(EXTEXIFLAGS) $(doc) --
 extracted =
 CLEANFILES += $(extracted) %D%/extracted.stamp
 %D%/extracted.stamp: $(doc) $(extexi)
