10082ac068b9895c0ace2af5f7bb296dc68b9862
Akim Demaille <akim.demaille@gmail.com>
2018-08-25 10:57:38 +0200

    "C++: restore copy-constructor for stack_symbol_type
    Benchmarks show that it is more efficient to keep this copy
    constructor, rather than forcing the use of the default constructor
    and then assignment.
    
    This reverts commit 7ab25ad0208d00f509613e1e151aa3043cf2862f.

--- data/lalr1.cc
+++ data/lalr1.cc
@@ -314,6 +314,8 @@ b4_location_define])])[
       typedef basic_symbol<by_state> super_type;
       /// Construct an empty symbol.
       stack_symbol_type ();
+      /// Copy construct (for efficiency).
+      stack_symbol_type (const stack_symbol_type& that);
       /// Steal the contents from \a sym to build this.
       stack_symbol_type (state_type s, symbol_type& sym);
       /// Assignment, needed by push_back.
@@ -581,6 +583,14 @@ m4_if(b4_prefix, [yy], [],
 
   ]b4_parser_class_name[::stack_symbol_type::stack_symbol_type ()
   {}
+
+  ]b4_parser_class_name[::stack_symbol_type::stack_symbol_type (const stack_symbol_type& that)
+    : super_type (that.state]b4_locations_if([, that.location])[)
+  {
+    ]b4_variant_if([b4_symbol_variant([that.type_get ()],
+                                      [value], [copy], [that.value])],
+                   [[value = that.value;]])[
+  }
 
   ]b4_parser_class_name[::stack_symbol_type::stack_symbol_type (state_type s, symbol_type& that)
     : super_type (s]b4_variant_if([], [, that.value])[]b4_locations_if([, that.location])[)
