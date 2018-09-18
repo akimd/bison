fb1e27cf479608161263488daaa5ac95d6c239cc
Akim Demaille <akim.demaille@gmail.com>
2018-09-18 06:12:11 +0200

    glr.c: prefer true/false to 1/0 in C++
    * data/glr.c: here.

--- data/glr.c
+++ data/glr.c
@@ -292,12 +292,14 @@ b4_percent_code_get[]dnl
 #define YYSIZEMAX ((size_t) -1)
 
 #ifdef __cplusplus
-   typedef bool yybool;
+  typedef bool yybool;
+# define yytrue true
+# define yyfalse false
 #else
-   typedef unsigned char yybool;
+  typedef unsigned char yybool;
+# define yytrue 1
+# define yyfalse 0
 #endif
-#define yytrue 1
-#define yyfalse 0
 
 #ifndef YYSETJMP
 # include <setjmp.h>
