a99021358224ffd3ba013df3db64c8e1884a122c
Akim Demaille <akim.demaille@gmail.com>
2018-09-22 11:12:54 +0200

    timevar: don't declare getrusage if we don't use it
    This fails on MinGW.
    Reported by Simon Sobisch.
    http://lists.gnu.org/archive/html/bug-bison/2018-09/msg00058.html
    
    * lib/timevar.c: Don't provide default prototypes for functions
    we don't use.

--- lib/timevar.c
+++ lib/timevar.c
@@ -48,16 +48,6 @@ struct tms
 };
 #endif
 
-#if defined HAVE_DECL_GETRUSAGE && !HAVE_DECL_GETRUSAGE
-int getrusage (int, struct rusage *);
-#endif
-#if defined HAVE_DECL_TIMES && !HAVE_DECL_TIMES
-clock_t times (struct tms *);
-#endif
-#if defined HAVE_DECL_CLOCK && !HAVE_DECL_CLOCK
-clock_t clock (void);
-#endif
-
 /* Calculation of scale factor to convert ticks to microseconds.
    We mustn't use CLOCKS_PER_SEC except with clock().  */
 #if HAVE_SYSCONF && defined _SC_CLK_TCK
@@ -84,6 +74,14 @@ clock_t clock (void);
 #elif defined HAVE_CLOCK
 # define USE_CLOCK
 # define HAVE_USER_TIME
+#endif
+
+#if defined USE_TIMES && !defined HAVE_DECL_TIMES
+clock_t times (struct tms *);
+#elif defined USE_GETRUSAGE && !defined HAVE_DECL_GETRUSAGE
+int getrusage (int, struct rusage *);
+#elif defined USE_CLOCK && !defined HAVE_DECL_CLOCK
+clock_t clock (void);
 #endif
 
 /* libc is very likely to have snuck a call to sysconf() into one of
@@ -172,7 +170,7 @@ set_to_current_time (struct timevar_time_def *now)
   {
 #ifdef USE_TIMES
     struct tms tms;
-    now->wall = times (&tms)  * ticks_to_msec;
+    now->wall = times (&tms) * ticks_to_msec;
     now->user = (tms.tms_utime + tms.tms_cutime) * ticks_to_msec;
     now->sys  = (tms.tms_stime + tms.tms_cstime) * ticks_to_msec;
 #elif defined USE_GETRUSAGE
