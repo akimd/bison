d03b1a7e8c2d14b8a838135f7e577dd687e1bc3c
Paul Eggert <eggert@cs.ucla.edu>
2018-09-28 16:41:09 -0700

    getargs: use LC_MESSAGES trick only on glibc
    * src/getargs.c (usage): Rely on setlocale (LC_MESSAGES, NULL)
    trick only on glibc, as POSIX does not specify the output
    of setlocale in this case, and the Gnulib localename module
    source code indicates that the trick works only on glibc.

--- src/getargs.c
+++ src/getargs.c
@@ -360,6 +360,8 @@ FEATURE is a list of comma separated words that can include:\n\
       fputs (_("General help using GNU software: "
                "<http://www.gnu.org/gethelp/>.\n"),
              stdout);
+
+#if (defined __GLIBC__ && __GLIBC__ >= 2) && !defined __UCLIBC__
       /* Don't output this redundant message for English locales.
          Note we still output for 'C' so that it gets included in the
          man page.  */
@@ -372,6 +374,7 @@ FEATURE is a list of comma separated words that can include:\n\
            email address.  */
         fputs (_("Report translation bugs to "
                  "<http://translationproject.org/team/>.\n"), stdout);
+#endif
       fputs (_("For complete documentation, run: info bison.\n"), stdout);
     }
 
