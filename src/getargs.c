26859f6d61d259e0da674f4225f398673968951f
Akim Demaille <akim.demaille@gmail.com>
2018-10-04 21:38:32 +0200

    main: fix error message for missing argument
    * src/getargs.c (getargs): Don't display any argv other that argv[0]
    when reporting a missing argument.
    * tests/bison.in: Neutralize path differences in stderr.
    * tests/input.at (Invalid number of arguments): New.

--- src/getargs.c
+++ src/getargs.c
@@ -724,7 +724,7 @@ getargs (int argc, char *argv[])
   if (argc - optind != 1)
     {
       if (argc - optind < 1)
-        error (0, 0, _("%s: missing operand"), quotearg_colon (argv[argc - 1]));
+        error (0, 0, _("missing operand"));
       else
         error (0, 0, _("extra operand %s"), quote (argv[optind + 1]));
       usage (EXIT_FAILURE);
