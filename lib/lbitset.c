5cdbc97c9d6a35e031d49a41dcb51db708015dcd
Akim Demaille <akim.demaille@gmail.com>
2018-10-16 18:51:50 +0200

    pacify syntax-checks
    * lib/lbitset.c, tests/c++.at: here.
    * cfg.mk: Add exceptions.

--- lib/lbitset.c
+++ lib/lbitset.c
@@ -887,7 +887,7 @@ lbitset_ones (bitset dst)
      bitset!  It makes a sparse bitset become dense.  An alternative
      is to have a flag that indicates that the bitset stores the
      complement of what it indicates.  */
-  
+
   bitset_windex windex
     = (BITSET_SIZE_ (dst) + BITSET_WORD_BITS - 1) / BITSET_WORD_BITS;
 
