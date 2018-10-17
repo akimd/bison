03a13ce793b88015626a7e636c356262ae885e34
Akim Demaille <akim.demaille@gmail.com>
2018-10-17 17:39:41 +0200

    reader: recognize C++ even when it's not lalr1.cc or glr.cc
    * src/reader.c (grammar_rule_check_and_complete): If a user uses her
    own skeleton but sets the language to C++, recognize it as C++.

--- src/reader.c
+++ src/reader.c
@@ -307,10 +307,10 @@ grammar_rule_check_and_complete (symbol_list *r)
           else
             {
               /* Install the default action only for C++.  */
-              bool is_cxx =
-                skeleton
-                ? STREQ (skeleton, "glr.cc") || STREQ (skeleton, "lalr1.cc")
-                : STREQ (language->language, "c++");
+              const bool is_cxx =
+                STREQ (language->language, "c++")
+                || (skeleton && (STREQ (skeleton, "glr.cc")
+                                 || STREQ (skeleton, "lalr1.cc")));
               if (is_cxx)
                 {
                   code_props_rule_action_init (&r->action_props, "{ $$ = $1; }",
