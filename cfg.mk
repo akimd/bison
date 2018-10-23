7c4b40de6195165367b733afda76e4944392f1f7
Akim Demaille <akim.demaille@gmail.com>
2018-10-23 20:10:54 +0200

    build: remove a few copies of the Copyright from the generated Makefile
    * build-aux/local.mk, cfg.mk, examples/calc++/local.mk,
    * examples/local.mk, examples/mfcalc/local.mk,
    * examples/rpcalc/local.mk, lib/local.mk, src/local.mk,
    * tests/local.mk:
    Use Automake comments so that we don't get a copy of each in the
    generated Makefile.

--- cfg.mk
+++ cfg.mk
@@ -1,18 +1,18 @@
-# Customize maint.mk                           -*- makefile -*-
-# Copyright (C) 2008-2015, 2018 Free Software Foundation, Inc.
+## Customize maint.mk                           -*- makefile -*-
+## Copyright (C) 2008-2015, 2018 Free Software Foundation, Inc.
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Update version, then recompile so that tests/bison --version be
 # up-to-date, then compile our parser again with our up-to-date bison.
