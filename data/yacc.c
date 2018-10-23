9d8fc7fd3ec43a781a610f56943cce3bec1593b0
Akim Demaille <akim.demaille@gmail.com>
2018-10-23 08:50:44 +0200

    yacc.c: work around strange typing issues
    On the CI, both GCC and Clang report:
    
        src/parse-gram.c: In function 'yy_lac':
        src/parse-gram.c:1479:29: error: format '%hd' expects argument of type 'int',
           but argument 3 has type 'yytype_int16 {aka long int}' [-Werror=format=]
                 YYDPRINTF ((stderr, " G%hd", yystate));
                                     ^
    
    Although yytype_int16 is supposed to be a short int, not a long int.
    This must be explored.
    
    * data/yacc.c (yy_lac): Work around typing issue.

--- data/yacc.c
+++ data/yacc.c
@@ -997,7 +997,7 @@ yy_lac (yytype_int16 *yyesa, yytype_int16 **yyes,
               }
             *++yyesp = yystate;
           }
-        YYDPRINTF ((stderr, " G%hd", yystate));
+        YYDPRINTF ((stderr, " G%d", (int) yystate));
       }
     }
 }]])[
