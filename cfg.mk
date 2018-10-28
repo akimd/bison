c8512e09e7b8d2f0ac326a4521044812814397ec
Akim Demaille <akim.demaille@gmail.com>
2018-10-28 18:02:50 +0100

    cfg.mk: remove exceptions for timevar
    They appear to be no longer needed.
    
    * cfg.mk: here.

--- cfg.mk
+++ cfg.mk
@@ -163,12 +163,9 @@ $(call exclude,                                                                 
   preprocessor_indentation=^data/|^lib/|^src/parse-gram.[ch]$$                          \
   program_name=^lib/main.c$$                                                            \
   prohibit_always-defined_macros=^data/yacc.c$$|^djgpp/                                 \
-  prohibit_always-defined_macros+=?|^lib/timevar.c$$                                    \
   prohibit_always-defined_macros+=?|^src/(parse-gram.c|system.h)$$                      \
   prohibit_always-defined_macros+=?|^tests/regression.at$$                              \
-  prohibit_always_true_header_tests=^djgpp/subpipe.h$$|^lib/timevar.c$$                 \
-  prohibit_always_true_header_tests+=?|^m4/timevar.m4$$                                 \
-  prohibit_defined_have_decl_tests=?|^lib/timevar.c$$                                   \
+  prohibit_always_true_header_tests=^djgpp/subpipe.h$$                                  \
   prohibit_doubled_word=^tests/named-refs.at$$                                          \
   prohibit_magic_number_exit=^doc/bison.texi$$                                          \
   prohibit_magic_number_exit+=?|^tests/(conflicts|regression).at$$                      \
