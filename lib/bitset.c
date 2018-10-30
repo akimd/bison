5d07f4f726de562778ab358d4bdf4fe8bae517bb
Akim Demaille <akim.demaille@gmail.com>
2018-10-30 07:00:34 +0100

    bitset: fix warning
    Reported by Hans Åberg.
    http://lists.gnu.org/archive/html/bug-bison/2018-10/msg00047.html
    
    * lib/bitset.c (bitset_count_): here.

--- lib/bitset.c
+++ lib/bitset.c
@@ -353,7 +353,7 @@ bitset_count_ (bitset src)
   {
     bitset_bindex next = 0;
     bitset_bindex num;
-    while (num = bitset_list (src, list, BITSET_LIST_SIZE, &next))
+    while ((num = bitset_list (src, list, BITSET_LIST_SIZE, &next)))
       count += num;
   }
 
