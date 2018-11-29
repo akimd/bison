6220e96e76301e4f06452ea1df368dec9da332d7
Akim Demaille <akim.demaille@gmail.com>
2018-11-29 21:21:13 +0100

    style: reduce scopes
    * src/symlist.c: here.

--- src/symlist.c
+++ src/symlist.c
@@ -174,9 +174,8 @@ symbol_list_length (symbol_list const *l)
 symbol_list *
 symbol_list_n_get (symbol_list *l, int n)
 {
-  int i;
   aver (0 <= n);
-  for (i = 0; i < n; ++i)
+  for (int i = 0; i < n; ++i)
     {
       l = l->next;
       aver (l);
