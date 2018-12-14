akimd/bison

commit 20b07467938cf880a1d30eb30d6e191843a21fec

parser: improve the error message for symbol class redefinition
Currently our error messages include both "symbol redeclared" and
"symbol redefined", and they mean something different.  This is
obscure, let's make this clearer.

I think the idea between 'definition' vs. 'declaration' is that in the
case of the nonterminals, the actual definition is its set of rules,
so %nterm would be about declaration.  The case of %token is less
clear.

* src/symtab.c (complain_class_redefined): New.
(symbol_class_set): Use it.
Simplify the logic of this function to clearly skip its body when the
preconditions are not met.
* tests/input.at (Symbol class redefinition): New.

src/symtab.c
@@ -302,6 +302,19 @@ semantic_type_redeclaration (semantic_type *s, const char *what, location first,
                    _("previous declaration"));
 }
 
+static void
+complain_class_redeclared (symbol *sym, symbol_class class, location second)
+{
+  unsigned i = 0;
+  complain_indent (&second, complaint, &i,
+                   class == token_sym
+                   ? _("symbol %s redeclared as a token")
+                   : _("symbol %s redeclared as a nonterminal"), sym->tag);
+  i += SUB_INDENT;
+  complain_indent (&sym->location, complaint, &i,
+                   _("previous definition"));
+}
+
 
 void
 symbol_location_as_lhs_set (symbol *sym, location loc)
@@ -430,27 +443,26 @@ symbol_precedence_set (symbol *sym, int prec, assoc a, location loc)
 void
 symbol_class_set (symbol *sym, symbol_class class, location loc, bool declaring)
 {
-  bool warned = false;
-  if (sym->content->class != unknown_sym && sym->content->class != class)
+  aver (class != unknown_sym);
+  sym_content *s = sym->content;
+  if (s->class != unknown_sym && s->class != class)
+    complain_class_redeclared (sym, class, loc);
+  else
     {
-      complain (&loc, complaint, _("symbol %s redefined"), sym->tag);
-      /* Don't report both "redefined" and "redeclared".  */
-      warned = true;
-    }
+      s->class = class;
 
-  if (class == nterm_sym && sym->content->class != nterm_sym)
-    sym->content->number = nvars++;
-  else if (class == token_sym && sym->content->number == NUMBER_UNDEFINED)
-    sym->content->number = ntokens++;
+      if (class == nterm_sym && s->class != nterm_sym)
+        s->number = nvars++;
+      else if (class == token_sym && s->number == NUMBER_UNDEFINED)
+        s->number = ntokens++;
 
-  sym->content->class = class;
-
-  if (declaring)
-    {
-      if (sym->content->status == declared && !warned)
-        complain (&loc, Wother, _("symbol %s redeclared"), sym->tag);
-      else
-        sym->content->status = declared;
+      if (declaring)
+        {
+          if (s->status == declared)
+            complain (&loc, Wother, _("symbol %s redeclared"), sym->tag);
+          else
+            s->status = declared;
+        }
     }
 }
 

tests/input.at
@@ -483,6 +483,45 @@ AT_CHECK_UNUSED_VALUES([1], [1])
 AT_CLEANUP
 
 
+## --------------------------- ##
+## Symbol class redefinition.  ##
+## --------------------------- ##
+
+AT_SETUP([Symbol class redefinition])
+
+AT_DATA([[input.y]],
+[[%token FOO
+%nterm FOO BAR
+%token BAR
+%%
+FOO: BAR
+BAR:
+]])
+
+AT_BISON_CHECK([-fcaret input.y], [1], [],
+[[input.y:2.1-6: warning: deprecated directive, use '%type' [-Wdeprecated]
+ %nterm FOO BAR
+ ^^^^^^
+input.y:2.8-10: error: symbol FOO redeclared as a nonterminal
+ %nterm FOO BAR
+        ^^^
+input.y:1.8-10:     previous definition
+ %token FOO
+        ^^^
+input.y:3.8-10: error: symbol BAR redeclared as a token
+ %token BAR
+        ^^^
+input.y:2.12-14:    previous definition
+ %nterm FOO BAR
+            ^^^
+input.y:5.1-3: error: rule given for FOO, which is a token
+ FOO: BAR
+ ^^^
+]])
+
+AT_CLEANUP
+
+
 ## --------------------------------------------- ##
 ## Default %printer and %destructor redeclared.  ##
 ## --------------------------------------------- ##

TODO
@@ -7,6 +7,11 @@ Several features are not available in all the backends.
 - token constructors: Java and C
 
 * Short term
+** consistency
+nonterminal vs non terminal vs non-terminal
+token vs terminal
+redeclaration vs redefinition
+
 ** yacc.c
 Now that ylwrap is fixed, we should include foo.tab.h from foo.tab.c rather
 than duplicating it.

commit aadf6c0bf355359c7cee6e69d80a4b9c8ab91340

parser: reprecate %nterm back
After having spent quite some time on cleaning the handling of symbol
declarations in the grammar files, I believe we should keep it.

It looks like it's a duplicate of %type, but it is not.  While POSIX
Yacc requires %type to apply only to nonterminal symbols, it appears
that both byacc and bison accept it for tokens too.  And some
experienced users do actually expect this feature to group
symbols (terminal or not) by type ("On the other hand, it is generally
more useful IMHO to group terminals and non-terminals with the same
type tag together",
http://lists.gnu.org/archive/html/bug-bison/2018-10/msg00000.html).
Even Bison's own parser does this today (see CHAR).

Basically reverts 7928c3e6fbdf47ff81184966cee937e6aa694b94.

* src/scan-gram.l (%nterm): Dedeprecate, but issue a Wyacc warning.
* tests/input.at: Adjust expectations.
(Yacc warnings  on symbols): New.
* src/symtab.c (symbol_class_set): Fix error introduced in
20b07467938cf880a1d30eb30d6e191843a21fec.

src/parse-gram.y
@@ -202,7 +202,7 @@
 %printer { fprintf (yyo, "%d", $$); } <int>
 
 %type <symbol*> id id_colon string_as_id symbol symbol.prec
-%printer { fprintf (yyo, "%s", $$->tag); } <symbol*>
+%printer { fprintf (yyo, "%s", $$ ? $$->tag : "<NULL>"); } <symbol*>
 %printer { fprintf (yyo, "%s:", $$->tag); } id_colon
 
 %type <assoc> precedence_declarator

src/scan-gram.l
@@ -243,6 +243,7 @@ eqopt    ([[:space:]]*=)?
   "%no-lines"                       return BISON_DIRECTIVE(NO_LINES);
   "%nonassoc"                       return PERCENT_NONASSOC;
   "%nondeterministic-parser"        return BISON_DIRECTIVE(NONDETERMINISTIC_PARSER);
+  "%nterm"                          return BISON_DIRECTIVE(NTERM);
   "%output"                         return BISON_DIRECTIVE(OUTPUT);
   "%param"                          RETURN_PERCENT_PARAM(both);
   "%parse-param"                    RETURN_PERCENT_PARAM(parse);
@@ -278,12 +279,6 @@ eqopt    ([[:space:]]*=)?
   "%output"{eqopt}                  DEPRECATED("%output");
   "%pure"[-_]"parser"               DEPRECATED("%pure-parser");
   "%token"[-_]"table"               DEPRECATED("%token-table");
-
-  "%nterm"  {
-    /* Deprecated since Bison 3.3, but was a rather stealth feature.  */
-    deprecated_directive (loc, yytext, "%type");
-    return PERCENT_NTERM;
-  }
 
   "%"{id} {
     complain (loc, complaint, _("invalid directive: %s"), quote (yytext));

src/symtab.c
@@ -450,12 +450,11 @@ symbol_class_set (symbol *sym, symbol_class class, location loc, bool declaring)
     complain_class_redeclared (sym, class, loc);
   else
     {
-      s->class = class;
-
       if (class == nterm_sym && s->class != nterm_sym)
         s->number = nvars++;
       else if (class == token_sym && s->number == NUMBER_UNDEFINED)
         s->number = ntokens++;
+      s->class = class;
 
       if (declaring)
         {

tests/input.at
@@ -130,6 +130,28 @@ AT_BISON_CHECK([input.y], [1], [],
 AT_CLEANUP
 
 
+## -------------------------- ##
+## Yacc warnings on symbols.  ##
+## -------------------------- ##
+
+AT_SETUP([Yacc warnings on symbols])
+
+AT_DATA([input.y],
+[[%nterm exp
+%token NUM 0x40 "number"
+%%
+exp: "number";
+]])
+
+AT_BISON_CHECK([-fcaret -Wyacc input.y], [0], [],
+[[input.y:1.1-6: warning: POSIX Yacc does not support %nterm [-Wyacc]
+ %nterm exp
+ ^^^^^^
+]])
+
+AT_CLEANUP
+
+
 ## --------------------- ##
 ## Invalid %nterm uses.  ##
 ## --------------------- ##
@@ -149,36 +171,21 @@ fact: "number";
 ]])
 
 AT_BISON_CHECK([-fcaret input.y], [1], [],
-[[input.y:1.1-6: warning: deprecated directive, use '%type' [-Wdeprecated]
- %nterm expr "expression";
- ^^^^^^
-input.y:1.13-24: error: nonterminals cannot be given a string alias
+[[input.y:1.13-24: error: nonterminals cannot be given a string alias
  %nterm expr "expression";
              ^^^^^^^^^^^^
-input.y:2.1-6: warning: deprecated directive, use '%type' [-Wdeprecated]
- %nterm term 123;
- ^^^^^^
 input.y:2.13-15: error: nonterminals cannot be given an explicit number
  %nterm term 123;
              ^^^
-input.y:3.1-6: warning: deprecated directive, use '%type' [-Wdeprecated]
- %nterm fact 124 "factor";
- ^^^^^^
 input.y:3.13-15: error: nonterminals cannot be given an explicit number
  %nterm fact 124 "factor";
              ^^^
 input.y:3.17-24: error: nonterminals cannot be given a string alias
  %nterm fact 124 "factor";
                  ^^^^^^^^
-input.y:4.1-6: warning: deprecated directive, use '%type' [-Wdeprecated]
- %nterm '+' '*';
- ^^^^^^
 input.y:4.8-10: error: character literals cannot be nonterminals
  %nterm '+' '*';
         ^^^
-input.y:5.1-6: warning: deprecated directive, use '%type' [-Wdeprecated]
- %nterm "number";
- ^^^^^^
 input.y:5.8-15: error: syntax error, unexpected string, expecting char or identifier or <tag>
  %nterm "number";
         ^^^^^^^^
@@ -499,10 +506,7 @@ BAR:
 ]])
 
 AT_BISON_CHECK([-fcaret input.y], [1], [],
-[[input.y:2.1-6: warning: deprecated directive, use '%type' [-Wdeprecated]
- %nterm FOO BAR
- ^^^^^^
-input.y:2.8-10: error: symbol FOO redeclared as a nonterminal
+[[input.y:2.8-10: error: symbol FOO redeclared as a nonterminal
  %nterm FOO BAR
         ^^^
 input.y:1.8-10:     previous definition