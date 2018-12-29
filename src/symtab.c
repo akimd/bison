4d7df9aec7dbb3410098e867dc803811dde5a5c6
Akim Demaille <akim.demaille@gmail.com>
2018-12-29 09:19:45 +0100

    symbol: don't crash on symbol without content
    When running with --trace=parse, we may crash.
    
    * src/symtab.c (symbol_print): Avoid that.

--- src/symtab.c
+++ src/symtab.c
@@ -210,11 +210,11 @@ semantic_type_new (uniqstr tag, const location *loc)
 `-----------------*/
 
 #define SYMBOL_ATTR_PRINT(Attr)                         \
-  if (s->content->Attr)                                 \
+  if (s->content && s->content->Attr)                   \
     fprintf (f, " %s { %s }", #Attr, s->content->Attr)
 
 #define SYMBOL_CODE_PRINT(Attr)                                         \
-  if (s->content->props[Attr].code)                                     \
+  if (s->content && s->content->props[Attr].code)                       \
     fprintf (f, " %s { %s }", #Attr, s->content->props[Attr].code)
 
 void
