6ada985ff3ea7a5b7a7d0654e0d3ae86a0183f7c
Akim Demaille <akim.demaille@gmail.com>
2019-01-05 15:23:28 +0100

    parsers: issue tname with i18n markup
    Some users would like to avoid having to "parse" the *.y file to find
    the strings to translate.  Let's issue the translatable tokens with N_
    to allow "parsing" the generated parsers instead.
    
    See
    https://lists.gnu.org/archive/html/bison-patches/2019-01/msg00015.html
    
    * src/output.c (prepare_symbol_names): Issue symbol_names with N_()
    markup.

--- src/output.c
+++ src/output.c
@@ -206,7 +206,9 @@ prepare_symbol_names (char const *muscle_name)
         : quotearg_alloc (symbols[i]->tag, -1, qo);
       /* Width of the next token, including the two quotes, the
          comma and the space.  */
-      int width = strlen (cp) + 2;
+      int width
+        = strlen (cp) + 2
+        + (!quote && symbols[i]->translatable ? strlen ("N_()") : 0);
 
       if (j + width > 75)
         {
@@ -216,7 +218,11 @@ prepare_symbol_names (char const *muscle_name)
 
       if (i)
         obstack_1grow (&format_obstack, ' ');
+      if (!quote && symbols[i]->translatable)
+        obstack_sgrow (&format_obstack, "N_(");
       obstack_escape (&format_obstack, cp);
+      if (!quote && symbols[i]->translatable)
+        obstack_1grow (&format_obstack, ')');
       free (cp);
       obstack_1grow (&format_obstack, ',');
       j += width;
