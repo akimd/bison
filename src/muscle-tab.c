7c63cc1f6b1ec81159c6dfdb4cd9c04598bef140
Akim Demaille <akim.demaille@gmail.com>
2019-01-13 13:11:35 +0100

    diagnostics: add fixit support for duplicate removal
    * src/muscle-tab.c (muscle_percent_define_insert): Register a fixit
    for duplicate removal.
    * tests/input.at: Adjust expectations.

--- src/muscle-tab.c
+++ src/muscle-tab.c
@@ -24,6 +24,7 @@
 
 #include "complain.h"
 #include "files.h"
+#include "fixits.h"
 #include "getargs.h"
 #include "muscle-tab.h"
 #include "quote.h"
@@ -532,6 +533,7 @@ muscle_percent_define_insert (char const *var, location variable_loc,
       i += SUB_INDENT;
       location loc = muscle_percent_define_get_loc (variable);
       complain_indent (&loc, complaint, &i, _("previous definition"));
+      fixits_register (&variable_loc, "");
       warned = true;
     }
 
