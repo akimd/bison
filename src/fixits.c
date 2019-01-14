7c5e508b3984cbebdc7b5abf741410a5e5c6051c
Akim Demaille <akim.demaille@gmail.com>
2019-01-14 20:02:05 +0100

    Revert the last two commits
    They should not have been pushed, sorry about that.
    
    This reverts
    - commit 8575bd06ae6e65f3a30b21a3e022a968e4c7ae7a.
    - commit 55bf52860eac5c1394dc344a691220272df32b09.

--- src/fixits.c
+++ src/fixits.c
@@ -33,7 +33,6 @@
 #include "quotearg.h"
 #include "vasnprintf.h"
 
-#include "complain.h"
 #include "files.h"
 
 typedef struct
@@ -97,13 +96,6 @@ fixits_run (void)
 {
   if (!fixits)
     return;
-
-  if (!update_flag)
-    {
-      complain (NULL, Wother,
-                _("some fixes can be applied.  Rerun with option '--update'."));
-      return;
-    }
 
   /* This is not unlike what is done in location_caret.  */
   uniqstr input = ((fixit *) gl_list_get_at (fixits, 0))->location.start.file;
--- src/main.c
+++ src/main.c
@@ -105,7 +105,8 @@ main (int argc, char *argv[])
 
   /* Fix input file now, even if there are errors: that's less
      warnings in the following runs.  */
-  fixits_run ();
+  if (update_flag)
+    fixits_run ();
   fixits_free ();
 
   if (complaint_status == status_complaint)
