8575bd06ae6e65f3a30b21a3e022a968e4c7ae7a
Akim Demaille <akim.demaille@gmail.com>
2019-01-14 08:52:34 +0100

    WIP

--- src/fixits.c
+++ src/fixits.c
@@ -33,6 +33,7 @@
 #include "quotearg.h"
 #include "vasnprintf.h"
 
+#include "complain.h"
 #include "files.h"
 
 typedef struct
@@ -96,6 +97,13 @@ fixits_run (void)
 {
   if (!fixits)
     return;
+
+  if (!update_flag)
+    {
+      complain (NULL, Wother,
+                _("some fixes can be applied.  Rerun with option '--update'."));
+      return;
+    }
 
   /* This is not unlike what is done in location_caret.  */
   uniqstr input = ((fixit *) gl_list_get_at (fixits, 0))->location.start.file;
--- src/main.c
+++ src/main.c
@@ -105,8 +105,7 @@ main (int argc, char *argv[])
 
   /* Fix input file now, even if there are errors: that's less
      warnings in the following runs.  */
-  if (update_flag)
-    fixits_run ();
+  fixits_run ();
   fixits_free ();
 
   if (complaint_status == status_complaint)
