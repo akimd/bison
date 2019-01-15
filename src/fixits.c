b7e0d3131d2e0e5a370d3dea4c89958366d59d28
Akim Demaille <akim.demaille@gmail.com>
2019-01-15 18:20:37 +0100

    fixits: avoid generating empty lines
    * src/fixits.c (fixits_run): If erase the content of a line, also
    erase the following \n.
    * tests/input.at (Deprecated directives): Update expectations.

--- src/fixits.c
+++ src/fixits.c
@@ -161,6 +161,21 @@ fixits_run (void)
             break;
           ++offset;
         }
+      /* If erasing the content of a full line, also remove the
+         end-of-line. */
+      if (f->fix[0] == 0 && f->location.start.column == 1)
+        {
+          int c = getc (in);
+          if (c == EOF)
+            break;
+          else if (c == '\n')
+            {
+              ++line;
+              offset = 1;
+            }
+          else
+            ungetc (c, in);
+        }
     }
   /* Paste the rest of the file.  */
   {
