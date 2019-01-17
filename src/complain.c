5879c8dc5a391ce3d9de68f50326121babc8d1be
Akim Demaille <akim.demaille@gmail.com>
2019-01-17 06:44:51 +0100

    fixits: handle per-rule duplicates
    * src/complain.c (duplicate_rule_directive): Here.
    * tests/actions.at (Invalid uses of %empty): Check it.

--- src/complain.c
+++ src/complain.c
@@ -421,4 +421,5 @@ duplicate_rule_directive (char const *directive,
   complain_indent (&second, complaint, &i, _("only one %s allowed per rule"), directive);
   i += SUB_INDENT;
   complain_indent (&first, complaint, &i, _("previous declaration"));
+  fixits_register (&second, "");
 }
