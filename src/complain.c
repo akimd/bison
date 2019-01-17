d5d148630ad3d10d68a9e7634c1c8f7767335938
Akim Demaille <akim.demaille@gmail.com>
2019-01-17 06:45:13 +0100

    fixits: fix warnings about duplicates
    * src/complain.c (duplicate_directive): Fix the complaint level.
    * tests/input.at: Adjust.

--- src/complain.c
+++ src/complain.c
@@ -409,7 +409,7 @@ duplicate_directive (char const *directive,
   else
     complain_indent (&second, Wother, &i, _("duplicate directive: %s"), directive);
   i += SUB_INDENT;
-  complain_indent (&first, complaint, &i, _("previous declaration"));
+  complain_indent (&first, Wother, &i, _("previous declaration"));
   fixits_register (&second, "");
 }
 
