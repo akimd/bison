05b70f47e8583d8bf601210dcef3542cde1d9542
Akim Demaille <akim.demaille@gmail.com>
2019-01-20 16:54:33 +0100

    style: prefer bool to char
    * src/state.h, src/state.c (state::consistent): Make it a bool.
    Adjust dependencies.

--- src/lalr.c
+++ src/lalr.c
@@ -329,7 +329,7 @@ state_lookahead_tokens_count (state *s, bool default_reduction_only_for_accept)
           && default_reduction_only_for_accept))
     n_lookahead_tokens += rp->num;
   else
-    s->consistent = 1;
+    s->consistent = true;
 
   return n_lookahead_tokens;
 }
--- src/state.c
+++ src/state.c
@@ -143,7 +143,7 @@ state_new (symbol_number accessing_symbol,
   res->reductions = NULL;
   res->errs = NULL;
   res->state_list = NULL;
-  res->consistent = 0;
+  res->consistent = false;
   res->solved_conflicts = NULL;
   res->solved_conflicts_xml = NULL;
 
