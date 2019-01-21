5ec9c996628f5340f6fa1daba7f49ab3c2ea4872
Akim Demaille <akim.demaille@gmail.com>
2019-01-21 19:37:10 +0100

    --update: when used, do not generate the output files
    It is inconvenient that we also generate the output files when we
    update the grammar file, and it's somewhat unexpected.  Let's not do
    that.
    
    * src/main.c (main): Skip generation when --update is passed.
    * src/getargs.c (usage): Update the help message.
    * doc/bison.texi (Bison Options): Likewise.
    * tests/input.at: Check that we don't generate the output.

--- src/getargs.c
+++ src/getargs.c
@@ -287,7 +287,7 @@ Operation modes:\n\
                              and exit\n\
       --print-datadir        output directory containing skeletons and XSLT\n\
                              and exit\n\
-  -u, --update               apply fixes to the source grammar file\n\
+  -u, --update               apply fixes to the source grammar file and exit\n\
   -y, --yacc                 emulate POSIX Yacc\n\
   -W, --warnings[=CATEGORY]  report the warnings falling in CATEGORY\n\
   -f, --feature[=FEATURE]    activate miscellaneous features\n\
