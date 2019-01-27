8b0b2955691f511eb91cc6eb0fec75601aea80ac
Akim Demaille <akim.demaille@gmail.com>
2019-01-27 06:38:27 +0100

    yacc: issue warnings, not errors, for Bison extensions
    Reported by Kiyoshi Kanazawa.
    http://lists.gnu.org/archive/html/bug-bison/2019-01/msg00029.html
    
    * src/getargs.c (getargs): Let --yacc imply -Wyacc, not -Werror=yacc.
    * tests/input.at: Adjust.
    * doc/bison.tex (Bison Options): Document.

--- src/getargs.c
+++ src/getargs.c
@@ -708,7 +708,7 @@ getargs (int argc, char *argv[])
         break;
 
       case 'y':
-        warning_argmatch ("error=yacc", 0, 6);
+        warning_argmatch ("yacc", 0, 0);
         yacc_loc = command_line_location ();
         break;
 
