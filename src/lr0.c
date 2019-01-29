5670677cb697aa79fef15978dd880ec2b40dbab3
Akim Demaille <akim.demaille@gmail.com>
2019-01-29 06:36:41 +0100

    lr(0): remove useless conditional
    * src/lr0.c (new_itemsets): There's no harm in setting a Boolean
    several times.

--- src/lr0.c
+++ src/lr0.c
@@ -185,9 +185,7 @@ new_itemsets (state *s)
     if (item_number_is_symbol_number (ritem[itemset[i]]))
       {
         symbol_number sym = item_number_as_symbol_number (ritem[itemset[i]]);
-        if (!kernel_size[sym])
-          bitset_set (shift_symbol, sym);
-
+        bitset_set (shift_symbol, sym);
         kernel_base[sym][kernel_size[sym]] = itemset[i] + 1;
         kernel_size[sym]++;
       }
