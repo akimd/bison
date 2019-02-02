dc654a925cb44f9c786fb70cace912958214c9c8
Akim Demaille <akim.demaille@gmail.com>
2019-02-02 10:44:39 +0100

    style: comment changes
    * src/reader.c, src/scan-code.l: here.

--- src/reader.c
+++ src/reader.c
@@ -619,7 +619,7 @@ packgram (void)
       /* If the midrule's $$ is set or its $n is used, remove the '$' from the
          symbol name so that it's a user-defined symbol so that the default
          %destructor and %printer apply.  */
-      if (lhs->midrule_parent_rule
+      if (lhs->midrule_parent_rule /* i.e., symbol_is_dummy (lhs->content.sym).  */
           && (lhs->action_props.is_value_used
               || (symbol_list_n_get (lhs->midrule_parent_rule,
                                      lhs->midrule_parent_rhs_index)
