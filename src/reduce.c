334cb8f2225919832728f5cb9ab3c6b10d8ea301
Akim Demaille <akim.demaille@gmail.com>
2019-02-03 14:41:29 +0100

    style: minor fixes
    * NEWS, src/reduce.c, src/reduce.h: Use 'nonterminal'.
    Fix comments.

--- src/reduce.c
+++ src/reduce.c
@@ -295,7 +295,7 @@ nonterminals_reduce (void)
     free (symbols_sorted);
   }
 
-  /* Renumber non terminal numbers in the RHS of the rules.  LHS are
+  /* Update nonterminal numbers in the RHS of the rules.  LHS are
      pointers to the symbol structure, they don't need renumbering. */
   {
     for (rule_number r = 0; r < nrules; ++r)
