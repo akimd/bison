akimd/bison

commit 05a80977798abf472bfc11c477751303ad604733

doc: run tests/bison, not src/bison
* doc/local.mk: Don't run src/bison, as it expects to find all its
files installed.  Instead, run tests/bison which is ready to run in
builddir.

doc/local.mk
@@ -88,8 +88,8 @@ EXTRA_DIST += $(top_srcdir)/doc/bison.help
 if ! CROSS_COMPILING
 MAINTAINERCLEANFILES += $(top_srcdir)/doc/bison.help
 $(top_srcdir)/doc/bison.help: src/bison$(EXEEXT)
-	$(AM_V_GEN)LC_ALL=C src/bison$(EXEEXT) --version >doc/bison.help.tmp
-	$(AM_V_at) LC_ALL=C src/bison$(EXEEXT) --help | \
+	$(AM_V_GEN)LC_ALL=C tests/bison --version >doc/bison.help.tmp
+	$(AM_V_at) LC_ALL=C tests/bison --help | \
 ## Avoid variations in the output depending on whether we are
 ## on a glibc system.
 	  sed '/translation bugs/d'  >>doc/bison.help.tmp
@@ -118,7 +118,7 @@ endif
 $(top_srcdir)/doc/bison.1: $(MAN_DEPS)
 	$(AM_V_GEN)$(HELP2MAN)			\
 	    --include=$(top_srcdir)/doc/bison.x	\
-	    --output=$@.tmp src/bison$(EXEEXT)
+	    --output=$@.tmp tests/bison
 	$(AM_V_at)if $(remove_time_stamp) $@ >$@a.tmp 2>/dev/null &&		\
 	   $(remove_time_stamp) $@.tmp | cmp $@a.tmp - >/dev/null 2>&1; then	\
 	  touch $@;								\

commit 0782ed327401d1b59c059c89c8630d002260b4f7

build: fix distcheck
Regression introduced in 05a80977798abf472bfc11c477751303ad604733.

* doc/local.mk (bison.help): Don't depend on the path of the bison
executable.

doc/local.mk
@@ -90,9 +90,11 @@ MAINTAINERCLEANFILES += $(top_srcdir)/doc/bison.help
 $(top_srcdir)/doc/bison.help: src/bison$(EXEEXT)
 	$(AM_V_GEN)LC_ALL=C tests/bison --version >doc/bison.help.tmp
 	$(AM_V_at) LC_ALL=C tests/bison --help | \
+## Avoid depending on the path to Bison.
+	  sed -e 's,^Usage: .*/bison \[OPTION\],Usage: bison [OPTION],g' \
 ## Avoid variations in the output depending on whether we are
 ## on a glibc system.
-	  sed '/translation bugs/d'  >>doc/bison.help.tmp
+	      -e '/translation bugs/d'  >>doc/bison.help.tmp
 	$(AM_V_at)$(top_srcdir)/build-aux/move-if-change doc/bison.help.tmp $@
 endif ! CROSS_COMPILING
 