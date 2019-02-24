ec8142391aa9dd07e628eb142963618097ba522b
Akim Demaille <akim.demaille@gmail.com>
2019-02-24 20:49:39 +0100

    style: rename function for clarity
    Commit db34f7988941444bdc5f2b6adcf7fb83648f9a18 renames the variable F
    as goto_follows, but forgot to rename this function.
    
    * src/lalr.c (initialize_F): Rename as...
    (initialize_goto_follows): this.

--- src/lalr.c
+++ src/lalr.c
@@ -158,7 +158,7 @@ map_goto (state_number src, symbol_number sym)
 
 /* Build goto_follows. */
 static void
-initialize_F (void)
+initialize_goto_follows (void)
 {
   goto_number **reads = xnmalloc (ngotos, sizeof *reads);
   goto_number *edge = xnmalloc (ngotos + 1, sizeof *edge);
@@ -457,7 +457,7 @@ lalr (void)
 {
   initialize_LA ();
   set_goto_map ();
-  initialize_F ();
+  initialize_goto_follows ();
   lookback = xcalloc (nLA, sizeof *lookback);
   build_relations ();
   compute_follows ();
