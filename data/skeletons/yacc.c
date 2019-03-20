b5cd777ad6f5c7a19faea397a0565969037767ff
Akim Demaille <akim.demaille@gmail.com>
2019-03-20 08:36:34 +0100

    yacc.c: don't suggest api.header.include when --defines is not used
    See 4e19ab9fcd28c9361ff08f46e5e353effb0a0520: the suggestion to
    include the header file should not be emitted when the header is not
    generated.
    
    * data/skeletons/yacc.c: Here.

--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -374,9 +374,10 @@ m4_if(b4_api_prefix, [yy], [],
 #endif
 
 ]b4_header_include_if([[#include ]b4_percent_define_get([[api.header.include]])],
-                      [/* Use api.header.include to #include this header
+                      [m4_ifval(m4_quote(b4_spec_header_file),
+                                [/* Use api.header.include to #include this header
    instead of duplicating it here.  */
-b4_shared_declarations])[
+])b4_shared_declarations])[
 
 ]b4_user_post_prologue[
 ]b4_percent_code_get[]dnl
