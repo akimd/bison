af99826ef47a5167568fda4fa92b23b7004b366f
Akim Demaille <akim.demaille@gmail.com>
2019-03-25 08:39:50 +0100

    style: remove now useless _GL_UNUSED
    * src/getargs.c (getargs_colors): Here.
    Useless since 4d34b06fb3a38eb050439084476a6b3e292c5680.

--- src/getargs.c
+++ src/getargs.c
@@ -585,7 +585,7 @@ command_line_location (void)
    request.  This is consistent with the way GCC and Clang behave.  */
 
 static void
-getargs_colors (int argc _GL_UNUSED, char *argv[] _GL_UNUSED)
+getargs_colors (int argc, char *argv[])
 {
   for (int i = 1; i < argc; i++)
     {
