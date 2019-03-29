d332ff3c77c3e65c154ff0cc13b00bd1cec90ae9
Akim Demaille <akim.demaille@gmail.com>
2019-03-29 22:36:47 +0100

    state: more debug traces
    * src/state.c (state_transitions_set): Show the transitions.

--- src/state.c
+++ src/state.c
@@ -27,6 +27,7 @@
 
 #include "closure.h"
 #include "complain.h"
+#include "getargs.h"
 #include "gram.h"
 #include "print-xml.h"
 
@@ -197,6 +198,18 @@ state_transitions_set (state *s, int num, state **dst)
 {
   aver (!s->transitions);
   s->transitions = transitions_new (num, dst);
+  if (trace_flag & trace_automaton)
+    {
+      const transitions *trans = s->transitions;
+      fprintf (stderr, "transitions of %d (%d):\n",
+               s->number, trans->num);
+      for (int i = 0; i < trans->num; ++i)
+        fprintf (stderr, "  %d: (%d, %s, %d)\n",
+                 i,
+                 s->number,
+                 symbols[s->transitions->states[i]->accessing_symbol]->tag,
+                 s->transitions->states[i]->number);
+    }
 }
 
 
