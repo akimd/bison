dff7454371003ddce2d6b07d940f965c896d8b81
Akim Demaille <akim.demaille@gmail.com>
2019-04-06 09:32:10 +0200

    doc: sort the warning categories
    * doc/bison.texi, src/getargs.c: here.

--- src/getargs.c
+++ src/getargs.c
@@ -335,13 +335,13 @@ Output:\n\
 
       fputs (_("\
 Warning categories include:\n\
-  'midrule-values'    unset or unused midrule values\n\
-  'yacc'              incompatibilities with POSIX Yacc\n\
   'conflicts-sr'      S/R conflicts (enabled by default)\n\
   'conflicts-rr'      R/R conflicts (enabled by default)\n\
   'deprecated'        obsolete constructs\n\
   'empty-rule'        empty rules without %empty\n\
+  'midrule-values'    unset or unused midrule values\n\
   'precedence'        useless precedence and associativity\n\
+  'yacc'              incompatibilities with POSIX Yacc\n\
   'other'             all other warnings (enabled by default)\n\
   'all'               all the warnings except 'yacc'\n\
   'no-CATEGORY'       turn off warnings in CATEGORY\n\
