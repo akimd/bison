61d53e606deaf311edc6b4f74347660199fec11d
Akim Demaille <akim.demaille@gmail.com>
2019-04-09 21:03:12 +0200

    yacc.c: minor style change
    * data/skeletons/yacc.c: To improve consistency with other similar
    pieces of code.

--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -1650,8 +1650,8 @@ yyread_pushed_token:]])[
   yystate = yyn;
   YY_IGNORE_MAYBE_UNINITIALIZED_BEGIN
   *++yyvsp = yylval;
-  YY_IGNORE_MAYBE_UNINITIALIZED_END
-]b4_locations_if([  *++yylsp = yylloc;])[
+  YY_IGNORE_MAYBE_UNINITIALIZED_END]b4_locations_if([
+  *++yylsp = yylloc;])[
   goto yynewstate;
 
 
