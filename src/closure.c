2ab70cf0c6373a1457565c8fd99c442dbec7ee14
Akim Demaille <akim.demaille@gmail.com>
2019-04-12 08:29:44 +0200

    style: comment changes
    * src/closure.h, src/closure.c, src/lalr.c: here.

--- src/closure.c
+++ src/closure.c
@@ -35,6 +35,9 @@
 item_number *itemset;
 size_t nitemset;
 
+/* RULESET contains a bit for each rule.  CLOSURE sets the bits for
+   all rules which could potentially describe the next input to be
+   read.  */
 static bitset ruleset;
 
 /* internal data.  See comments before set_fderives and set_firsts.  */
--- src/lalr.c
+++ src/lalr.c
@@ -72,6 +72,12 @@ static bitsetv LA = NULL;
 size_t nLA;
 
 
+/* "(p, A) includes (p', B)" iff
+   B → βAγ, γ nullable, and p'-- β --> p (i.e., state p' reaches p on label β).
+
+   Definition p.621 [DeRemer 1982].
+
+   INCLUDES[(p, A)] = [(p', B),...] */
 static goto_number **includes;
 
 /* "(q, A → ω) lookback (p, A)" iff state p reaches state q on label ω.
