9ad7524659a340d3c8968eaa6c7ebe7e29d46031
Akim Demaille <akim.demaille@gmail.com>
2019-04-14 18:08:39 +0200

    traces: make closure() less verbose
    * src/getargs.h, src/getargs.c (trace_closure): New.
    * src/closure.c (closure): Use it.

--- src/closure.c
+++ src/closure.c
@@ -184,7 +184,7 @@ closure_new (unsigned n)
 void
 closure (item_number const *core, size_t n)
 {
-  if (trace_flag & trace_sets)
+  if (trace_flag & trace_closure)
     closure_print ("input", core, n);
 
   bitset_zero (ruleset);
@@ -221,7 +221,7 @@ closure (item_number const *core, size_t n)
       c++;
     }
 
-  if (trace_flag & trace_sets)
+  if (trace_flag & trace_closure)
     closure_print ("output", itemset, nitemset);
 }
 
--- src/getargs.h
+++ src/getargs.h
@@ -103,6 +103,7 @@ enum trace
     trace_m4        = 1 << 10, /**< M4 traces. */
     trace_muscles   = 1 << 11, /**< M4 definitions of the muscles. */
     trace_ielr      = 1 << 12, /**< IELR conversion. */
+    trace_closure   = 1 << 13, /**< Input/output of closure(). */
     trace_all       = ~0       /**< All of the above.  */
   };
 /** What debug items bison displays during its run.  */
