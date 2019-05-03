8c06cb9130a8d80d252f3e16a9d3a166a5139811
Akim Demaille <akim.demaille@gmail.com>
2019-05-03 09:19:21 +0200

    fixits: be sure to preserve the action when adding %empty
    Currently we remove the rhs to install %empty instead.
    
    * src/reader.c (grammar_rule_check_and_complete): Insert the missing
    %empty in front of the rhs, not in replacement thereof.
    * tests/actions.at (Add missing %empty): Check that.

--- src/reader.c
+++ src/reader.c
@@ -357,9 +357,10 @@ grammar_rule_check_and_complete (symbol_list *r)
       && !r->percent_empty_loc.start.file
       && warning_is_enabled (Wempty_rule))
     {
-      complain (&r->rhs_loc, Wempty_rule,
-                _("empty rule without %%empty"));
-      fixits_register (&r->rhs_loc, " %empty ");
+      complain (&r->rhs_loc, Wempty_rule, _("empty rule without %%empty"));
+      location loc = r->rhs_loc;
+      loc.end = loc.start;
+      fixits_register (&loc, " %empty ");
     }
 
   /* See comments in grammar_current_rule_prec_set for how POSIX
