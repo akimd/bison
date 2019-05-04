22a00cfbf2afc5544ed5aa9192131a17dd998fc2
Akim Demaille <akim.demaille@gmail.com>
2019-05-04 17:50:57 +0200

    style: use warning_is_enabled instead of duplicating it
    * src/complain.c (deprecated_directive): Here.

--- src/complain.c
+++ src/complain.c
@@ -521,7 +521,7 @@ deprecated_directive (location const *loc, char const *old, char const *upd)
               _("deprecated directive: %s, use %s"),
               quote (old), quote_n (1, upd));
   /* Register updates only if -Wdeprecated is enabled.  */
-  if (warnings_flag[warning_deprecated] != severity_disabled)
+  if (warning_is_enabled (Wdeprecated))
     fixits_register (loc, upd);
 }
 
