d62d5c4b33d01a4442b3549c4cf85c83e04139f7
Akim Demaille <akim.demaille@gmail.com>
2019-05-11 09:19:16 +0200

    style: remove incorrect comment
    * src/getargs.c: here.
    It's documented in getargs.h anyway.

--- src/getargs.c
+++ src/getargs.c
@@ -45,7 +45,7 @@ bool no_lines_flag = false;
 bool token_table_flag = false;
 location yacc_loc = EMPTY_LOCATION_INIT;
 bool update_flag = false; /* for -u */
-bool color_debug = false; /* --style=debug. */
+bool color_debug = false;
 
 bool nondeterministic_parser = false;
 bool glr_parser = false;
