7dc3c71a9709564d2304170ca3637ac41c518112
neok-m4700 <neok-m4700@users.noreply.github.com>
2019-05-19 13:53:50 +0200

    Fix gettext segfault

--- src/complain.c
+++ src/complain.c
@@ -255,7 +255,7 @@ complain_init_color (void)
       || color_mode == color_html
       || (color_mode == color_tty && isatty (STDERR_FILENO)))
     {
-      style_file_prepare ("BISON_STYLE", NULL, pkgdatadir (),
+      style_file_prepare ("BISON_STYLE", "", pkgdatadir (),
                           "bison-default.css");
       /* As a fallback, use the default in the current directory.  */
       struct stat statbuf;
