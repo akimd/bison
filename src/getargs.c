0428c429a1d1bc324414fe4fcb7a2e8ed381517b
Akim Demaille <akim.demaille@gmail.com>
2019-05-21 06:28:01 +0200

    remove "experimental" warnings
    Sadly enough, AFAIK, there were never answers to the "More user
    feedback will help to stabilize it" sentences.  Remove them.
    
    * src/getargs.c: IELR, canonical LR and XML output are here to stay,
    and they are no more experimental than some other features.
    * doc/bison.texi: Likewise.
    Also remove "experimental" warning for Java, LAC, LR tuning options,
    and named references.

--- src/getargs.c
+++ src/getargs.c
@@ -275,8 +275,7 @@ usage (int status)
       printf (_("Usage: %s [OPTION]... FILE\n"), program_name);
       fputs (_("\
 Generate a deterministic LR or generalized LR (GLR) parser employing\n\
-LALR(1), IELR(1), or canonical LR(1) parser tables.  IELR(1) and\n\
-canonical LR(1) support is experimental.\n\
+LALR(1), IELR(1), or canonical LR(1) parser tables.\n\
 \n\
 "), stdout);
 
@@ -332,7 +331,6 @@ Output:\n\
   -o, --output=FILE          leave output to FILE\n\
   -g, --graph[=FILE]         also output a graph of the automaton\n\
   -x, --xml[=FILE]           also output an XML report of the automaton\n\
-                             (the XML schema is experimental)\n\
 "), stdout);
       putc ('\n', stdout);
 
