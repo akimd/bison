d90023af5faeb08e6fdb75206ffb67a4c551be2c
Akim Demaille <akim.demaille@gmail.com>
2019-07-06 09:09:51 +0200

    doc: fix inaccuracies wrt --define and --force-define
    The doc says that -Dfoo=bar is the same as %define foo "bar".  It is
    not: the quotes are not added (and it makes a difference).
    
    * doc/bison.texi (Tuning the Parser): Fix the definition of -D/-F
    * src/getargs.c (usage): Likewise.

--- src/getargs.c
+++ src/getargs.c
@@ -404,10 +404,10 @@ Tuning the Parser:\n\
   -t, --debug                      instrument the parser for tracing\n\
                                    same as '-Dparse.trace'\n\
       --locations                  enable location support\n\
-  -D, --define=NAME[=VALUE]        similar to '%define NAME \"VALUE\"'\n\
-  -F, --force-define=NAME[=VALUE]  override '%define NAME \"VALUE\"'\n\
+  -D, --define=NAME[=VALUE]        similar to '%define NAME VALUE'\n\
+  -F, --force-define=NAME[=VALUE]  override '%define NAME VALUE'\n\
   -p, --name-prefix=PREFIX         prepend PREFIX to the external symbols\n\
-                                   deprecated by '-Dapi.prefix=PREFIX'\n\
+                                   deprecated by '-Dapi.prefix={PREFIX}'\n\
   -l, --no-lines                   don't generate '#line' directives\n\
   -k, --token-table                include a table of token names\n\
   -y, --yacc                       emulate POSIX Yacc\n\
