964c6508b14d05f16d966ed8753ff06445452f38
Akim Demaille <akim.demaille@gmail.com>
2019-07-06 09:00:59 +0200

    doc: put diagnostics related options together
    * doc/bison.texi (Diagnostics): New section.
    Move --warning, --color and --style there.
    * src/getargs.c (usage): Likewise.

--- src/getargs.c
+++ src/getargs.c
@@ -365,9 +365,9 @@ Mandatory arguments to long options are mandatory for short options too.\n\
       fputs (_("\
 The same is true for optional arguments.\n\
 "), stdout);
+      putc ('\n', stdout);
 
       fputs (_("\
-\n\
 Operation Modes:\n\
   -h, --help                 display this help and exit\n\
   -V, --version              output version information and exit\n\
@@ -376,12 +376,26 @@ Operation Modes:\n\
       --print-datadir        output directory containing skeletons and XSLT\n\
                              and exit\n\
   -u, --update               apply fixes to the source grammar file and exit\n\
-  -W, --warnings[=CATEGORY]  report the warnings falling in CATEGORY\n\
-      --color[=WHEN]         whether to colorize the diagnostics\n\
-      --style=FILE           specify the CSS FILE for colorizer diagnostics\n\
   -f, --feature[=FEATURES]   activate miscellaneous features\n\
 \n\
 "), stdout);
+
+      argmatch_feature_usage (stdout);
+      putc ('\n', stdout);
+
+      fputs (_("\
+Diagnostics:\n\
+  -W, --warnings[=CATEGORY]  report the warnings falling in CATEGORY\n\
+      --color[=WHEN]         whether to colorize the diagnostics\n\
+      --style=FILE           specify the CSS FILE for colorizer diagnostics\n\
+\n\
+"), stdout);
+
+      warning_usage (stdout);
+      putc ('\n', stdout);
+
+      argmatch_color_usage (stdout);
+      putc ('\n', stdout);
 
       fputs (_("\
 Tuning the Parser:\n\
@@ -416,16 +430,7 @@ Output Files:\n\
 "), stdout);
       putc ('\n', stdout);
 
-      argmatch_color_usage (stdout);
-      putc ('\n', stdout);
-
-      warning_usage (stdout);
-      putc ('\n', stdout);
-
       argmatch_report_usage (stdout);
-      putc ('\n', stdout);
-
-      argmatch_feature_usage (stdout);
       putc ('\n', stdout);
 
       printf (_("Report bugs to <%s>.\n"), PACKAGE_BUGREPORT);
