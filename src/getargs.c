9bdefd7984b877203663cb1e37fa9e7ec5f0bc33
Akim Demaille <akim.demaille@gmail.com>
2019-07-07 12:13:30 +0200

    style: comment change
    * src/getargs.c: here.

--- src/getargs.c
+++ src/getargs.c
@@ -110,6 +110,7 @@ typedef void (usage_fn) (FILE *out);
  *
  *  \param opt        option being decoded (e.g., --report).
  *  \param xargmatch  matching function.
+ *  \param usage      function that implement --help for this option.
  *  \param all        the value of the argument 'all'.
  *  \param flags      the flags to update
  *  \param args       comma separated list of effective subarguments to decode.
