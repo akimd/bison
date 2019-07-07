f99956b550119bf85709f5c3a3198eda9aef937f
Akim Demaille <akim.demaille@gmail.com>
2019-07-07 15:01:45 +0200

    style: clarify control flow
    * src/getargs.c (language_argmatch): Initialize msg.
    Check it instead of relying on a return.

--- src/getargs.c
+++ src/getargs.c
@@ -506,7 +506,7 @@ skeleton_arg (char const *arg, int prio, location loc)
 void
 language_argmatch (char const *arg, int prio, location loc)
 {
-  char const *msg;
+  char const *msg = NULL;
 
   if (prio < language_prio)
     {
@@ -521,10 +521,9 @@ language_argmatch (char const *arg, int prio, location loc)
     }
   else if (language_prio == prio)
     msg = _("multiple language declarations are invalid");
-  else
-    return;
 
-  complain (&loc, complaint, msg, quotearg_colon (arg));
+  if (msg)
+    complain (&loc, complaint, msg, quotearg_colon (arg));
 }
 
 /*----------------------.
