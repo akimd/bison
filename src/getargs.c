d233a2e314eb03c31962f7830e8279ffaa4a4054
Akim Demaille <akim.demaille@gmail.com>
2019-07-07 08:01:55 +0200

    doc: remove the --report=look-aheads alias
    Years ago we moved from 'look-ahead' to 'lookahead', and that alias
    was kept for backward compatibility.  But now that we use argmatch to
    generate the documentation, that value clutters the doc.
    
    * src/getargs.c (argmatch_report_args): Remove the
    --report=look-aheads alias.

--- src/getargs.c
+++ src/getargs.c
@@ -220,7 +220,6 @@ static const argmatch_report_arg argmatch_report_args[] =
   { "states",      report_states },
   { "itemsets",    report_states | report_itemsets },
   { "lookaheads",  report_states | report_lookahead_tokens },
-  { "look-ahead",  report_states | report_lookahead_tokens },
   { "solved",      report_states | report_solved_conflicts },
   { "all",         report_all },
   { NULL, report_none },
