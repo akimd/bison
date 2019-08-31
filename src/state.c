989a7aa865f36b0c11704783d297da49d2f5af70
Akim Demaille <akim.demaille@gmail.com>
2019-08-31 18:07:26 -0500

    check for memory exhaustion
    hash_initialize returns NULL when out of memory.  Check for it, and
    die cleanly instead of crashing.
    
    Reported by 江 祖铭 (Zu-Ming Jiang).
    https://lists.gnu.org/archive/html/bug-bison/2019-08/msg00015.html
    
    * src/muscle-tab.c, src/state.c, src/symtab.c, src/uniqstr.c:
    Check the value returned by hash_initialize.

--- src/state.c
+++ src/state.c
@@ -364,6 +364,8 @@ state_hash_new (void)
                                  state_hasher,
                                  state_comparator,
                                  NULL);
+  if (!state_table)
+    xalloc_die ();
 }
 
 
