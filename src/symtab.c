378963b1396272a1707e2f888dea7e2376a0e8ae
Akim Demaille <akim.demaille@gmail.com>
2019-09-06 17:47:16 +0200

    tests: check token redeclaration
    * src/symtab.c (symbol_class_set): Report previous definitions when
    redeclared.
    * tests/input.at (Symbol redeclared): New.

--- src/symtab.c
+++ src/symtab.c
@@ -462,7 +462,13 @@ symbol_class_set (symbol *sym, symbol_class class, location loc, bool declaring)
       if (declaring)
         {
           if (s->status == declared)
-            complain (&loc, Wother, _("symbol %s redeclared"), sym->tag);
+            {
+              unsigned i = 0;
+              complain (&loc, Wother, _("symbol %s redeclared"), sym->tag);
+              i += SUB_INDENT;
+              complain_indent (&sym->location, Wother, &i,
+                               _("previous declaration"));
+            }
           else
             s->status = declared;
         }
