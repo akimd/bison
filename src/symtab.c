f8db8fe4d7cb9db4cfbc414fca6f6ff9d74f0db6
Akim Demaille <akim.demaille@gmail.com>
2019-09-07 16:32:20 +0200

    fix: don't die when EOF token is defined twice
    With
    
        %token EOF 0 EOF 0
    
    we get
    
        input.y:3.14-16: warning: symbol EOF redeclared [-Wother]
            3 | %token EOF 0 EOF 0
              |              ^~~
        input.y:3.8-10: previous declaration
            3 | %token EOF 0 EOF 0
              |        ^~~
        Assertion failed: (nsyms == ntokens + nvars), function check_and_convert_grammar,
            file /Users/akim/src/gnu/bison/src/reader.c, line 839.
    
    Reported by Marc Schönefeld.
    
    * src/symtab.c (symbol_user_token_number_set): Register only the
    first definition of the end of input token.
    * tests/input.at (Symbol redeclared): Check that case.

--- src/symtab.c
+++ src/symtab.c
@@ -495,7 +495,7 @@ symbol_user_token_number_set (symbol *sym, int user_token_number, location loc)
     {
       *user_token_numberp = user_token_number;
       /* User defined $end token? */
-      if (user_token_number == 0)
+      if (user_token_number == 0 && !endtoken)
         {
           endtoken = sym->content->symbol;
           /* It is always mapped to 0, so it was already counted in
