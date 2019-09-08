d80815ac23192b0b3a2ebc82e6a7e512968bacb1
Akim Demaille <akim.demaille@gmail.com>
2019-09-08 18:48:49 +0200

    build: fix distcheck
    * configure.ac (gl_LIBOBJS): Adjust so that the generated files are
    indeed the expected ones.

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -1,4 +1,4 @@
-/* A Bison parser, made by GNU Bison 3.4.  */
+/* A Bison parser, made by GNU Bison 3.4.1.26-d17af.  */
 
 /* Bison implementation for Yacc-like parsers in C
 
@@ -48,7 +48,7 @@
 #define YYBISON 1
 
 /* Bison version.  */
-#define YYBISON_VERSION "3.4"
+#define YYBISON_VERSION "3.4.1.26-d17af"
 
 /* Skeleton name.  */
 #define YYSKELETON_NAME "yacc.c"
@@ -886,6 +886,7 @@ yy_symbol_value_print (FILE *yyo, int yytype, YYSTYPE const * const yyvaluep, YY
 /* "%code pre-printer" blocks.  */
 tron (yyo);
 
+  YY_IGNORE_MAYBE_UNINITIALIZED_BEGIN
   switch (yytype)
     {
     case 3: /* "string"  */
@@ -1069,6 +1070,7 @@ tron (yyo);
       default:
         break;
     }
+  YY_IGNORE_MAYBE_UNINITIALIZED_END
 /* "%code post-printer" blocks.  */
 troff (yyo);
 
--- src/parse-gram.h
+++ src/parse-gram.h
@@ -1,4 +1,4 @@
-/* A Bison parser, made by GNU Bison 3.4.  */
+/* A Bison parser, made by GNU Bison 3.4.1.26-d17af.  */
 
 /* Bison interface for Yacc-like parsers in C
 
