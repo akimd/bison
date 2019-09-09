d120a07e6be65657b5a017fa3503aa900fbfa595
Akim Demaille <akim.demaille@gmail.com>
2019-09-09 20:13:04 +0200

    diagnostics: beware of unexpected EOF when quoting the source file
    When the input file contains lone CRs (aka, ^M, \r), the locations see
    a new line.  Diagnostics look only at \n as end-of-line, so sometimes
    there is an offset in diagnostics.  Worse yet: sometimes we loop
    endlessly waiting for \n to come from a continuous stream of EOF.
    
    Fix that:
    - check for EOF
    - beware not to call end_use_class if begin_use_class was not
      called (which would abort).  This could happen if the actual
      line is shorter that the expected one.
    
    Prompted by a (private) report from Marc Schönefeld.
    
    * src/location.c (location_caret): here.
    * tests/diagnostics.at (Carriage return): New.

--- src/location.c
+++ src/location.c
@@ -229,7 +229,13 @@ location_caret (location loc, const char *style, FILE *out)
 
   /* Advance to the line's position, keeping track of the offset.  */
   while (caret_info.line < loc.start.line)
-    caret_info.line += getc (caret_info.source) == '\n';
+    {
+      int c = getc (caret_info.source);
+      if (c == EOF)
+        /* Something is wrong, that line number does not exist.  */
+        return;
+      caret_info.line += c == '\n';
+    }
   caret_info.offset = ftell (caret_info.source);
 
   /* Read the actual line.  Don't update the offset, so that we keep a pointer
@@ -238,32 +244,43 @@ location_caret (location loc, const char *style, FILE *out)
     int c = getc (caret_info.source);
     if (c != EOF)
       {
+        bool single_line = loc.start.line == loc.end.line;
         /* Quote the file (at most the first line in the case of
            multiline locations).  */
-        fprintf (out, "%5d | ", loc.start.line);
-        bool single_line = loc.start.line == loc.end.line;
-        /* Consider that single point location (with equal boundaries)
-           actually denote the character that they follow.  */
-        int byte_end = loc.end.byte +
-          (single_line && loc.start.byte == loc.end.byte);
-        /* Byte number.  */
-        int byte = 1;
-        while (c != EOF && c != '\n')
-          {
-            if (byte == loc.start.byte)
-              begin_use_class (style, out);
-            fputc (c, out);
-            c = getc (caret_info.source);
-            ++byte;
-            if (single_line
-                ? byte == byte_end
-                : c == '\n' || c == EOF)
-              end_use_class (style, out);
-          }
-        putc ('\n', out);
-
         {
-          /* Print the carets with the same indentation as above.  */
+          fprintf (out, "%5d | ", loc.start.line);
+          /* Consider that single point location (with equal boundaries)
+             actually denote the character that they follow.  */
+          int byte_end = loc.end.byte +
+            (single_line && loc.start.byte == loc.end.byte);
+          /* Byte number.  */
+          int byte = 1;
+          /* Whether we opened the style.  If the line is not as
+             expected (maybe the file was changed since the scanner
+             ran), we might reach the end before we actually saw the
+             opening column.  */
+          bool opened = false;
+          while (c != EOF && c != '\n')
+            {
+              if (byte == loc.start.byte)
+                {
+                  begin_use_class (style, out);
+                  opened = true;
+                }
+              fputc (c, out);
+              c = getc (caret_info.source);
+              ++byte;
+              if (opened
+                  && (single_line
+                      ? byte == byte_end
+                      : c == '\n' || c == EOF))
+                end_use_class (style, out);
+            }
+          putc ('\n', out);
+        }
+
+        /* Print the carets with the same indentation as above.  */
+        {
           fprintf (out, "      | %*s", loc.start.column - 1, "");
           begin_use_class (style, out);
           putc ('^', out);
@@ -275,8 +292,8 @@ location_caret (location loc, const char *style, FILE *out)
           for (int i = loc.start.column + 1; i < len; ++i)
             putc ('~', out);
           end_use_class (style, out);
+          putc ('\n', out);
         }
-        putc ('\n', out);
       }
   }
 }
