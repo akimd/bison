32dff87c1d0ab58f94e093b8468389654ecb9941
Akim Demaille <akim.demaille@gmail.com>
2019-09-13 21:04:49 +0200

    diagnostics: fix use of complain_indent
    * src/symtab.c (symbol_class_set): Here.
    * tests/diagnostics.at, tests/input.at, tests/regression.at: Adjust
    expectations.

--- src/symtab.c
+++ src/symtab.c
@@ -464,7 +464,8 @@ symbol_class_set (symbol *sym, symbol_class class, location loc, bool declaring)
           if (s->status == declared)
             {
               unsigned i = 0;
-              complain (&loc, Wother, _("symbol %s redeclared"), sym->tag);
+              complain_indent (&loc, Wother, &i,
+                               _("symbol %s redeclared"), sym->tag);
               i += SUB_INDENT;
               complain_indent (&sym->location, Wother, &i,
                                _("previous declaration"));
