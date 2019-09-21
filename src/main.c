4901ee115b43d77088add86d50b44775bf4757f1
Akim Demaille <akim.demaille@gmail.com>
2019-09-21 15:01:45 +0200

    quotearg: avoid leaks
    Reported by Tomasz Kłoczko.
    https://lists.gnu.org/archive/html/bug-bison/2019-09/msg00008.html
    
    * src/main.c (main): Free quotearg's memory later.

--- src/main.c
+++ src/main.c
@@ -223,7 +223,6 @@ main (int argc, char *argv[])
   muscle_free ();
   code_scanner_free ();
   skel_scanner_free ();
-  quotearg_free ();
   timevar_pop (tv_free);
 
   if (trace_flag & trace_bitsets)
@@ -249,6 +248,7 @@ main (int argc, char *argv[])
   uniqstrs_free ();
 
   complain_free ();
+  quotearg_free ();
 
   return complaint_status ? EXIT_FAILURE : EXIT_SUCCESS;
 }
