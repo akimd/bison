5f45cb05f11031ded8ffb6355bf91b900da37f54
Akim Demaille <akim.demaille@gmail.com>
2019-09-21 07:57:49 +0200

    diagnostics: don't print ellipsis on the caret line
    From
    
        9 | ...TUVWXYZ  ABCDEFGHIJKLMNOPQRSTUVWXYZ  ABCDEFGHIJKL
          | ...         ^~~~~~~~~~~~~~~~~~~~~~~~~~
    
    to
    
        9 | ...TUVWXYZ  ABCDEFGHIJKLMNOPQRSTUVWXYZ  ABCDEFGHI...
          |             ^~~~~~~~~~~~~~~~~~~~~~~~~~
    
    * src/location.c (location_caret): here.
    * tests/diagnostics.at: Adjust expectations.

--- src/location.c
+++ src/location.c
@@ -388,9 +388,8 @@ location_caret (location loc, const char *style, FILE *out)
 
         /* Print the carets with the same indentation as above.  */
         {
-          fprintf (out, "      | %s%*s",
-                   skip ? "..." : "",
-                   loc.start.column - 1 - skip, "");
+          fprintf (out, "      | %*s",
+                   loc.start.column - 1 - skip + (skip ? 3 : 0), "");
           begin_use_class (style, out);
           putc ('^', out);
           /* Underlining a multiline location ends with the first
