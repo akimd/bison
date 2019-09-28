4971409e392c5b9a326a9a7ec1c0e4d990327f4f
Akim Demaille <akim.demaille@gmail.com>
2019-09-28 08:26:15 +0200

    style: change misleading macro argument name
    * data/skeletons/glr.c, data/skeletons/yacc.c
    (yypact_value_is_default): It does not take a rule number as argument.

--- data/skeletons/glr.c
+++ data/skeletons/glr.c
@@ -967,8 +967,8 @@ yylhsNonterm (yyRuleNum yyrule)
   return yyr1[yyrule];
 }
 
-#define yypact_value_is_default(Yystate) \
-  ]b4_table_value_equals([[pact]], [[Yystate]], [b4_pact_ninf])[
+#define yypact_value_is_default(Yyn) \
+  ]b4_table_value_equals([[pact]], [[Yyn]], [b4_pact_ninf])[
 
 /** True iff LR state YYSTATE has only a default reduction (regardless
  *  of token).  */
--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -645,8 +645,8 @@ static const ]b4_int_type_for([b4_toknum])[ yytoknum[] =
 
 #define YYPACT_NINF ]b4_pact_ninf[
 
-#define yypact_value_is_default(Yystate) \
-  ]b4_table_value_equals([[pact]], [[Yystate]], [b4_pact_ninf])[
+#define yypact_value_is_default(Yyn) \
+  ]b4_table_value_equals([[pact]], [[Yyn]], [b4_pact_ninf])[
 
 #define YYTABLE_NINF ]b4_table_ninf[
 
