e69b47cd189d7eb45668acce4cf17815401bf2a5
Paul Eggert <eggert@cs.ucla.edu>
2019-10-05 03:41:53 -0700

    * data/skeletons/glr.c (yysplitStack): Pacify Clang 8.

--- data/skeletons/glr.c
+++ data/skeletons/glr.c
@@ -1506,7 +1506,8 @@ yysplitStack (yyGLRStack* yystackp, ptrdiff_t yyk)
     {
       yyGLRState** yynewStates = YY_NULLPTR;
       yybool* yynewLookaheadNeeds;
-      ptrdiff_t half_max_capacity = YYSIZEMAX / (2 * sizeof yynewStates[0]);
+      ptrdiff_t state_size = sizeof yynewStates[0];
+      ptrdiff_t half_max_capacity = YYSIZEMAX / 2 / state_size;
 
       if (half_max_capacity < yystackp->yytops.yycapacity)
         yyMemoryExhausted (yystackp);
