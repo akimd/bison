beceb2fa9382fd44816cae382dd12ffc6a210d5e
Paul Eggert <eggert@cs.ucla.edu>
2019-10-06 11:49:56 -0700

    Work around GCC 4.8 false alarms without casts
    * data/skeletons/yacc.c (yyparse):
    Initialize yyes_capacity with a signed expression.
    * tests/local.at (AT_YYLEX_DEFINE(c)):
    Use enum to avoid cast.

--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -1490,7 +1490,7 @@ b4_function_define([[yyparse]], [[int]], b4_parse_param)[
   yystacksize = YYINITDEPTH;]b4_lac_if([[
 
   yyes = yyesa;
-  yyes_capacity = (YYPTRDIFF_T) (sizeof yyesa / sizeof *yyes);
+  yyes_capacity = ]b4_percent_define_get([[parse.lac.es-capacity-initial]])[;
   if (YYMAXDEPTH < yyes_capacity)
     yyes_capacity = YYMAXDEPTH;]])[
 
