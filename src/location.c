d9d37a11963f969a84865f1196ce018adf63085b
Akim Demaille <akim.demaille@gmail.com>
2019-10-12 10:43:53 +0200

    i18n: don't push too hard for '…'
    Suggested by Paul Eggert.
    
    * src/location.c (ellipsis): Clarify comment for translators.

--- src/location.c
+++ src/location.c
@@ -242,8 +242,11 @@ static struct
 void caret_init (void)
 {
   screen_width = columns ();
-  /* TRANSLATORS: use the appropriate character (e.g. "…") if
-     available.  */
+  /* TRANSLATORS: This is used when a line is too long, and is
+     displayed truncated.  Use an ellipsis appropriate for your
+     language, remembering that "…" (U+2026 HORIZONTAL ELLIPSIS)
+     sometimes misdisplays and that "..." (three ASCII periods) is a
+     safer choice in some locales.  */
   ellipsis = _("...");
   ellipsize = mbswidth (ellipsis, 0);
 }
