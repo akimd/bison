ec64a0bc7eabb45783e7031729c5b1ff37821cbd
Akim Demaille <akim.demaille@gmail.com>
2019-10-21 17:03:44 +0200

    main: also free memory on errors
    * src/derives.c (derives_free): Beware of NULL.
    * src/main.c (main): Let the 'finish' label include memory release.

--- src/derives.c
+++ src/derives.c
@@ -109,6 +109,9 @@ derives_compute (void)
 void
 derives_free (void)
 {
-  free (derives[0]);
-  free (derives);
+  if (derives)
+    {
+      free (derives[0]);
+      free (derives);
+    }
 }
--- src/main.c
+++ src/main.c
@@ -207,6 +207,8 @@ main (int argc, char *argv[])
       timevar_pop (tv_parser);
     }
 
+ finish:
+
   timevar_push (tv_free);
   nullable_free ();
   derives_free ();
@@ -227,8 +229,6 @@ main (int argc, char *argv[])
 
   if (trace_flag & trace_bitsets)
     bitset_stats_dump (stderr);
-
- finish:
 
   /* Stop timing and print the times.  */
   timevar_stop (tv_total);
