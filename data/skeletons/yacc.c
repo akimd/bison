c86b7815fc74bf827b845d01e31db8d4da8ccc5d
kaneko y <spiketeika@gmail.com>
2019-10-22 21:07:04 +0900

    yacc.c: fix a typo
    * data/skeletons/yacc.c (yysetstate): fix comment.

--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -1479,7 +1479,7 @@ yynewstate:
 
 
 /*--------------------------------------------------------------------.
-| yynewstate -- set current state (the top of the stack) to yystate.  |
+| yysetstate -- set current state (the top of the stack) to yystate.  |
 `--------------------------------------------------------------------*/
 yysetstate:
   YYDPRINTF ((stderr, "Entering state %d\n", yystate));
