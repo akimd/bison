3945beb1d2abd171c4d813b7b55228c31681b4c3
Yuichiro Kaneko <spiketeika@gmail.com>
2019-10-23 08:30:15 +0200

    style: update comment in reader.c
    rrhs and rlhs were removed by b2ed6e5826e772162719db595446b2c58e4ac5d6.
    
    * src/reader.c (packgram): Update comment.

--- src/reader.c
+++ src/reader.c
@@ -605,10 +605,9 @@ grammar_current_rule_expect_rr (int count, location loc)
 }
 
 
-/*---------------------------------------------------------------.
-| Convert the rules into the representation using RRHS, RLHS and |
-| RITEM.                                                         |
-`---------------------------------------------------------------*/
+/*---------------------------------------------.
+| Build RULES and RITEM from what was parsed.  |
+`---------------------------------------------*/
 
 static void
 packgram (void)
