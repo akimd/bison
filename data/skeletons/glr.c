fead28d9e38603a932619a6df2edf95a93377df3
Akim Demaille <akim.demaille@gmail.com>
2019-10-29 08:59:18 +0100

    style: glr.c: comment changes
    * data/skeletons/glr.c: here.

--- data/skeletons/glr.c
+++ data/skeletons/glr.c
@@ -623,16 +623,16 @@ yytnamerr (char *yyres, const char *yystr)
 
 #endif /* !YYERROR_VERBOSE */
 
-/** State numbers, as in LALR(1) machine */
+/** State numbers. */
 typedef int yyStateNum;
 
-/** Rule numbers, as in LALR(1) machine */
+/** Rule numbers. */
 typedef int yyRuleNum;
 
-/** Grammar symbol */
+/** Grammar symbol. */
 typedef int yySymbol;
 
-/** Item references, as in LALR(1) machine */
+/** Item references. */
 typedef short yyItemNum;
 
 typedef struct yyGLRState yyGLRState;
