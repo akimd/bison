17d34c231b185b5612dbe4f842f76f2c9490f2f4
Yuichiro Kaneko <spiketeika@gmail.com>
2019-11-11 08:57:15 +0900

    gram.c: also print terminals in grammar_dump
    * src/gram.c (grammar_dump): Print terminals likewise non terminals.
    * tests/sets.at (Reduced Grammar): Update test case to catch up the
    change and add a test case where prec and assoc are used.

--- src/gram.c
+++ src/gram.c
@@ -259,16 +259,25 @@ grammar_dump (FILE *out, const char *title)
            "ntokens = %d, nvars = %d, nsyms = %d, nrules = %d, nritems = %d\n\n",
            ntokens, nvars, nsyms, nrules, nritems);
 
-
-  fprintf (out, "Variables\n---------\n\n");
+  fprintf (out, "Tokens\n------\n\n");
   {
     fprintf (out, "Value  Sprec  Sassoc  Tag\n");
 
-    for (symbol_number i = ntokens; i < nsyms; i++)
+    for (symbol_number i = 0; i < ntokens; i++)
       fprintf (out, "%5d  %5d   %5d  %s\n",
                i,
                symbols[i]->content->prec, symbols[i]->content->assoc,
                symbols[i]->tag);
+    fprintf (out, "\n\n");
+  }
+
+  fprintf (out, "Non terminals\n-------------\n\n");
+  {
+    fprintf (out, "Value  Tag\n");
+
+    for (symbol_number i = ntokens; i < nsyms; i++)
+      fprintf (out, "%5d  %s\n",
+               i, symbols[i]->tag);
     fprintf (out, "\n\n");
   }
 
