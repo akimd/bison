46ab1d0cbe69b429e2e5f1934faa53d1de46620d
Akim Demaille <akim.demaille@gmail.com>
2020-01-22 22:29:09 +0100

    diagnostics: report syntax errors in color
    * src/parse-gram.y (parse.error): Set to 'custom'.
    (yyreport_syntax_error): New.
    * data/bison-default.css (.expected, .unexpected): New.
    * tests/diagnostics.at: Adjust.

--- src/complain.c
+++ src/complain.c
@@ -477,10 +477,10 @@ static void
 complains (const location *loc, int *indent, warnings flags,
            const char *message, va_list args)
 {
-  severity s = warning_severity (flags);
   if ((flags & complaint) && complaint_status < status_complaint)
     complaint_status = status_complaint;
 
+  severity s = warning_severity (flags);
   if (severity_warning <= s)
     {
       if (severity_error <= s && ! complaint_status)
