16c77b4ba2ece1b52bfd9c381ee8b6226b3e3db5
Akim Demaille <akim.demaille@gmail.com>
2020-01-22 18:19:20 +0100

    yacc.c: fixes
    * data/skeletons/yacc.c: Avoid warnings about unused functions.
    Fix typo.

--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -1193,12 +1193,15 @@ yysyntax_error_arguments (const yyparse_context_t *yyctx,
          [custom],
 [b4_locations_if([[/*  The location of this context.  */
 static YYLTYPE *
+yyparse_context_location (const yyparse_context_t *yyctx) YY_ATTRIBUTE_UNUSED;
+
+static YYLTYPE *
 yyparse_context_location (const yyparse_context_t *yyctx)
 {
   return yyctx->yylloc;
 }]])[
 
-/* User defined funtion to report a syntax error.  */
+/* User defined function to report a syntax error.  */
 ]b4_function_declare([yyreport_syntax_error], [static int],
                      [[[const yyparse_context_t *yyctx]], [[yyctx]]],
                      b4_parse_param)],
