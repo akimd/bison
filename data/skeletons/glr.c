792fc34016b029154e19a84be09041804492554a
Akim Demaille <akim.demaille@gmail.com>
2020-01-29 19:24:40 +0100

    glr.c: add support for parse.error=custom
    * data/skeletons/glr.c (yyreportSyntaxError): Call the user's
    yyreport_syntax_error in custom mode.
    * tests/calc.at: Check it.

--- data/skeletons/glr.c
+++ data/skeletons/glr.c
@@ -2180,6 +2180,25 @@ yysyntax_error_arguments (const yyGLRStack* yystackp,
 }
 ]])[
 
+]m4_case(b4_percent_define_get([[parse.error]]),
+         [custom],
+[[/* User defined function to report a syntax error.  */
+typedef yyGLRStack yyparse_context_t;
+static int
+yyreport_syntax_error (const yyGLRStack* yystackp]b4_user_formals[);
+
+]b4_locations_if([[/*  The location of this context.  */
+static YYLTYPE *
+yyparse_context_location (const yyGLRStack *yystackp) YY_ATTRIBUTE_UNUSED;
+
+static YYLTYPE *
+yyparse_context_location (const yyGLRStack *yystackp)
+{
+  YYUSE (yystackp);
+  return &yylloc;
+}]])])[
+
+
 static void
 yyreportSyntaxError (yyGLRStack* yystackp]b4_user_formals[)
 {
