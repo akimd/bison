bb02acc2ed08207fb7e87e6ba878706d45edb2a0
Adrian Vogelsgesang <avogelsgesang@tableau.com>
2020-02-07 21:14:52 +0100

    style: stylistic cleanups in the C skeletons
    * data/skeletons/glr.c, data/skeletons/yacc.c:
    Avoid duplicated declaration of yysymbol_name.

--- data/skeletons/glr.c
+++ data/skeletons/glr.c
@@ -626,11 +626,7 @@ yysymbol_name (yySymbol yysymbol)
 {
   return yytname[yysymbol];
 }]],
-[[/* The user-facing name of the symbol whose (internal) number is
-   YYSYMBOL.  No bounds checking.  */
-static const char *yysymbol_name (yySymbol yysymbol) YY_ATTRIBUTE_UNUSED;
-
-static const char *
+[[static const char *
 yysymbol_name (yySymbol yysymbol)
 {
   static const char *const yy_sname[] =
--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -621,11 +621,7 @@ yysymbol_name (int yysymbol)
 {
   return yytname[yysymbol];
 }]],
-[[/* The user-facing name of the symbol whose (internal) number is
-   YYSYMBOL.  No bounds checking.  */
-static const char *yysymbol_name (int yysymbol) YY_ATTRIBUTE_UNUSED;
-
-static const char *
+[[static const char *
 yysymbol_name (int yysymbol)
 {
   static const char *const yy_sname[] =
