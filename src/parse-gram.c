cc3760ef516655a1cc2fce51c241d0033afb3b40
Akim Demaille <akim.demaille@gmail.com>
2020-02-13 18:20:31 +0100

    news: 3.5.2
    * NEWS: Update.

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -1,4 +1,4 @@
-/* A Bison parser, made by GNU Bison 3.5.22-948cd-dirty.  */
+/* A Bison parser, made by GNU Bison 3.5.2.  */
 
 /* Bison implementation for Yacc-like parsers in C
 
@@ -48,7 +48,7 @@
 #define YYBISON 1
 
 /* Bison version.  */
-#define YYBISON_VERSION "3.5.22-948cd-dirty"
+#define YYBISON_VERSION "3.5.2"
 
 /* Skeleton name.  */
 #define YYSKELETON_NAME "yacc.c"
