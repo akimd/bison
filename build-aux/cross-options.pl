192e9fdf77376a8303e4204046fd513446d840d7
Akimn Demaille <akim.demaille@gmail.com>
2020-02-15 10:49:14 +0100

    build: fix typo
    * build-aux/cross-options.pl: here.

--- build-aux/cross-options.pl
+++ build-aux/cross-options.pl
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-# Written by Akimn Demaille.
+# Written by Akim Demaille.
 
 use warnings;
 use 5.005;
