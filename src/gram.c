f7f2c99c289cb5f6eadd6b3b212fbe02abcf208a
Akim Demaille <akim.demaille@gmail.com>
2020-02-22 21:36:41 +0100

    gram: more debugging information
    * src/gram.c (ritem_print): Show indices in ritem.

--- src/gram.c
+++ src/gram.c
@@ -155,11 +155,22 @@ void
 ritem_print (FILE *out)
 {
   fputs ("RITEM\n", out);
+  bool first = true;
   for (int i = 0; i < nritems; ++i)
-    if (ritem[i] >= 0)
-      fprintf (out, "  %s", symbols[ritem[i]]->tag);
-    else
-      fprintf (out, "  (rule %d)\n", item_number_as_rule_number (ritem[i]));
+    {
+      if (first)
+        {
+          fprintf (out, "  %d: ", i);
+          first = false;
+        }
+      if (ritem[i] >= 0)
+        fprintf (out, "  %s", symbols[ritem[i]]->tag);
+      else
+        {
+          fprintf (out, "  (rule %d)\n", item_number_as_rule_number (ritem[i]));
+          first = true;
+        }
+    }
   fputs ("\n\n", out);
 }
 
