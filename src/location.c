e21ff47f5d0b64da693a47b7dd200a1a44a5bbeb
Akim Demaille <akim.demaille@gmail.com>
2020-03-07 09:57:03 +0100

    diagnostics: be sure to close the styling when lines are too short
         bar.y:4.12-17: <error>error:</error> redefining user token number of foo
        -    4 | %token foo <error>123
        +    4 | %token foo <error>123</error>
               |            <error>^~~~~~</error>
    
    * src/location.c (location_caret): Be sure to close.
    * tests/diagnostics.at (Line is too short, and then you die): New.

--- src/location.c
+++ src/location.c
@@ -461,6 +461,12 @@ location_caret (location loc, const char *style, FILE *out)
                   break;
                 }
             }
+          // The line is shorter than expected.
+          if (opened)
+            {
+              end_use_class (style, out);
+              opened = false;
+            }
           putc ('\n', out);
         }
 
