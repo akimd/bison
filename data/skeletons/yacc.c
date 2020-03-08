e3812bb8c317203014f615c6f0cb15fd657ae293
Akim Demaille <akim.demaille@gmail.com>
2020-03-08 07:56:39 +0100

    yacc.c: make sure we properly propagated the user's number for error
    * data/skeletons/yacc.c (YYERRCODE): Be truthful.
    * tests/input.at (Redefining the error token): Check that.

--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -673,7 +673,7 @@ static const ]b4_int_type_for([b4_toknum])[ yytoknum[] =
 
 /* Error token number */
 #define YYTERROR        1
-#define YYERRCODE       256
+#define YYERRCODE       ]b4_symbol(1, user_number)[
 
 ]b4_locations_if([[
 ]b4_yylloc_default_define[
