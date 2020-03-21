00b0d02955e787e639f68d77550d578a427eee2d
Akim Demaille <akim.demaille@gmail.com>
2020-03-21 09:21:38 +0100

    tests: yacc.c: avoid yysyntax_error_arguments
    Because glr.c shares the same testing routines, we also need to
    convert it.
    
    * data/skeletons/glr.c (yyparse_context_token): New.
    * tests/local.at (yyreport_syntax_error): here.

--- data/skeletons/glr.c
+++ data/skeletons/glr.c
@@ -2118,6 +2118,10 @@ yyexpected_tokens (const yyGLRStack* yystackp,
 
 static int
 yysyntax_error_arguments (const yyGLRStack* yystackp,
+                          int yyarg[], int yyargn) YY_ATTRIBUTE_UNUSED;
+
+static int
+yysyntax_error_arguments (const yyGLRStack* yystackp,
                           int yyarg[], int yyargn)
 {
   yySymbol yytoken = yychar == YYEMPTY ? YYEMPTY : YYTRANSLATE (yychar);
@@ -2172,7 +2176,19 @@ typedef yyGLRStack yyparse_context_t;
 static int
 yyreport_syntax_error (const yyGLRStack* yystackp]b4_user_formals[);
 
-]b4_locations_if([[/*  The location of this context.  */
+/* The token type of the lookahead of this context.  */
+static int
+yyparse_context_token (const yyGLRStack *yystackp) YY_ATTRIBUTE_UNUSED;
+
+static int
+yyparse_context_token (const yyGLRStack *yystackp)
+{
+  YYUSE (yystackp);
+  yySymbol yytoken = yychar == YYEMPTY ? YYEMPTY : YYTRANSLATE (yychar);
+  return yytoken;
+}
+
+]b4_locations_if([[/* The location of the lookahead of this context.  */
 static YYLTYPE *
 yyparse_context_location (const yyGLRStack *yystackp) YY_ATTRIBUTE_UNUSED;
 
