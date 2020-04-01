d3db22d788d2bd30c25a1b944447108a1799df21
Akim Demaille <akim.demaille@gmail.com>
2020-04-01 08:13:17 +0200

    regen

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -608,7 +608,7 @@ union yyalloc
 
 /* YYTRANSLATE(TOKEN-NUM) -- Symbol number corresponding to TOKEN-NUM
    as returned by yylex, with out-of-bounds checking.  */
-#define YYTRANSLATE(YYX) (YYX)
+#define YYTRANSLATE(YYX) YY_CAST (yysymbol_type_t, YYX)
 
 #if GRAM_DEBUG
   /* YYRLINE[YYN] -- Source line where rule number YYN was defined.  */
