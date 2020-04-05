9b70d69f39910fe3c63f58d972b1f0b144d0ea48
Akim Demaille <akim.demaille@gmail.com>
2020-04-05 12:47:55 +0200

    m4: we don't need undef_token_number
    It's replaced by YYSYMBOL_YYUNDEF.

--- src/output.c
+++ src/output.c
@@ -254,7 +254,6 @@ prepare_symbols (void)
   MUSCLE_INSERT_INT ("tokens_number", ntokens);
   MUSCLE_INSERT_INT ("nterms_number", nvars);
   MUSCLE_INSERT_INT ("symbols_number", nsyms);
-  MUSCLE_INSERT_INT ("undef_token_number", undeftoken->content->number);
   MUSCLE_INSERT_INT ("user_token_number_max", max_user_token_number);
 
   muscle_insert_symbol_number_table ("translate",
