93fec67c4630283ddfa8836a1e8b07e585093b93
Akim Demaille <akim.demaille@gmail.com>
2020-04-24 08:23:27 +0200

    regen

--- src/parse-gram.c
+++ src/parse-gram.c
@@ -425,6 +425,11 @@ typedef int yy_state_fast_t;
 # endif
 #endif
 
+#ifndef N_
+# define N_(Msgid) Msgid
+#endif
+
+
 #ifndef YY_ATTRIBUTE_PURE
 # if defined __GNUC__ && 2 < __GNUC__ + (96 <= __GNUC_MINOR__)
 #  define YY_ATTRIBUTE_PURE __attribute__ ((__pure__))
