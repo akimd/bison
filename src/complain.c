4378e2dc3ccb757828941ddd94e2a47094c98eb9
Akim Demaille <akim.demaille@gmail.com>
2020-04-24 08:21:07 +0200

    diagnostics: fix a typo
    * src/complain.c: here.

--- src/complain.c
+++ src/complain.c
@@ -600,7 +600,7 @@ syntax_error (location loc,
       CASE (2, _("expected %1$s before %0$s"));
       CASE (3, _("expected %1$s or %2$s before %0$s"));
       CASE (4, _("expected %1$s or %2$s or %3$s before %0$s"));
-      CASE (5, _("expected %1$s or %2$s or %4$s or %5$s before %0$s"));
+      CASE (5, _("expected %1$s or %2$s or %3$s or %4$s before %0$s"));
 #undef CASE
     }
   location_print (loc, stderr);
