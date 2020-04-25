150dc95395bdd21ced63d6502c9bc04b835b0d54
Akim Demaille <akim.demaille@gmail.com>
2020-04-25 11:03:13 +0200

    style: clarify #endif
    We could try to avoid the weird "#if 1", but then the indentation of
    the inner #if would be wrong.  Let' keep it this way.
    
    * data/skeletons/yacc.c: here.
    Also, avoid sticking the comment to the directive.

--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -547,7 +547,7 @@ void free (void *); /* INFRINGES ON USER NAME SPACE */
 #  endif
 # endif]b4_lac_if([[
 # define YYCOPY_NEEDED 1]])[
-#endif]b4_lac_if([], [b4_parse_error_case([simple], [[/* !defined yyoverflow */]])])[
+#endif /* ]b4_lac_if([[1]], [b4_parse_error_case([simple], [[!defined yyoverflow]], [[1]])])[ */
 
 #if (! defined yyoverflow \
      && (! defined __cplusplus \
--- src/parse-gram.c
+++ src/parse-gram.c
@@ -529,7 +529,7 @@ void free (void *); /* INFRINGES ON USER NAME SPACE */
 #  endif
 # endif
 # define YYCOPY_NEEDED 1
-#endif
+#endif /* 1 */
 
 #if (! defined yyoverflow \
      && (! defined __cplusplus \
