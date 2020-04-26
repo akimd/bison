9c72d3c5a8e2267c1065b2c9ac46a1ec87a8941d
Akim Demaille <akim.demaille@gmail.com>
2020-04-26 13:32:11 +0200

    style: prefer b4_has_translations_if
    * data/skeletons/glr.c, data/skeletons/yacc.c: here.

--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -681,7 +681,7 @@ yysymbol_name (yysymbol_kind_t yysymbol)
   static const char *const yy_sname[] =
   {
   ]b4_symbol_names[
-  };]m4_ifdef([b4_translatable], [[
+  };]b4_has_translations_if([[
   /* YYTRANSLATABLE[SYMBOL-NUM] -- Whether YY_SNAME[SYMBOL-NUM] is
      internationalizable.  */
   static ]b4_int_type_for([b4_translatable])[ yytranslatable[] =
