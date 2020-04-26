bc083be055b7acd105ec1cb5b22fa2e84188ed00
Akim Demaille <akim.demaille@gmail.com>
2020-04-26 08:50:23 +0200

    style: glr.c: fix indentation issue
    * data/skeletons/glr.c (yyparse): here.

--- data/skeletons/glr.c
+++ data/skeletons/glr.c
@@ -2490,7 +2490,8 @@ b4_dollar_popdef])[]dnl
                   /* Don't issue an error message again for exceptions
                      thrown from the scanner.  */
                   if (yychar != YYFAULTYTOK)
-  ]])[                  yyreportSyntaxError (&yystack]b4_user_args[);
+  ]], [[
+]])[                  yyreportSyntaxError (&yystack]b4_user_args[);
                   goto yyuser_error;
                 }
               else
