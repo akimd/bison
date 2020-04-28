3b05de2d05a067d467b8c89a6b40e6d29f1e1e89
Akim Demaille <akim.demaille@gmail.com>
2020-04-28 08:26:49 +0200

    yacc.c: install backward compatibility for YYERRCODE
    Some people have been using that symbol.  Some even have #defined it
    themselves.
    https://lists.gnu.org/r/bison-patches/2020-04/msg00138.html
    
    Let's provide backward compatibility, having it point to YYUNDEF, so
    that an error message is generated.
    
    * data/skeletons/yacc.c (YYERRCODE): New, at the exact same location
    it was defined before.

--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -746,6 +746,9 @@ enum { YYENOMEM = -2 };
       }                                                           \
   while (0)
 
+/* Backward compatibility with an undocumented macro.
+   Use ]b4_symbol(1, id)[ or ]b4_symbol(2, id)[. */
+#define YYERRCODE ]b4_symbol(2, id)[
 ]b4_locations_if([[
 ]b4_yylloc_default_define[
 #define YYRHSLOC(Rhs, K) ((Rhs)[K])
--- src/parse-gram.c
+++ src/parse-gram.c
@@ -931,6 +931,9 @@ enum { YYENOMEM = -2 };
       }                                                           \
   while (0)
 
+/* Backward compatibility with an undocumented macro.
+   Use GRAM_error or GRAM_UNDEF. */
+#define YYERRCODE GRAM_UNDEF
 
 /* YYLLOC_DEFAULT -- Set CURRENT to span from RHS[1] to RHS[N].
    If N is 0, then set CURRENT to the empty location which ends
