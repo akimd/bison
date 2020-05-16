62799450031c54ca0a9a5e7f170d62dd63e8e00f
Akim Demaille <akim.demaille@gmail.com>
2020-05-16 15:21:59 +0200

    cex: fix a crash
    * src/state-item.c (init_state_items): If the rule has no reductions
    at all, don't read at all in its list of reduced rules.

--- src/state-item.c
+++ src/state-item.c
@@ -150,7 +150,7 @@ init_state_items (void)
           state_item_set (sidx, s, s->items[j]);
           state_item *si = state_items + sidx;
           const rule *r = item_rule (si->item);
-          if (red->rules[rule_search_idx] < r)
+          if (rule_search_idx < red->num && red->rules[rule_search_idx] < r)
             ++rule_search_idx;
           if (rule_search_idx < red->num && r == red->rules[rule_search_idx])
             {
