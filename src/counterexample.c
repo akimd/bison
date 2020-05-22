c19af5cbb4c01f1be2b7ef78a155925aab0432a1
Vincent Imbimbo <vmi6@cornell.edu>
2020-05-21 22:13:16 -0400

    cex: fix bad reference counting
    * src/counterexample.c (si_bfs_free): Fix reference_count
    decrementing.

--- src/counterexample.c
+++ src/counterexample.c
@@ -158,13 +158,10 @@ si_bfs_free (si_bfs_node *n)
 {
   if (n == NULL)
     return;
+  --n->reference_count;
   if (n->reference_count == 0)
     {
-      if (n->parent)
-        {
-          --n->parent->reference_count;
-          si_bfs_free (n->parent);
-        }
+      si_bfs_free (n->parent);
       free (n);
     }
 }
