a53c6026cd367c75d5594e6e649ee1cdda93d5ae
Akim Demaille <akim.demaille@gmail.com>
2020-06-09 07:08:11 +0200

    api.header.include: document it, and fix its default value
    While defining api.header.include worked as expected, its default
    value was incorrectly defined.  As a result, by default, the generated
    parsers still duplicated the content of the generated header instead
    of including it.
    
    * data/skeletons/yacc.c (api.header.include): Fix its default value.
    * tests/output.at: Check it.
    * doc/bison.texi (%define Summary): Document api.header.include.
    While at it, move the definition of api.namespace at the proper
    place.

--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -335,7 +335,7 @@ m4_define([b4_header_include_if],
                                    [$2])],
           [$2])])
 
-m4_if(b4_spec_header_file, [[y.tab.h]],
+m4_if(b4_spec_header_file, [y.tab.h], [],
       [b4_percent_define_default([[api.header.include]],
                                  [["@basename(]b4_spec_header_file[@)"]])])
 
