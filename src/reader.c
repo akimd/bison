fe10eca6c979d9cc7af0197eb94f939baa4aac3e
Valentin Tolmer <valentin.tolmer@gmail.com>
2020-07-01 11:02:36 +0200

    glr2.cc: fork glr.cc to a c++ version
    This is a fork of glr.cc to be c++-first instead of a wrapper around
    glr.c.

--- src/reader.c
+++ src/reader.c
@@ -297,6 +297,7 @@ grammar_rule_check_and_complete (symbol_list *r)
               const bool is_cxx =
                 STREQ (language->language, "c++")
                 || (skeleton && (STREQ (skeleton, "glr.cc")
+                                 || STREQ (skeleton, "glr2.cc")
                                  || STREQ (skeleton, "lalr1.cc")));
               if (is_cxx)
                 {
