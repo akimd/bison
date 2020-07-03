d7f7fcd9c7ca64d312196b081aa09def3a518979
Akim Demaille <akim.demaille@gmail.com>
2020-07-03 06:36:50 +0200

    dot: also use a dot in the output
    * src/print-graph.c (print_core): Use a dot instead of a point.
    * doc/figs/example-reduce.gv, doc/figs/example-reduce.txt,
    * doc/figs/example-shift.gv, doc/figs/example-shift.txt,
    * doc/figs/example.gv: Update.
    * tests/output.at, tests/report.at: Adjust.

--- src/print-graph.c
+++ src/print-graph.c
@@ -84,7 +84,7 @@ print_core (struct obstack *oout, state *s)
           obstack_1grow (oout, ' ');
         }
 
-      obstack_1grow (oout, '.');
+      obstack_sgrow (oout, "•");
 
       if (0 <= *r->rhs)
         for (item_number const *sp = sp1; 0 <= *sp; ++sp)
