bad07a7f6631331709eedc8e2d1ceacb29695794
Akim Demaille <akim.demaille@gmail.com>
2020-07-14 14:21:52 +0200

    style: comments changes
    * src/print.c: here.

--- src/print-xml.c
+++ src/print-xml.c
@@ -202,11 +202,11 @@ print_errs (FILE *out, int level, state *s)
 }
 
 
-/*-------------------------------------------------------------------------.
+/*-------------------------------------------------------------------.
 | Report a reduction of RULE on LOOKAHEAD (which can be 'default').  |
-| If not ENABLED, the rule is masked by a shift or a reduce (S/R and       |
-| R/R conflicts).                                                          |
-`-------------------------------------------------------------------------*/
+| If not ENABLED, the rule is masked by a shift or a reduce (S/R and |
+| R/R conflicts).                                                    |
+`-------------------------------------------------------------------*/
 
 static void
 print_reduction (FILE *out, int level, char const *lookahead,
--- src/print.c
+++ src/print.c
@@ -180,11 +180,11 @@ print_errs (FILE *out, const state *s)
 }
 
 
-/*-------------------------------------------------------------------------.
+/*-------------------------------------------------------------------.
 | Report a reduction of RULE on LOOKAHEAD (which can be 'default').  |
-| If not ENABLED, the rule is masked by a shift or a reduce (S/R and       |
-| R/R conflicts).                                                          |
-`-------------------------------------------------------------------------*/
+| If not ENABLED, the rule is masked by a shift or a reduce (S/R and |
+| R/R conflicts).                                                    |
+`-------------------------------------------------------------------*/
 
 static void
 print_reduction (FILE *out, size_t width,
