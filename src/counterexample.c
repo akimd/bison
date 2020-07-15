4d18195ebc8eb44c0da984bffa23e1db9fc0fd0e
Akim Demaille <akim.demaille@gmail.com>
2020-07-15 06:40:35 +0200

    cex: style changes
    * src/counterexample.c: here.

--- src/counterexample.c
+++ src/counterexample.c
@@ -113,7 +113,7 @@ free_counterexample (counterexample *cex)
 }
 
 static void
-print_counterexample (counterexample *cex, FILE *out, const char *prefix)
+print_counterexample (const counterexample *cex, FILE *out, const char *prefix)
 {
   fprintf (out, "  %s%-20s ",
            prefix, cex->unifying ? _("Example") : _("First example"));
