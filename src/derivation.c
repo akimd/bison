431774d1f6817b3306f8941c0226926eb295c279
Akim Demaille <akim.demaille@gmail.com>
2020-07-21 07:41:06 +0200

    cex: update NEWS for 3.7
    * NEWS: Update to the current style of cex display.

--- src/derivation.c
+++ src/derivation.c
@@ -193,9 +193,8 @@ all_spaces (const char *s)
 //
 // This function implements this.
 //
-// When COND is true, put S on OUT, preceeded by *PADDING white
-// spaces.  Otherwise add the width to *PADDING.  Return the width of
-// S.
+// When COND is true, put S on OUT, preceded by *PADDING white spaces.
+// Otherwise add the width to *PADDING.  Return the width of S.
 static int
 fputs_if (bool cond, FILE *out, int *padding, const char *s)
 {
