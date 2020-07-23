d0bec3175ff5cf6582ffbf584b73ea6aaea838d0
Akim Demaille <akim.demaille@gmail.com>
2020-07-23 19:55:24 +0200

    style: avoid warnings with GCC 4.6
    With have a clash with the "max" function.
    
        src/counterexample.c: In function 'visited_hasher':
        src/counterexample.c:720:48: error: declaration of 'max' shadows a global declaration [-Werror=shadow]
        src/counterexample.c:116:12: error: shadowed declaration is here [-Werror=shadow]
    
    * src/counterexample.c (visited_hasher): Alpha conversion.

--- src/counterexample.c
+++ src/counterexample.c
@@ -717,10 +717,10 @@ ssb_equals (const search_state_bundle *s1, const search_state_bundle *s2)
 typedef gl_list_t ssb_list;
 
 static size_t
-visited_hasher (const search_state *ss, size_t max)
+visited_hasher (const search_state *ss, size_t maximum)
 {
-  return (parse_state_hasher (ss->states[0], max)
-          + parse_state_hasher (ss->states[1], max)) % max;
+  return (parse_state_hasher (ss->states[0], maximum)
+          + parse_state_hasher (ss->states[1], maximum)) % maximum;
 }
 
 static bool
