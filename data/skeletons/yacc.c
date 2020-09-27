9b18cac96bdad4a71b546a3c16ca4b7e25c0a657
Akim Demaille <akim.demaille@gmail.com>
2020-09-27 13:07:57 +0200

    multistart: start more thorough testing
    * tests/local.at (AT_MULTISTART_IF): New.
    * tests/calc.at: Adjust to check multiple start symbols.
    * data/skeletons/yacc.c (yy_parse_impl): Fix.

--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -2182,7 +2182,8 @@ yypushreturn:]], [[
 ]b4_parse_error_bmatch([detailed\|verbose],
 [[  if (yymsg != yymsgbuf)
     YYSTACK_FREE (yymsg);]])[]m4_ifdef([b4_start_symbols], [[
-  yyimpl->yynerrs = yynerrs;]])[
+  if (yyimpl)
+    yyimpl->yynerrs = yynerrs;]])[
   return yyresult;
 }
 ]b4_push_if([b4_parse_state_variable_macros([b4_macro_undef])])[
