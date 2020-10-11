e9a43ed4ae9c9c4d6f4e250e4f929ac9602d7452
Akim Demaille <akim.demaille@gmail.com>
2020-10-11 15:41:17 +0200

    ielr: make some conditions about items easier to understand
    Checking that an item index is > 1 means ruling out `$accept: • input
    $end` and `$accept: input • $end`.  But actually only the latter is
    possible there, i.e., we're checking whether this item is about a
    successor of a (kernel) item of the initial state ($accept: input •
    $end).
    
    * src/ielr.c (is_successor_of_initial_item): Use a variable to name
    this condition.

--- src/ielr.c
+++ src/ielr.c
@@ -413,7 +413,10 @@ ielr_item_has_lookahead (state *s, symbol_number lhs, size_t item,
          didn't change this test to an aver just in case the usage of this
          function evolves to need those two cases.  In both cases, the current
          implementation returns the right result.  */
-      if (s->items[item] > 1)
+      aver (s->items[item] != 1);
+      const bool is_successor_of_initial_item = s->items[item] <= 1;
+      aver (!is_successor_of_initial_item);
+      if (!is_successor_of_initial_item)
         {
           /* If the LHS symbol of this item isn't known (because this is a
              top-level invocation), go get it.  */
@@ -622,9 +625,11 @@ ielr_compute_lookaheads (bitsetv follow_kernel_items, bitsetv always_follows,
          a special case to avoid the - 2 below, but the next successor can be
          handled fine without special casing it.  */
       aver (t->items[t_item] != 0);
-      if (t->items[t_item] > 1
+      const bool is_successor_of_initial_item = t->items[t_item] <= 1;
+      if (!is_successor_of_initial_item
           && !bitset_empty_p (lookahead_filter[t_item]))
         {
+          /* Is this kernel item next to the beginning of the RHS?  */
           if (item_number_is_rule_number (ritem[t->items[t_item] - 2]))
             ielr_compute_goto_follow_set (
               follow_kernel_items, always_follows, s,
