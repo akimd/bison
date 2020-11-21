38cdb2aba2e98ee9960fa9d751dd68d1934fd1ca
Akim Demaille <akim.demaille@gmail.com>
2020-11-21 14:57:29 +0100

    multistart: also use the api.prefix in the implementation
    We were declaring foo_parse_expr but implementing yyparse_expr.
    
    * data/skeletons/yacc.c (_b4_define_sub_yyparse): Use api.prefix for
    the parse function name.

--- data/skeletons/yacc.c
+++ data/skeletons/yacc.c
@@ -198,7 +198,7 @@ m4_define([_b4_first_switching_token],
 m4_define([_b4_define_sub_yyparse],
 [[
 ]b4_prefix[parse_]_b4_symbol($1, id)[_t
-yyparse_]_b4_symbol($1, id)[ (]m4_ifset([b4_parse_param], [b4_formals(b4_parse_param)], [void])[)
+]b4_prefix[parse_]_b4_symbol($1, id)[ (]m4_ifset([b4_parse_param], [b4_formals(b4_parse_param)], [void])[)
 {
   ]b4_prefix[parse_]_b4_symbol($1, id)[_t yyres;
   yy_parse_impl_t yyimpl;
