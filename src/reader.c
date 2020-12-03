0e78a9028e749de2e5af5d15e3fcb9b29cced537
Akim Demaille <akim.demaille@gmail.com>
2020-12-03 19:46:20 +0100

    portability: beware of GCC 4.6
        src/reader.c: In function 'grammar_start_symbols_add':
        src/reader.c:67:24: error: declaration of 'dup' shadows a global declaration [-Werror=shadow]
    
    * src/reader.c (grammar_start_symbols_add): Rename dup as dupl.

--- src/reader.c
+++ src/reader.c
@@ -64,10 +64,10 @@ grammar_start_symbols_add (symbol_list *syms)
       if (first)
         {
           duplicate_directive ("%start", first->sym_loc, l->sym_loc);
-          symbol_list *dup = l;
+          symbol_list *dupl = l;
           l = l->next;
-          dup->next = NULL;
-          symbol_list_free (dup);
+          dupl->next = NULL;
+          symbol_list_free (dupl);
         }
       else
         {
