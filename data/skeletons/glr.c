a700a138228ed0d94bbfcf4feeb90093d2fbeca9
Akim Demaille <akim.demaille@gmail.com>
2020-12-05 06:49:48 +0100

    glr.cc: don't "leak" yyparse
    When using glr.cc, the C function yyparse is an internal detail that
    should not be exposed.  Users might call it by accident (I did).
    
    * data/skeletons/glr.c (yyparse): When used for glr.cc, rename as yy_parse_impl.
    * data/skeletons/glr.cc: Adjust.

--- data/skeletons/glr.c
+++ data/skeletons/glr.c
@@ -215,7 +215,7 @@ b4_copyright([Skeleton implementation for Bison GLR parsers in C],
 #define YYLTYPE ]b4_api_PREFIX[LTYPE]])])[
 ]m4_if(b4_prefix, [yy], [],
 [[/* Substitute the variable and function names.  */
-#define yyparse ]b4_prefix[parse
+#define ]b4_glr_cc_if([yy_parse_impl], [yyparse])[ ]b4_prefix[]b4_glr_cc_if([_parse_impl], [parse])[
 #define yylex   ]b4_prefix[lex
 #define yyerror ]b4_prefix[error
 #define yydebug ]b4_prefix[debug]]b4_pure_if([], [[
@@ -2420,7 +2420,7 @@ yyrecoverSyntaxError (yyGLRStack* yystackp]b4_user_formals[)
 `----------*/
 
 int
-yyparse (]m4_ifset([b4_parse_param], [b4_formals(b4_parse_param)], [void])[)
+]b4_glr_cc_if([yy_parse_impl], [yyparse])[ (]m4_ifset([b4_parse_param], [b4_formals(b4_parse_param)], [void])[)
 {
   int yyresult;
   yyGLRStack yystack;
