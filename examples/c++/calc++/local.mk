d50a3e450e7e469cee7b6a782b6877ef9e47d7b6
Akim Demaille <akim.demaille@gmail.com>
2021-02-01 07:54:03 +0100

    graph: output foo.gv by default, instead of *.dot
    That change was started in Bison 3.4.  The announcement for 3.7 stated
    that in Bison 3.8 we would use *.gv by default.
    
    * src/files.c (compute_output_file_names): spec_graph_file defaults
    too *.gv.
    * doc/bison.texi, examples/c++/calc++/local.mk, tests/output.at:
    Adjust.

--- examples/c++/calc++/local.mk
+++ examples/c++/calc++/local.mk
@@ -31,7 +31,7 @@ $(calcxx_sources_generated): %D%/parser.stamp
 	@test -f $@ || $(MAKE) $(AM_MAKEFLAGS) %D%/parser.stamp
 CLEANFILES +=					\
   $(calcxx_sources_generated)			\
-  %D%/parser.dot				\
+  %D%/parser.gv					\
   %D%/parser.output				\
   %D%/parser.stamp				\
   %D%/scanner.cc
